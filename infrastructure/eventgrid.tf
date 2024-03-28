8def6363d3b606caf8b1a896398a730d6b86ea4d
MacMur <maciej.murawski@nordcloud.com>
2024-03-28 14:17:51 +0000

    Event Topic deployment

---
+++ infrastructure/eventgrid.tf
@@ -0,0 +1,13 @@
+module "eventgrid" {
+  source = ".//modules/event-grid"
+
+  names = module.config.names
+
+  resource_group_name = module.baseline.resource_group_names[var.event_grid.topic.resource_group_index]
+  location            = module.baseline.resource_group_locations[var.event_grid.topic.resource_group_index]
+
+  name_suffix = var.event_grid.topic.name_suffix
+
+  tags = var.tags
+
+}
