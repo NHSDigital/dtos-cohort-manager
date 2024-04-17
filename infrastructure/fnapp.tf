825bd72e7c9fb28f7a2c56dcb68f31a8da3828d1
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-04-17 10:30:15 +0100

    feat: added log analytics workspace

--- infrastructure/fnapp.tf
+++ infrastructure/fnapp.tf
@@ -4,12 +4,15 @@ module "functionapp" {
   names = module.config.names
 
   function_app        = var.function_app.fa_config
-  resource_group_name = module.baseline.resource_group_names[var.function_app.resource_group_index]
-  location            = module.baseline.resource_group_locations[var.function_app.resource_group_index]
+  resource_group_name = module.baseline.resource_group_names[var.function_app.resource_group_key]
+  location            = module.baseline.resource_group_locations[var.function_app.resource_group_key]
 
   asp_id     = module.app-plan.app_service_plan_id
   sa_name    = module.storage.storage_account_name
   sa_prm_key = module.storage.storage_account_primary_access_key
+
+  ai_connstring = module.app_insights.ai_connection_string
+  worker_32bit  = var.function_app.worker_32bit
 
   tags = var.tags
 
---
+++ infrastructure/modules/log-analytics-workspace/output.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = azurerm_log_analytics_workspace.law.id
+}
+
+output "name" {
+  value = azurerm_log_analytics_workspace.law.name
+}
