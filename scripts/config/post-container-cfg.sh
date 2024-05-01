62bff2fc80b4e91a58561623b71e5a3d19b1568d
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-05-01 18:35:26 +0100

    feat: bash script for dev container post-deploy configuration

---
+++ scripts/config/post-container-cfg.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
+source ~/.bashrc
+make config
