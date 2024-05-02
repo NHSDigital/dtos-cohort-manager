1f6087a762dcd1da90f9f98a756b63116b07f000
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-05-02 18:48:31 +0100

    feat: devcontainer definition and post build configuration

---
+++ scripts/config/post-build-cfg.sh
@@ -0,0 +1,8 @@
+echo "Post build commands..."
+sudo apt-get update
+sudo apt-get install gnupg2 -y
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
+source ~/.bashrc
+make config
