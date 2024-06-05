e4f55a00d70698aedd042284477503a19d8a52bc
alex-clayton-1 <alexander.clayton1@nhs.net>
2024-06-05 10:05:57 +0100

    refactor: Renamed GetValidationExceptions to ExceptionDataService

--- application/CohortManager/src/Functions/screeningDataServices/ExceptionDataService/GetValidationExceptions.cs
+++ application/CohortManager/src/Functions/screeningDataServices/ExceptionDataService/GetValidationExceptions.cs
@@ -7,20 +7,20 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-public class GetValidationExceptions
+public class ExceptionDataService
 {
-    private readonly ILogger<GetValidationExceptions> _logger;
+    private readonly ILogger<ExceptionDataService> _logger;
     private readonly ICreateResponse _createResponse;
     private readonly IValidationData _validationData;
 
-    public GetValidationExceptions(ILogger<GetValidationExceptions> logger, ICreateResponse createResponse, IValidationData validationData)
+    public ExceptionDataService(ILogger<ExceptionDataService> logger, ICreateResponse createResponse, IValidationData validationData)
     {
         _logger = logger;
         _createResponse = createResponse;
         _validationData = validationData;
     }
 
-    [Function("GetValidationExceptions")]
+    [Function("GetExceptions")]
     public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
         foreach (var exception in _validationData.GetAll())
