0c5b7e8a3737ad421efdd3724e70aed9bb1cbd7f
andrewmorris43 <andrew.morris43@nhs.net>
2024-06-06 14:36:44 +0000

    feat: add FileValidation function and unit tests

---
+++ application/CohortManager/src/Functions/ScreeningValidationService/FileValidation/FileValidation.cs
@@ -0,0 +1,41 @@
+﻿namespace NHS.CohortManager.ScreeningValidationService;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+public class FileValidation
+{
+    private readonly ILogger<FileValidation> _logger;
+    public FileValidation(ILogger<FileValidation> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("FileValidation")]
+    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        FileValidationRequestBody requestBody;
+
+        try
+        {
+            string requestBodyJson;
+            using (var reader = new StreamReader(req.Body, Encoding.UTF8))
+            {
+                requestBodyJson = await reader.ReadToEndAsync();
+            }
+
+            requestBody = JsonSerializer.Deserialize<FileValidationRequestBody>(requestBodyJson);
+        }
+        catch
+        {
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        _logger.LogInformation("File validation exception: {ExceptionMessage} from {FileName}", requestBody.ExceptionMessage, requestBody.FileName);
+        return req.CreateResponse(HttpStatusCode.OK);
+    }
+}
---
+++ tests/ScreeningValidationServiceTests/FileValidation/FileValidationTests.cs
@@ -0,0 +1,92 @@
+namespace NHS.CohortManager.Tests.ScreeningValidationService;
+
+using System.Net;
+using System.Text.Json;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using NHS.CohortManager.ScreeningValidationService;
+
+[TestClass]
+public class FileValidationTests
+{
+    private readonly Mock<ILogger<FileValidation>> _logger = new();
+    private readonly Mock<FunctionContext> _context = new();
+    private readonly Mock<HttpRequestData> _request;
+    private readonly FileValidationRequestBody _requestBody;
+    private readonly FileValidation _function;
+
+    public FileValidationTests()
+    {
+        _request = new Mock<HttpRequestData>(_context.Object);
+
+        _requestBody = new FileValidationRequestBody("There was an exception.", "test-file.csv");
+
+        _function = new FileValidation(_logger.Object);
+
+        _request.Setup(r => r.CreateResponse()).Returns(() =>
+        {
+            var response = new Mock<HttpResponseData>(_context.Object);
+            response.SetupProperty(r => r.Headers, new HttpHeadersCollection());
+            response.SetupProperty(r => r.StatusCode);
+            response.SetupProperty(r => r.Body, new MemoryStream());
+            return response.Object;
+        });
+    }
+
+    [TestMethod]
+    public async Task Run_Should_Return_BadRequest_When_Request_Body_Empty()
+    {
+        // Act
+        var result = await _function.RunAsync(_request.Object);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task Run_Should_Return_BadRequest_When_Request_Body_Invalid()
+    {
+        // Arrange
+        SetUpRequestBody("Invalid request body");
+
+        // Act
+        var result = await _function.RunAsync(_request.Object);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task Run_Should_Return_OK_And_Log_Request_When_Request_Body_Valid()
+    {
+        // Arrange
+        var json = JsonSerializer.Serialize(_requestBody);
+        SetUpRequestBody(json);
+
+        // Act
+        var result = await _function.RunAsync(_request.Object);
+
+        // Assert
+        _logger.Verify(log =>
+            log.Log(
+            LogLevel.Information,
+            0,
+            It.Is<It.IsAnyType>((state, type) => state.ToString().Contains("There was an exception.")),
+            null,
+            (Func<object, Exception, string>)It.IsAny<object>()
+            ));
+
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+    }
+
+    private void SetUpRequestBody(string json)
+    {
+        var byteArray = Encoding.ASCII.GetBytes(json);
+        var bodyStream = new MemoryStream(byteArray);
+
+        _request.Setup(r => r.Body).Returns(bodyStream);
+    }
+}
