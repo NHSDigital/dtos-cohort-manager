5f4e1c6055ff46b23338375fe1a1a315bd6275ac
SamRobinson75684 <166817125+SamRobinson75684@users.noreply.github.com>
2024-06-06 12:16:35 +0100

    feat: Validation Service & Exception Data Service (#18)
    * cqode clean up according to linter
    
    * Update: deleting unwated rules. Removing unwanted git ingore files
    
    * Update: removing git version from sln file
    
    * Merge branch 'main' of https://github.com/NHSDigital/dtos-landing-zone
    
    * format: formatting all files
    
    * format: getting rid of VS version in sln file
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: trying to fix formatting
    
    * format: adding to git leaks ingore to ingore all files that contained verson numbers
    
    * update: moving namspace to the top of the file
    
    * update: addressing comments made in pr
    
    * update: addressing comments made in pr
    
    * style: Fix indentation in solution file
    
    * chore: Moved Validation tests to root-level tests folder
    
    * style: Fix indentation in solution file
    
    * test: Fix validation tests
    
    * style: Updated formatting of files for consistency
    
    * style: Renamed files
    
    * feat: Updated validation rules
    
    * test: Fixed failing validation tests
    
    * fix: Reverted an unintentional column rename in INSERT statement
    
    * feat: Validation function workflow is no longer hardcoded and can be passed in the request
    
    * refactor: Renamed ValidationFunction to LookupValidation
    
    * feat: Created static validation function
    
    * fix: Fixed NHS Number regex in static validation rules
    
    * test: Added extra test cases to static validation test
    
    * feat: Updated validation functions with new db table format
    
    * feat: Updated GetValidationExceptions to work with new data structure
    
    * refactor: Removed unused using directives
    
    * test: Fixed tests that broke after the merge
    
    * refactor: Moved data access code to Data project
    
    * refactor: Renamed ValidationDataService to ScreeningValidationService
    
    * feat: Added call to static validation from update participant management service
    
    * fix: Added exception handling for bad response from validation
    
    * test: Fixed validation tests
    
    * refactor: Renamed exception db table
    
    * test: Fixed broken tests
    
    * refactor: Refactored ValidationData
    
    * test: Removed redundant code from updateParticipantDetails tests
    
    * refactor: Renamed GetValidationExceptions to ExceptionDataService
    
    * build: Created Dockerfiles and updated Docker compose
    
    * refactor: Updated validation functions to handle invalid request bodies
    
    * fix: Fixed broken project path in solution file
    
    ---------
    
    Co-authored-by: alex-clayton-1 <alexander.clayton1@nhs.net>

---
+++ application/CohortManager/src/Functions/ScreeningValidationService/LookupValidation/LookupValidationRequestBody.cs
@@ -0,0 +1,17 @@
+namespace NHS.CohortManager.ScreeningValidationService;
+
+using Model;
+
+public class LookupValidationRequestBody
+{
+    public string Workflow { get; set; }
+    public Participant ExistingParticipant { get; set; }
+    public Participant NewParticipant { get; set; }
+
+    public LookupValidationRequestBody(string workflow, Participant existingParticipant, Participant newParticipant)
+    {
+        Workflow = workflow;
+        ExistingParticipant = existingParticipant;
+        NewParticipant = newParticipant;
+    }
+}
