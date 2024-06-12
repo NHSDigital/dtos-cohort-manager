7718f6cd626083a9b4e566c8d1bafce7419de4a3
Sam Robinson <sam.robinson24@nhs.net>
2024-06-12 11:04:29 +0100

    update: Making sure actions match the latest test file

--- application/CohortManager/src/Functions/Shared/Model/Actions.cs
+++ application/CohortManager/src/Functions/Shared/Model/Actions.cs
@@ -2,7 +2,7 @@ namespace Model;
 
 public static class Actions
 {
-    public const string New = "new";
+    public const string New = "New";
     public const string Amended = "Amended";
     public const string Removed = "Removed";
 }
