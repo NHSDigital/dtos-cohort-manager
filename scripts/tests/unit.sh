2bd6bef1f65f4ae478a3d6b8450c27ed1c12ad11
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-06-12 12:15:46 +0000

    fix: issue with paths resolved

--- scripts/tests/unit.sh
+++ scripts/tests/unit.sh
@@ -9,16 +9,19 @@ UnitDir="$dir/tests/"
 ResDir="$UnitDir"results-unit
 Format="trx"
 
+# Find all *.csproj files and execute dotnet test, with build for now
 find "$UnitDir" -name '*.csproj' | while read -r file; do
     echo -e "\nRunning unit tests for:\n$file"
     dotnet test "$file" --logger $Format --verbosity quiet
 done
 
+# Move all trx result files into a separate folder, for easier reporting
 mkdir -p "$ResDir"
 find "$UnitDir" -name "*.$Format" -not -path "$ResDir/*" | while read -r resfile; do
     mv "$resfile" "$ResDir"
 done
 
+# List created results
 echo -e "\nCreated result files:\n"
 find "$ResDir" -name "*.$Format"
 
