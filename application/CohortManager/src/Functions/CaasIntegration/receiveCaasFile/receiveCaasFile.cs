77723829a7c5d6a5078e09ec590f71fee3a920ad
warren <warren.pitterson1@nhs.net>
2024-06-17 12:48:58 +0100

    feat: removed 1 level of indentation, changed error handling for partial failures and added logging

--- application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
+++ application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
@@ -9,84 +9,77 @@ using CsvHelper;
 using CsvHelper.Configuration;
 using System.Globalization;
 
-
-    public class ReceiveCaasFile
+public class ReceiveCaasFile
+{
+    private readonly ILogger<ReceiveCaasFile> _logger;
+    private readonly ICallFunction _callFunction;
+    public ReceiveCaasFile(ILogger<ReceiveCaasFile> logger,
+                            ICallFunction callFunction)
     {
-        private readonly ILogger<ReceiveCaasFile> _logger;
-        private readonly ICallFunction _callFunction;
-
-        public ReceiveCaasFile(ILogger<ReceiveCaasFile> logger,
-                                ICallFunction callFunction)
+        _logger = logger;
+        _callFunction = callFunction;
+    }
+    [Function(nameof(ReceiveCaasFile))]
+    public async Task Run([BlobTrigger("inbound/{name}", Connection = "caasfolder_STORAGE")] Stream stream, string name)
+    {
+        var badRecords = new Dictionary<int, string>();
+        var cohort = new Cohort();
+        var rowNumber = 0;
+        CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
         {
-            _logger = logger;
-            _callFunction = callFunction;
+            IgnoreBlankLines = true,
+            TrimOptions = TrimOptions.Trim,
+            Delimiter = ",",
+            MissingFieldFound = null,
+            HeaderValidated = null
+        };
+        try
+        {
+            using var blobStreamReader = new StreamReader(stream);
+            using var csv = new CsvReader(blobStreamReader, config);
+            csv.Context.RegisterClassMap<ParticipantMap>();
+            foreach (var participant in csv.GetRecords<Participant>())
+            {
+                rowNumber++;
+                try
+                {
+                    if (participant != null)
+                    {
+                        cohort.Participants.Add(participant);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    badRecords.Add(rowNumber, csv.Context.Parser.RawRecord);
+                    _logger.LogError("Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", rowNumber, ex.Message, ex.StackTrace);
+                }
+            }
         }
-
-        [Function(nameof(ReceiveCaasFile))]
-        public async Task Run([BlobTrigger("inbound/{name}", Connection = "caasfolder_STORAGE")] Stream stream, string name)
+        catch (HeaderValidationException ex)
         {
-            var badRecords = new Dictionary<int, string>();
-            var cohort = new Cohort();
-            var rowNumber = 0;
-
-            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            _logger.LogError("Header validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Failed to read csv.\nMessage:{ExMessage}.\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
+        }
+        try
+        {
+            if (cohort.Participants.Count > 0)
             {
-                IgnoreBlankLines = true,
-                TrimOptions = TrimOptions.Trim,
-                Delimiter = ",",
-                MissingFieldFound = null,
-                HeaderValidated = null
-            };
-
-            try
-            {
-                using var blobStreamReader = new StreamReader(stream);
-                using var csv = new CsvReader(blobStreamReader, config);
-
-                csv.Context.RegisterClassMap<ParticipantMap>();
-
-                foreach (var participant in csv.GetRecords<Participant>())
-                {
-                    rowNumber++;
-                    try
-                    {
-                        if (participant != null)
-                        {
-                            cohort.Participants.Add(participant);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        badRecords.Add(rowNumber, csv.Context.Parser.RawRecord);
-                        _logger.LogError("Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", rowNumber, ex.Message, ex.StackTrace);
-                    }
-                }
+                var json = JsonSerializer.Serialize(cohort);
+                await _callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
+                _logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
             }
-            catch (HeaderValidationException ex)
+            if (badRecords.Count > 0 || cohort.Participants.Count == 0)
             {
-                _logger.LogError("Header validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
+                _logger.LogError("Failed to create {BadRecordsCount} Objects", badRecords.Count);
+                _logger.LogError("All failed Records - {BadRecords}", badRecords);
             }
-            catch (Exception ex)
-            {
-                _logger.LogError("Failed to read csv.\nMessage:{ExMessage}.\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
-            }
-
-            try
-            {
-                if (cohort.Participants.Count > 0)
-                {
-                    var json = JsonSerializer.Serialize(cohort);
-                    await _callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
-                    _logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
-                }
-                else
-                {
-                    _logger.LogError("Failed to create {BadRecordCount} Objects.", badRecords.Count);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError("Unable to call function.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
-            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Unable to call function.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
         }
     }
+}
