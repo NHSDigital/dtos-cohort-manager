3b78b1d516b6426354947ed5ab17f1f03c579508
Sam Robinson <sam.robinson24@nhs.net>
2024-06-17 15:09:12 +0100

    adding new table to create sql

--- application/CohortManager/Set-up/database/create_statement.sql
+++ application/CohortManager/Set-up/database/create_statement.sql
@@ -178,3 +178,12 @@ CREATE TABLE DEMOGRAPHIC_DATA (
   home_phone_textphone BIT,
   emergency_contact_phone_number VARCHAR(255)
 );
+
+/*==============================================================*/
+/* Table: VALIDATION_FILE_EXCEPTION Table                       */
+/*==============================================================*/
+
+create table dbo.VALIDATION_FILE_EXCEPTION (
+    FILE_NAME NVARCHAR(50) not null,
+    EXCEPTION NVARCHAR(50) not null,
+);
