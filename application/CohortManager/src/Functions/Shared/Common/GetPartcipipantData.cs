6b0eb5a72d9770c88ece484eb5f292a22458cad7
seraynhs <nurhan.noyan1@nhs.net>
2024-06-26 10:00:59 +0100

    dtoss-3117: created participant data service

--- application/CohortManager/src/Functions/Shared/Common/GetPartcipipantData.cs
+++ application/CohortManager/src/Functions/Shared/Common/GetPartcipipantData.cs
@@ -7,6 +7,12 @@ using Model;
 
 public class GetParticipantData : IGetParticipantData
 {
+    private readonly ICallFunction _callFunction;
+
+    public GetParticipantData(ICallFunction callFunction)
+    {
+        _callFunction = callFunction;
+    }
     public async Task<Participant> GetParticipantDetails(HttpRequestData req)
     {
         var participantData = new Participant();
@@ -15,6 +21,15 @@ public class GetParticipantData : IGetParticipantData
             string requestBody = await reader.ReadToEndAsync();
             participantData = JsonSerializer.Deserialize<Participant>(requestBody);
         }
+
+        return participantData;
+    }
+    public async Task<Participant> GetParticipantAsync(string NHSId, string ParticipantFunctionURI)
+    {
+        var url = $"{ParticipantFunctionURI}?Id={NHSId}";
+
+        var response = await _callFunction.SendGet(url);
+        var participantData = JsonSerializer.Deserialize<Participant>(response);
 
         return participantData;
     }
--- application/CohortManager/src/Functions/Shared/Common/Interfaces/IGetParticipantData.cs
+++ application/CohortManager/src/Functions/Shared/Common/Interfaces/IGetParticipantData.cs
@@ -5,5 +5,6 @@ using Microsoft.Azure.Functions.Worker.Http;
 
 public interface IGetParticipantData
 {
+    public Task<Participant> GetParticipantAsync(string NHSId, string ParticipantFunctionURI);
     public Task<Participant> GetParticipantDetails(HttpRequestData req);
 }
