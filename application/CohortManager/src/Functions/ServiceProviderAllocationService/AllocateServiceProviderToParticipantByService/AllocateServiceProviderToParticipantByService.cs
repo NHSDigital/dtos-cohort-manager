a361642b8a8d5791356988da1a35cbb7741f6f09
krca1 <kristel.caisip1@nhs.net>
2024-06-27 09:13:42 +0100

    Update the code with the tests

---
+++ application/CohortManager/src/Functions/ServiceProviderAllocationService/AllocateServiceProviderToParticipantByService/AllocateServiceProviderToParticipantByService.cs
@@ -0,0 +1,101 @@
+namespace NHS.CohortManager.ServiceProviderAllocationService;
+
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Common;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Model;
+using RulesEngine.Models;
+
+public class AllocateServiceProviderToParticipantByService
+{
+    private readonly ILogger _logger;
+    private readonly ICreateResponse _createResponse;
+    private readonly ICallFunction _callFunction;
+
+    public AllocateServiceProviderToParticipantByService(ILogger<AllocateServiceProviderToParticipantByService> logger, ICreateResponse createResponse, ICallFunction callFunction)
+    {
+        _logger = logger;
+        _createResponse = createResponse;
+        _callFunction = callFunction;
+    }
+
+    [Function("AllocateServiceProviderToParticipantByService")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+
+        // Cohort Distribution still WIP, and nothing is calling this function yet so we use the following as temp values:
+        // Post Code: Participant data
+        // Screening Service: Hardcoded "BSS" for BS Select
+        string screeningService = "BSS";
+        string requestBody = "";
+        Participant? allocationData = null;
+
+        _logger.LogInformation("AllocateServiceProviderToParticipantByService is called...");
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+            {
+                requestBody = await reader.ReadToEndAsync();
+            }
+
+            allocationData = JsonSerializer.Deserialize<Participant>(requestBody);
+
+            // use the Rules Engine
+            string staticRulesFile = File.ReadAllText("staticRules.json");
+            var staticRules = JsonSerializer.Deserialize<Workflow[]>(staticRulesFile);
+
+            var reSettings = new ReSettings
+            {
+                CustomTypes = [typeof(Regex)]
+            };
+
+            var rulesEngine = new RulesEngine.RulesEngine(staticRules, reSettings);
+
+            var ruleParameters = new[] {
+                new RuleParameter("participant", allocationData),
+            };
+
+            var resultList = await rulesEngine.ExecuteAllRulesAsync("AllocationDataValidation", ruleParameters);
+
+            foreach (var result in resultList)
+            {
+                _logger.LogInformation("***** Log ResultList", result);
+            }
+
+            // If the Allocation Data has missing required information for allocation, and call Create Validation Exception
+            // if(allocationData == null || string.IsNullOrEmpty(allocationData.Postcode) || string.IsNullOrEmpty(screeningService))
+            // {
+            //     string requestBodyJson = new ValidationException
+            //     {
+            //         FileName = null,
+            //         NhsNumber = allocationData.NhsNumber,
+            //         DateCreated = DateTime.UtcNow,
+            //         RuleDescription = ruleDetails[1],
+            //         RuleContent = ruleDetails[1],
+            //         DateResolved = DateTime.MaxValue,
+            //         ScreeningService = screeningService,
+            //     }
+            // }
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            return _createResponse.CreateHttpResponse(HttpStatusCode.BadRequest, req);
+        }
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Successfully Allocated user to BS Select");
+
+        return response;
+    }
+}
+
