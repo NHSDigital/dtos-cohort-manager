31a266924c7bae76877d6a6f0255ec182b4eac95
andrewmorris43 <andrew.morris43@nhs.net>
2024-07-09 11:37:29 +0000

    feat: add lookup validation into markParticipantAsIneligible function

--- application/CohortManager/src/Functions/screeningDataServices/markParticipantAsIneligible/markParticipantAsIneligible.cs
+++ application/CohortManager/src/Functions/screeningDataServices/markParticipantAsIneligible/markParticipantAsIneligible.cs
@@ -15,16 +15,18 @@ public class MarkParticipantAsIneligible
     private readonly ILogger<MarkParticipantAsIneligible> _logger;
     private readonly IUpdateParticipantData _updateParticipantData;
     private readonly ICreateResponse _createResponse;
+    private readonly ICallFunction _callFunction;
 
-    public MarkParticipantAsIneligible(ILogger<MarkParticipantAsIneligible> logger, ICreateResponse createResponse, IUpdateParticipantData updateParticipantData)
+    public MarkParticipantAsIneligible(ILogger<MarkParticipantAsIneligible> logger, ICreateResponse createResponse, IUpdateParticipantData updateParticipantData, ICallFunction callFunction)
     {
         _logger = logger;
         _updateParticipantData = updateParticipantData;
         _createResponse = createResponse;
+        _callFunction = callFunction;
     }
 
     [Function("markParticipantAsIneligible")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
         string postData = "";
         using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
@@ -33,23 +35,31 @@ public class MarkParticipantAsIneligible
         }
 
         var participantCsvRecord = JsonSerializer.Deserialize<ParticipantCsvRecord>(postData);
-        var participant = participantCsvRecord.Participant;
+        var participantData = participantCsvRecord.Participant;
+
+        // Check if a participant with the supplied NHS Number already exists
+        var existingParticipantData = _updateParticipantData.GetParticipant(participantData.NhsNumber);
+        if (!await ValidateData(existingParticipantData, participantData, participantCsvRecord.FileName))
+        {
+            _logger.LogInformation("The participant has not been removed due to a bad request.");
+            return _createResponse.CreateHttpResponse(HttpStatusCode.BadRequest, req);
+        }
 
         try
         {
             var updated = false;
 
-            if (participant != null)
+            if (participantData != null)
             {
-                updated = _updateParticipantData.UpdateParticipantAsEligible(participant, 'N');
+                updated = _updateParticipantData.UpdateParticipantAsEligible(participantData, 'N');
             }
             if (updated)
             {
-                _logger.LogInformation($"record updated for participant {participant.NhsNumber}");
+                _logger.LogInformation($"record updated for participant {participantData.NhsNumber}");
                 return _createResponse.CreateHttpResponse(HttpStatusCode.OK, req);
             }
 
-            _logger.LogError($"an error occurred while updating data for {participant.NhsNumber}");
+            _logger.LogError($"an error occurred while updating data for {participantData.NhsNumber}");
 
             return _createResponse.CreateHttpResponse(HttpStatusCode.BadRequest, req);
         }
@@ -58,5 +68,27 @@ public class MarkParticipantAsIneligible
             _logger.LogError($"an error occurred: {ex}");
             return _createResponse.CreateHttpResponse(HttpStatusCode.BadRequest, req);
         }
+    }
+
+    private async Task<bool> ValidateData(Participant existingParticipant, Participant newParticipant, string fileName)
+    {
+        var json = JsonSerializer.Serialize(new LookupValidationRequestBody(existingParticipant, newParticipant, fileName));
+
+        try
+        {
+            var response = await _callFunction.SendPost(Environment.GetEnvironmentVariable("LookupValidationURL"), json);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation($"Lookup validation failed.\nMessage: {ex.Message}\nParticipant: {ex.StackTrace}");
+            return false;
+        }
+
+        return false;
     }
 }
