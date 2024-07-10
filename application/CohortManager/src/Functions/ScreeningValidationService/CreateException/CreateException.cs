e3dc6509e11adb2d4f258d844e15b2bcc4ba3d68
SamRobinson75684 <sam.robinson24@nhs.net>
2024-07-10 09:14:35 +0100

    Feat: remformating handleException file

--- application/CohortManager/src/Functions/ScreeningValidationService/CreateException/CreateException.cs
+++ application/CohortManager/src/Functions/ScreeningValidationService/CreateException/CreateException.cs
@@ -11,52 +11,56 @@ using System.Runtime.CompilerServices;
 using Common;
 
 public class CreateException
+{
+    private readonly ILogger<CreateException> _logger;
+
+    private readonly ICallFunction _callFunction;
+    public CreateException(ILogger<CreateException> logger, ICallFunction callFunction)
     {
-        private readonly ILogger<CreateException> _logger;
+        _logger = logger;
+        _callFunction = callFunction;
+    }
 
-        private readonly ICallFunction _callFunction;
-        public CreateException(ILogger<CreateException> logger, ICallFunction callFunction)
+    [Function("CreateException")]
+    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+
+        ValidationException validationException;
+
+        try
         {
-            _logger = logger;
-            _callFunction = callFunction;
-        }
+            string requestBody;
 
-        [Function("CreateException")]
-        public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
-
-            ValidationException validationException;
-
-            try{
-                string requestBody;
-
-                using (var reader = new StreamReader(req.Body, Encoding.UTF8))
-                {
-                    requestBody = await reader.ReadToEndAsync();
-                }
-
-                if(String.IsNullOrEmpty(requestBody)){
-                    _logger.LogError("CreateException received an empty payload");
-                    return req.CreateResponse(HttpStatusCode.BadRequest);
-                }
-
-                validationException = JsonSerializer.Deserialize<ValidationException>(requestBody);
-
-                var jsonString  = JsonSerializer.Serialize<ValidationException>(validationException);
-                var createResponse = await _callFunction.SendPost(Environment.GetEnvironmentVariable("CreateValidationExceptionURL"),jsonString);
-
-                if(createResponse.StatusCode != HttpStatusCode.OK){
-                    return req.CreateResponse(HttpStatusCode.BadRequest);
-                }
-                return req.CreateResponse(HttpStatusCode.OK);
-
-
-
+            using (var reader = new StreamReader(req.Body, Encoding.UTF8))
+            {
+                requestBody = await reader.ReadToEndAsync();
             }
-            catch(Exception ex){
-                _logger.LogError(ex,"there has been an error while creating an exception record: {Message}", ex.Message);
+
+            if (String.IsNullOrEmpty(requestBody))
+            {
+                _logger.LogError("CreateException received an empty payload");
                 return req.CreateResponse(HttpStatusCode.BadRequest);
             }
+
+            validationException = JsonSerializer.Deserialize<ValidationException>(requestBody);
+
+            var jsonString = JsonSerializer.Serialize<ValidationException>(validationException);
+            var createResponse = await _callFunction.SendPost(Environment.GetEnvironmentVariable("CreateValidationExceptionURL"), jsonString);
+
+            if (createResponse.StatusCode != HttpStatusCode.OK)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return req.CreateResponse(HttpStatusCode.OK);
+
+
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "there has been an error while creating an exception record: {Message}", ex.Message);
+            return req.CreateResponse(HttpStatusCode.BadRequest);
         }
     }
+}
 
