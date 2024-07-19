09587a550147d0fb9f913856393db92ebb327d3d
SamTyrrellNHS <samuel.tyrell1@nhs.net>
2024-07-19 11:04:00 +0100

    feat: adding validation for the record count in the file name (#84)
    * adding validation for the record count in the file name and adding unit tests for new function. Also signing properly.
    
    * removing white space and signing properly
    
    * Removing white space and signing properly
    
    * adding try catch to deal with possible exceptions, adding await and removing unnecessary throwing of exceptions
    
    * fixing build error and fixing tests
    
    ---------
    
    Co-authored-by: Tyrrellion <104721569+Tyrrellion@users.noreply.github.com>

--- application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
+++ application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
@@ -9,6 +9,7 @@ using CsvHelper;
 using CsvHelper.Configuration;
 using System.Globalization;
 using System.Net;
+using System.IO;
 
 public class ReceiveCaasFile
 {
@@ -24,66 +25,96 @@ public class ReceiveCaasFile
     [Function(nameof(ReceiveCaasFile))]
     public async Task Run([BlobTrigger("inbound/{name}", Connection = "caasfolder_STORAGE")] Stream stream, string name)
     {
-        FileExtensionCheck(name);
-
-        var badRecords = new Dictionary<int, string>();
-        Cohort cohort = new()
-        {
-            FileName = name
-        };
-        var rowNumber = 0;
-        CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
-        {
-            TrimOptions = TrimOptions.Trim,
-            Delimiter = ",",
-            HeaderValidated = null
-        };
         try
         {
-            using var blobStreamReader = new StreamReader(stream);
-            using var csv = new CsvReader(blobStreamReader, config);
-            csv.Context.RegisterClassMap<ParticipantMap>();
-            foreach (var participant in csv.GetRecords<Participant>())
+            FileExtensionCheck(name);
+            var numberOfRecords = await GetNumberOfRecordsFromFileName(name);
+            if (numberOfRecords == null)
             {
-                rowNumber++;
-                try
+                return;
+            }
+
+            var badRecords = new Dictionary<int, string>();
+            Cohort cohort = new()
+            {
+                FileName = name
+            };
+            var rowNumber = 0;
+            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                TrimOptions = TrimOptions.Trim,
+                Delimiter = ",",
+                HeaderValidated = null
+            };
+            try
+            {
+                using var blobStreamReader = new StreamReader(stream);
+                using var csv = new CsvReader(blobStreamReader, config);
+                csv.Context.RegisterClassMap<ParticipantMap>();
+                var records = csv.GetRecords<Participant>();
+
+                foreach (var participant in records)
                 {
-                    if (participant != null)
+                    rowNumber++;
+                    try
                     {
-                        cohort.Participants.Add(participant);
+                        if (participant != null)
+                        {
+                            cohort.Participants.Add(participant);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        badRecords.Add(rowNumber, csv.Context.Parser.RawRecord);
+                        _logger.LogError("Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", rowNumber, ex.Message, ex.StackTrace);
+                        await InsertValidationErrorIntoDatabase(name);
                     }
                 }
-                catch (Exception ex)
+
+                if (rowNumber != numberOfRecords)
                 {
-                    badRecords.Add(rowNumber, csv.Context.Parser.RawRecord);
-                    _logger.LogError("Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", rowNumber, ex.Message, ex.StackTrace);
+                    _logger.LogError("File name record count not equal to actual record count. File name count: " + name + "| Actual count: " + rowNumber);
                     await InsertValidationErrorIntoDatabase(name);
+                    return;
+                }
+                if (rowNumber == 0)
+                {
+                    _logger.LogError("File contains no records. File name:" + name);
+                    await InsertValidationErrorIntoDatabase(name);
+                    return;
                 }
             }
-        }
-        catch (Exception ex) when (ex is HeaderValidationException || ex is CsvHelperException)
-        {
-            _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
-            await InsertValidationErrorIntoDatabase(name);
-        }
-        try
-        {
-            if (cohort.Participants.Count > 0)
+            catch (Exception ex) when (ex is HeaderValidationException || ex is CsvHelperException || ex is FileFormatException)
             {
-                var json = JsonSerializer.Serialize(cohort);
-                await _callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
-                _logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
+                _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
+                await InsertValidationErrorIntoDatabase(name);
+                return;
             }
-            if (badRecords.Count > 0 || cohort.Participants.Count == 0)
+            try
             {
-                _logger.LogError("Failed to create {BadRecordsCount} Objects", badRecords.Count);
-                _logger.LogError("All failed Records - {BadRecords}", badRecords);
+                if (cohort.Participants.Count > 0)
+                {
+                    var json = JsonSerializer.Serialize(cohort);
+                    await _callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
+                    _logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
+                }
+                if (badRecords.Count > 0 || cohort.Participants.Count == 0)
+                {
+                    _logger.LogError("Failed to create {BadRecordsCount} Objects", badRecords.Count);
+                    _logger.LogError("All failed Records - {BadRecords}", badRecords);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to call function.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
+                await InsertValidationErrorIntoDatabase(name);
             }
         }
         catch (Exception ex)
         {
-            _logger.LogError("Unable to call function.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
+            _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
             await InsertValidationErrorIntoDatabase(name);
+            return;
         }
     }
 
@@ -110,5 +141,22 @@ public class ReceiveCaasFile
         {
             throw new NotSupportedException("Invalid file type. Only CSV files are allowed.");
         };
+    }
+
+    private async Task<int?> GetNumberOfRecordsFromFileName(string name)
+    {
+        var str = name.Remove(name.IndexOf('.'));
+        var numberOfRecords = (str.Split('_')[2]).Substring(1);
+
+        if (Int32.TryParse(numberOfRecords, out int n))
+        {
+            return n;
+        }
+        else
+        {
+            _logger.LogError("File name is invalid. File name: " + name);
+            await InsertValidationErrorIntoDatabase(name);
+            return null;
+        }
     }
 }
