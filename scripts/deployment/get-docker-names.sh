e064388767fee38e14e0f7a3ad754ac0df4282a1
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2024-07-22 12:15:55 +0100

    Update get-docker-names.sh

--- scripts/deployment/get-docker-names.sh
+++ scripts/deployment/get-docker-names.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $BASH_VERSION
+
 declare -A docker_functions_map=(["CohortDistributionServices/AddCohortDistributionData"]="add-cohort-distribution-data")
 # for dir in $CHANGED_FOLDERS; do
 # echo "${docker_functions_map[$dir]}"
