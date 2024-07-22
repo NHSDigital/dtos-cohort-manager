52b1f15d9205d148469c9f26b6676c91a2f84835
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2024-07-22 12:06:16 +0100

    test

--- scripts/deployment/get-docker-names.sh
+++ scripts/deployment/get-docker-names.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 declare -A docker_functions_map=(["CohortDistributionServices/AddCohortDistributionData"]="add-cohort-distribution-data")
-for dir in $CHANGED_FOLDERS; do
-echo "${docker_functions_map[$dir]}"
-done
+# for dir in $CHANGED_FOLDERS; do
+# echo "${docker_functions_map[$dir]}"
+# done
+
+echo "FUNC_NAMES=${docker_functions_map[$CHANGED_FOLDERS]}" >> "$GITHUB_OUTPUT" 
