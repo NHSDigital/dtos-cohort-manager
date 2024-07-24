1fcb413103658a2784adf91545bdc7378f7f7136
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2024-07-24 07:43:52 +0100

    feat: name prefix transformation

--- application/CohortManager/src/Functions/CohortDistributionServices/TransformDataService/TransformDataService.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/TransformDataService/TransformDataService.cs
@@ -8,6 +8,13 @@ using System.Net;
 using System.Text;
 using Model;
 using Common;
+using System.Collections;
+using System.Text.RegularExpressions;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.OpenApi.Models;
+// using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+// using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+// using Microsoft.AspNetCore.Http;
 
 public class TransformDataService
 {
@@ -18,6 +25,9 @@ public class TransformDataService
     }
 
     [Function("TransformDataService")]
+    // [OpenApiOperation(operationId: "greeting", tags: new[] { "greeting" }, Summary = "Greetings", Description = "This shows a welcome message.", Visibility = OpenApiVisibilityType.Important)]
+    // [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    // [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Summary = "The response", Description = "This returns the response")]
     public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
         TransformDataRequestBody requestBody;
@@ -60,6 +70,8 @@ public class TransformDataService
             NamePrefix = GetTransformedData(resultList, "NamePrefix") ?? participant.NamePrefix
         };
 
+        var pparticipant = TransformNamePrefix(transformedParticipant);
+
         var response = JsonSerializer.Serialize(transformedParticipant);
         return _createResponse.CreateHttpResponse(HttpStatusCode.OK, req, response);
     }
@@ -67,5 +79,51 @@ public class TransformDataService
     private string GetTransformedData(List<RuleResultTree> results, string field)
     {
         return (string)results.Find(x => x.Rule.RuleName.Split('.')[1] == field)?.ActionResult.Output;
+    }
+
+    public async Task<Participant> TransformNamePrefix(Participant participant) {
+
+        var ruleTable = new Dictionary<string, string>
+            {
+                {"37.NamePrefix.AirMarshal", "A.ML"},
+                {"38.NamePrefix.Admiral", "ADM"},
+                {"39.NamePrefix.Brigadier", "BRIG"},
+                {"40.NamePrefix.Brother", "BRO"}, {"41.NamePrefix.Br", "BRO"},
+                {"42.NamePrefix.Captain", "CAPT"}, {"43.NamePrefix.Cpt", "CAPT"},
+                {"44.NamePrefix.Commander", "CMDR"}, {"45.NamePrefix.Cdr", "CMDR"},
+                {"46.NamePrefix.Colonel", "COL"}, {"48.NamePrefix.Doctor", "DR"},
+                {"49.NamePrefix.Father", "FR"},{"50.NamePrefix.Fath", "FR"},
+                {"51.NamePrefix.General", "GEN"}, {"52.NamePrefix.Honourable", "HON"},
+                {"53.NamePrefix.Judge", "HON"}, {"54.NamePrefix.HisRoyalHighness", "HRH"},
+                {"55.NamePrefix.HerRoyalHighness", "HRH"},{"56.NamePrefix.Baroness", "LADY"},
+                {"57.NamePrefix.Baron", "LORD"}, {"58.NamePrefix.Duchess.", "LADY"},
+                {"59.NamePrefix.Duke.", "LORD"}, {"60.NamePrefix.LieutenantColonel", "LT.C"},
+                {"61.NamePrefix.Major.", "MAJ"}, {"62.NamePrefix.Master.", "MR"},
+                {"63.NamePrefix.Mast", "MR"}, {"64.NamePrefix.Mister", "MR"},
+                {"65.NamePrefix.Mstr", "MR"}, {"67.NamePrefix.Count", "R.HN"},
+                {"68.NamePrefix.Countess", "R.HN"}, {"72.NamePrefix.Bishop", "R.RV"},
+                {"76.NamePrefix.Mosignor", "REV"}, {"77.NamePrefix.Pastor", "REV"},
+                {"80.NamePrefix.Sister", "SR"}, {"82.NamePrefix.WingCommander", "WCOM"}
+            };
+
+        string json = File.ReadAllText("namePrefixRules.json");
+        var rules = JsonSerializer.Deserialize<Workflow[]>(json);
+
+        var re = new RulesEngine.RulesEngine(rules);
+
+        // var reSettings = new ReSettings{
+		// 	CustomTypes = new Type[]{typeof(Regex)}
+		// };
+
+        // Execute rules
+        var rulesList = await re.ExecuteAllRulesAsync("NamePrefix");//, reSettings);
+        System.Console.WriteLine("participant title: " + participant.NamePrefix);
+        System.Console.WriteLine("rules executed: " + GetTransformedData(rulesList, "NamePrefix")); 
+        // turn list into string
+
+        // Apply name prefix transformation
+        //participant.NamePrefix = ruleTable["*RULE NAME*"];
+
+        return participant;
     }
 }
