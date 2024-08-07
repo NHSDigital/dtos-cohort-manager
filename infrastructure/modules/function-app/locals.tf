b54ed5ae6a75c32be69c22b13d4f7d7296c32523
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-08-07 12:23:12 +0000

    fix: renamed function apps and added missing env variables

--- infrastructure/modules/function-app/locals.tf
+++ infrastructure/modules/function-app/locals.tf
@@ -1,20 +1,26 @@
 locals {
   fnapp_urls = {
 
-    processCaasFile             = "https://${var.names.function-app}-${lower(var.function_app.ProcessCaasFile.name_suffix)}.azurewebsites.net/api/processCaasFile"
-    fileValidation              = "https://${var.names.function-app}-${lower(var.function_app.FileValidation.name_suffix)}.azurewebsites.net/api/FileValidation"
-    addParticipant              = "https://${var.names.function-app}-${lower(var.function_app.AddNewParticipant.name_suffix)}.azurewebsites.net/api/addParticipant"
-    removeParticipant           = "https://${var.names.function-app}-${lower(var.function_app.RemoveParticipant.name_suffix)}.azurewebsites.net/api/RemoveParticipant"
-    updateParticipant           = "https://${var.names.function-app}-${lower(var.function_app.UpdateParticipant.name_suffix)}.azurewebsites.net/api/updateParticipant"
-    demographicDataFunction     = "https://${var.names.function-app}-${lower(var.function_app.DemographicDataManagement.name_suffix)}.azurewebsites.net/api/DemographicDataFunction"
-    createParticipant           = "https://${var.names.function-app}-${lower(var.function_app.CreateParticipant.name_suffix)}.azurewebsites.net/api/CreateParticipant"
-    markParticipantAsEligible   = "https://${var.names.function-app}-${lower(var.function_app.MarkParticipantEligible.name_suffix)}.azurewebsites.net/api/markParticipantAsEligible"
-    staticValidation            = "https://${var.names.function-app}-${lower(var.function_app.StaticValidation.name_suffix)}.azurewebsites.net/api/StaticValidation"
-    markParticipantAsIneligible = "https://${var.names.function-app}-${lower(var.function_app.MarkParticipantAsIneligible.name_suffix)}.azurewebsites.net/api/markParticipantAsIneligible"
-    updateParticipant           = "https://${var.names.function-app}-${lower(var.function_app.UpdateParticipantDetails.name_suffix)}.azurewebsites.net/api/updateParticipantDetails"
-    lookupValidation            = "https://${var.names.function-app}-${lower(var.function_app.LookupValidation.name_suffix)}.azurewebsites.net/api/LookupValidation"
-    createValidationException   = "https://${var.names.function-app}-${lower(var.function_app.CreateValidationExceptions.name_suffix)}.azurewebsites.net/api/CreateValidationException"
-    demographicDataService      = "https://${var.names.function-app}-${lower(var.function_app.DemographicDataService.name_suffix)}.azurewebsites.net/api/DemographicDataService"
+    processCaasFile                  = "https://${var.names.function-app}-${lower(var.function_app.ProcessCaasFile.name_suffix)}.azurewebsites.net/api/processCaasFile"
+    fileValidation                   = "https://${var.names.function-app}-${lower(var.function_app.FileValidation.name_suffix)}.azurewebsites.net/api/FileValidation"
+    addParticipant                   = "https://${var.names.function-app}-${lower(var.function_app.AddParticipant.name_suffix)}.azurewebsites.net/api/addParticipant"
+    removeParticipant                = "https://${var.names.function-app}-${lower(var.function_app.RemoveParticipant.name_suffix)}.azurewebsites.net/api/RemoveParticipant"
+    updateParticipant                = "https://${var.names.function-app}-${lower(var.function_app.UpdateParticipant.name_suffix)}.azurewebsites.net/api/updateParticipant"
+    updateParticipantDetails         = "https://${var.names.function-app}-${lower(var.function_app.UpdateParticipantDetails.name_suffix)}.azurewebsites.net/api/updateParticipantDetails"
+    demographicDataFunction          = "https://${var.names.function-app}-${lower(var.function_app.DemographicDataManagement.name_suffix)}.azurewebsites.net/api/DemographicDataFunction"
+    createParticipant                = "https://${var.names.function-app}-${lower(var.function_app.CreateParticipant.name_suffix)}.azurewebsites.net/api/CreateParticipant"
+    markParticipantAsEligible        = "https://${var.names.function-app}-${lower(var.function_app.MarkParticipantAsEligible.name_suffix)}.azurewebsites.net/api/markParticipantAsEligible"
+    staticValidation                 = "https://${var.names.function-app}-${lower(var.function_app.StaticValidation.name_suffix)}.azurewebsites.net/api/StaticValidation"
+    markParticipantAsIneligible      = "https://${var.names.function-app}-${lower(var.function_app.MarkParticipantAsIneligible.name_suffix)}.azurewebsites.net/api/markParticipantAsIneligible"
+    lookupValidation                 = "https://${var.names.function-app}-${lower(var.function_app.LookupValidation.name_suffix)}.azurewebsites.net/api/LookupValidation"
+    createException                  = "https://${var.names.function-app}-${lower(var.function_app.CreateException.name_suffix)}.azurewebsites.net/api/CreateException"
+    demographicDataService           = "https://${var.names.function-app}-${lower(var.function_app.DemographicDataService.name_suffix)}.azurewebsites.net/api/DemographicDataService"
+    retrieveParticipantData          = "https://${var.names.function-app}-${lower(var.function_app.RetrieveParticipantData.name_suffix)}.azurewebsites.net/api/RetrieveParticipantData"
+    allocateServiceProvider          = "https://${var.names.function-app}-${lower(var.function_app.AllocateServiceProvider.name_suffix)}.azurewebsites.net/api/AllocateServiceProvider"
+    transformDataService             = "https://${var.names.function-app}-${lower(var.function_app.TransformDataService.name_suffix)}.azurewebsites.net/api/TransformDataService"
+    addCohortDistributionData        = "https://${var.names.function-app}-${lower(var.function_app.AddCohortDistributionData.name_suffix)}.azurewebsites.net/api/AddCohortDistributionData"
+    removeFromCohortDistributionData = "https://${var.names.function-app}-${lower(var.function_app.RemoveFromCohortDistributionData.name_suffix)}.azurewebsites.net/api/RemoveFromCohortDistributionData"
+    createCohortDistribution         = "https://${var.names.function-app}-${lower(var.function_app.CreateCohortDistribution.name_suffix)}.azurewebsites.net/api/CreateCohortDistribution"
 
   }
 
@@ -33,7 +39,7 @@ locals {
 locals {
   app_settings = {
 
-    receiveCaasFile = {
+    ReceiveCaasFile = {
 
       caasfolder_STORAGE = var.caasfolder_STORAGE
       targetFunction     = local.fnapp_urls.processCaasFile
@@ -48,50 +54,60 @@ locals {
       DemographicURI       = local.fnapp_urls.demographicDataFunction
     }
 
-    AddNewParticipant = {
+    AddParticipant = {
 
       DSaddParticipant            = local.fnapp_urls.createParticipant
       DSmarkParticipantAsEligible = local.fnapp_urls.markParticipantAsEligible
       DemographicURIGet           = local.fnapp_urls.demographicDataFunction
       StaticValidationURL         = local.fnapp_urls.staticValidation
+      exceptionFunctionURL        = local.fnapp_urls.createException
     }
 
     RemoveParticipant = {
 
-      markParticipantAsIneligible = local.fnapp_urls.markParticipantAsIneligible
+      markParticipantAsIneligible     = local.fnapp_urls.markParticipantAsIneligible
+      DemographicURI                  = local.fnapp_urls.demographicDataFunction
+      removeFromCohortDistributionURL = local.fnapp_urls.removeFromCohortDistributionData
+      exceptionFunctionURL            = local.fnapp_urls.createException
     }
 
     UpdateParticipant = {
 
-      UpdateParticipant   = local.fnapp_urls.updateParticipant
-      StaticValidationURL = local.fnapp_urls.staticValidation
-      DemographicURIGet   = local.fnapp_urls.demographicDataFunction
+      UpdateParticipant            = local.fnapp_urls.updateParticipantDetails
+      StaticValidationURL          = local.fnapp_urls.staticValidation
+      DemographicURIGet            = local.fnapp_urls.demographicDataFunction
+      cohortDistributionServiceURL = local.fnapp_urls.createCohortDistribution
+      exceptionFunctionURL         = local.fnapp_urls.createException
     }
 
     CreateParticipant = {
 
       DtOsDatabaseConnectionString = local.db_connection_string
       LookupValidationURL          = local.fnapp_urls.lookupValidation
+      exceptionFunctionURL         = local.fnapp_urls.createException
     }
 
-    MarkParticipantEligible = {
+    MarkParticipantAsEligible = {
 
       DtOsDatabaseConnectionString = local.db_connection_string
+      exceptionFunctionURL         = local.fnapp_urls.createException
     }
 
     MarkParticipantAsIneligible = {
 
       DtOsDatabaseConnectionString = local.db_connection_string
       LookupValidationURL          = local.fnapp_urls.lookupValidation
+      exceptionFunctionURL         = local.fnapp_urls.createException
     }
 
     UpdateParticipantDetails = {
 
       DtOsDatabaseConnectionString = local.db_connection_string
       LookupValidationURL          = local.fnapp_urls.lookupValidation
+      exceptionFunctionURL         = local.fnapp_urls.createException
     }
 
-    CreateValidationExceptions = {
+    CreateException = {
 
       DtOsDatabaseConnectionString = local.db_connection_string
     }
@@ -108,18 +124,18 @@ locals {
 
     FileValidation = {
 
-      CreateValidationExceptionURL = local.fnapp_urls.createValidationException
-      inboundBlobName              = "file-exceptions"
+      exceptionFunctionURL = local.fnapp_urls.createException
+      inboundBlobName      = "file-exceptions"
     }
 
     StaticValidation = {
 
-      CreateValidationExceptionURL = local.fnapp_urls.createValidationException
+      exceptionFunctionURL = local.fnapp_urls.createException
     }
 
     LookupValidation = {
 
-      CreateValidationExceptionURL = local.fnapp_urls.createValidationException
+      exceptionFunctionURL = local.fnapp_urls.createException
     }
 
     DemographicDataManagement = {
@@ -137,22 +153,26 @@ locals {
       DtOsDatabaseConnectionString = local.db_connection_string
     }
 
-    RemoveCohortDistributionData = {
+    RemoveFromCohortDistributionData = {
 
       DtOsDatabaseConnectionString = local.db_connection_string
     }
 
-    TransformData = {
+    TransformDataService = {
 
     }
 
     AllocateServiceProvider = {
 
-      CreateValidationExceptionURL = local.fnapp_urls.createValidationException
+      exceptionFunctionURL = local.fnapp_urls.createException
     }
 
     CreateCohortDistribution = {
 
+      retrieveParticipantDataURL = local.fnapp_urls.retrieveParticipantData
+      allocateServiceProviderURL = local.fnapp_urls.allocateServiceProvider
+      transformDataServiceURL    = local.fnapp_urls.transformDataService
+      addCohortDistributionURL   = local.fnapp_urls.addCohortDistributionData
     }
 
     RetrieveParticipantData = {
