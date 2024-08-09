8d6a9464dd3282eaa6221522c1d3bbcde99b3aa6
Warren-Pitterson <168638743+Warren-Pitterson@users.noreply.github.com>
2024-08-09 11:41:14 +0100

    feat: renamed function name to match name in solution File (#124)
    * feat: renamed function name to match name in solution File
    
    * feat: remove from naming removed from tests

--- tests/ParticipantManagementServicesTests/RemoveParticipantTests/RemoveParticipantTests.cs
+++ tests/ParticipantManagementServicesTests/RemoveParticipantTests/RemoveParticipantTests.cs
@@ -29,7 +29,7 @@ public class RemoveParticipantTests
     {
         Environment.SetEnvironmentVariable("markParticipantAsIneligible", "markParticipantAsIneligible");
         Environment.SetEnvironmentVariable("DemographicURIGet", "DemographicURIGet");
-        Environment.SetEnvironmentVariable("RemoveFromCohortDistributionURL", "RemoveFromCohortDistributionURL");
+        Environment.SetEnvironmentVariable("RemoveCohortDistributionURL", "RemoveCohortDistributionURL");
 
         _participantCsvRecord = new ParticipantCsvRecord
         {
@@ -67,7 +67,7 @@ public class RemoveParticipantTests
         _callFunction.Setup(call => call.SendPost(It.Is<string>(s => s.Contains("markParticipantAsIneligible")), It.IsAny<string>()))
             .Returns(Task.FromResult<HttpWebResponse>(_webResponse.Object));
 
-        _callFunction.Setup(call => call.SendGet(It.Is<string>(s => s.Contains("RemoveFromCohortDistributionURL")), It.IsAny<Dictionary<string,string>>()))
+        _callFunction.Setup(call => call.SendGet(It.Is<string>(s => s.Contains("RemoveCohortDistributionURL")), It.IsAny<Dictionary<string,string>>()))
             .Returns(Task.FromResult<string>("Participant Removed"));
 
         _checkDemographic.Setup(x => x.GetDemographicAsync(It.IsAny<string>(), It.Is<string>(s => s.Contains("DemographicURIGet"))))
