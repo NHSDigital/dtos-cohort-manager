d0a8c647e31d13f837940e39a31b3d85261e97b8
warren <warren.pitterson1@nhs.net>
2024-08-13 14:42:55 +0100

    fix: formatting fix and if statement replace with null-coalescing operator

--- application/CohortManager/src/Functions/Shared/Common/FileReader.cs
+++ application/CohortManager/src/Functions/Shared/Common/FileReader.cs
@@ -21,20 +21,14 @@ public class FileReader : IFileReader
         string basePath = AppDomain.CurrentDomain.BaseDirectory;
         string[] potentialDirectories =
         {
-        Path.Combine(basePath, $"{fileName}.json"),
-        Path.Combine(basePath, "..", "..", $"{fileName}.json"),
-        Path.Combine(basePath, "TestUtils", "MockData", $"{fileName}.json"),
-        Path.Combine(basePath, "..", "..", "..", "..", "..", "..", "tests", "TestUtils", "MockData", $"{fileName}.json"),
-    };
+            Path.Combine(basePath, $"{fileName}.json"),
+            Path.Combine(basePath, "..", "..", $"{fileName}.json"),
+            Path.Combine(basePath, "TestUtils", "MockData", $"{fileName}.json"),
+            Path.Combine(basePath, "..", "..", "..", "..", "..", "..", "tests", "TestUtils", "MockData", $"{fileName}.json"),
+        };
 
-        string filePath = potentialDirectories
-            .Select(Path.GetFullPath)
-            .FirstOrDefault(File.Exists);
-
-        if (filePath == null)
-        {
-            throw new FileNotFoundException($"File '{fileName}.json' not found in any of the specified locations.");
-        }
+        string filePath = potentialDirectories.Select(Path.GetFullPath).FirstOrDefault(File.Exists)
+        ?? throw new FileNotFoundException($"File '{fileName}.json' not found in any of the specified locations.");
 
         return File.ReadAllText(filePath);
     }
