33d24031bff732c7d3ccc4710e432978b777d30c
adamsheldon1 <adam.sheldon1@nhs.net>
2024-08-13 12:24:33 +0100

    refactor: moved appsettings into separate classes
    * Moved the AppSettings, ConnectionStrings and FilePaths classes into their own respective files.
    * Updated TestConfig and cleaned up.

--- tests/IntegrationTests/Config/TestConfig.cs
+++ tests/IntegrationTests/Config/TestConfig.cs
@@ -3,24 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System.IO;
 
-public class AppSettings
-{
-    public ConnectionStrings ConnectionStrings { get; set; }
-    public FilePaths FilePaths { get; set; }
-    public string BlobContainerName { get; set; }
-    public string AzureWebJobsStorage {get; set; }
-}
-
-public class ConnectionStrings
-{
-    public string DtOsDatabaseConnectionString { get; set; }
-}
-
-public class FilePaths
-{
-    public string Local { get; set; }
-}
-
 public static class TestConfig
 {
     private static ServiceProvider _serviceProvider;
