d42dfc7edb35eb9192b23d3a2b2cac67940153fc
andrewmorris43 <andrew.morris43@nhs.net>
2024-08-16 08:20:59 +0100

    fix: dependency injection errors

--- application/CohortManager/src/Functions/CohortDistributionServices/CreateCohortDistribution/Program.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/CreateCohortDistribution/Program.cs
@@ -1,5 +1,8 @@
+using System.Data;
+using System.Data.Common;
 using Common;
 using Data.Database;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -7,9 +10,17 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
+        DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
+        services.AddSingleton<IDbConnection>(provider =>
+        {
+            var providerFactory = DbProviderFactories.GetFactory("System.Data.SqlClient");
+            var conn = providerFactory.CreateConnection();
+            return conn;
+        });
         services.AddSingleton<ICallFunction, CallFunction>();
         services.AddSingleton<ICreateResponse, CreateResponse>();
         services.AddSingleton<ICohortDistributionHelper, CohortDistributionHelper>();
+        services.AddSingleton<IDatabaseHelper, DatabaseHelper>();
         services.AddSingleton<IParticipantManagerData, ParticipantManagerData>();
     })
     .AddExceptionHandler()
