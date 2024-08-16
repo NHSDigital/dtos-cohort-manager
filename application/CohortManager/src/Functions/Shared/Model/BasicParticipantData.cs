ef1794f00f322890bdbc711a8cf4ca66b90f1419
Andrew Morris <167291702+andrewmorris43@users.noreply.github.com>
2024-08-16 10:08:46 +0100

    feat: implement screening service lookup (#136)
    * feat: add screening service lookup
    
    * fix: file formatting
    
    * fix: file formatting
    
    * feat: use screening service lookup
    
    * chore: remove unnecessary console log
    
    * fix: make IDbConnection a singleton
    
    * fix: define screening lookup table columns
    
    * fix: use database helper to get string value
    
    * fix: update file name regex
    
    * fix: file formatting
    
    * fix: screening service model and SQL

--- application/CohortManager/src/Functions/Shared/Model/BasicParticipantData.cs
+++ application/CohortManager/src/Functions/Shared/Model/BasicParticipantData.cs
@@ -24,4 +24,6 @@ public class BasicParticipantData
     public string? ChangeReasonCode { get; set; }
     public string? RemovalReason { get; set; }
     public string? RemovalEffectiveFromDate { get; set; }
+    public string? ScreeningId { get; set; }
+    public string? ScreeningName { get; set; }
 }
