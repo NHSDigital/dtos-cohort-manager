c843113736cdadc9bcf9e05ef63bf77f658d9b00
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2024-08-19 11:34:59 +0100

    feat: readme updates & removing secrets

--- application/CohortManager/Set-up/azurite/send-sample-file.py
+++ application/CohortManager/Set-up/azurite/send-sample-file.py
@@ -1,6 +1,15 @@
-import sys
-import os
-from azure.storage.blob import BlobServiceClient, BlobClient
+"""A simple scipt to send sample files to azurite
+    Requirements:
+        python-dotenv
+        azure-storage-blob"""
+
+try:
+    import sys
+    import os
+    from azure.storage.blob import BlobServiceClient
+    from dotenv  import load_dotenv
+except ModuleNotFoundError:
+    sys.exit("Requirements not installed, please run 'pip install azure-storage-blob python-dotenv'")
 
 if len(sys.argv) < 2:
     print("""Description:
@@ -13,8 +22,10 @@ Options:
     1  The BSS_20240628191800_n1.csv sample file
     10  The BSS_20240601121212_n10.csv sample file
     100  The BSS_20240628191800_n100.csv sample file
-Example:
-    python send-sample-file.py 100""")
+    <other file> Any argument not in the list above will be treated as a path to a custom file
+Examples:
+    python send-sample-file.py 100
+    python send-sample-file.py custom-file.csv""")
     sys.exit()
 elif len(sys.argv) > 2:
     sys.exit("This script accepts only one argument, run the command with no arguments to view the help page")
@@ -25,19 +36,26 @@ elif sys.argv[1] == "10":
 elif sys.argv[1] == "100":
     sample_file = "BSS_20240628191800_n100.csv"
 else:
-    sys.exit("Argument not recognised, acceptable arguments are '1', '10', and '100'")
+    sample_file = sys.argv[1]
 
-
-connect_str = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-print("Connected to Azurite")
+try:
+    load_dotenv(dotenv_path="../../.env")
+    CONNECTION_STRING = os.getenv('AZURITE_CONNECTION_STRING')
+    blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+    print("Connected to Azurite")
+except FileNotFoundError:
+    sys.exit(".env file not found, please follow the instructions in the docs to create one.")
+except ValueError:
+    sys.exit("Could not find the azurite connection string in the .env file, please make sure you have the correct one")
 
 inbound_client = blob_service_client.get_container_client("inbound")
-sample_file = "BSS_20240601121212_n10.csv"
 blob_client = inbound_client.get_blob_client(sample_file)
 print("blob client established")
 
-with open(sample_file, "rb") as data:
-    blob_client.upload_blob(data, overwrite=True)
+try:
+    with open(sample_file, "rb") as data:
+        blob_client.upload_blob(data, overwrite=True)
+except FileNotFoundError:
+    sys.exit(f"File {sample_file} could not been found, please download the files from confluence and put them in the directory")
 
 print("Sample file uploaded")
