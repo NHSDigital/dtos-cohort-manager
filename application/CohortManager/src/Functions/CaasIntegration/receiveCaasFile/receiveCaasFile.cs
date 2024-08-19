eb9fe6cec5371926776d4b1f8d2e908a66d7b4d6
Sudip Mondal <SUKU6@HSCIC.GOV.UK>
2024-08-19 12:57:26 +0100

    feat: recieveCaasFile function modied to accept Parquet file from blob

--- application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
+++ application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
@@ -1,125 +1,176 @@
+
 namespace NHS.Screening.ReceiveCaasFile;
 
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
+using Azure.Storage.Blobs;
+using ChoETL;
 using Model;
 using Common;
-using CsvHelper;
-using CsvHelper.Configuration;
 using System.Globalization;
 using System.Net;
 using System.IO;
-using System.Text.RegularExpressions;
+using Model.Enums;
+using System.Collections.Generic;
 
-public class ReceiveCaasFile
+public class ReceiveCaasFile(ILogger<ReceiveCaasFile> logger, ICallFunction callFunction)
 {
-    private readonly ILogger<ReceiveCaasFile> _logger;
-    private readonly ICallFunction _callFunction;
-
-    public ReceiveCaasFile(ILogger<ReceiveCaasFile> logger, ICallFunction callFunction)
-    {
-        _logger = logger;
-        _callFunction = callFunction;
-    }
-
     [Function(nameof(ReceiveCaasFile))]
-    public async Task Run([BlobTrigger("inbound/{name}", Connection = "caasfolder_STORAGE")] Stream stream, string name)
+    public async Task Run([BlobTrigger("inbound/{name}", Connection = "caasfolder_STORAGE")] Stream strm, string name)
     {
         try
         {
-            if (!FileNameAndFileExtensionIsValid(name))
-            {
-                _logger.LogError("File name or file extension is invalid. Not in format BSS_ccyymmddhhmmss_n8.csv. file Name: " + name);
-                await InsertValidationErrorIntoDatabase(name);
-                return;
-            }
+            FileExtensionCheck(name);
 
-            var numberOfRecords = await GetNumberOfRecordsFromFileName(name);
-            if (numberOfRecords == null)
-            {
-                return;
-            }
-
-            var badRecords = new Dictionary<int, string>();
             Cohort cohort = new()
             {
                 FileName = name
             };
+            List<Cohort> chunks = new List<Cohort>();
             var rowNumber = 0;
-            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                TrimOptions = TrimOptions.Trim,
-                Delimiter = ",",
-                HeaderValidated = null
-            };
+
             try
             {
-                using var blobStreamReader = new StreamReader(stream);
-                using var csv = new CsvReader(blobStreamReader, config);
-                csv.Context.RegisterClassMap<ParticipantMap>();
-                var records = csv.GetRecords<Participant>();
+                var azureStorage = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+                var blobContainerName = Environment.GetEnvironmentVariable("BlobContainerName");
 
-                foreach (var participant in records)
+                BlobServiceClient blobServiceClient = new BlobServiceClient(azureStorage);
+                BlobContainerClient container = blobServiceClient.GetBlobContainerClient(blobContainerName);
+                var blobs = container.GetBlobs().Where(x => x.Name.Equals(name));
+
+                try
                 {
-                    rowNumber++;
-                    try
+                    foreach (var item in blobs)
                     {
-                        if (participant != null)
-                        {
-                            cohort.Participants.Add(participant);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        badRecords.Add(rowNumber, csv.Context.Parser.RawRecord);
-                        _logger.LogError("Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", rowNumber, ex.Message, ex.StackTrace);
-                        await InsertValidationErrorIntoDatabase(name);
+                        var blob = container.GetBlobClient(item.Name);
+                        await using var stream = await blob.OpenReadAsync();
+
+                       foreach (dynamic e in new ChoParquetReader(stream))
+                       {
+                           rowNumber++;
+
+                           Participant participant = new Participant();
+                           participant.RecordType = Convert.ToString(e.Record_Type);
+                           participant.ChangeTimeStamp = e.Change_Time_Stamp.ToString("F0", CultureInfo.InvariantCulture);
+                           participant.SerialChangeNumber = Convert.ToString(e.Serial_Change_Number);
+                           participant.NhsNumber = Convert.ToString(e.NHS_Number);
+                           participant.SupersededByNhsNumber = Convert.ToString(e.Superseded_by_NHS_number);
+                           participant.PrimaryCareProvider =Convert.ToString( e.Primary_Care_Provider);
+                           participant.PrimaryCareProviderEffectiveFromDate = Convert.ToString(e.Primary_Care_Provider_Business_Effective_From_Date);
+                           participant.CurrentPosting = Convert.ToString(e.Current_Posting);
+                           participant.CurrentPostingEffectiveFromDate = Convert.ToString(e.Current_Posting_Business_Effective_From_Date);
+                           participant.PreviousPosting = Convert.ToString(e.Previous_Posting);
+                           participant.PreviousPostingEffectiveFromDate = Convert.ToString(e.Previous_Posting_Business_Effective_To_Date);
+                           participant.NamePrefix = Convert.ToString(e.Name_Prefix);
+                           participant.FirstName = Convert.ToString(e.Given_Name);
+                           participant.OtherGivenNames = Convert.ToString(e.Other_Given_Name);
+                           participant.Surname = Convert.ToString(e.Family_Name);
+                           participant.PreviousSurname = Convert.ToString(e.Previous_Family_Name);
+                           participant.DateOfBirth = Convert.ToString(e.Date_of_Birth);
+                           if (Enum.IsDefined(typeof(Gender),Convert.ToInt16(e.Gender)))
+                            {
+                                participant.Gender = (Gender)Enum.ToObject(typeof(Gender), e.Gender);
+                            }
+                            else
+                            {
+                                logger.LogError($"Validation failed for field name 'Gender' on line {rowNumber}. File name: {name}.");
+                                await InsertValidationErrorIntoDatabase(name);
+                                return;
+                            }
+                           participant.AddressLine1 = Convert.ToString(e.Address_line_1);
+                           participant.AddressLine2 = Convert.ToString(e.Address_line_2);
+                           participant.AddressLine3 = Convert.ToString(e.Address_line_3);
+                           participant.AddressLine4 = Convert.ToString(e.Address_line_4);
+                           participant.AddressLine5 = Convert.ToString(e.Address_line_5);
+                           participant.Postcode = Convert.ToString(e.Postcode);
+                           participant.PafKey = Convert.ToString(e.PAF_key);
+                           participant.UsualAddressEffectiveFromDate = Convert.ToString(e.Usual_Address_Business_Effective_From_Date);
+                           participant.ReasonForRemoval = Convert.ToString(e.Reason_for_Removal);
+                           participant.ReasonForRemovalEffectiveFromDate = Convert.ToString(e.Reason_for_Removal_Business_Effective_From_Date);
+                           participant.DateOfDeath = Convert.ToString(e.Date_of_Death);
+                           if (Enum.IsDefined(typeof(Status),Convert.ToInt16(e.Death_Status)))
+                            {
+                                participant.DeathStatus = (Status)Enum.ToObject(typeof(Status), e.Death_Status);
+                            }
+                            else
+                            {
+                                logger.LogError($"Validation failed for field name 'Status' on line {rowNumber}. File name: {name}.");
+                                await InsertValidationErrorIntoDatabase(name);
+                                return;
+                            }
+                           participant.TelephoneNumber = Convert.ToString(e.Telephone_Number_Home);
+                           participant.TelephoneNumberEffectiveFromDate = Convert.ToString(e.Telephone_Number_Home_Business_Effective_From_Date);
+                           participant.MobileNumber = Convert.ToString(e.Telephone_Number_Mobile);
+                           participant.MobileNumberEffectiveFromDate = Convert.ToString(e.Telephone_Number_Mobile_Business_Effective_From_Date);
+                           participant.EmailAddress = Convert.ToString(e.Email_address_Home);
+                           participant.EmailAddressEffectiveFromDate = Convert.ToString(e.Email_address_Home_Business_Effective_From_Date);
+                           participant.PreferredLanguage = Convert.ToString(e.Preferred_Language);
+                           participant.IsInterpreterRequired = Convert.ToString(e.Interpreter_required);
+                           participant.InvalidFlag = Convert.ToString(e.Invalid_Flag);
+                           participant.RecordIdentifier = Convert.ToString(e.Record_Identifier);
+                           participant.ChangeReasonCode = Convert.ToString(e.Change_Reason_Code);
+
+                           cohort.Participants.Add(participant);
+
+                           if (cohort.Participants.Count == 1000)
+                           {
+                             //124928
+                               chunks.Add(cohort);
+                               cohort.Participants.Clear();
+                           }
+                       }
+
                     }
                 }
-
-                if (rowNumber != numberOfRecords)
+                catch (Exception ex)
                 {
-                    _logger.LogError("File name record count not equal to actual record count. File name count: " + name + "| Actual count: " + rowNumber);
+                    logger.LogError("Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", rowNumber, ex.Message, ex.StackTrace);
                     await InsertValidationErrorIntoDatabase(name);
-                    return;
                 }
+
                 if (rowNumber == 0)
                 {
-                    _logger.LogError("File contains no records. File name:" + name);
+                    logger.LogError("File contains no records. File name:" + name);
                     await InsertValidationErrorIntoDatabase(name);
                     return;
                 }
+
             }
-            catch (Exception ex) when (ex is HeaderValidationException || ex is CsvHelperException || ex is FileFormatException)
+            catch (Exception ex)
             {
-                _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
+                logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
                 await InsertValidationErrorIntoDatabase(name);
                 return;
             }
             try
             {
-                if (cohort.Participants.Count > 0)
+                if(chunks.Count > 0)
+                {
+                    foreach (Cohort chunk in chunks)
+                    {
+                    var json = JsonSerializer.Serialize(chunk);
+                    await callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
+                    logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
+                    }
+                }
+                 if(cohort.Participants.Count > 0)
                 {
                     var json = JsonSerializer.Serialize(cohort);
-                    await _callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
-                    _logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
+                    await callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
+                    logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
                 }
-                if (badRecords.Count > 0 || cohort.Participants.Count == 0)
-                {
-                    _logger.LogError("Failed to create {BadRecordsCount} Objects", badRecords.Count);
-                    _logger.LogError("All failed Records - {BadRecords}", badRecords);
-                }
+
             }
             catch (Exception ex)
             {
-                _logger.LogError("Unable to call function.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
+                logger.LogError("Unable to call function.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.Message, ex.StackTrace);
                 await InsertValidationErrorIntoDatabase(name);
             }
         }
         catch (Exception ex)
         {
-            _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
+            logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
             await InsertValidationErrorIntoDatabase(name);
             return;
         }
@@ -133,40 +184,20 @@ public class ReceiveCaasFile
             FileName = fileName
         });
 
-        var result = await _callFunction.SendPost(Environment.GetEnvironmentVariable("FileValidationURL"), json);
+        var result = await callFunction.SendPost(Environment.GetEnvironmentVariable("FileValidationURL"), json);
         if (result.StatusCode == HttpStatusCode.OK)
         {
-            _logger.LogInformation("file failed checks and has been moved to the poison blob storage");
+            logger.LogInformation("file failed checks and has been moved to the poison blob storage");
         }
-        _logger.LogError("there was a problem saving and or moving the failed file");
+        logger.LogError("there was a problem saving and or moving the failed file");
     }
 
-    private static bool FileNameAndFileExtensionIsValid(string name)
+    private static void FileExtensionCheck(string name)
     {
-        /* for file format BSS_ccyymmddhhmmss_n8.csv
-        '^BSS_' Matches the literal string BSS_ at the start of the line
-        '\d{14}' Matches exactly 14 digits, representing ccyymmddhhmmss
-        '_n' Matches the literal _n
-        '([1-9]\d*|0)' Matches any number with no leading zeros OR The number 0.
-        '\.csv$' matches .csv at the end of the string */
-        var match = Regex.Match(name, @"^BSS_\d{14}_n([1-9]\d*|0)\.csv$", RegexOptions.IgnoreCase);
-        return match.Success;
-    }
-
-    private async Task<int?> GetNumberOfRecordsFromFileName(string name)
-    {
-        var str = name.Remove(name.IndexOf('.'));
-        var numberOfRecords = (str.Split('_')[2]).Substring(1);
-
-        if (Int32.TryParse(numberOfRecords, out int n))
+        var fileExtension = Path.GetExtension(name).ToLower();
+        if (fileExtension != FileFormats.Parquet)
         {
-            return n;
-        }
-        else
-        {
-            _logger.LogError("File name is invalid. File name: " + name);
-            await InsertValidationErrorIntoDatabase(name);
-            return null;
-        }
+            throw new NotSupportedException("Invalid file type. Only parquet files are allowed.");
+        };
     }
 }
--- application/CohortManager/src/Functions/Shared/Model/Constants/FileFormats.cs
+++ application/CohortManager/src/Functions/Shared/Model/Constants/FileFormats.cs
@@ -3,4 +3,5 @@ namespace Model;
 public static class FileFormats
 {
     public const string CSV = ".csv";
+    public const string Parquet = ".parquet";
 }
