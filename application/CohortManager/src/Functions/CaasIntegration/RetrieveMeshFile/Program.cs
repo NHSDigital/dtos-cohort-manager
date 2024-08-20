899c00d9756828e79217c7b621cf825a5b4110ea
Michael Clayson <michael.clayson1@nhs.net>
2024-08-20 14:14:36 +0100

    feat: add certificate auth

--- application/CohortManager/src/Functions/CaasIntegration/RetrieveMeshFile/Program.cs
+++ application/CohortManager/src/Functions/CaasIntegration/RetrieveMeshFile/Program.cs
@@ -3,13 +3,22 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using NHS.MESH.Client;
 using Common;
+using System.Security.Cryptography.X509Certificates;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddMeshClient(_ => _.MeshApiBaseUrl = Environment.GetEnvironmentVariable("MeshApiBaseUrl"));
+        services
+            .AddMeshClient(_ => _.MeshApiBaseUrl = Environment.GetEnvironmentVariable("MeshApiBaseUrl"))
+            .AddMailbox("X26ABC1",new NHS.MESH.Client.Configuration.MailboxConfiguration
+            {
+                Password = Environment.GetEnvironmentVariable("MeshPassword"),
+                SharedKey = Environment.GetEnvironmentVariable("MeshSharedKey"),
+                Cert = new X509Certificate2("mycert.pfx",Environment.GetEnvironmentVariable("MeshKeyPassphrase")) //THIS WILL NEED CHANGING TO PULL FROM A KEYSTORE OR BLOB
+            })
+            .Build();
         services.AddSingleton<IBlobStorageHelper, BlobStorageHelper>();
         services.AddTransient<IMeshToBlobTransferHandler,MeshToBlobTransferHandler>();
     })
