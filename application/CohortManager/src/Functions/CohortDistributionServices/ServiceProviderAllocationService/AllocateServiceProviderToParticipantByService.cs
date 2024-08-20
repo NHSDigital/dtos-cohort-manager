cb76778b664a20dd2efa1654edd75a7c8dfc2b4d
will-larkin-nhs <160748088+will-larkin-nhs@users.noreply.github.com>
2024-08-20 15:56:27 +0100

    feat: docker updates (#139)
    * feat: azurite setup container
    
    * fix: docker env variables
    
    * feat: sample file script help page
    
    * Update send-sample-file.py
    
    * Update send-sample-file.py
    
    * Update compose.yaml
    
    * fix: formatting
    
    * fix: pr checks
    
    * feat: readme updates & removing secrets
    
    * Update .gitignore
    
    * fix: formatting
    
    * fix: formatting
    
    * refactor: moving azurite stuff
    
    * refactor: moving azurite folder
    
    * fix: secrets scan failed

--- application/CohortManager/src/Functions/CohortDistributionServices/ServiceProviderAllocationService/AllocateServiceProviderToParticipantByService.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/ServiceProviderAllocationService/AllocateServiceProviderToParticipantByService.cs
@@ -26,7 +26,7 @@ public class AllocateServiceProviderToParticipantByService
     }
 
     [Function("AllocateServiceProviderToParticipantByService")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
 
         // Cohort Distribution still WIP, and nothing is calling this function yet
