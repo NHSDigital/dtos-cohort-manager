f192b5250e4c461aae7a07d81caf02c3c972a2e4
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-08-21 13:58:54 +0000

    fix: added missing variables into appinsights module

--- infrastructure/appinsights.tf
+++ infrastructure/appinsights.tf
@@ -6,7 +6,6 @@ module "app_insights" {
     azurerm.audit = azurerm.audit
   }
 
-
   names = module.config.names
 
   name_suffix         = var.app_insights.name_suffix
@@ -14,7 +13,10 @@ module "app_insights" {
   location            = module.baseline.resource_group_locations[var.app_insights.resource_group_key]
   appinsights_type    = var.app_insights.appinsights_type
 
-  law_id = module.log_analytics_workspace.id
+  law_id       = module.log_analytics_workspace.id
+  audit_law_id = module.log_analytics_workspace.audit_id
+
+  audit_resource_group_name = module.baseline.resource_group_names[var.app_insights.audit_resource_group_key]
 
   tags = var.tags
 
