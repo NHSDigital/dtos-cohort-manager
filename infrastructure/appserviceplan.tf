2626098ceacaa457d4834daac675c1588414d201
Maciej Murawski <114145154+MacMur85@users.noreply.github.com>
2024-08-23 08:22:31 +0100

    feat: app service plan autoscale based on memory percentage (#155)
    * feat: app service plan autoscale based on memory percentage
    
    * fix: terraform lint
    
    * fix: added variables for autoscaler configuration

--- infrastructure/appserviceplan.tf
+++ infrastructure/appserviceplan.tf
@@ -10,4 +10,30 @@ module "app-plan" {
 
   tags = var.tags
 
+  ## autoscale rule
+
+  metric = var.app_service_plan.autoscale.memory_percentage.metric
+
+  capacity_min = var.app_service_plan.autoscale.memory_percentage.capacity_min
+  capacity_max = var.app_service_plan.autoscale.memory_percentage.capacity_max
+  capacity_def = var.app_service_plan.autoscale.memory_percentage.capacity_def
+
+  time_grain       = var.app_service_plan.autoscale.memory_percentage.time_grain
+  statistic        = var.app_service_plan.autoscale.memory_percentage.statistic
+  time_window      = var.app_service_plan.autoscale.memory_percentage.time_window
+  time_aggregation = var.app_service_plan.autoscale.memory_percentage.time_aggregation
+
+  inc_operator        = var.app_service_plan.autoscale.memory_percentage.inc_operator
+  inc_threshold       = var.app_service_plan.autoscale.memory_percentage.inc_threshold
+  inc_scale_direction = var.app_service_plan.autoscale.memory_percentage.inc_scale_direction
+  inc_scale_type      = var.app_service_plan.autoscale.memory_percentage.inc_scale_type
+  inc_scale_value     = var.app_service_plan.autoscale.memory_percentage.inc_scale_value
+  inc_scale_cooldown  = var.app_service_plan.autoscale.memory_percentage.inc_scale_cooldown
+
+  dec_operator        = var.app_service_plan.autoscale.memory_percentage.dec_operator
+  dec_threshold       = var.app_service_plan.autoscale.memory_percentage.dec_threshold
+  dec_scale_direction = var.app_service_plan.autoscale.memory_percentage.dec_scale_direction
+  dec_scale_type      = var.app_service_plan.autoscale.memory_percentage.dec_scale_type
+  dec_scale_value     = var.app_service_plan.autoscale.memory_percentage.dec_scale_value
+  dec_scale_cooldown  = var.app_service_plan.autoscale.memory_percentage.dec_scale_cooldown
 }
--- infrastructure/modules/app-service-plan/variables.tf
+++ infrastructure/modules/app-service-plan/variables.tf
@@ -29,3 +29,104 @@ variable "tags" {
   description = "Resource tags to be applied throughout the deployment."
   default     = {}
 }
+
+## autoscale rule ##
+
+variable "metric" {
+  type    = string
+  default = "MemoryPercentage"
+}
+variable "capacity_min" {
+  type    = string
+  default = "1"
+}
+
+variable "capacity_max" {
+  type    = string
+  default = "5"
+}
+
+variable "capacity_def" {
+  type    = string
+  default = "1"
+}
+
+variable "time_grain" {
+  type    = string
+  default = "PT1M"
+}
+
+variable "statistic" {
+  type    = string
+  default = "Average"
+}
+
+variable "time_window" {
+  type    = string
+  default = "PT10M"
+}
+
+variable "time_aggregation" {
+  type    = string
+  default = "Average"
+}
+
+variable "inc_operator" {
+  type    = string
+  default = "GreaterThan"
+}
+
+variable "inc_threshold" {
+  type    = number
+  default = 70
+}
+
+variable "inc_scale_direction" {
+  type    = string
+  default = "Increase"
+}
+
+variable "inc_scale_type" {
+  type    = string
+  default = "ChangeCount"
+}
+
+variable "inc_scale_value" {
+  type    = string
+  default = "1"
+}
+
+variable "inc_scale_cooldown" {
+  type    = string
+  default = "PT5M"
+}
+
+variable "dec_operator" {
+  type    = string
+  default = "LessThan"
+}
+
+variable "dec_threshold" {
+  type    = number
+  default = 25
+}
+
+variable "dec_scale_direction" {
+  type    = string
+  default = "Decrease"
+}
+
+variable "dec_scale_type" {
+  type    = string
+  default = "ChangeCount"
+}
+
+variable "dec_scale_value" {
+  type    = string
+  default = "1"
+}
+
+variable "dec_scale_cooldown" {
+  type    = string
+  default = "PT5M"
+}
