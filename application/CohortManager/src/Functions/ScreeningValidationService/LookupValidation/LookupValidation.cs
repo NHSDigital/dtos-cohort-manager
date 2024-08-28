a4b00f46264dd018c4f80b79b836633690aca083
SamRobinson75684 <sam.robinson24@nhs.net>
2024-08-28 12:33:59 +0100

    fix: removing changes to the validation rules function

--- application/CohortManager/src/Functions/ScreeningValidationService/LookupValidation/LookupValidation.cs
+++ application/CohortManager/src/Functions/ScreeningValidationService/LookupValidation/LookupValidation.cs
@@ -55,7 +55,7 @@ public class LookupValidation
             var existingParticipant = requestBody.ExistingParticipant;
             newParticipant = requestBody.NewParticipant;
 
-            var ruleFileName = $"{newParticipant.ScreeningName}_{GetValidationRulesName(requestBody.RulesType)}".Replace(" ", "_");
+            var ruleFileName =  $"{newParticipant.ScreeningName}_{GetValidationRulesName(requestBody.RulesType)}".Replace(" ", "_");
 
             var json = await _readRulesFromBlobStorage.GetRulesFromBlob(Environment.GetEnvironmentVariable("AzureWebJobsStorage"),
                                                                         Environment.GetEnvironmentVariable("BlobContainerName"),
