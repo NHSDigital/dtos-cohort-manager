eae7a0026486b9b088ed57f0ce21703588b0b03a
Adam Sheldon <167909651+adamsheldon1@users.noreply.github.com>
2024-08-30 14:01:33 +0100

    test: Added Epic 3 Happy Path e2e test and refactoring (#168)
    * test: Added Epic 3 Happy Path e2e test and refactoring
    
    * Added new e2e automated test to validate that 'add' happy path is working by uploading valid CSV and verifying records in BS_COHORT_DISTRIBUTION table.
    * Refactored both tests to use Dependency Injection (DI) for improved maintainability and testability.
      - Injected 'AppSettings' for centralised config management across tests.
      - Injected helpers such as 'BlobstorageHelper' for reusability.
    * Moved BlobStorageHelper to it's own dedicated class for modularity.
    * Added 'ParquetHelperService' in preparation for transition from CSV to Parquet.
    * Updated BaseIntegrationTest class, centralised critical config assertions.
    * Added TestFiles sub-directories for 'add', 'update' and 'remove' for better organisation and added csv and parquet files to .gitignore to ensure they are not committed to the repository.
    
    * fix: Removing whitespace
    
    * fix: Whitespace fixes
    
    * fix: adding Integration Test csproj to allowlist
    
    * refactor: addressing feedback for DB table and field name validation
    
    * Updated DatabaseValidationHelper to include validation for table and field names.
    * Refactored BaseIntegrationTest to implement early returns on config validation failures, ensuring test setup fails quickly if any critical config is missing.
    * Cleaned up gitignore and removed duplication.
    
    * fix: Added missing final newline to gitignore

--- tests/IntegrationTests/Helpers/CsvHelper.cs
+++ tests/IntegrationTests/Helpers/CsvHelper.cs
@@ -21,4 +21,28 @@ public static class CsvHelperService
             return records;
         }
     }
+
+    public static List<Dictionary<string, string>> ReadCsv(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            var records = new List<Dictionary<string, string>>();
+            csv.Read();
+            csv.ReadHeader();
+            var headers = csv.HeaderRecord;
+
+            while (csv.Read())
+            {
+                var record = new Dictionary<string, string>();
+                foreach (var header in headers)
+                {
+                    record[header] = csv.GetField(header);
+                }
+                records.Add(record);
+            }
+
+            return records;
+        }
+    }
 }
