bf09dcedc6155249a348e2c2976c0a5764a3f471
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-09-02 22:45:16 +0000

    fix: new outputs for appinsights in audit sub

--- infrastructure/modules/app-insights/output.tf
+++ infrastructure/modules/app-insights/output.tf
@@ -5,3 +5,14 @@ output "ai_instrumentation_key" {
 output "ai_connection_string" {
   value = azurerm_application_insights.appins.connection_string
 }
+
+
+### audit sub
+
+output "ai_instrumentation_key_audit" {
+  value = azurerm_application_insights.appins_audit.instrumentation_key
+}
+
+output "ai_connection_string_audit" {
+  value = azurerm_application_insights.appins_audit.connection_string
+}
