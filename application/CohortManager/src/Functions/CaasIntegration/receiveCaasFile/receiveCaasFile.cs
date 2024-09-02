b670ef122ceb31e617f6cff11b97399668926174
Sudip Mondal <sudip.mondal1@nhs.net>
2024-09-02 12:53:32 +0100

    feat: receiveCaasFile function modified to read parquet file

--- application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
+++ application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/receiveCaasFile.cs
@@ -5,13 +5,13 @@ using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using Model;
 using Common;
-using CsvHelper;
-using CsvHelper.Configuration;
-using System.Globalization;
 using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
+using Azure.Storage.Blobs;
 using Data.Database;
+using Model.Enums;
+using ParquetSharp.RowOriented;
 
 public class ReceiveCaasFile
 {
@@ -19,7 +19,8 @@ public class ReceiveCaasFile
     private readonly ICallFunction _callFunction;
     private readonly IScreeningServiceData _screeningServiceData;
 
-    public ReceiveCaasFile(ILogger<ReceiveCaasFile> logger, ICallFunction callFunction, IScreeningServiceData screeningServiceData)
+    public ReceiveCaasFile(ILogger<ReceiveCaasFile> logger, ICallFunction callFunction,
+        IScreeningServiceData screeningServiceData)
     {
         _logger = logger;
         _callFunction = callFunction;
@@ -29,70 +30,156 @@ public class ReceiveCaasFile
     [Function(nameof(ReceiveCaasFile))]
     public async Task Run([BlobTrigger("inbound/{name}", Connection = "caasfolder_STORAGE")] Stream stream, string name)
     {
+        var downloadFilePath = string.Empty;
         try
         {
             _logger.LogInformation("loading file from blob {name}", name);
             if (!FileNameAndFileExtensionIsValid(name))
             {
-                _logger.LogError("File name or file extension is invalid. Not in format BSS_ccyymmddhhmmss_n8.csv. file Name: " + name);
+                _logger.LogError(
+                    "File name or file extension is invalid. Not in format BSS_ccyymmddhhmmss_n8.csv. file Name: " +
+                    name);
                 await InsertValidationErrorIntoDatabase(name);
                 return;
             }
 
             var numberOfRecords = await GetNumberOfRecordsFromFileName(name);
-            if (numberOfRecords == null)
-            {
-                return;
-            }
+            if (numberOfRecords == null) return;
 
-            var badRecords = new Dictionary<int, string>();
             Cohort cohort = new()
             {
                 FileName = name
             };
+            var chunks = new List<Cohort>();
             var rowNumber = 0;
-            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                TrimOptions = TrimOptions.Trim,
-                Delimiter = ",",
-                HeaderValidated = null
-            };
+
             try
             {
-                using var blobStreamReader = new StreamReader(stream);
-                using var csv = new CsvReader(blobStreamReader, config);
-                csv.Context.RegisterClassMap<ParticipantMap>();
-                var records = csv.GetRecords<Participant>();
-                var screeningService = GetScreeningService(name);
+                var azureStorage = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+                var blobContainerName = Environment.GetEnvironmentVariable("BlobContainerName");
+                var blobServiceClient = new BlobServiceClient(azureStorage);
+                var container = blobServiceClient.GetBlobContainerClient(blobContainerName);
 
-                _logger.LogInformation("screeningService {screeningService}", screeningService.ScreeningName);
-
-                foreach (var participant in records)
+                try
                 {
-                    rowNumber++;
-                    try
+                    downloadFilePath = Path.Combine(Path.GetTempPath(), name + ".parquet");
+                    var blob = container.GetBlobClient(name);
+                    await blob.DownloadToAsync(downloadFilePath);
+                    var screeningService = GetScreeningService(name);
+                    _logger.LogInformation("screeningService {screeningService}", screeningService.ScreeningName);
+                    using (var rowReader = ParquetFile.CreateRowReader<ParticipantsParquetMap>(downloadFilePath))
                     {
-                        if (participant != null)
+                        for (var i = 0; i < rowReader.FileMetaData.NumRowGroups; ++i)
                         {
-                            participant.ScreeningId = screeningService.ScreeningId;
-                            participant.ScreeningName = screeningService.ScreeningName;
-                            cohort.Participants.Add(participant);
+                            var values = rowReader.ReadRows(i);
+                            foreach (var rec in values)
+                            {
+                                rowNumber++;
+
+                                var participant = new Participant();
+                                participant.RecordType = Convert.ToString(rec.RecordType);
+                                participant.ChangeTimeStamp = Convert.ToString(rec.ChangeTimeStamp);
+                                participant.SerialChangeNumber = Convert.ToString(rec.SerialChangeNumber);
+                                participant.NhsNumber = Convert.ToString(rec.NhsNumber);
+                                participant.SupersededByNhsNumber = Convert.ToString(rec.SupersededByNhsNumber);
+                                participant.PrimaryCareProvider = Convert.ToString(rec.PrimaryCareProvider);
+                                participant.PrimaryCareProviderEffectiveFromDate =
+                                    Convert.ToString(rec.PrimaryCareProviderEffectiveFromDate);
+                                participant.CurrentPosting = Convert.ToString(rec.CurrentPosting);
+                                participant.CurrentPostingEffectiveFromDate =
+                                    Convert.ToString(rec.CurrentPostingEffectiveFromDate);
+                                participant.PreviousPosting = Convert.ToString(rec.PreviousPosting);
+                                participant.PreviousPostingEffectiveFromDate =
+                                    Convert.ToString(rec.PreviousPostingEffectiveFromDate);
+                                participant.NamePrefix = Convert.ToString(rec.NamePrefix);
+                                participant.FirstName = Convert.ToString(rec.FirstName);
+                                participant.OtherGivenNames = Convert.ToString(rec.OtherGivenNames);
+                                participant.Surname = Convert.ToString(rec.SurnamePrefix);
+                                participant.PreviousSurname = Convert.ToString(rec.PreviousSurnamePrefix);
+                                participant.DateOfBirth = Convert.ToString(rec.DateOfBirth);
+                                if (Enum.IsDefined(typeof(Gender), Convert.ToInt16(rec.Gender)))
+                                {
+                                    participant.Gender =
+                                        (Gender)Enum.ToObject(typeof(Gender), Convert.ToInt16(rec.Gender));
+                                }
+                                else
+                                {
+                                    _logger.LogError(
+                                        $"Validation failed for field name 'Gender' on line {rowNumber}. File name: {name}.");
+                                    await InsertValidationErrorIntoDatabase(name);
+                                    return;
+                                }
+
+                                participant.AddressLine1 = Convert.ToString(rec.AddressLine1);
+                                participant.AddressLine2 = Convert.ToString(rec.AddressLine2);
+                                participant.AddressLine3 = Convert.ToString(rec.AddressLine3);
+                                participant.AddressLine4 = Convert.ToString(rec.AddressLine4);
+                                participant.AddressLine5 = Convert.ToString(rec.AddressLine5);
+                                participant.Postcode = Convert.ToString(rec.Postcode);
+                                participant.PafKey = Convert.ToString(rec.PafKey);
+                                participant.UsualAddressEffectiveFromDate =
+                                    Convert.ToString(rec.UsualAddressEffectiveFromDate);
+                                participant.ReasonForRemoval = Convert.ToString(rec.ReasonForRemoval);
+                                participant.ReasonForRemovalEffectiveFromDate =
+                                    Convert.ToString(rec.ReasonForRemovalEffectiveFromDate);
+                                participant.DateOfDeath = Convert.ToString(rec.DateOfDeath);
+                                if (Enum.IsDefined(typeof(Status), Convert.ToInt16(rec.DeathStatus)))
+                                {
+                                    participant.DeathStatus = (Status)Enum.ToObject(typeof(Status),
+                                        Convert.ToInt16(rec.DeathStatus));
+                                }
+                                else
+                                {
+                                    _logger.LogError(
+                                        $"Validation failed for field name 'Status' on line {rowNumber}. File name: {name}.");
+                                    await InsertValidationErrorIntoDatabase(name);
+                                    return;
+                                }
+
+                                participant.TelephoneNumber = Convert.ToString(rec.TelephoneNumber);
+                                participant.TelephoneNumberEffectiveFromDate =
+                                    Convert.ToString(rec.TelephoneNumberEffectiveFromDate);
+                                participant.MobileNumber = Convert.ToString(rec.MobileNumber);
+                                participant.MobileNumberEffectiveFromDate =
+                                    Convert.ToString(rec.MobileNumberEffectiveFromDate);
+                                participant.EmailAddress = Convert.ToString(rec.EmailAddress);
+                                participant.EmailAddressEffectiveFromDate =
+                                    Convert.ToString(rec.EmailAddressEffectiveFromDate);
+                                participant.IsInterpreterRequired = Convert.ToString(rec.IsInterpreterRequired);
+                                participant.PreferredLanguage = Convert.ToString(rec.PreferredLanguage);
+                                participant.InvalidFlag = Convert.ToString(rec.InvalidFlag);
+                                participant.RecordIdentifier = Convert.ToString(rec.RecordIdentifier);
+                                participant.ChangeReasonCode = Convert.ToString(rec.ChangeReasonCode);
+
+                                cohort.Participants.Add(participant);
+
+                                if (cohort.Participants.Count == 20000)
+                                {
+                                    chunks.Add(cohort);
+                                    cohort.Participants.Clear();
+                                }
+                            }
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        badRecords.Add(rowNumber, csv.Context.Parser.RawRecord);
-                        _logger.LogError("Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", rowNumber, ex.Message, ex.StackTrace);
-                        await InsertValidationErrorIntoDatabase(name);
-                    }
+
+                    if (File.Exists(downloadFilePath)) File.Delete(downloadFilePath);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(
+                        "Unable to create object on line {RowNumber}.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}",
+                        rowNumber, ex.Message, ex.StackTrace);
+                    await InsertValidationErrorIntoDatabase(name);
                 }
 
                 if (rowNumber != numberOfRecords)
                 {
-                    _logger.LogError("File name record count not equal to actual record count. File name count: " + name + "| Actual count: " + rowNumber);
+                    _logger.LogError("File name record count not equal to actual record count. File name count: " +
+                                     name + "| Actual count: " + rowNumber);
                     await InsertValidationErrorIntoDatabase(name);
                     return;
                 }
+
                 if (rowNumber == 0)
                 {
                     _logger.LogError("File contains no records. File name:" + name);
@@ -100,24 +187,29 @@ public class ReceiveCaasFile
                     return;
                 }
             }
-            catch (Exception ex) when (ex is HeaderValidationException || ex is CsvHelperException || ex is FileFormatException)
+            catch (Exception ex)
             {
-                _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
+                _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}",
+                    ex.GetType().Name, ex.Message, ex.StackTrace);
                 await InsertValidationErrorIntoDatabase(name);
                 return;
             }
+
             try
             {
+                if (chunks.Count > 0)
+                    foreach (var chunk in chunks)
+                    {
+                        var json = JsonSerializer.Serialize(chunk);
+                        await _callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
+                        _logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
+                    }
+
                 if (cohort.Participants.Count > 0)
                 {
                     var json = JsonSerializer.Serialize(cohort);
                     await _callFunction.SendPost(Environment.GetEnvironmentVariable("targetFunction"), json);
                     _logger.LogInformation("Created {CohortCount} Objects.", cohort.Participants.Count);
-                }
-                if (badRecords.Count > 0 || cohort.Participants.Count == 0)
-                {
-                    _logger.LogError("Failed to create {BadRecordsCount} Objects", badRecords.Count);
-                    _logger.LogError("All failed Records - {BadRecords}", badRecords);
                 }
             }
             catch (Exception ex)
@@ -128,9 +220,14 @@ public class ReceiveCaasFile
         }
         catch (Exception ex)
         {
-            _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}", ex.GetType().Name, ex.Message, ex.StackTrace);
+            _logger.LogError("{MessageType} validation failed.\nMessage:{ExMessage}\nStack Trace: {ExStackTrace}",
+                ex.GetType().Name, ex.Message, ex.StackTrace);
             await InsertValidationErrorIntoDatabase(name);
             return;
+        }
+        finally
+        {
+            if (File.Exists(downloadFilePath)) File.Delete(downloadFilePath);
         }
     }
 
@@ -144,11 +241,8 @@ public class ReceiveCaasFile
 
         var result = await _callFunction.SendPost(Environment.GetEnvironmentVariable("FileValidationURL"), json);
         if (result.StatusCode != HttpStatusCode.OK)
-        {
             _logger.LogError("An error occurred while saving or moving the failed file {fileName}.", fileName);
-        }
         _logger.LogInformation("File failed checks and has been moved to the poison blob storage");
-
     }
 
     private static bool FileNameAndFileExtensionIsValid(string name)
@@ -159,16 +253,16 @@ public class ReceiveCaasFile
         '_n' Matches the literal _n
         '([1-9]\d*|0)' Matches any number with no leading zeros OR The number 0.
         '\.csv$' matches .csv at the end of the string */
-        var match = Regex.Match(name, @"^\w{1,}_\d{14}_n([1-9]\d*|0)\.csv$", RegexOptions.IgnoreCase);
+        var match = Regex.Match(name, @"^\w{1,}_\d{14}_n([1-9]\d*|0)\.parquet$", RegexOptions.IgnoreCase);
         return match.Success;
     }
 
     private async Task<int?> GetNumberOfRecordsFromFileName(string name)
     {
         var str = name.Remove(name.IndexOf('.'));
-        var numberOfRecords = (str.Split('_')[2]).Substring(1);
+        var numberOfRecords = str.Split('_')[2].Substring(1);
 
-        if (Int32.TryParse(numberOfRecords, out int n))
+        if (int.TryParse(numberOfRecords, out var n))
         {
             return n;
         }
---
+++ application/CohortManager/src/Functions/Shared/Model/Mapping/ParticipantsParquetMap.cs
@@ -0,0 +1,130 @@
+namespace Model;
+using ParquetSharp.RowOriented;
+
+public struct ParticipantsParquetMap
+{
+    [MapToColumn("Record_Type")]
+    public string? RecordType;
+
+    [MapToColumn("Change_Time_Stamp")]
+    public Double? ChangeTimeStamp;
+
+    [MapToColumn("Serial_Change_Number")]
+    public Int64? SerialChangeNumber;
+
+    [MapToColumn("NHS_Number")]
+    public Int64? NhsNumber;
+
+    [MapToColumn("Superseded_by_NHS_number")]
+    public string? SupersededByNhsNumber;
+
+    [MapToColumn("Primary_Care_Provider")]
+    public string? PrimaryCareProvider;
+
+    [MapToColumn("Primary_Care_Provider_Business_Effective_From_Date")]
+    public Int64? PrimaryCareProviderEffectiveFromDate;
+
+    [MapToColumn("Current_Posting")]
+    public string? CurrentPosting;
+
+    [MapToColumn("Current_Posting_Business_Effective_From_Date")]
+    public Int64? CurrentPostingEffectiveFromDate;
+
+    [MapToColumn("Previous_Posting")]
+    public string? PreviousPosting;
+
+    [MapToColumn("Previous_Posting_Business_Effective_To_Date")]
+    public string? PreviousPostingEffectiveFromDate;
+
+    [MapToColumn("Name_Prefix")]
+    public string? NamePrefix;
+
+    [MapToColumn("Given_Name")]
+    public string? FirstName;
+
+    [MapToColumn("Other_Given_Name")]
+    public string? OtherGivenNames;
+
+    [MapToColumn("Family_Name")]
+    public string? SurnamePrefix;
+
+    [MapToColumn("Previous_Family_Name")]
+    public string? PreviousSurnamePrefix;
+
+    [MapToColumn("Date_of_Birth")]
+    public Int64? DateOfBirth;
+
+    [MapToColumn("Gender")]
+    public Int64? Gender;
+
+    [MapToColumn("Address_line_1")]
+    public string? AddressLine1;
+
+    [MapToColumn("Address_line_2")]
+    public string? AddressLine2;
+
+    [MapToColumn("Address_line_3")]
+    public string? AddressLine3;
+
+    [MapToColumn("Address_line_4")]
+    public string? AddressLine4;
+
+    [MapToColumn("Address_line_5")]
+    public string? AddressLine5;
+
+    [MapToColumn("Postcode")]
+    public string? Postcode;
+
+    [MapToColumn("PAF_key")]
+    public string? PafKey;
+
+    [MapToColumn("Usual_Address_Business_Effective_From_Date")]
+    public Int64? UsualAddressEffectiveFromDate;
+
+    [MapToColumn("Reason_for_Removal")]
+    public string? ReasonForRemoval;
+
+    [MapToColumn("Reason_for_Removal_Business_Effective_From_Date")]
+    public Int64? ReasonForRemovalEffectiveFromDate;
+
+    [MapToColumn("Date_of_Death")]
+    public string? DateOfDeath;
+
+    [MapToColumn("Death_Status")]
+    public string? DeathStatus;
+
+    [MapToColumn("Telephone_Number_Home")]
+    public string? TelephoneNumber;
+
+    [MapToColumn("Telephone_Number_Home_Business_Effective_From_Date")]
+    public Int64? TelephoneNumberEffectiveFromDate;
+
+    [MapToColumn("Telephone_Number_Mobile")]
+    public string? MobileNumber;
+
+    [MapToColumn("Telephone_Number_Mobile_Business_Effective_From_Date")]
+    public Int64? MobileNumberEffectiveFromDate;
+
+    [MapToColumn("Email_address_Home")]
+    public string? EmailAddress;
+
+    [MapToColumn("Email_address_Home_Business_Effective_From_Date")]
+    public Int64? EmailAddressEffectiveFromDate;
+
+    [MapToColumn("Is_Interpreter_Required")]
+    public string? IsInterpreterRequired;
+
+    [MapToColumn("Preferred_Language")]
+    public string? PreferredLanguage;
+
+    [MapToColumn("Invalid_Flag")]
+    public Boolean? InvalidFlag;
+
+    [MapToColumn("Record_Identifier")]
+    public string? RecordIdentifier;
+
+    [MapToColumn("Change_Reason_Code")]
+    public string? ChangeReasonCode;
+}
+
+
