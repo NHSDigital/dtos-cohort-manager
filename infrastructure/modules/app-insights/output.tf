1f7268b89c849321d6cdfe58ad2e747f61db3936
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-09-03 09:17:30 +0000

    fix: delete unused application insights outputs

--- infrastructure/modules/app-insights/output.tf
+++ infrastructure/modules/app-insights/output.tf
@@ -1,17 +1,9 @@
-output "ai_instrumentation_key" {
-  value = azurerm_application_insights.appins.instrumentation_key
-}
 
 output "ai_connection_string" {
   value = azurerm_application_insights.appins.connection_string
 }
 
-
 ### audit sub
-
-output "ai_instrumentation_key_audit" {
-  value = azurerm_application_insights.appins_audit.instrumentation_key
-}
 
 output "ai_connection_string_audit" {
   value = azurerm_application_insights.appins_audit.connection_string
