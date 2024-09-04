76483da0374bb74846601ffdc65f850edb0808b8
Alastair Lock <alastair.lock@gmail.com>
2024-09-04 10:14:03 +0100

    DTOSS-3383: Create a terraform module for subnets
    Adding vnet

---
+++ infrastructure/modules/vnet/main.tf
@@ -0,0 +1,10 @@
+/*--------------------------------------------------------------------------------------------------
+  VNets
+--------------------------------------------------------------------------------------------------*/
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+}
---
+++ infrastructure/subnets.tf
@@ -0,0 +1,13 @@
+module "subnet" {
+  source = ".//modules/subnet"
+
+  name                                          = module.config.names.subnet
+  vnet_name                                     = module.vnet.name
+  vnet_address_space                            = var.vnet.vnet_address_space
+  resource_group_name                           = module.baseline.resource_group_names[var.subnet.resource_group_key]
+  location                                      = module.baseline.resource_group_locations[var.subnet.resource_group_key]
+  is_private_endpoint_network_policies_enabled  = var.subnet.is_private_endpoint_network_policies_enabled
+
+  tags = var.tags
+
+}
