7e5b73ac782444b2ed5e24dbfedb1c7bdce1616a
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-09-05 11:15:31 +0000

    fix: acr counter fix

--- infrastructure/modules/azure-container-registry/identity.tf
+++ infrastructure/modules/azure-container-registry/identity.tf
@@ -1,5 +1,8 @@
 
 resource "azurerm_user_assigned_identity" "uai" {
+
+  count = var.deployacr ? 1 : 0
+
   location            = var.location
   resource_group_name = var.resource_group_name
   name                = var.uai_name
@@ -7,7 +10,10 @@ resource "azurerm_user_assigned_identity" "uai" {
 
 # create role assignment for acr
 resource "azurerm_role_assignment" "ra" {
-  scope                = azurerm_container_registry.acr[0].id
+
+  count = var.deployacr ? 1 : 0
+
+  scope                = azurerm_container_registry.acr[count.index].id
   role_definition_name = "AcrPush"
   principal_id         = azurerm_user_assigned_identity.uai.principal_id
 }
--- infrastructure/modules/azure-container-registry/output.tf
+++ infrastructure/modules/azure-container-registry/output.tf
@@ -1,6 +1,6 @@
 
 output "login_server" {
-  value = azurerm_container_registry.acr[0].login_server
+  value = azurerm_container_registry.acr[count.index].login_server
 }
 
 output "mi_id" {
