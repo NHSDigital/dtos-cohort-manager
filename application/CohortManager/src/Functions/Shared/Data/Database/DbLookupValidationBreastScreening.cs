6525e1dd8106938dcc9513d01067959965d49c0f
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2024-09-09 09:44:55 +0100

    feat: practice code validation

--- application/CohortManager/src/Functions/Shared/Data/Database/DbLookupValidationBreastScreening.cs
+++ application/CohortManager/src/Functions/Shared/Data/Database/DbLookupValidationBreastScreening.cs
@@ -4,8 +4,18 @@ using Model;
 using System.Data;
 using System.Data.SqlClient;
 
+/// <summary>
+/// Various validation methods for use in the breast screening lookup/ cohort rules
+/// </summary>
 public static class DbLookupValidationBreastScreening {
     private static SqlConnection _connection = new SqlConnection(Environment.GetEnvironmentVariable("DtOsDatabaseConnectionString"));
+
+    /// <summary>
+    /// Used in rule 36 in the lookup rules, and rule 54 in the cohort rules.
+    /// Validates the participants primary care provider (GP practice code)
+    /// </summary>
+    /// <param name="primaryCareProvider">The participant's primary care provider.</param>
+    /// <returns>bool, whether or not the GP practice code exists in the DB.<returns>
     public static bool ValidatePrimaryCareProvider(string primaryCareProvider)
     {
         string sql = $"SELECT GP_PRACTICE_CODE FROM [dbo].[BS_SELECT_GP_PRACTICE_LKP] WHERE GP_PRACTICE_CODE = @primaryCareProvider";
@@ -15,6 +25,30 @@ public static class DbLookupValidationBreastScreening {
             using (SqlCommand command = new SqlCommand(sql, _connection))
             {
                 command.Parameters.AddWithValue("@primaryCareProvider", primaryCareProvider);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    return reader.Read();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Used in rule 54 in the cohort rules. Validates the participants outcode (1st part of the postcode)
+    /// </summary>
+    /// <param name="postcode">The participant's postcode.</param>
+    /// <returns>bool, whether or not the outcode code exists in the DB.<returns>
+    public static bool ValidateOutcode(string postcode)
+    {
+        var outcode = postcode.Substring(0, postcode.IndexOf(" "));
+        string sql = $"SELECT OUTCODE FROM [dbo].[BS_SELECT_OUTCODE_MAPPING_LKP] WHERE OUTCODE = @outcode";
+
+        using (_connection)
+        {
+            _connection.Open();
+            using (SqlCommand command = new SqlCommand(sql, _connection))
+            {
+                command.Parameters.AddWithValue("@outcode", outcode);
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
                     return reader.Read();
