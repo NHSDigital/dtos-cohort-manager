e70bb3f760de43ebbebd44b052e01d53bda1ca05
Alastair Lock <alastair.lock@gmail.com>
2024-09-10 16:51:38 +0100

    Add network watcher

--- infrastructure/modules/vnet/main.tf
+++ infrastructure/modules/vnet/main.tf
@@ -7,4 +7,13 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = var.resource_group_name
   address_space       = var.vnet_address_space
   location            = var.location
+
+  tags = var.tags
+}
+
+
+resource "azurerm_network_watcher" "network_watcher" {
+  name                = "${var.name}-network-watcher"
+  location            = azurerm_virtual_network.vnet.location
+  resource_group_name = var.resource_group_name
 }
