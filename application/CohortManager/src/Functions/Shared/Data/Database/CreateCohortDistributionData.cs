c4ea040109d6beec8a6199ce56efb3664edf0cd8
warren <warren.pitterson1@nhs.net>
2024-09-11 11:08:29 +0100

    chore: explicitly declared columns

--- application/CohortManager/src/Functions/Shared/Data/Database/CreateCohortDistributionData.cs
+++ application/CohortManager/src/Functions/Shared/Data/Database/CreateCohortDistributionData.cs
@@ -199,7 +199,40 @@ public class CreateCohortDistributionData : ICreateCohortDistributionData
     public List<CohortDistributionParticipant> GetCohortDistributionParticipantsByRequestId(string requestId)
     {
 
-        var SQL = "SELECT * " +
+        var SQL = "SELECT" +
+                " [PARTICIPANT_ID], " +
+                " [NHS_NUMBER], " +
+                " [SUPERSEDED_NHS_NUMBER], " +
+                " [PRIMARY_CARE_PROVIDER], " +
+                " [PRIMARY_CARE_PROVIDER_FROM_DT], " +
+                " [NAME_PREFIX], " +
+                " [GIVEN_NAME], " +
+                " [OTHER_GIVEN_NAME], " +
+                " [FAMILY_NAME], " +
+                " [PREVIOUS_FAMILY_NAME], " +
+                " [DATE_OF_BIRTH], " +
+                " [GENDER], " +
+                " [ADDRESS_LINE_1], " +
+                " [ADDRESS_LINE_2], " +
+                " [ADDRESS_LINE_3], " +
+                " [ADDRESS_LINE_4], " +
+                " [ADDRESS_LINE_5], " +
+                " [POST_CODE], " +
+                " [USUAL_ADDRESS_FROM_DT], " +
+                " [DATE_OF_DEATH], " +
+                " [TELEPHONE_NUMBER_HOME], " +
+                " [TELEPHONE_NUMBER_HOME_FROM_DT], " +
+                " [TELEPHONE_NUMBER_MOB], " +
+                " [TELEPHONE_NUMBER_MOB_FROM_DT], " +
+                " [EMAIL_ADDRESS_HOME], " +
+                " [EMAIL_ADDRESS_HOME_FROM_DT], " +
+                " [PREFERRED_LANGUAGE], " +
+                " [INTERPRETER_REQUIRED], " +
+                " [REASON_FOR_REMOVAL], " +
+                " [REASON_FOR_REMOVAL_DT], " +
+                " [RECORD_INSERT_DATETIME], " +
+                " [RECORD_UPDATE_DATETIME], " +
+                " [IS_EXTRACTED] " +
             " FROM [dbo].[BS_COHORT_DISTRIBUTION] " +
             " WHERE REQUEST_ID = @RequestId";
 
