de4ca166c19701b135824d4b21e8316dffff4ce1
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-09-11 22:07:01 +0000

    fix: rg and location per sa

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,9 +1,9 @@
-output "resource_group_names" {
-  description = "Names of Resource Groups"
-  value       = module.baseline.resource_group_names
-}
+# output "resource_group_names" {
+#   description = "Names of Resource Groups"
+#   value       = module.baseline.resource_group_names
+# }
 
-output "resource_group_locations" {
-  description = "Locations of Resource Groups"
-  value       = module.baseline.resource_group_locations
-}
+# output "resource_group_locations" {
+#   description = "Locations of Resource Groups"
+#   value       = module.baseline.resource_group_locations
+# }
--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -10,5 +10,4 @@ module "storage" {
 
   tags = var.tags
 
-
 }
