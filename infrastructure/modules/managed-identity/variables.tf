0b415e220645f7e282358e4f9ec588ef313dc9e3
Richard Kingston <richard.kingston@nordcloud.com>
2024-09-18 20:13:35 +0100

    Reorganised RBAC for Managed Identity

--- infrastructure/modules/managed-identity/variables.tf
+++ infrastructure/modules/managed-identity/variables.tf
@@ -1,8 +1,3 @@
-variable "name" {
-  type        = string
-  description = "The name of the vnet."
-}
-
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which to create the VNET. Changing this forces a new resource to be created."
@@ -11,15 +6,6 @@ variable "resource_group_name" {
 variable "location" {
   type        = string
   description = ""
-}
-
-variable "role_definition_name" {
-  type = string
-}
-
-variable "role_assignment_resource_id" {
-  type    = string
-  default = ""
 }
 
 variable "uai_name" {
