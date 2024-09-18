720dd7b76c3780536ac1c9940b9805d218443284
Alastair Lock <alastair.lock@gmail.com>
2024-09-18 16:12:44 +0100

    testing out the pipeline

--- application/CohortManager/src/Functions/DevOpsTestingService/ControllerTest.py
+++ application/CohortManager/src/Functions/DevOpsTestingService/ControllerTest.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
-    return jsonify(message="Hello, World again!")
+    return jsonify(message="Hello, World again 123!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
