fde619df9b0161f5e36f946f456cc85855b328e6
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2024-09-19 13:03:08 +0100

    fix: unit tests

--- tests/ScreeningValidationServiceTests/StaticValidation/StaticValidationTests.cs
+++ tests/ScreeningValidationServiceTests/StaticValidation/StaticValidationTests.cs
@@ -235,51 +235,6 @@ public class StaticValidationTests
     }
     #endregion
 
-    #region Current Posting (Rule 58)
-    [TestMethod]
-    [DataRow(null)]
-    [DataRow("")]
-    [DataRow("England")]
-    [DataRow("Wales")]
-    [DataRow("IoM")]
-    public async Task Run_Should_Not_Create_Exception_When_CurrentPosting_Rule_Passes(string currentPosting)
-    {
-        // Arrange
-        _participantCsvRecord.Participant.CurrentPosting = currentPosting;
-        var json = JsonSerializer.Serialize(_participantCsvRecord);
-        SetUpRequestBody(json);
-
-        // Act
-        await _function.RunAsync(_request.Object);
-
-        // Assert
-        _handleException.Verify(handleException => handleException.CreateValidationExceptionLog(
-            It.Is<IEnumerable<RuleResultTree>>(r => r.Any(x => x.Rule.RuleName == "58.CurrentPosting")),
-            It.IsAny<ParticipantCsvRecord>()),
-            Times.Never());
-    }
-
-    [TestMethod]
-    [DataRow("Scotland")]
-    public async Task Run_Should_Return_BadRequest_And_Create_Exception_When_CurrentPosting_Rule_Fails(string currentPosting)
-    {
-        // Arrange
-        _participantCsvRecord.Participant.CurrentPosting = currentPosting;
-        var json = JsonSerializer.Serialize(_participantCsvRecord);
-        SetUpRequestBody(json);
-
-        // Act
-        var result = await _function.RunAsync(_request.Object);
-
-        // Assert
-        Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
-        _handleException.Verify(handleException => handleException.CreateValidationExceptionLog(
-            It.Is<IEnumerable<RuleResultTree>>(r => r.Any(x => x.Rule.RuleName == "58.CurrentPosting.NonFatal")),
-            It.IsAny<ParticipantCsvRecord>()),
-            Times.Once());
-    }
-    #endregion
-
     #region Reason For Removal (Rule 14)
     [TestMethod]
     [DataRow(null)]
@@ -743,7 +698,7 @@ public class StaticValidationTests
     }
     #endregion
 
-    #region Reason For Removal Effective From Date (Rule 19)
+    #region Reason For Removal Effective From Date (Rule 71)
     [TestMethod]
     [DataRow(null)]
     [DataRow("")]
@@ -762,7 +717,7 @@ public class StaticValidationTests
 
         // Assert
         _handleException.Verify(handleException => handleException.CreateValidationExceptionLog(
-            It.Is<IEnumerable<RuleResultTree>>(r => r.Any(x => x.Rule.RuleName == "19.ReasonForRemovalEffectiveFromDate")),
+            It.Is<IEnumerable<RuleResultTree>>(r => r.Any(x => x.Rule.RuleName == "71.ReasonForRemovalEffectiveFromDate")),
             It.IsAny<ParticipantCsvRecord>()),
             Times.Never());
     }
@@ -785,7 +740,7 @@ public class StaticValidationTests
         // Assert
         Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
         _handleException.Verify(handleException => handleException.CreateValidationExceptionLog(
-            It.Is<IEnumerable<RuleResultTree>>(r => r.Any(x => x.Rule.RuleName == "19.ReasonForRemovalEffectiveFromDate.NonFatal")),
+            It.Is<IEnumerable<RuleResultTree>>(r => r.Any(x => x.Rule.RuleName == "71.ReasonForRemovalEffectiveFromDate.NonFatal")),
             It.IsAny<ParticipantCsvRecord>()),
             Times.Once());
     }
