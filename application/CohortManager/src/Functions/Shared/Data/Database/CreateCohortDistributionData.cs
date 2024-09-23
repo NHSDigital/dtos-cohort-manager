3ece4aa544cfd3681dbe7b42503a67930cacbd00
warren <warren.pitterson1@nhs.net>
2024-09-23 09:45:22 +0100

    chore: removed duplicate error message

--- application/CohortManager/src/Functions/Shared/Data/Database/CreateCohortDistributionData.cs
+++ application/CohortManager/src/Functions/Shared/Data/Database/CreateCohortDistributionData.cs
@@ -500,7 +500,6 @@ public class CreateCohortDistributionData : ICreateCohortDistributionData
             transaction.Rollback();
             _dbConnection.Close();
             _logger.LogError(ex, "An error occurred while inserting new Cohort Distribution records: {ExceptionMessage}", ex.Message);
-            _logger.LogError(ex, "An error occurred while inserting new Cohort Distribution records: {ExceptionMessage}", ex.Message);
             return false;
         }
     }
