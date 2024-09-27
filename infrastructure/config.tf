322cd8126075c598da3a376b459fbe9e655fff2c
Maciej Murawski <114145154+MacMur85@users.noreply.github.com>
2024-09-27 13:16:45 +0100

    fix: updated commit reference for a module template

--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -2,7 +2,7 @@ module "regions_config" {
   for_each = var.regions
 
   # Source location updated to use the git:: prefix to avoid URL encoding issues - note // between the URL and the path is required
-  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=feat/DTOSS-4393-Terraform-Modules"
+  source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/shared-config?ref=2296f761f4edc3b413e2629c98309df9c6fa0849"
 
   location    = each.key
   application = var.application
