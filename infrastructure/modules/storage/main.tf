92de3b41dad520e82d769c1ae605466c1d9a3c69
Richard Kingston <richard.kingston@nordcloud.com>
2024-10-06 12:17:42 +0100

    TF Format

--- infrastructure/modules/storage/main.tf
+++ infrastructure/modules/storage/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_storage_account" "storage_account" {
 
-  name                          = var.name
-  resource_group_name           = var.resource_group_name
-  location                      = var.location
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
   account_replication_type      = var.account_replication_type
   account_tier                  = var.account_tier
