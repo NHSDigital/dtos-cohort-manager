b76fa90eced387854529379c9eb17f4aa1100ad6
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-10-08 14:06:42 +0000

    fix: new feature variable include inside the module

--- infrastructure/modules/log-analytics-workspace/output.tf
+++ infrastructure/modules/log-analytics-workspace/output.tf
@@ -1,10 +1,10 @@
 ### audit sub
 output "audit_id" {
   #value = azurerm_log_analytics_workspace.law_audit[0].id
-  value = law_deploy_enabled ? element(azurerm_log_analytics_workspace.law_audit[*].id, 0) : null
+  value = var.law_deploy_enabled ? element(azurerm_log_analytics_workspace.law_audit[*].id, 0) : null
 }
 
 output "audit_name" {
   #value = azurerm_log_analytics_workspace.law_audit[0].name
-  value = law_deploy_enabled ? element(azurerm_log_analytics_workspace.law_audit[*].name, 0) : null
+  value = var.law_deploy_enabled ? element(azurerm_log_analytics_workspace.law_audit[*].name, 0) : null
 }
