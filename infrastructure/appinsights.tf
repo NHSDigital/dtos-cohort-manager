7bbda49360df22ebaba145e74b606d9d43ccb145
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-10-08 14:17:43 +0000

    feat: AppInsights feature and outputs configuration

--- infrastructure/appinsights.tf
+++ infrastructure/appinsights.tf
@@ -16,8 +16,8 @@ module "app_insights" {
   location            = module.baseline.resource_group_locations[var.app_insights.resource_group_key]
   appinsights_type    = var.app_insights.appinsights_type
 
-  law_id       = module.log_analytics_workspace.id
-  audit_law_id = module.log_analytics_workspace.audit_id
+  #law_id       = module.log_analytics_workspace.id[0]
+  audit_law_id = module.log_analytics_workspace.audit_id[0]
 
   audit_resource_group_name = module.baseline.resource_group_names_audit[var.app_insights.audit_resource_group_key]
   tags                      = var.tags
