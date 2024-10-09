1cec512dc573a8d5fa72613bd24880d306be2602
Sudip Mondal <sudip.mondal1@nhs.net>
2024-10-09 10:17:48 +0100

    feat: DTOS-3638 file format check fix

--- application/CohortManager/src/Functions/Shared/Common/Interfaces/IReceiveCaasFileHelper.cs
+++ application/CohortManager/src/Functions/Shared/Common/Interfaces/IReceiveCaasFileHelper.cs
@@ -4,8 +4,7 @@ using Model;
 public interface IReceiveCaasFileHelper
 {
     Task<bool> InitialChecks(Stream blobStream, string name);
-    Task<Participant?> MapParticipant(ParticipantsParquetMap rec, Participant participant, string name,
-       int rowNumber);
+    Task<Participant?> MapParticipant(ParticipantsParquetMap rec, Participant participant, string name, int rowNumber);
     Task SerializeParquetFile(List<Cohort> chunks, Cohort cohort, string filename, int rowNumber);
     Task<int?> GetNumberOfRecordsFromFileName(string name);
     Task InsertValidationErrorIntoDatabase(string fileName, string errorRecord);
