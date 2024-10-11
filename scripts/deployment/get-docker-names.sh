914e661fbbff547ff3037066d08f50a20dff138d
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-10-11 12:22:21 +0000

    fix: verify github runner disk space

--- scripts/deployment/get-docker-names.sh
+++ scripts/deployment/get-docker-names.sh
@@ -38,15 +38,15 @@ if [ -z "$CHANGED_FOLDERS" ]; then
 elif [[ "$CHANGED_FOLDERS" == *Shared* ]]; then
     echo "Shared folder changed, returning all functions"
     for key in "${!docker_functions_map[@]}"; do
-        changed_functions+=" ${docker_functions_map[$key]}"
+        changed_functions+=", ${docker_functions_map[$key]}"
         echo "Adding in: ${docker_functions_map[$key]}"
     done
 else
     echo "files changed $CHANGED_FOLDERS "
     for folder in $CHANGED_FOLDERS; do
-      echo "Add this function in: ${folder},"
+      echo "Add this function in: ${folder} "
       echo "Add this which maps to: ${docker_functions_map[$folder]} "
-      changed_functions+=" ${docker_functions_map[$folder]}"
+      changed_functions+=", ${docker_functions_map[$folder]}"
     done
 fi
 
