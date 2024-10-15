aafae97dd195637b8db05cbdc33c29bb4fc5d9e2
Christopher Jones <chris.jones82@nhs.net>
2024-10-15 14:20:24 +0100

    Transformation logic added for DateOfDeath + Unit testing

--- tests/TransformDataServiceTests/TransformDataServiceTests.cs
+++ tests/TransformDataServiceTests/TransformDataServiceTests.cs
@@ -373,4 +373,66 @@ public class TransformDataServiceTests
 
         _request.Setup(r => r.Body).Returns(bodyStream);
     }
+
+    [TestMethod]
+    public async Task Run_DateOfDeathSuppliedAndReasonForRemovalIsNotDea_SetDateOfDeathToNull()
+    {
+        // Arrange
+        _requestBody.Participant.ReasonForRemoval = "NOTDEA";
+        _requestBody.Participant.DateOfDeath = "2024-01-01";
+
+        var json = JsonSerializer.Serialize(_requestBody);
+        SetUpRequestBody(json);
+
+        // Act
+        var result = await _function.RunAsync(_request.Object);
+
+        // Assert
+        var expectedResponse = new CohortDistributionParticipant
+        {
+            NhsNumber = "1",
+            FirstName = "John",
+            FamilyName = "Smith",
+            NamePrefix = "MR",
+            Gender = Gender.Male,
+            ReasonForRemoval = "NOTDEA",
+            DateOfDeath = null, // Expect DateOfDeath set to null
+            //DeathStatus = null  // Expect DeathStatus set to null
+        };
+
+        string responseBody = await AssertionHelper.ReadResponseBodyAsync(result);
+        Assert.AreEqual(JsonSerializer.Serialize(expectedResponse), responseBody);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task Run_DateOfDeathSuppliedAndReasonForRemovalIsDea_ShouldNotChangeDateOfDeath()
+    {
+        // Arrange
+        _requestBody.Participant.ReasonForRemoval = "DEA";
+        _requestBody.Participant.DateOfDeath = "2024-01-01";
+
+        var json = JsonSerializer.Serialize(_requestBody);
+        SetUpRequestBody(json);
+
+        // Act
+        var result = await _function.RunAsync(_request.Object);
+
+        // Assert
+        var expectedResponse = new CohortDistributionParticipant
+        {
+            NhsNumber = "1",
+            FirstName = "John",
+            FamilyName = "Smith",
+            NamePrefix = "MR",
+            Gender = Gender.Male,
+            ReasonForRemoval = "DEA",
+            DateOfDeath = "2024-01-01",
+            //DeathStatus = 1
+        };
+
+        string responseBody = await AssertionHelper.ReadResponseBodyAsync(result);
+        Assert.AreEqual(JsonSerializer.Serialize(expectedResponse), responseBody);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+    }
 }
