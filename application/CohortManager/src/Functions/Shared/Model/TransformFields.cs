a52122c2edb2cacac179124383e54a77b81ea109
andrewmorris43 <andrew.morris43@nhs.net>
2024-10-17 11:29:10 +0100

    feat: refactor TransformAction and rules to allow multiple fields to be transformed

---
+++ application/CohortManager/src/Functions/Shared/Model/TransformFields.cs
@@ -0,0 +1,8 @@
+namespace Model;
+
+public class TransformFields
+{
+    public required string field { get; set; }
+    public required string value { get; set; }
+    public bool isExpression { get; set; }
+}
