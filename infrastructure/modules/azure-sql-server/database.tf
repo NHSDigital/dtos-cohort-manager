84ebd18d5b41476cde89a6c72c2159aa1259c2ed
fahadbaigNC <fahad.baig@nordcloud.com>
2024-10-18 12:24:19 +0100

    feat/DTOS-4600-New-Integration-Environment-Rebased-FAHAD changes made to separate the diagnostic settings as a stand alone module

---
+++ infrastructure/modules/azure-sql-server/database.tf
@@ -0,0 +1,18 @@
+
+resource "azurerm_mssql_database" "defaultdb" {
+  name         = var.db_name_suffix # using only the suffix, as this is the naming convention from DToS Devs
+  server_id    = azurerm_mssql_server.azure_sql_server.id
+  collation    = var.collation
+  license_type = var.licence_type
+  max_size_gb  = var.max_gb
+  read_scale   = var.read_scale
+  sku_name     = var.sku
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [tags]
+    # prevent the possibility of accidental data loss (variables may not be used here)
+    prevent_destroy = false
+  }
+}
