0064b68cefbe3d8dff1ed228a3b3518f021e2195
Richard Kingston <richard.kingston@nordcloud.com>
2024-10-21 18:04:16 +0100

    Added Azure Monitor Private Linked Scopes

---
+++ infrastructure/modules/private-link-scope/output.tf
@@ -0,0 +1,3 @@
+output "scope_name" {
+  value = azurerm_monitor_private_link_scope.ampls.name
+}
---
+++ infrastructure/private-link-scopes.tf
@@ -0,0 +1,39 @@
+# Create the private link service in the hub subscription
+module "private_link_scoped_service" {
+  for_each = {
+    for key, region in var.regions :
+    key => region if region.is_primary_region && var.features.private_endpoints_enabled
+  }
+
+  source = ".//modules/private-link-scoped-service"
+
+  providers = {
+    azurerm.audit = azurerm.dev-hub
+  }
+
+  name                = "${module.regions_config[each.key].names.log-analytics-workspace}-${var.law.name_suffix}-ampls-service"
+  resource_group_name = data.terraform_remote_state.hub.outputs.vnets_hub[each.key].vnet.resource_group_name
+
+  linked_resource_id = module.log_analytics_workspace.id
+  #scope_name         = module.private_link_scope[each.key].name
+  scope_name         = "${module.regions_config[each.key].names.log-analytics-workspace}-${var.law.name_suffix}-ampls"
+}
+
+# Create the private link scope in the spoke subscription
+module "private_link_scope" {
+  for_each = {
+    for key, region in var.regions :
+    key => region if region.is_primary_region && var.features.private_endpoints_enabled
+  }
+
+  source = ".//modules/private-link-scope"
+
+  name                = "${module.regions_config[each.key].names.log-analytics-workspace}-${var.law.name_suffix}-ampls"
+  resource_group_name = module.baseline.resource_group_names[var.law.resource_group_key]
+
+  ingestion_access_mode = "PrivateOnly"
+  query_access_mode     = "Open"
+
+  tags = var.tags
+
+}
