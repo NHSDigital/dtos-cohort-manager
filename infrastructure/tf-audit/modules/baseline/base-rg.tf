36a375b128c4bc8faca040d3e3348027ea12a912
Maciej Murawski <maciej.murawski@nordcloud.com>
2024-10-22 20:21:40 +0000

    fix: rg names with mac

--- infrastructure/tf-audit/modules/baseline/base-rg.tf
+++ infrastructure/tf-audit/modules/baseline/base-rg.tf
@@ -2,8 +2,6 @@
 resource "azurerm_resource_group" "rg-audit" {
   for_each = var.resource_groups_audit
 
-  provider = azurerm.audit
-
   name     = each.value.name
   location = each.value.location
 
