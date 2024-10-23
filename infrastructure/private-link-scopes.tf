c125a0b49ca6188b56bfb9706f64e6cc322ad638
Richard Kingston <richard.kingston@nordcloud.com>
2024-10-23 21:11:38 +0100

    Temporarily remove AMPLS code

--- infrastructure/private-link-scopes.tf
+++ infrastructure/private-link-scopes.tf
@@ -1,38 +1,38 @@
-# Create the private link service in the hub subscription
-module "private_link_scoped_service" {
-  for_each = {
-    for key, region in var.regions :
-    key => region if var.features.private_endpoints_enabled
-  }
+# # Create the private link service in the hub subscription
+# module "private_link_scoped_service" {
+#   for_each = {
+#     for key, region in var.regions :
+#     key => region if var.features.private_endpoints_enabled
+#   }
 
-  source = ".//modules/private-link-scoped-service"
+#   source = ".//modules/private-link-scoped-service"
 
-  providers = {
-    azurerm = azurerm.hub
-  }
+#   providers = {
+#     azurerm = azurerm.hub
+#   }
 
-  name                = "${module.regions_config[each.key].names.log-analytics-workspace}-${var.law.name_suffix}-ampls-service"
-  resource_group_name = data.terraform_remote_state.hub.outputs.vnets_hub[each.key].vnet.resource_group_name
+#   name                = "${module.regions_config[each.key].names.log-analytics-workspace}-${var.law.name_suffix}-ampls-service"
+#   resource_group_name = data.terraform_remote_state.hub.outputs.vnets_hub[each.key].vnet.resource_group_name
 
-  linked_resource_id = module.log_analytics_workspace.id
-  scope_name         = module.private_link_scope[each.key].scope_name
-}
+#   linked_resource_id = module.log_analytics_workspace.id
+#   scope_name         = module.private_link_scope[each.key].scope_name
+# }
 
-# Create the private link scope in the spoke subscription
-module "private_link_scope" {
-  for_each = {
-    for key, region in var.regions :
-    key => region if var.features.private_endpoints_enabled
-  }
+# # Create the private link scope in the spoke subscription
+# module "private_link_scope" {
+#   for_each = {
+#     for key, region in var.regions :
+#     key => region if var.features.private_endpoints_enabled
+#   }
 
-  source = ".//modules/private-link-scope"
+#   source = ".//modules/private-link-scope"
 
-  name                = "${module.regions_config[each.key].names.log-analytics-workspace}-${var.law.name_suffix}-ampls"
-  resource_group_name = module.baseline.resource_group_names[var.law.resource_group_key]
+#   name                = "${module.regions_config[each.key].names.log-analytics-workspace}-${var.law.name_suffix}-ampls"
+#   resource_group_name = module.baseline.resource_group_names[var.law.resource_group_key]
 
-  ingestion_access_mode = "PrivateOnly"
-  query_access_mode     = "Open"
+#   ingestion_access_mode = "PrivateOnly"
+#   query_access_mode     = "Open"
 
-  tags = var.tags
+#   tags = var.tags
 
-}
+# }
