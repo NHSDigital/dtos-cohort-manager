e091f1649f4554838f3e94f25d2c521874d238cb
fahadbaigNC <fahad.baig@nordcloud.com>
2024-10-23 17:41:18 +0100

    dfasfadsf

--- infrastructure/sqlserver.tf
+++ infrastructure/sqlserver.tf
@@ -24,9 +24,9 @@ module "azure_sql_server" {
   diagnostic_setting_properties = var.features.diagnostic_settings_enabled ? {
     diagnostic_settings_globally_enabled = var.diagnostic_setting_properties.diagnostic_settings_globally_enabled
     log_analytics_workspace_id           = module.log_analytics_workspace.audit_id
-    log_categories                       { 
+    log_categories               =        { 
       log_categories =  "SQLSecurityAuditEvents"
-    } ,
+    },
     sql_security_audit_logs_enabled = var.diagnostic_setting_properties.diagnostic_setting_audit_logs_enabled
   } : null
 
