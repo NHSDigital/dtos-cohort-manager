c7aadb094a8a1394478c90189d745d2945b8195a
fahadbaigNC <103640920+fahadbaigNC@users.noreply.github.com>
2024-11-08 15:53:21 +0000

    feat: DTOSS 5205 diagnostic settings module fahad (#374)
    * feat DTOSS 5205 setting to query audit tfstatefile
    
    * feat DTOSS 5205 setting to query audit tfstatefile
    
    * feat DTOSS 5205 setting to query audit tfstatefile
    
    * feat DTOSS 5205 setting to query audit tfstatefile
    
    * feat DTOSS 5205 change of int firewall ploicy.

---
+++ infrastructure/tf-core/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  #   #   Get the primary region from the regions map:
+  primary_region_map = { for region_key, region in var.regions : region_key => region if region.is_primary_region }
+  primary_region     = keys(local.primary_region_map)[0]
+}
--- infrastructure/tf-core/variables.tf
+++ infrastructure/tf-core/variables.tf
@@ -8,6 +8,31 @@ variable "AUDIT_SUBSCRIPTION_ID" {
   type        = string
 }
 
+variable "AUDIT_BACKEND_AZURE_SUBSCRIPTION_ID" {
+  description = "ID of the Azure Storage Account for the audit backend"
+  type        = string
+}
+
+variable "AUDIT_BACKEND_AZURE_STORAGE_ACCOUNT_NAME" {
+  description = "The name of the Azure Storage Account for the audit backend"
+  type        = string
+}
+
+variable "AUDIT_BACKEND_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME" {
+  description = "The name of the container in the Audit Azure Storage Account for the backend"
+  type        = string
+}
+
+variable "AUDIT_BACKEND_AZURE_STORAGE_KEY" {
+  description = "The name of the Statefile for the audit  resources"
+  type        = string
+}
+
+variable "AUDIT_BACKEND_AZURE_RESOURCE_GROUP_NAME" {
+  description = "The name of the audit resource group for the Azure Storage Account"
+  type        = string
+}
+
 variable "ACR_SUBSCRIPTION_ID" {
   description = "ID of the subscription hosting the ACR used in current environment"
   type        = string
