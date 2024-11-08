664cd1a5d5774803c38720c292eaebccee851274
fahadbaigNC <fahad.baig@nordcloud.com>
2024-11-08 14:46:28 +0000

    feat DTOSS 5205 setting to query audit tfstatefile

--- infrastructure/tf-audit/outputs.tf
+++ infrastructure/tf-audit/outputs.tf
@@ -1,15 +1,3 @@
-# output "log_analytics_workspace_id" {
-#   value = module.log_analytics_workspace_audit.id
-# }
-
-# output "log_analytics_workspace_id" {
-#   value = {
-#     for ws in module.log_analytics_workspace_audit : ws => {
-#       id = module.log_analytics_workspace_audit[ws].id
-#     }
-#   }
-# }
-
 output "log_analytics_workspace_id" {
   value = {
     for k, v in module.log_analytics_workspace_audit : k => {
