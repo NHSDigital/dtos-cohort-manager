e4c4981321c7cad0e45c45f4872ea3f58c9e28ec
Michael Clayson <michael.clayson1@nhs.net>
2024-11-11 11:06:26 +0000

    temp: unit tests

---
+++ tests/DataServiceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DataServiceTests;
+
+[TestClass]
+public class DataService<TEntity> where TEntity : class
+{
+
+    public DataService(Parameters)
+    {
+
+    }
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
