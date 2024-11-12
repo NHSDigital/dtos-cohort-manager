a92909096dcae8b1e59aaeebbfd08dfb17791a42
fahadbaigNC <fahad.baig@nordcloud.com>
2024-11-12 15:43:34 +0000

    feat DTOSS 5205 sqlserver changes DS

--- infrastructure/tf-core/sql_server.tf
+++ infrastructure/tf-core/sql_server.tf
@@ -13,7 +13,6 @@ module "azure_sql_server" {
   kv_id                      = module.key_vault[each.key].key_vault_id
   log_analytics_workspace_id = data.terraform_remote_state.audit.outputs.log_analytics_workspace_id[local.primary_region].id
   law_sku                    = var.law_sku
-  retention_in_days          = var.retention_in_days
 
   sql_uai_name         = var.sqlserver.sql_uai_name
   sql_admin_group_name = var.sqlserver.sql_admin_group_name
