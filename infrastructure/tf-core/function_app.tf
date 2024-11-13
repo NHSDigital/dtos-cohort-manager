c9a8180749a5e8671179b1c0117ddd478191429d
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-13 00:18:47 +0000

    Update function_app.tf

--- infrastructure/tf-core/function_app.tf
+++ infrastructure/tf-core/function_app.tf
@@ -9,9 +9,8 @@ module "functionapp" {
 
   app_settings = local.app_settings[each.value.region_key][each.value.function_key]
 
-  #  log_analytics_workspace_id = data.terraform_remote_state.audit.outputs.log_analytics_workspace_id[local.primary_region] #"uksouth"
-  log_analytics_workspace_id = data.terraform_remote_state.audit.outputs.log_analytics_workspace_id[local.primary_region].id
-
+  log_analytics_workspace_id = data.terraform_remote_state.audit.outputs.log_analytics_workspace_id[local.primary_region]
+  
   public_network_access_enabled = var.features.public_network_access_enabled
   vnet_integration_subnet_id    = module.subnets["${module.regions_config[each.value.region_key].names.subnet}-apps"].id
 
