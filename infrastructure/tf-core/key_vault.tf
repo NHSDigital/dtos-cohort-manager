b2d7ef702be828c8d06567ebc2120f0e2d8f6d68
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-26 11:49:11 +0000

    fix: non-live DNS zone name and private DNS tidy (#449)
    * Fix: non-live DNS zone name and private DNS tidy
    
    * Fix: refactor function_app.tf, Key Vault RBAC
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- infrastructure/tf-core/key_vault.tf
+++ infrastructure/tf-core/key_vault.tf
@@ -16,9 +16,12 @@ module "key_vault" {
   purge_protection_enabled = var.key_vault.purge_prot
   sku_name                 = var.key_vault.sku_name
 
+  enable_rbac_authorization = true
+  rbac_roles                = local.rbac_roles_key_vault
+
   # Private Endpoint Configuration if enabled
   private_endpoint_properties = var.features.private_endpoints_enabled ? {
-    private_dns_zone_ids_keyvault        = [data.terraform_remote_state.hub.outputs.private_dns_zone_key_vault[each.key].private_dns_zone.id]
+    private_dns_zone_ids_keyvault        = [data.terraform_remote_state.hub.outputs.private_dns_zones["${each.key}-key_vault"].id]
     private_endpoint_enabled             = var.features.private_endpoints_enabled
     private_endpoint_subnet_id           = module.subnets["${module.regions_config[each.key].names.subnet}-pep"].id
     private_endpoint_resource_group_name = azurerm_resource_group.rg_private_endpoints[each.key].name
