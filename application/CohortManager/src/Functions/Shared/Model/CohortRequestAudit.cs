beb5721465df1a69e7a8d33651b63ba98d75ddd1
warren <warren.pitterson1@nhs.net>
2024-11-27 13:08:52 +0000

    chore: revert to correct response headers

--- application/CohortManager/src/Functions/Shared/Model/CohortRequestAudit.cs
+++ application/CohortManager/src/Functions/Shared/Model/CohortRequestAudit.cs
@@ -1,13 +1,7 @@
 namespace Model;
-
-using System.Text.Json.Serialization;
-
-public class CohortRequestAudit
+    public class CohortRequestAudit
     {
-        [JsonPropertyName("REQUEST_ID")]
         public string? RequestId { get; set; }
-        [JsonPropertyName("STATUS_CODE")]
         public string? StatusCode { get; set; }
-        [JsonPropertyName("CREATED_DATETIME")]
         public string? CreatedDateTime { get; set; }
     }
