c2c64504d486b7ddeeb1b4bcc36db5cec988c47c
Joseph2910 <184524642+Joseph2910@users.noreply.github.com>
2024-11-28 11:51:52 +0000

    feat: Added script for select_outcode_mapping_lpk table

---
+++ application/CohortManager/Set-up/scripts/update-sql-statements/select_outcode_mapping_lpk.py
@@ -0,0 +1,32 @@
+# Note: this works with schema
+#   OUTCODE VARCHAR(4) PRIMARY KEY,
+#   BSO VARCHAR(4),
+#   AUDIT_ID NUMERIC(38),
+#   AUDIT_CREATED_TIMESTAMP DATETIME,
+#   AUDIT_LAST_MODIFIED_TIMESTAMP DATETIME,
+#   AUDIT_TEXT VARCHAR(50)
+
+# Issues:
+#   1. Writes whole sql query when over 1000 items it breaks sql limits. (Fix: every 1000 lines add a new insert statement)
+#   2. At the end of insert statement change last record to end with ; instead of ,
+
+
+import csv
+filepath = input("Enter your file path: ")
+with open(filepath, mode ='r')as input_file:
+  csvFile = csv.reader(input_file)
+  with open('select_outcode_mapping_lkp_output.txt', mode = 'x') as output_file:
+    headers = next(csvFile)
+    for schema in csvFile:
+        # Named variables for readability.
+        outcode = schema[0]
+        bso = schema[1]
+        audit_id = schema[2]
+        audit_created_timestamp = schema[3]
+        audit_last_modified_timestamp = schema[4]
+        audit_text = schema[5]
+
+        output_file.write('\t(\'' + outcode + '\', \'' + bso + '\', \'' + audit_id + '\', CONVERT(DATETIME, \'' + audit_created_timestamp[0:19] 
+              + '\', 120), CONVERT(DATETIME, \'' + audit_last_modified_timestamp[0:19] + '\', 120), \'' + audit_text +'\'), \n')
+        
+print('File created!')
