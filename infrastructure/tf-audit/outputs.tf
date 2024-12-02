946fc58f55bb6faa21ec6e15717595d34b644166
Richard Kingston <richard.kingston@nordcloud.com>
2024-12-02 18:12:07 +0000

    Reinstate log_analytics_workspace_id in output

--- infrastructure/tf-audit/outputs.tf
+++ infrastructure/tf-audit/outputs.tf
@@ -1,3 +1,7 @@
+output "log_analytics_workspace_id" {
+  value = { for k, v in module.log_analytics_workspace_audit : k => v.id }
+}
+
 output "storage_account_audit" {
   value = {
     for k, v in module.storage : k => {
