038048dac181e69edb52761fb21ed859eddbc784
Michael Clayson <michael.clayson1@nhs.net>
2024-12-06 15:34:42 +0000

    feat: addRange added to Data Service Client

--- application/CohortManager/src/Functions/Shared/DataServices.Client/IDataServiceClient.cs
+++ application/CohortManager/src/Functions/Shared/DataServices.Client/IDataServiceClient.cs
@@ -7,5 +7,6 @@ public interface IDataServiceClient<TEntity>
     Task<IEnumerable<TEntity>> GetAll();
     Task<TEntity> GetSingle(string id);
     Task<IEnumerable<TEntity>> GetByFilter(Expression<Func<TEntity,bool>> predicate);
+    Task<bool> AddRange(IEnumerable<TEntity> entity);
 
 }
