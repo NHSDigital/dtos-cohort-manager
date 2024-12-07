2e69f3ea4ededafe12fa25b0b2e3a06925461290
Richard Kingston <richard.kingston@nordcloud.com>
2024-12-07 14:52:10 +0000

    feat/dtoss-5997-amend-github-config

--- infrastructure/tf-audit/log_analytics_workspace.tf
+++ infrastructure/tf-audit/log_analytics_workspace.tf
@@ -3,8 +3,9 @@ module "log_analytics_workspace_audit" {
 
   source = "../../../dtos-devops-templates/infrastructure/modules/log-analytics-workspace"
 
-  name     = module.regions_config[each.key].names.log-analytics-workspace
-  location = each.key
+  name                = module.regions_config[each.key].names.log-analytics-workspace
+  resource_group_name = azurerm_resource_group.audit[each.key].name
+  location            = each.key
 
   law_sku        = var.law.law_sku
   retention_days = var.law.retention_days
@@ -12,23 +13,21 @@ module "log_analytics_workspace_audit" {
   monitor_diagnostic_setting_log_analytics_workspace_enabled_logs = local.monitor_diagnostic_setting_log_analytics_workspace_enabled_logs
   monitor_diagnostic_setting_log_analytics_workspace_metrics      = local.monitor_diagnostic_setting_log_analytics_workspace_metrics
 
-  resource_group_name = azurerm_resource_group.audit[each.key].name
-
   tags = var.tags
 }
 
 # Add a data export rule to forward logs to the Event Hub in the Hub subscription
 module "log_analytics_data_export_rule" {
-  for_each = var.regions
+  for_each = { for key, val in var.regions : key => val if var.law.export_enabled }
 
   source = "../../../dtos-devops-templates/infrastructure/modules/log-analytics-data-export-rule"
 
   name                    = "${module.regions_config[each.key].names.log-analytics-workspace}-export-rule"
   resource_group_name     = azurerm_resource_group.audit[each.key].name
   workspace_resource_id   = module.log_analytics_workspace_audit[each.key].id
-  destination_resource_id = data.terraform_remote_state.hub.outputs.eventhubs["dtos-hub-${each.key}"]["cohort-manager"].id
+  destination_resource_id = data.terraform_remote_state.hub.outputs.eventhubs["dtos-hub-${each.key}"]["${var.application_full_name}-${lower(var.environment)}"].id
   table_names             = var.law.export_table_names
-  enabled                 = var.law.export_enabled
+  enabled                 = true # could re-use var.law.export_enabled but may as well create/destroy the resource instead of keeping it but in disabled state
 }
 
 /*--------------------------------------------------------------------------------------------------
@@ -51,7 +50,3 @@ For receiving events from the Event Hub (i.e. remote resource):
 #   role_definition_name = "Azure Event Hubs Data Sender"
 #   scope                = data.terraform_remote_state.hub.outputs.eventhub_law_export_id["dtos-hub-${each.key}"]
 # }
-
-output "log_analytics_workspace_audit" {
-  value = module.log_analytics_workspace_audit
-}
