88284f298d99d59690b0faed34d5ff8fe8ddb0ed
Richard Kingston <98461689+rfk-nc@users.noreply.github.com>
2024-12-10 12:00:50 +0000

    fix: Add peering dendency checks (#496)
    * Add peering dendency checks
    
    * Increase database max size

--- infrastructure/tf-core/sql_server.tf
+++ infrastructure/tf-core/sql_server.tf
@@ -56,5 +56,10 @@ module "azure_sql_server" {
     private_service_connection_is_manual = var.features.private_service_connection_is_manual
   } : null
 
+  depends_on = [
+    module.peering_spoke_hub,
+    module.peering_hub_spoke
+  ]
+
   tags = var.tags
 }
