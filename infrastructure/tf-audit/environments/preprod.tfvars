bf63693406a81c0b4827571a6ad6b744bbddab90
Richard Kingston <richard.kingston@nordcloud.com>
2024-12-11 00:09:53 +0000

    Disable log exports for integration

--- infrastructure/tf-audit/environments/preprod.tfvars
+++ infrastructure/tf-audit/environments/preprod.tfvars
@@ -3,9 +3,10 @@ application_full_name = "cohort-manager"
 environment           = "PRE"
 
 features = {
-  private_endpoints_enabled            = true
-  private_service_connection_is_manual = false
-  public_network_access_enabled        = false
+  private_endpoints_enabled              = true
+  private_service_connection_is_manual   = false
+  public_network_access_enabled          = false
+  log_analytics_data_export_rule_enabled = true
 }
 
 tags = {
@@ -38,9 +39,9 @@ app_insights = {
 }
 
 law = {
-  law_sku             = "PerGB2018"
-  retention_days      = 30
-  export_enabled      = true
+  export_enabled = true
+  law_sku        = "PerGB2018"
+  retention_days = 30
   export_table_names = [
     "Alert",
     "AppDependencies",
--- infrastructure/tf-audit/log_analytics_workspace.tf
+++ infrastructure/tf-audit/log_analytics_workspace.tf
@@ -18,7 +18,7 @@ module "log_analytics_workspace_audit" {
 
 # Add a data export rule to forward logs to the Event Hub in the Hub subscription
 module "log_analytics_data_export_rule" {
-  for_each = { for key, val in var.regions : key => val if var.law.export_enabled }
+  for_each = var.features.log_analytics_data_export_rule_enabled ? var.regions : {}
 
   source = "../../../dtos-devops-templates/infrastructure/modules/log-analytics-data-export-rule"
 
@@ -27,7 +27,7 @@ module "log_analytics_data_export_rule" {
   workspace_resource_id   = module.log_analytics_workspace_audit[each.key].id
   destination_resource_id = data.terraform_remote_state.hub.outputs.event_hubs["dtos-hub-${each.key}"]["${var.application_full_name}-${lower(var.environment)}"].id
   table_names             = var.law.export_table_names
-  enabled                 = true # could re-use var.law.export_enabled but may as well create/destroy the resource instead of keeping it but in disabled state
+  enabled                 = var.law.export_enabled
 }
 
 /*--------------------------------------------------------------------------------------------------
@@ -35,7 +35,7 @@ module "log_analytics_data_export_rule" {
 --------------------------------------------------------------------------------------------------*/
 /*
 For sending events to the Event Hub:
-* Azure Event Hubs Data Sender: Grants permissions to send events to the Event Hub.  
+* Azure Event Hubs Data Sender: Grants permissions to send events to the Event Hub.
 * For receiving events from the Event Hub:
 
 For receiving events from the Event Hub (i.e. remote resource):
