6ee43196c102e0d477af322f4b1901085c26a60c
Joseph2910 <184524642+Joseph2910@users.noreply.github.com>
2024-12-17 22:27:49 +0000

    feat: Add environment variable to set is_extracted on table

--- application/CohortManager/src/Functions/CohortDistributionServices/CreateCohortDistribution/CreateCohortDistribution.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/CreateCohortDistribution/CreateCohortDistribution.cs
@@ -76,7 +76,7 @@ public class CreateCohortDistribution
                 }
             }
 
-            if (ParticipantHasException(basicParticipantCsvRecord.NhsNumber, participantData.ScreeningServiceId) && !RetrieveEnvironmentalVariableAsBool("IgnoreParticipantExceptions")) // Will only run if IgnoreParticipantExceptions is false.
+            if (ParticipantHasException(basicParticipantCsvRecord.NhsNumber, participantData.ScreeningServiceId))
             {
                 _logger.LogInformation($"Unable to add to cohort distribution. As participant with ParticipantId: {participantData.ParticipantId}. Has an Exception against it");
                 await HandleErrorResponseAsync($"Unable to add to cohort distribution. As participant with ParticipantId: {participantData.ParticipantId}. Has an Exception against it",
@@ -88,7 +88,7 @@ public class CreateCohortDistribution
             // Validate cohort distribution record & transform data service
             participantData.RecordType = basicParticipantCsvRecord.RecordType;
             var validationRecordCreated = await _CohortDistributionHelper.ValidateCohortDistributionRecordAsync(basicParticipantCsvRecord.NhsNumber, basicParticipantCsvRecord.FileName, participantData);
-            if (!validationRecordCreated || RetrieveEnvironmentalVariableAsBool("IgnoreParticipantExceptions"))
+            if (!validationRecordCreated)
             {
                 _logger.LogInformation("Validation has passed the record with NHS number: {NhsNumber} will be added to the database", participantData.NhsNumber);
 
@@ -139,6 +139,7 @@ public class CreateCohortDistribution
 
     private async Task<HttpWebResponse> AddCohortDistribution(CohortDistributionParticipant transformedParticipant)
     {
+        transformedParticipant.Extracted = RetrieveEnvironmentalVariableAsInt("IsExtractedToBSSelect").ToString();
         var json = JsonSerializer.Serialize(transformedParticipant);
         var response = await _callFunction.SendPost(Environment.GetEnvironmentVariable("AddCohortDistributionURL"), json);
 
@@ -155,10 +156,22 @@ public class CreateCohortDistribution
 
     private static bool RetrieveEnvironmentalVariableAsBool(string environmentVariableName) 
     {
-        if (Environment.GetEnvironmentVariable(environmentVariableName) == "true") 
+        var value = Environment.GetEnvironmentVariable(environmentVariableName);
+        if (value == "true" || value == "1") 
         {
             return true;
         }
         return false;
+    }
+
+    private static int RetrieveEnvironmentalVariableAsInt(string environmentVariableName) 
+    {
+        var value = Environment.GetEnvironmentVariable(environmentVariableName);
+        if (value == "true" || value == "1") 
+        {
+            return 1;
+        }
+        return 0;
+
     }
 }
