998207b5c6e2b909b5be5dad7632f682acb789d7
Michael Clayson <michael.clayson1@nhs.net>
2024-12-20 09:14:54 +0000

    fix: missing reference to common (#521)

--- application/CohortManager/src/Functions/Shared/DataServices.Core/RequestHandler.cs
+++ application/CohortManager/src/Functions/Shared/DataServices.Core/RequestHandler.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
+using Common;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Logging;
