3ec198d2bca11ba0e36e1faacc47ed2186e9fa8f
SamRobinson75684 <166817125+SamRobinson75684@users.noreply.github.com>
2025-01-03 14:10:16 +0000

    chore: adding new tests and fixing all tests to create better code coverage of receive caas file function (#531)
    * chore: adding new tests and fixing all tests to create better code coverage of the receive caas file function
    
    * chore: adding some tests to process caas file
    
    * chore: adding more unit tests to make sure code coverage is above 80 percent for process caas file
    
    * chore: adding more unit tests for AddBatchToQueue
    
    * chore: adding unit tests for records proccsssed tracker and tidying up other tests
    
    * chore: addressing comments
    
    * fix: removing build for dempgrragphic from compose
    
    * chore: addresing comments
    
    * chore: addresing comments
    
    * chore: addressing comments

--- application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/Program.cs
+++ application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/Program.cs
@@ -7,6 +7,8 @@ using Common.Interfaces;
 using Microsoft.Extensions.Logging;
 using NHS.Screening.ReceiveCaasFile;
 using receiveCaasFile;
+using Microsoft.Extensions.Azure;
+using Azure.Identity;
 
 var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
 var logger = loggerFactory.CreateLogger("program.cs");
@@ -28,7 +30,15 @@ try
         services.AddScoped<ICheckDemographic, CheckDemographic>();
         services.AddScoped<ICreateBasicParticipantData, CreateBasicParticipantData>();
         services.AddScoped<IAddBatchToQueue, AddBatchToQueue>();
-        services.AddScoped<IRecordsProcessedTracker,RecordsProcessedTracker>(); //Do not change the lifetime of this.
+        services.AddScoped<IRecordsProcessedTracker, RecordsProcessedTracker>(); //Do not change the lifetime of this.
+        services.AddScoped<IValidateDates, ValidateDates>();
+        services.AddAzureClients(builder =>
+        {
+            // Use the environment credential by default
+            builder.UseCredential(new DefaultAzureCredential());
+            builder.AddQueueServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage") ?? "")
+              .ConfigureOptions(c => c.MessageEncoding = Azure.Storage.Queues.QueueMessageEncoding.Base64);
+        });
     })
     .AddExceptionHandler()
     .AddDatabaseConnection()
---
+++ tests/CaasIntegrationTests/AddBatchToQueueTest/AddBatchToQueueTest.cs
@@ -0,0 +1,102 @@
+﻿namespace NHS.CohortManager.Tests.CaasIntegrationTests;
+
+using Azure.Storage.Queues;
+using Microsoft.Extensions.Logging;
+using Model;
+using Moq;
+using NHS.Screening.ReceiveCaasFile;
+
+
+
+[TestClass]
+public class AddBatchToQueueTest
+{
+    private readonly Mock<ILogger<AddBatchToQueue>> _loggerMock = new();
+    private readonly Mock<QueueServiceClient> mockQueueServiceClient = new();
+    private readonly Mock<QueueClient> mockQueueClient = new();
+    private AddBatchToQueue _addBatchToQueue;
+
+
+
+    public AddBatchToQueueTest()
+    {
+        mockQueueServiceClient
+          .Setup(qsc => qsc.GetQueueClient(It.IsAny<string>()))
+          .Returns(mockQueueClient.Object);
+
+        _addBatchToQueue = new AddBatchToQueue(_loggerMock.Object, mockQueueServiceClient.Object);
+    }
+
+    [TestMethod]
+    public void Constructor_ShouldGetQueueClient()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("AzureWebJobsStorage", "AzureWebJobsStorage");
+        Environment.SetEnvironmentVariable("AddQueueName", "AddQueueName");
+
+
+        // Act
+        var addBatchToQueue = new AddBatchToQueue(_loggerMock.Object, mockQueueServiceClient.Object);
+
+        // Assert
+        mockQueueServiceClient.Verify(qsc => qsc.GetQueueClient("AddQueueName"), Times.AtLeastOnce);
+    }
+
+    [TestMethod]
+    public async Task ProcessBatch_ValidRecord_ProcessSuccessfully()
+    {
+        //arrange 
+        Environment.SetEnvironmentVariable("AzureWebJobsStorage", "AzureWebJobsStorage");
+        Environment.SetEnvironmentVariable("AddQueueName", "AddQueueName");
+
+        Batch batch = new Batch();
+        BasicParticipantCsvRecord basicParticipantCsvRecord = new BasicParticipantCsvRecord();
+        basicParticipantCsvRecord.FileName = "TestFile";
+        basicParticipantCsvRecord.Participant = new BasicParticipantData() { NhsNumber = "1234567890" };
+        basicParticipantCsvRecord.participant = new Participant() { NhsNumber = "1234567890" };
+        batch.AddRecords.Enqueue(basicParticipantCsvRecord);
+
+        Environment.SetEnvironmentVariable("AddQueueName", "AddQueueName");
+
+        // Act
+        await _addBatchToQueue.ProcessBatch(batch);
+
+        //Assert
+        mockQueueClient.Verify(x => x.SendMessageAsync(It.IsAny<string>()), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task ProcessBatch_NoAddRecords_SendMessageNotCalled()
+    {
+        //arrange 
+        Environment.SetEnvironmentVariable("AzureWebJobsStorage", "AzureWebJobsStorage");
+        Environment.SetEnvironmentVariable("AddQueueName", "AddQueueName");
+
+        Batch batch = new Batch();
+        Environment.SetEnvironmentVariable("AddQueueName", "AddQueueName");
+
+        // Act
+        await _addBatchToQueue.ProcessBatch(batch);
+
+        //Assert
+        mockQueueClient.Verify(x => x.SendMessageAsync(It.IsAny<string>()), Times.Never);
+    }
+
+    [TestMethod]
+    public async Task ProcessBatch_BatchIsNull_SendMessageNotCalled()
+    {
+        //arrange 
+        Environment.SetEnvironmentVariable("AzureWebJobsStorage", "AzureWebJobsStorage");
+        Environment.SetEnvironmentVariable("AddQueueName", "AddQueueName");
+
+
+        Environment.SetEnvironmentVariable("AddQueueName", "AddQueueName");
+
+        // Act
+        await _addBatchToQueue.ProcessBatch(null);
+
+        //Assert
+        mockQueueClient.Verify(x => x.SendMessageAsync(It.IsAny<string>()), Times.Never);
+    }
+
+}
