0c2516b37349fa29877578abea4a54c4fc69845b
Michael Clayson <michael.clayson1@nhs.net>
2025-01-24 16:29:56 +0000

    chore: remove unused create participant files

--- application/CohortManager/src/Functions/screeningDataServices/createParticipant/Program.cs
+++ application/CohortManager/src/Functions/screeningDataServices/createParticipant/Program.cs
@@ -12,7 +12,6 @@ var host = new HostBuilder()
         .Build()
     .ConfigureServices(services =>
     {
-        services.AddTransient<ICreateParticipantData, CreateParticipantData>();
         services.AddTransient<IParticipantManagerData, ParticipantManagerData>();
         services.AddSingleton<ICreateResponse, CreateResponse>();
         services.AddTransient<IDatabaseHelper, DatabaseHelper>();
