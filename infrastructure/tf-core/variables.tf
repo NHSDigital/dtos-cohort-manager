6235099af3a0103b826acdab99bae56ad0a12d51
fahadbaigNC <fahad.baig@nordcloud.com>
2025-01-27 10:07:55 +0000

    feat: DTOS 7068 configuring the option to enable app service log for function apps

--- infrastructure/tf-core/variables.tf
+++ infrastructure/tf-core/variables.tf
@@ -171,9 +171,9 @@ variable "function_apps" {
     always_on                              = bool
     app_insights_name                      = string
     app_insights_rg_name                   = string
+    app_service_logs_disk_quota_mb         = optional(number)
     app_service_logs_retention_period_days = optional(number)
     cont_registry_use_mi                   = bool
-    disk_quota_mb                          = optional(number)
     docker_CI_enable                       = string
     docker_env_tag                         = string
     docker_img_prefix                      = string
