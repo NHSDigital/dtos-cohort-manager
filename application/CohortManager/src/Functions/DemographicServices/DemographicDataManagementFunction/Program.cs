91b17e387a6a4d1915a11a29725e08fcd11fc5aa
SamRobinson75684 <166817125+SamRobinson75684@users.noreply.github.com>
2025-02-03 15:00:29 +0000

    feat: migration of the CreateDemographicData (#628)
    * feat: migration of the CreateDemographicData
    
    * fix: fixing unit tests in DemographicDataFunctionTests
    
    * chore: addressinging sonar cube
    
    * fix: fixing unit tests
    
    * chore: addressinging sonar cube
    
    * chore: addressinging sonar cube
    
    * chore: updating docker files to reflect changes
    
    * chore: adding back demographic-data-service to the compose
    
    * chore: changing DemographicDataServiceURL to ParticipantDemographicDataServiceURL
    
    * chore: changing DemographicDataServiceURL to ParticipantDemographicDataServiceURL
    
    * feat: new env variable for demographic-data-management and retrieve-participant-data
    
    * fix: get Participant data now calls the demograpgic get function to get the data it needs instead of calling the data service
    
    * fix: fixing unit tests and adressing comments on pr
    
    * chore: addressing comments
    
    * chore: addressing comments
    
    ---------
    
    Co-authored-by: Maciej Murawski <maciej.murawski@nordcloud.com>

--- application/CohortManager/src/Functions/DemographicServices/DemographicDataManagementFunction/Program.cs
+++ application/CohortManager/src/Functions/DemographicServices/DemographicDataManagementFunction/Program.cs
@@ -1,13 +1,20 @@
 using Common;
+using Data.Database;
+using DataServices.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Model;
 
 var host = new HostBuilder()
+    .AddDataServicesHandler()
+        .AddDataService<ParticipantDemographic>(Environment.GetEnvironmentVariable("ParticipantDemographicDataServiceURL"))
+        .Build()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
         services.AddScoped<ICallFunction, CallFunction>();
         services.AddSingleton<ICreateResponse, CreateResponse>();
+        services.AddTransient<ICreateDemographicData, CreateDemographicData>();
     })
     .Build();
 
--- infrastructure/tf-core/environments/development.tfvars
+++ infrastructure/tf-core/environments/development.tfvars
@@ -565,6 +565,10 @@ function_apps = {
         {
           env_var_name     = "ExceptionFunctionURL"
           function_app_key = "CreateException"
+        },
+        {
+          env_var_name     = "ParticipantDemographicDataServiceURL"
+          function_app_key = "ParticipantDemographicDataService"
         }
       ]
     }
@@ -677,6 +681,10 @@ function_apps = {
         {
           env_var_name     = "ExceptionFunctionURL"
           function_app_key = "CreateException"
+        },
+        {
+          env_var_name     = "ParticipantDemographicDataServiceURL"
+          function_app_key = "ParticipantDemographicDataService"
         }
       ]
     }
