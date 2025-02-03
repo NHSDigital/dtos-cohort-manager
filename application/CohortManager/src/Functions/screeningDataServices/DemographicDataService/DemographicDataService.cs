79362d0a76b1aaba45f84405b3e4bcf499726c7f
SamRobinson75684 <sam.robinson24@nhs.net>
2025-02-03 10:02:31 +0000

    chore: addressinging sonar cube

--- application/CohortManager/src/Functions/screeningDataServices/DemographicDataService/DemographicDataService.cs
+++ application/CohortManager/src/Functions/screeningDataServices/DemographicDataService/DemographicDataService.cs
@@ -12,19 +12,6 @@ using Model;
 
 public class DemographicDataService
 {
-    private readonly ILogger<DemographicDataService> _logger;
-    private readonly ICreateResponse _createResponse;
-    private readonly ICreateDemographicData _createDemographicData;
-    private readonly IExceptionHandler _exceptionHandler;
-
-    public DemographicDataService(ILogger<DemographicDataService> logger, ICreateResponse createResponse, ICreateDemographicData createDemographicData, IExceptionHandler exceptionHandler)
-    {
-        _logger = logger;
-        _createResponse = createResponse;
-        _createDemographicData = createDemographicData;
-        _exceptionHandler = exceptionHandler;
-    }
-
     [Function("DemographicDataService")]
     public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
     {
