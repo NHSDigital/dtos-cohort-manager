77154a89d4516e635f3c8c6d66b07543bc5a5b89
Richard Kingston <richard.kingston@nordcloud.com>
2025-02-06 16:07:40 +0000

    Add Audit rbac roles

--- infrastructure/tf-audit/config.tf
+++ infrastructure/tf-audit/config.tf
@@ -10,7 +10,31 @@ resource "azurerm_resource_group" "audit" {
 }
 
 # Add a role assignment to the audit resource group for each role defined in locals:
-# - log_analytics_contributor
+# First create a map of all roles for all resource groups defined in var.regions:
+module "rbac_assignments" {
+  for_each = local.rbac_roles_resource_groups_map
+
+  source = "../../../dtos-devops-templates/infrastructure/modules/rbac-assignment"
+
+  principal_id         = data.azurerm_client_config.current.object_id
+  role_definition_name = each.value.role_name
+  scope                = azurerm_resource_group.audit[each.value.region_key].id
+}
+
+locals {
+  rbac_roles_resource_groups_flatlist = flatten([
+    for region_key, region_val in var.regions : [
+      for role in local.rbac_roles_resource_group : {
+        key        = "${role}-${region_key}"
+        role_name = role
+        region_key = region_key
+      }
+    ]
+  ])
+
+  # Project the above list into a map with unique keys for consumption in a for_each meta argument
+  rbac_roles_resource_groups_map = { for role in local.rbac_roles_resource_groups_flatlist : role.key => role }
+}
 
 module "regions_config" {
   for_each = var.regions
---
+++ infrastructure/tf-audit/rbac.tf
@@ -0,0 +1,14 @@
+locals {
+  rbac_roles_resource_group = [
+    "Contributor",
+    "Key Vault Certificates Officer",
+    "Key Vault Certificate User",
+    "Key Vault Crypto Officer",
+    "Key Vault Crypto User",
+    "Key Vault Secrets Officer",
+    "Key Vault Secrets User",
+    "Storage Account Contributor",
+    "Storage Blob Data Owner",
+    "Storage Queue Data Contributor"
+  ]
+}
