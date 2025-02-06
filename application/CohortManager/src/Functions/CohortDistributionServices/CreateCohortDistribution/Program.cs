92d031c4a664cfc7f10608e6d266fcf01ba2c725
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2025-02-06 14:58:37 +0000

    removing unused data service

--- application/CohortManager/src/Functions/CohortDistributionServices/CreateCohortDistribution/Program.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/CreateCohortDistribution/Program.cs
@@ -8,9 +8,6 @@ using Model;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .AddDataServicesHandler()
-        .AddDataService<ParticipantManagement>(Environment.GetEnvironmentVariable("ParticipantManagementUrl"))
-        .Build()
     .ConfigureServices(services =>
     {
         services.AddSingleton<ICallFunction, CallFunction>();
