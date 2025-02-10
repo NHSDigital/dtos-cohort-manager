b523a997d0c41edba38feaf8abb47abfab1e100b
Richard Kingston <richard.kingston@nordcloud.com>
2025-02-10 11:10:00 +0000

    remove specified fn app role assignments

--- infrastructure/tf-core/rbac.tf
+++ infrastructure/tf-core/rbac.tf
@@ -1,15 +1,7 @@
 locals {
-  rbac_roles_key_vault = [
-    "Key Vault Certificate User",
-    "Key Vault Crypto User",
-    "Key Vault Secrets User"
-  ]
+  rbac_roles_key_vault = []
 
-  rbac_roles_storage = [
-    "Storage Account Contributor",
-    "Storage Blob Data Owner",
-    "Storage Queue Data Contributor"
-  ]
+  rbac_roles_storage = []
 
   rbac_roles_database = [
     "Contributor"
