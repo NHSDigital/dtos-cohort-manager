906a9879dfd61b54ff196398594f6778a69acc8a
SamRobinson75684 <sam.robinson24@nhs.net>
2025-02-11 09:27:34 +0000

    feat: adidng the new ReceiveServiceNowMessage fucnton

---
+++ application/CohortManager/src/Functions/ServiceNowIntegrationService/ReceiveServiceNowMessage.cs
@@ -0,0 +1,29 @@
+namespace ServiceNowIntegrationService;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Common;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+
+public class ReceiveServiceNowMessage
+{
+    private readonly ILogger<ReceiveServiceNowMessage> _logger;
+
+    private readonly ICreateResponse _createResponse;
+
+
+    public ReceiveServiceNowMessage(ILogger<ReceiveServiceNowMessage> logger, ICreateResponse createResponse)
+    {
+        _logger = logger;
+        _createResponse = createResponse;
+    }
+
+    [Function("ReceiveServiceNowMessage")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("ReceiveServiceNowMessage function processed a request.");
+        return _createResponse.CreateHttpResponse(HttpStatusCode.OK, req);
+    }
+}
+
