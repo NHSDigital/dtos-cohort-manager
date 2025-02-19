864b9b63f2126d6aa4b48f1fa69746ebe31caa0f
fahadbaigNC <fahad.baig@nordcloud.com>
2025-02-19 12:37:34 +0000

    feat: DTOS 7520 changes to the connection string updte to remove connection reset and max pool size set to 300

--- infrastructure/tf-core/function_app.tf
+++ infrastructure/tf-core/function_app.tf
@@ -126,7 +126,7 @@ locals {
 
             # Database connection string
             length(config.db_connection_string) > 0 ? {
-              (config.db_connection_string) = "Server=${module.regions_config[region].names.sql-server}.database.windows.net; Authentication=Active Directory Managed Identity; Database=${var.sqlserver.dbs.cohman.db_name_suffix}"
+              (config.db_connection_string) = "Server=${module.regions_config[region].names.sql-server}.database.windows.net; Authentication=Active Directory Managed Identity; Database=${var.sqlserver.dbs.cohman.db_name_suffix}; ApplicationIntent=ReadWrite; Pooling=true; MultipleActiveResultSets=True; ConnectionTimeout=30; MaxPoolSize=300;"
             } : {}
           )
 
