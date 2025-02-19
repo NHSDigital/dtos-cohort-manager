274f38822ffdefa4893ccdabb39874056273bd9c
Michael Clayson <michael.clayson1@nhs.net>
2025-02-19 15:27:38 +0000

    fix: ensure all db is done with awaits.

--- application/CohortManager/src/Functions/Shared/DataServices.Core/DataServiceAccessor.cs
+++ application/CohortManager/src/Functions/Shared/DataServices.Core/DataServiceAccessor.cs
@@ -29,8 +29,7 @@ public class DataServiceAccessor<TEntity> : IDataServiceAccessor<TEntity> where 
 
     public async Task<List<TEntity>> GetRange(Expression<Func<TEntity, bool>> predicates)
     {
-        var result = _context.Set<TEntity>().AsNoTracking().Where(predicates).ToList();
-        await Task.CompletedTask;
+        var result = await _context.Set<TEntity>().AsNoTracking().Where(predicates).ToListAsync();
         return result;
     }
 
@@ -54,28 +53,41 @@ public class DataServiceAccessor<TEntity> : IDataServiceAccessor<TEntity> where 
 
     public async Task<bool> Remove(Expression<Func<TEntity, bool>> predicate)
     {
-        var result = _context.Set<TEntity>().AsNoTracking().SingleOrDefault(predicate);
-        await Task.CompletedTask;
+
+        using var transaction = await _context.Database.BeginTransactionAsync();
+        var result = await _context.Set<TEntity>().AsNoTracking().SingleOrDefaultAsync(predicate);
+
         if (result == null)
         {
             return false;
         }
         _context.Set<TEntity>().Remove(result);
-        await _context.SaveChangesAsync();
+        var rowsEffected =  await _context.SaveChangesAsync();
+        if(rowsEffected > 1)
+        {
+            await _context.Database.RollbackTransactionAsync();
+
+            _logger.LogError("There was an error while trying to deleted despite a record being found");
+            throw new MultipleRecordsFoundException("Multiple Records were updated by PUT request, Changes have been Rolled-back");
+        }
+
+        await _context.Database.CommitTransactionAsync();
         return true;
+
     }
 
     public async Task<TEntity> Update(TEntity entity, Expression<Func<TEntity, bool>> predicate)
     {
 
-        var existingEntity = _context.Set<TEntity>().AsNoTracking().SingleOrDefault(predicate);
-        await Task.CompletedTask;
+
+        using var transaction = await _context.Database.BeginTransactionAsync();
+
+        var existingEntity = await _context.Set<TEntity>().AsNoTracking().SingleOrDefaultAsync(predicate);
 
         if (existingEntity == null)
         {
             return null;
         }
-        using var transaction = await _context.Database.BeginTransactionAsync();
         _context.Update(entity);
         var rowsEffected  = await _context.SaveChangesAsync();
 
