6dfafa6e66d673a05d60935f1a85acd9e5775346
sudipnhs <152979070+sudipnhs@users.noreply.github.com>
2025-02-25 10:53:53 +0000

    feat: DTOSS-7515 healthchecks implemented on ReceiveCaasFile function (#718)
    * feat: health check using nuget package
    
    * feat:DTOSS-7515 Health Checks implemented in ReceiveCaasFile function
    
    * feat:DTOSS-7515 Health Checks sonar cloud error fix
    
    * feat: DTOSS-7515 resolve PR comments for health checks
    
    * feat: DTOSS-7515 refactor health check
    
    * feat:DTOSS-7515 Health Checks sonar cloud error fix
    
    * feat:DTOSS-7515 Health Checks remove unnecessay boolean var
    
    * feat:DTOSS-7515 latency thresold in config
    
    * feat:DTOSS-7515 resolve PR feedbacks

---
+++ application/CohortManager/src/Functions/Shared/HealthChecks/BlobStorageHealthCheck.cs
@@ -0,0 +1,37 @@
+﻿namespace HealthChecks;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+public class BlobStorageHealthCheck : IHealthCheck
+{
+    private readonly BlobServiceClient _blobServiceClient;
+    private readonly ILogger<BlobStorageHealthCheck> _logger;
+
+    public BlobStorageHealthCheck(ILogger<BlobStorageHealthCheck> logger, BlobServiceClient blobServiceClient)
+    {
+        _logger = logger;
+        _blobServiceClient = blobServiceClient;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        _logger.LogInformation("Running health check for Azure Blob Storage...");
+
+        try
+        {
+            // Check if the blob service is accessible
+            await _blobServiceClient.GetPropertiesAsync(cancellationToken: cancellationToken);
+            return HealthCheckResult.Healthy("Blob storage is accessible.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Blob storage health check failed.");
+            return HealthCheckResult.Unhealthy("Blob storage is inaccessible.", ex);
+        }
+    }
+}
