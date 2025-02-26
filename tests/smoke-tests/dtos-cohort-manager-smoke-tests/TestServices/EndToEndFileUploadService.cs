e6bd86eb8a37221707594a21a14dda98ed71215d
charanNHS <charan.thotakura1@nhs.net>
2025-02-26 12:35:45 +0000

    feat: dtoss-7598 config switch between dev and local (#732)
    * Introduced IsCloudEnvironment check
    
    * Fixed indentation
    
    * Included namespace and modifies database helper methods
    
    * Changed appsettings.json value to true
    
    * Tested in local works fine
    
    * Tested solution for config, mod readme file , added appsettings local
    
    * Rectified format for Readme

--- tests/smoke-tests/dtos-cohort-manager-smoke-tests/TestServices/EndToEndFileUploadService.cs
+++ tests/smoke-tests/dtos-cohort-manager-smoke-tests/TestServices/EndToEndFileUploadService.cs
@@ -11,18 +11,23 @@ public class EndToEndFileUploadService
     private readonly ILogger<EndToEndFileUploadService> _logger;
     private readonly AppSettings _appSettings;
     private readonly BlobStorageHelper _blobStorageHelper;
-    private readonly string _connectionString;
-    //public string LocalFilePath => _appSettings.FilePaths.Local;
-    private readonly string _managedIdentityClientId;
+    private readonly SqlConnectionWithAuthentication _sqlConnectionWithAuthentication;
 
     public EndToEndFileUploadService(ILogger<EndToEndFileUploadService> logger, AppSettings appSettings, BlobStorageHelper blobStorageHelper)
     {
         _logger = logger;
         _appSettings = appSettings;
         _blobStorageHelper = blobStorageHelper;
-        _connectionString = _appSettings.ConnectionStrings.DtOsDatabaseConnectionString;
-        _managedIdentityClientId = _appSettings.ManagedIdentityClientId;
+
+        // Read from appsettings.json
+        string connectionString = _appSettings.ConnectionStrings.DtOsDatabaseConnectionString;
+        string? managedIdentityClientId = _appSettings.ManagedIdentityClientId;
+        bool isCloudEnvironment = _appSettings.AzureSettings.IsCloudEnvironment; // Instead of hardcoded AZURE_ENVIRONMENT
+
+        // Pass to SqlConnectionWithAuthentication
+        _sqlConnectionWithAuthentication = new SqlConnectionWithAuthentication(connectionString, managedIdentityClientId, isCloudEnvironment);
     }
+
 
     public async Task CleanDatabaseAsync(IEnumerable<string> nhsNumbers)
     {
@@ -33,19 +38,19 @@ public class EndToEndFileUploadService
             foreach (var nhsNumber in nhsNumbers)
             {
                 //  parameterized queries to prevent SQL injection
-                await DatabaseHelper.ExecuteNonQueryAsync(_connectionString, _managedIdentityClientId,
+                await  DatabaseHelper.ExecuteNonQueryAsync(_sqlConnectionWithAuthentication,
                     "DELETE FROM PARTICIPANT_MANAGEMENT WHERE NHS_Number = @nhsNumber",
                     new SqlParameter("@nhsNumber", nhsNumber));
 
-                await DatabaseHelper.ExecuteNonQueryAsync(_connectionString, _managedIdentityClientId,
+                await  DatabaseHelper.ExecuteNonQueryAsync(_sqlConnectionWithAuthentication,
                     "DELETE FROM PARTICIPANT_DEMOGRAPHIC WHERE NHS_Number = @nhsNumber",
                     new SqlParameter("@nhsNumber", nhsNumber));
 
-                await DatabaseHelper.ExecuteNonQueryAsync(_connectionString, _managedIdentityClientId,
+                await  DatabaseHelper.ExecuteNonQueryAsync(_sqlConnectionWithAuthentication,
                     "DELETE FROM BS_COHORT_DISTRIBUTION WHERE NHS_Number = @nhsNumber",
                     new SqlParameter("@nhsNumber", nhsNumber));
 
-                await DatabaseHelper.ExecuteNonQueryAsync(_connectionString, _managedIdentityClientId,
+                await  DatabaseHelper.ExecuteNonQueryAsync(_sqlConnectionWithAuthentication,
                     "DELETE FROM EXCEPTION_MANAGEMENT WHERE NHS_Number = @nhsNumber",
                     new SqlParameter("@nhsNumber", nhsNumber));
             }
@@ -58,15 +63,6 @@ public class EndToEndFileUploadService
             throw; // Re-throw the exception to be handled elsewhere
         }
     }
-
-
-    //public List<string> ExtractNhsNumbers()
-    //{
-    //    _logger.LogInformation("Extracting NHS numbers from file: {FilePath}", LocalFilePath);
-    //    var nhsNumbers = CsvHelperService.ExtractNhsNumbersFromCsv(LocalFilePath);
-    //    _logger.LogInformation("Extracted {Count} NHS numbers from the file.", nhsNumbers.Count);
-    //    return nhsNumbers;
-    //}
 
     public List<string> ExtractNhsNumbersFromParquet(string filePath)
     {
@@ -81,36 +77,36 @@ public class EndToEndFileUploadService
 
     public async Task UploadFileAsync(string filePath)
     {
-     if (!File.Exists(filePath))
-     {
-         _logger.LogError("File not found at {FilePath}", filePath);
-         throw new FileNotFoundException($"File not found at {filePath}");
-     }
+        if (!File.Exists(filePath))
+        {
+            _logger.LogError("File not found at {FilePath}", filePath);
+            throw new FileNotFoundException($"File not found at {filePath}");
+        }
 
-     int retryCount = 0;
-     const int maxRetries = 5;
-     TimeSpan delay = TimeSpan.FromSeconds(1);
+        int retryCount = 0;
+        const int maxRetries = 5;
+        TimeSpan delay = TimeSpan.FromSeconds(1);
 
-     while (retryCount < maxRetries)
-     {
-         try
-         {
-             _logger.LogInformation("Uploading file {FilePath} to Blob Storage (Attempt {AttemptNumber}).", filePath, retryCount + 1);
-             await _blobStorageHelper.UploadFileToBlobStorageAsync(filePath, _appSettings.BlobContainerName);
-             _logger.LogInformation("File uploaded successfully.");
-             return; // Exit the loop if successful
-         }
-         catch (Exception ex)
-         {
-             _logger.LogError(ex, "Error uploading file {FilePath} to Blob Storage (Attempt {AttemptNumber}).", filePath, retryCount + 1);
-             retryCount++;
-             await Task.Delay(delay);
-             delay = delay * 2; // Exponential backoff
-         }
-     }
+        while (retryCount < maxRetries)
+        {
+            try
+            {
+                _logger.LogInformation("Uploading file {FilePath} to Blob Storage (Attempt {AttemptNumber}).", filePath, retryCount + 1);
+                await _blobStorageHelper.UploadFileToBlobStorageAsync(filePath, _appSettings.BlobContainerName);
+                _logger.LogInformation("File uploaded successfully.");
+                return; // Exit the loop if successful
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error uploading file {FilePath} to Blob Storage (Attempt {AttemptNumber}).", filePath, retryCount + 1);
+                retryCount++;
+                await Task.Delay(delay);
+                delay = delay * 2; // Exponential backoff
+            }
+        }
 
-     _logger.LogError("Failed to upload file {FilePath} to Blob Storage after {MaxRetries} retries.", filePath, maxRetries);
-     throw new Exception($"Failed to upload file {filePath} to Blob Storage after {maxRetries} retries.");
+        _logger.LogError("Failed to upload file {FilePath} to Blob Storage after {MaxRetries} retries.", filePath, maxRetries);
+        throw new Exception($"Failed to upload file {filePath} to Blob Storage after {maxRetries} retries.");
     }
 
     public async Task<bool> VerifyRecordCountAsync(string tableName, int originalCount, int expectedIncrement, int retries = 10, int delay = 1000)
@@ -119,7 +115,7 @@ public class EndToEndFileUploadService
 
         for (int i = 0; i < retries; i++)
         {
-            var newCount = await DatabaseHelper.GetRecordCountAsync(_connectionString, tableName);
+            var newCount = await DatabaseHelper.GetRecordCountAsync(_sqlConnectionWithAuthentication, tableName);
             if (newCount == originalCount + expectedIncrement)
             {
                 _logger.LogInformation("Record count verified: Expected = {Expected}, Actual = {Actual}.", originalCount + expectedIncrement, newCount);
@@ -137,7 +133,7 @@ public class EndToEndFileUploadService
     public async Task VerifyNhsNumbersAsync(string tableName, List<string> nhsNumbers)
     {
         _logger.LogInformation("Validating NHS numbers in table {TableName}.", tableName);
-        await DatabaseValidationHelper.VerifyNhsNumbersAsync(_connectionString, tableName, nhsNumbers, _logger,_managedIdentityClientId);
+        await DatabaseValidationHelper.VerifyNhsNumbersAsync(_sqlConnectionWithAuthentication, tableName, nhsNumbers, _logger);
         _logger.LogInformation("Validation of NHS numbers completed successfully.");
     }
 
@@ -146,7 +142,7 @@ public class EndToEndFileUploadService
         _logger.LogInformation("Validating NHS number count in table {TableName}.", tableName);
         Func<Task> act = async () =>
         {
-            var nhsNumberCount = await DatabaseValidationHelper.GetNhsNumberCount(_connectionString, tableName, nhsNumber, _logger, _managedIdentityClientId);
+            var nhsNumberCount = await DatabaseValidationHelper.GetNhsNumberCount(_sqlConnectionWithAuthentication, tableName, nhsNumber, _logger);
             nhsNumberCount.Should().Be(expectedCount);
         };
 
@@ -159,7 +155,7 @@ public class EndToEndFileUploadService
     {
         Func<Task> act = async () =>
         {
-            var result = await DatabaseValidationHelper.VerifyFieldUpdateAsync(_connectionString, tableName, nhsNumber,fieldName,_managedIdentityClientId, expectedValue, _logger);
+            var result = await DatabaseValidationHelper.VerifyFieldUpdateAsync(_sqlConnectionWithAuthentication, tableName, nhsNumber, fieldName, expectedValue, _logger);
             result.Should().BeTrue();
         };
 
