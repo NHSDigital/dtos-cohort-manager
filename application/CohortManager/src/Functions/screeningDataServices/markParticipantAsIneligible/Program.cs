ffd35338b4cbd4ce70086ec4707ec72718b794f7
Joseph2910 <184524642+Joseph2910@users.noreply.github.com>
2025-03-03 13:59:25 +0000

    fix: Removed references to old class

--- application/CohortManager/src/Functions/screeningDataServices/markParticipantAsIneligible/Program.cs
+++ application/CohortManager/src/Functions/screeningDataServices/markParticipantAsIneligible/Program.cs
@@ -13,7 +13,6 @@ var host = new HostBuilder()
     .Build()
     .ConfigureServices(services =>
     {
-        services.AddTransient<IGetParticipantData, GetParticipantData>();
         services.AddSingleton<ICreateResponse, CreateResponse>();
         services.TryAddTransient<IDatabaseHelper, DatabaseHelper>();
         services.AddSingleton<ICallFunction, CallFunction>();
