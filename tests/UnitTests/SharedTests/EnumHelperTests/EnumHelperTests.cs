95dc07ceb3272c8ffc18115ae6b9b0ca516913aa
Joe P <184524642+Joseph2910@users.noreply.github.com>
2025-03-03 10:29:00 +0000

    fix: UpdatedTestNames

--- tests/UnitTests/SharedTests/EnumHelperTests/EnumHelperTests.cs
+++ tests/UnitTests/SharedTests/EnumHelperTests/EnumHelperTests.cs
@@ -9,7 +9,7 @@ public class EnumHelperTests
 {
 
     [TestMethod]
-    public void ReturnDisplayNameWhenValidInput ()
+    public void GetDisplayName_ValidInput_ReturnDisplayName()
     {
         //No Arrange 
         //Act
@@ -22,7 +22,7 @@ public class EnumHelperTests
     }
 
     [TestMethod]
-    public void ReturnEmptyStringWhenDisplayNameIsNull() 
+    public void GetDisplayName_NullInput_ReturnEmptyString() 
     {
         //No Arrange
         //Act
@@ -31,19 +31,6 @@ public class EnumHelperTests
         //Assert
         string expectedOutput = "";
         Assert.AreEqual(expectedOutput, result);
-    }
-
-    // May remove this and associated method after discussion with team
-    [TestMethod]
-    public void ReturnListOfHTTPStatusCodesCorrectly() 
-    {
-        //No Arrange
-        //Act
-        List<string> actual = EnumHelper.GetHttpStatusCodeStringList();
-
-        //Assert
-        List<string> expectedList = ["100","101","102","103","200","201","202","203","204","205","206","207","208","226","300","300","301","301","302","302","303","303","304","305","306","307","307","308","400","401","402","403","404","405","406","407","408","409","410","411","412","413","414","415","416","417","421","422","422","423","424","426","428","429","431","451","500","501","502","503","504","505","506","507","508","510","511"];
-        CollectionAssert.AreEqual(expectedList, actual);
     }
 
 }
