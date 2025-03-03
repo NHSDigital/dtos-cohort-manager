43ad696c99bc825cceb53c91fdc7474ff92ce471
Swapnil261188 <Swapnil.patil2@nhs.net>
2025-03-03 17:25:10 +0000

    feat: 7684-added tests for GetMissingAddress to increase the coverage

--- application/CohortManager/src/Functions/Shared/Data/Database/GetMissingAddress.cs
+++ application/CohortManager/src/Functions/Shared/Data/Database/GetMissingAddress.cs
@@ -17,36 +17,44 @@ public class GetMissingAddress
     public CohortDistributionParticipant GetAddress()
     {
         try
-        {
-            string sql = $"SELECT POST_CODE, ADDRESS_LINE_1, ADDRESS_LINE_2, ADDRESS_LINE_3, ADDRESS_LINE_4, ADDRESS_LINE_5 " +
-                    $"FROM [dbo].[BS_COHORT_DISTRIBUTION] " +
-                    $"WHERE PARTICIPANT_ID = @ParticipantId";
-            using (_connection)
             {
+                string sql = $"SELECT POST_CODE, ADDRESS_LINE_1, ADDRESS_LINE_2, ADDRESS_LINE_3, ADDRESS_LINE_4, ADDRESS_LINE_5 " +
+                        $"FROM [dbo].[BS_COHORT_DISTRIBUTION] " +
+                        $"WHERE PARTICIPANT_ID = @ParticipantId";
+
                 _connection.Open();
-                using (SqlCommand command = new SqlCommand(sql, (SqlConnection)_connection))
+
+                using (IDbCommand command = _connection.CreateCommand())
                 {
-                    command.Parameters.AddWithValue("@ParticipantId", _participant.ParticipantId);
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    command.CommandText = sql;
+                    command.CommandType = System.Data.CommandType.Text;
+
+                    IDbDataParameter parameter = command.CreateParameter();
+                    parameter.ParameterName = "@ParticipantId";
+                    parameter.Value = _participant.ParticipantId;
+                    command.Parameters.Add(parameter);
+
+                    using (IDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            if (_participant.Postcode != reader["POST_CODE"] as string)
+                            string storedPostcode = reader["POST_CODE"] as string;
+
+                            if (_participant.Postcode != storedPostcode)
                             {
                                 throw new ArgumentException("Participant has an empty address and postcode does not match existing data");
                             }
 
-                            _participant.AddressLine1 = reader["ADDRESS_LINE_1"] as string ?? null;
-                            _participant.AddressLine2 = reader["ADDRESS_LINE_2"] as string ?? null;
-                            _participant.AddressLine3 = reader["ADDRESS_LINE_3"] as string ?? null;
-                            _participant.AddressLine4 = reader["ADDRESS_LINE_4"] as string ?? null;
-                            _participant.AddressLine5 = reader["ADDRESS_LINE_5"] as string ?? null;
+                            _participant.AddressLine1 = reader["ADDRESS_LINE_1"] as string;
+                            _participant.AddressLine2 = reader["ADDRESS_LINE_2"] as string;
+                            _participant.AddressLine3 = reader["ADDRESS_LINE_3"] as string;
+                            _participant.AddressLine4 = reader["ADDRESS_LINE_4"] as string;
+                            _participant.AddressLine5 = reader["ADDRESS_LINE_5"] as string;
                         }
                     }
                 }
+                return _participant;
             }
-            return _participant;
-        }
         finally
         {
             if (_connection != null)
