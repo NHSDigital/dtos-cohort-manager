ffc3c4009d4d2de0fc50f90063af8a05229718c6
SamRobinson75684 <sam.robinson24@nhs.net>
2025-03-05 15:14:33 +0000

    feat: adding better config

---
+++ application/CohortManager/src/Functions/CohortDistributionServices/AddCohortDistributionData/AddCohortDistributionDataConfig.cs
@@ -0,0 +1,8 @@
+namespace NHS.CohortManager.CohortDistributionDataServices;
+
+public class AddCohortDistributionDataConfig
+{
+    public string ExceptionFunctionURL { get; set; }
+    public string DtOsDatabaseConnectionString { get; set; }
+    public string CohortDistributionDataServiceURL { get; set; }
+}
