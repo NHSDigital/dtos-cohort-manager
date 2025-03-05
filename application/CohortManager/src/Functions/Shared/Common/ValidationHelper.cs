71512521298b6f33e22c086e0c90cabff819eeda
Joseph2910 <184524642+Joseph2910@users.noreply.github.com>
2025-03-05 14:49:12 +0000

    fix: Added method back
    Method appears to be used by another test class although I'm unsure why.

--- application/CohortManager/src/Functions/Shared/Common/ValidationHelper.cs
+++ application/CohortManager/src/Functions/Shared/Common/ValidationHelper.cs
@@ -1,13 +1,23 @@
 namespace Common;
 
-using System.Diagnostics.Eventing.Reader;
 using System.Globalization;
-using System.Reflection.Metadata;
 
 public static class ValidationHelper
 {
+    private static readonly string[] DateFormats = ["yyyyMMdd", "yyyyMM", "yyyy", "yyyy-MM-dd", "dd/MM/yyyy HH:mm:ss", "d/MM/yyyy hh:mm:ss tt", "dd/MM/yyyy HH:mm:ss tt", "yyyy"];
     private static readonly string NilReturnFileNhsNumber = "0000000000";
+    
     // Validates that the date is not in the future and that it is in one of the expected formats
+    public static bool ValidatePastDate(string dateString)
+    {
+        var date = ParseDate(dateString);
+        if(date.HasValue)
+        {
+            return date < DateTime.Today;
+        }
+        return false;
+    }
+    
     public static bool ValidateNHSNumber(string nhsNumber)
     {
         // Check the NHS number is a number
@@ -61,5 +71,14 @@ public static class ValidationHelper
             return false;
         }
         return true;
+    }
+
+    private static DateTime? ParseDate(string dateString)
+    {
+        if (DateTime.TryParseExact(dateString, DateFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+        {
+            return date;
+        }
+        return null;
     }
 }
