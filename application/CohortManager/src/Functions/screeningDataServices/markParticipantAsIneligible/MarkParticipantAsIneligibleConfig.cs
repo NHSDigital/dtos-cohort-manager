8b2d70be24180fbd33e882c103ee4c3972fa2408
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-03-12 13:53:00 +0000

    Removed env var direct usage from markParticipantAsIneligible function

--- application/CohortManager/src/Functions/screeningDataServices/markParticipantAsIneligible/MarkParticipantAsIneligibleConfig.cs
+++ application/CohortManager/src/Functions/screeningDataServices/markParticipantAsIneligible/MarkParticipantAsIneligibleConfig.cs
@@ -6,4 +6,6 @@ public class MarkParticipantAsIneligibleConfig
 {
     [Required]
     public string ParticipantManagementUrl {get; set;}
+    [Required]
+    public string LookupValidationURL { get; set; }
 }
