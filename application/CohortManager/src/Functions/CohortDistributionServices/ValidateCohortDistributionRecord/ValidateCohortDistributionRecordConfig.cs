a05abfebe79de3a4a5807643ca1368efe2d48a04
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-03-12 12:26:45 +0000

    Removed environment variable usage from ValidateCohortDistributionRecord function

--- application/CohortManager/src/Functions/CohortDistributionServices/ValidateCohortDistributionRecord/ValidateCohortDistributionRecordConfig.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/ValidateCohortDistributionRecord/ValidateCohortDistributionRecordConfig.cs
@@ -6,4 +6,6 @@ public class ValidateCohortDistributionRecordConfig
 {
     [Required]
     public string CohortDistributionDataServiceURL {get; set;}
+    [Required]
+    public string LookupValidationURL {get; set;}
 }
