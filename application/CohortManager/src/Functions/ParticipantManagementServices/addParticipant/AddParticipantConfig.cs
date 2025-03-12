790a630fd8417b4e5d7fae294d6c4b81d77b2d8b
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-03-12 12:52:39 +0000

    Removed direct usage of env vars from RetrievePDSDemographic and addParticipant functions

--- application/CohortManager/src/Functions/ParticipantManagementServices/addParticipant/AddParticipantConfig.cs
+++ application/CohortManager/src/Functions/ParticipantManagementServices/addParticipant/AddParticipantConfig.cs
@@ -6,4 +6,10 @@ public class AddParticipantConfig
 {
     [Required]
     public string DemographicURIGet {get; set;}
+    [Required]
+    public string DSaddParticipant {get; set;}
+    [Required]
+    public string DSmarkParticipantAsEligible {get; set;}
+    [Required]
+    public string StaticValidationURL {get; set;}
 }
