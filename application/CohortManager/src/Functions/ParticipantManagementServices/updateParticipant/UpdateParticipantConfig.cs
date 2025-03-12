6d4a4e84af5d23d3c682e5a33c5cd36fa3f5ae17
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-03-12 13:39:59 +0000

    Removed env var direct usage from updateParticipant

--- application/CohortManager/src/Functions/ParticipantManagementServices/updateParticipant/UpdateParticipantConfig.cs
+++ application/CohortManager/src/Functions/ParticipantManagementServices/updateParticipant/UpdateParticipantConfig.cs
@@ -5,5 +5,13 @@ using System.ComponentModel.DataAnnotations;
 public class UpdateParticipantConfig
 {
     [Required]
-    public string DemographicURIGet {get; set;}
+    public string DemographicURIGet { get; set; }
+    [Required]
+    public string UpdateParticipant { get; set; }
+    [Required]
+    public string StaticValidationURL { get; set; }
+    [Required]
+    public string DSmarkParticipantAsEligible { get; set; }
+    [Required]
+    public string markParticipantAsIneligible { get; set; }
 }
