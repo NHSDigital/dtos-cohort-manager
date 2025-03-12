c57b3c357b68bf7c8ed4cdcb67f5f713e9b34d14
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-03-12 14:15:08 +0000

    Updated FileValidation function Program.cs to include new config class

--- application/CohortManager/src/Functions/ScreeningValidationService/FileValidation/Program.cs
+++ application/CohortManager/src/Functions/ScreeningValidationService/FileValidation/Program.cs
@@ -1,8 +1,10 @@
 using Common;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NHS.Screening.FileValidation;
 
 var host = new HostBuilder()
+    .AddConfiguration<FileValidationConfig>(out FileValidationConfig config)
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
