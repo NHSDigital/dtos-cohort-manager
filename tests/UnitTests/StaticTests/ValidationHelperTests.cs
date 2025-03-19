50e4a310a248bfddab1b7ecb22a87f21c8c99993
SamRobinson75684 <166817125+SamRobinson75684@users.noreply.github.com>
2025-03-19 09:56:46 +0000

    fix: 7757 creating more test coverage (#824)
    * fix: adding more unit tests ValidationHelperTests
    
    * fix: removing unwanted test
    
    * making sure changes are pushed
    
    * chore: adding new test that passees in an always future date

--- tests/UnitTests/StaticTests/ValidationHelperTests.cs
+++ tests/UnitTests/StaticTests/ValidationHelperTests.cs
@@ -2,6 +2,7 @@ namespace NHS.CohortManager.Tests.UnitTests.StaticTests;
 
 using Common;
 
+
 [TestClass]
 public class ValidationHelperTests
 {
@@ -31,6 +32,56 @@ public class ValidationHelperTests
         var result = ValidationHelper.ValidateNHSNumber(nhsNumber);
 
         //assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    [DataRow("1990-10-10")]
+    [DataRow("2002-04-06")]
+    [DataRow("1997-12-23")]
+    [DataRow("2010-01-01")]
+    [DataRow("2005-12-10")]
+    public void ValidatePastDate_ValidPastDate_ReturnsTrue(string pastDate)
+    {
+        var result = ValidationHelper.ValidatePastDate(pastDate);
+
+        Assert.IsTrue(result);
+    }
+
+
+    [TestMethod]
+    [DataRow("1990-10-10")]
+    [DataRow("2002-04-06")]
+    [DataRow("1997-12-23")]
+    [DataRow("2010-01-01")]
+    [DataRow("2005-12-10")]
+    public void ValidateDate_ValidDate_ReturnsTrue(string date)
+    {
+        var result = ValidationHelper.IsValidDate(date);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    [DataRow("9999-10-10")]
+    [DataRow("3034-04-06")]
+    [DataRow("6060-12-23")]
+    [DataRow("7070-01-01")]
+    [DataRow("7070-01-01")]
+    [DataRow("dfbgdfdggfggg")]
+    public void ValidatePastDate_InvalidPastDate_ReturnsFalse(string pastDate)
+    {
+        var result = ValidationHelper.ValidatePastDate(pastDate);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void ValidatePastDate_InvalidPastDateAlwaysFuture_ReturnsFalse()
+    {
+
+        var result = ValidationHelper.ValidatePastDate(DateTime.Today.AddDays(1).ToString());
+
         Assert.IsFalse(result);
     }
 }
