356291b833164dd3a794a898dc5a04c8d847d828
SamRobinson75684 <sam.robinson24@nhs.net>
2025-03-20 14:21:06 +0000

    fix: adding new tests for adding updating and bulk adding recording

--- application/CohortManager/e2e/EndToEndTestScript.spec.ts
+++ application/CohortManager/e2e/EndToEndTestScript.spec.ts
@@ -22,7 +22,7 @@ test('Upload Add file to Azure Blob and check database', async () => {
     await blobClient.uploadFile(testfileName);
     await delay(20000);
 
-    const recordFromDB = await checkDatabase();
+    const recordFromDB = await checkDatabase(2);
     var res = recordFromDB["nhs_number"] == '3112728165';
     expect(res).toBeTruthy();
 });
@@ -45,7 +45,7 @@ test('Upload Add file to Azure Blob twice and expect it error in database', asyn
     await blobClient.uploadFile(testfileName);
 
 
-    await delay(10000);
+    await delay(15000);
     // Check that the cohort distribution has made it to the database via an HTTP API call.
     const error = await checkDatabaseForErrors();
     
@@ -71,7 +71,7 @@ test('Upload Add file to Azure Blob and expect it to error in database with erro
     // upload same file again and expect to it cause an error in database
     await delay(20000);
     // Check that the cohort distribution has made it to the database via an HTTP API call.
-    const itemsFromDatabase = await checkDatabase();
+    const itemsFromDatabase = await checkDatabase(5);
     
     const error = await checkDatabaseForErrors();
     
@@ -85,15 +85,134 @@ test('Upload Add file to Azure Blob and expect it to error in database with erro
 });
 
 
-async function checkDatabase(): Promise<[]> {
-  const rowCount = 5;
+
+test('Upload update file and except it be in the database', async () => {
+  const amendedItems = new Array(0);
+  var arrIndex = 0;
+
+  // Initialize the Azure Blob Service Client and get the container and blob clients
+  const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blobName = "AMENDED_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet";
+  const blobClient: BlockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  const path = require('path');
+  const testfileName = path.join(__dirname, 'AMENDED_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+
+  
+  await blobClient.uploadFile(testfileName);
+  // upload same file again and expect to it cause an error in database
+  await delay(20000);
+
+  (await checkDatabase(5)).forEach((item) =>{
+    if(item["nhs_number"] == '2612514171') {
+      amendedItems[arrIndex] = item
+      arrIndex++
+    }
+  });
+
+  
+  expect(amendedItems.length == 1).toBeTruthy()
+
+});
+
+
+
+test('Upload update file and except it not to be in database and error 22 in error table', async () => {
+  // Initialize the Azure Blob Service Client and get the container and blob clients
+  const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blobName = "AMENDED_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet";
+  const blobClient: BlockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  const path = require('path');
+  const testfileName = path.join(__dirname, 'AMENDED1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+
+  
+  await blobClient.uploadFile(testfileName);
+  await delay(20000);
+  // upload same file again and expect to it cause an error in database
+  const itemsFromDatabase = await checkDatabase(5);
+    
+  const error = await checkDatabaseForErrors();
+  
+  var Rule22InDatabase = error.find(x => x["RuleId"] == '22');
+  
+  expect(Rule22InDatabase != null || Rule22InDatabase != undefined).toBeTruthy()
+  var res = itemsFromDatabase["nhs_number"] == '2312514176';
+  expect(res).toBeFalsy()
+});
+
+
+test('Upload file with 2 records and except 2 records to be returned from API', async () => {
+  
+  const amendedItems = new Array(0);
+  var arrIndex = 0; 
+  
+  // Initialize the Azure Blob Service Client and get the container and blob clients
+  const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blobName = "ADD_2_RECORDS_-_CAAS_BREAST_SCREENING_COHORT.parquet";
+  const blobClient: BlockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  const path = require('path');
+  const testfileName = path.join(__dirname, 'ADD_2_RECORDS_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+
+  
+  await blobClient.uploadFile(testfileName);
+  // upload same file again and expect to it cause an error in database
+  await delay(20000);
+
+  
+  (await checkDatabase(5)).forEach((item) =>{
+    if(item["nhs_number"] == '1111110662' || item["nhs_number"] == '2222211794') {
+      amendedItems[arrIndex] = item
+      arrIndex++
+    }
+  });
+
+
+  expect(amendedItems.length == 2).toBeTruthy()
+});
+
+
+
+
+test('Upload file with 300 records and except 300 records to be returned from API', async () => {
+  
+  test.setTimeout(100000); // Sets timeout to 45 seconds for this test
+  
+  // Initialize the Azure Blob Service Client and get the container and blob clients
+  const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blobName = "ADD_500_2_-_CAAS_BREAST_SCREENING_COHORT.parquet";
+  const blobClient: BlockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  const path = require('path');
+  const testfileName = path.join(__dirname, 'ADD_500_2_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+
+  
+  await blobClient.uploadFile(testfileName);
+  // upload same file again and expect to it cause an error in database
+  await delay(60000);
+
+  var hasMoreThan500Records = (await checkDatabase(300)).length >= 300
+
+  expect(hasMoreThan500Records).toBeTruthy()
+});
+
+
+async function checkDatabase(rowCount): Promise<[]> {
+  
   const url = `http://localhost:7095/api/RetrieveCohortDistributionData?rowCount=${rowCount}`;
   try {
     const response = await fetch(url);
     if (response.ok) {
         var responseJson = await response.json();
-        let items = responseJson[0]
-        return items;
+        if (responseJson.length > 1) {
+          return responseJson;
+        }
+        return responseJson[0];
     } else {
       console.error(`Error: ${response.status}`);
   
