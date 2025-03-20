176fcf9edcb1aacfc2a24c4a11504704b56e3ba5
Charan Thotakura <charan.thotakura1@nhs.net>
2025-03-20 10:29:15 +0000

    Renamed name spaces and project

--- tests/e2e-tests/dtos-cohort-manager-e2e-tests/Config/FilePaths.cs
+++
@@ -1,8 +0,0 @@
-namespace dtos_cohort_manager_e2e_tests.Config;
-
-public class FilePaths
-{
-    public string Add { get; set; }
-    public string Amended { get; set; }
-    public string Remove { get; set; }
-}
--- tests/e2e-tests/dtos-cohort-manager-e2e-tests/Models/FieldsTable.cs
+++
@@ -1,7 +0,0 @@
-namespace dtos_cohort_manager_e2e_tests.Models;
-
-public class FieldsTable
-{
-    public string? FieldName { get; set; }
-    public string? FieldValue { get; set; }
-}
