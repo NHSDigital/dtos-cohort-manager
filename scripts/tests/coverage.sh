b7f4ac3ea3d583f41800658724b2850f43687c56
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-03-25 16:34:04 +0000

    attempted to fix pipeline run by clarifying full path to coverage

--- scripts/tests/coverage.sh
+++ scripts/tests/coverage.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-
 set -euo pipefail
 
 # Move to the root directory of the repository.
 cd "$(git rev-parse --show-toplevel)"
+ROOT_DIR="$PWD"
 
 # Run the tests and generate the coverage report.
 dotnet test --collect:"XPlat Code Coverage" \
             /p:CollectCoverage=true \
-            /p:CoverletOutput="coverage/coverage.xml" \
+            /p:CoverletOutput="${ROOT_DIR}/coverage/coverage.xml" \
             /p:CoverletOutputFormat=opencover
 
 # Set the report output environment variable.
-REPORT_OUTPUT="$(pwd)/coverage/coverage.xml"
+REPORT_OUTPUT="${ROOT_DIR}/coverage/coverage.xml"
 export REPORT_OUTPUT
 
 # Echo the path for the workflow to pick up.
