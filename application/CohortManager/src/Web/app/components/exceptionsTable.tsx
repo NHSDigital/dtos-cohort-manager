057df3f868a56e82653657b54221ab164ebb91ef
Adam Chrimes <iChrimes@live.co.uk>
2025-03-26 08:48:57 +0000

    fix: remove unnecessary roles from the table component

--- application/CohortManager/src/Web/app/components/exceptionsTable.tsx
+++ application/CohortManager/src/Web/app/components/exceptionsTable.tsx
@@ -16,7 +16,7 @@ export default function ExceptionsTable({
       <caption className="nhsuk-table__caption">
         {caption ? `${caption}` : "Total breast screening exceptions"}
       </caption>
-      <thead role="rowgroup" className="nhsuk-table__head">
+      <thead className="nhsuk-table__head">
         <tr role="row">
           <th role="columnheader" scope="col">
             Exception ID
@@ -39,7 +39,7 @@ export default function ExceptionsTable({
             className="nhsuk-table__row"
             key={exception.exceptionId}
           >
-            <td role="cell" className="nhsuk-table__cell">
+            <td className="nhsuk-table__cell">
               <span
                 className="nhsuk-table-responsive__heading"
                 aria-hidden="true"
@@ -50,7 +50,7 @@ export default function ExceptionsTable({
                 {exception.exceptionId}
               </Link>
             </td>
-            <td role="cell" className="nhsuk-table__cell app-u-no-wrap">
+            <td className="nhsuk-table__cell app-u-no-wrap">
               <span
                 className="nhsuk-table-responsive__heading"
                 aria-hidden="true"
@@ -59,7 +59,7 @@ export default function ExceptionsTable({
               </span>
               {formatNhsNumber(exception.nhsNumber ?? "")}
             </td>
-            <td role="cell" className="nhsuk-table__cell app-u-no-wrap">
+            <td className="nhsuk-table__cell app-u-no-wrap">
               <span
                 className="nhsuk-table-responsive__heading"
                 aria-hidden="true"
@@ -68,7 +68,7 @@ export default function ExceptionsTable({
               </span>
               {formatDate(exception.dateCreated)}
             </td>
-            <td role="cell" className="nhsuk-table__cell">
+            <td className="nhsuk-table__cell">
               <span
                 className="nhsuk-table-responsive__heading"
                 aria-hidden="true"
