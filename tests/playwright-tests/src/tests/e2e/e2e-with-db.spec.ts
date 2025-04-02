2baf631b8d997ee6cf86ce216a0ef44a56b70642
Abhinav S <abhinav.sharma2@nhs.net>
2025-04-02 15:22:50 +0100

    test: feat smoke test along with mvp helpers

---
+++ tests/playwright-tests/src/tests/e2e/e2e-with-db.spec.ts
@@ -0,0 +1,129 @@
+import { test } from '@playwright/test';
+import { cleanupDatabase, validateSqlData } from '../../database/sqlVerifier';
+import { processFileViaStorage, validateSqlDatabase } from '../steps/steps'
+
+
+test.describe('Smoke Tests', () => {
+  test('@smoke @DTOSS-6256 Verify file upload and cohort distribution process for ADD', async () => {
+
+    const nhsNumbers = ['1111110662', '2222211794']; // TODO use data generator utility to pass nhs numbers in run-time
+
+    await cleanupDatabase(nhsNumbers); //TODO re-think try remove participant api
+    await processFileViaStorage('ADD_2_RECORDS_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+
+    await test.step('Validation in Cohort', async () => {
+      //TODO move to validation.json
+      const checkInDatabase = {
+        "validations": [{
+          "validations": {
+            "tableName": "BS_COHORT_DISTRIBUTION",
+            "columnName": "NHS_Number",
+            "columnValue": nhsNumbers[0]
+          }
+        }, {
+          "validations": {
+            "tableName": "BS_COHORT_DISTRIBUTION",
+            "columnName": "NHS_Number",
+            "columnValue": nhsNumbers[1]
+          }
+        },
+        {
+          "validations": {
+            "tableName": "PARTICIPANT_MANAGEMENT",
+            "columnName": "NHS_Number",
+            "columnValue": nhsNumbers[0],
+            "columnName2": "EXCEPTION_FLAG",
+            "columnValue2": "0"
+          }
+        }, {
+          "validations": {
+            "tableName": "PARTICIPANT_MANAGEMENT",
+            "columnName": "NHS_Number",
+            "columnValue": nhsNumbers[1],
+            "columnName2": "EXCEPTION_FLAG",
+            "columnValue2": "0"
+          }
+        }
+        ]
+      }
+      await validateSqlDatabase(checkInDatabase.validations);
+    });
+  });
+
+  test('@smoke @DTOSS-6257 Verify file upload and cohort distribution process for ADD followed by AMENDED records', async () => {
+    const nhsNumbers = ['2312514176'];
+
+    test.step(`ADD process`, async () => {
+      await cleanupDatabase(nhsNumbers); //TODO move to before this test
+      await processFileViaStorage('ADD1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+
+      await test.step('Validation in Cohort For ADD', async () => {
+        const checkInDatabaseForNHSNumber = {
+          "validations": [
+            {
+              "validations": {
+                "tableName": "BS_COHORT_DISTRIBUTION",
+                "columnName": "NHS_Number",
+                "columnValue": nhsNumbers[0]
+              }
+            }
+          ]
+        }
+        await validateSqlData(checkInDatabaseForNHSNumber.validations);
+      });
+    });
+
+    test.step(`AMEND process followed by ADD`, async () => {
+      await processFileViaStorage(`AMENDED1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet`);
+
+      test.step(`Validation in Cohort For AMEND followed by ADD`, async () => {
+
+        //DB Validation
+        const checkInDatabase = {
+          "validations": [
+            {
+              "validations": {
+                "tableName": "BS_COHORT_DISTRIBUTION",
+                "columnName": "NHS_Number",
+                "columnValue": nhsNumbers[0]
+              }
+            }, {
+              "validations": {
+                "tableName": "BS_COHORT_DISTRIBUTION",
+                "columnName": "GIVEN_NAME",
+                "columnValue": "AMENDEDNewTest1"
+              }
+            }
+          ]
+        }
+        await validateSqlDatabase(checkInDatabase.validations);
+      })
+    });
+  });
+
+  test('@smoke @DTOSS-7960 Verify GP Practice Code Exception, flag in participant management is set to 1', async () => {
+
+    const nhsNumbers = ['2612314172'];
+
+    await cleanupDatabase(nhsNumbers); //TODO move to before this test
+    await processFileViaStorage(`Exception_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet`);
+
+    test.step(`Validation in Cohort For ADD`, async () => {
+      const checkInDatabase = {
+        "validations": [
+          {
+            "validations": {
+              "tableName": "PARTICIPANT_MANAGEMENT",
+              "columnName": "NHS_Number",
+              "columnValue": nhsNumbers[0],
+              "columnName2": "EXCEPTION_FLAG",
+              "columnValue2": "1"
+            }
+          }
+        ]
+      }
+      await validateSqlDatabase(checkInDatabase.validations);
+    })
+  });
+})
+
