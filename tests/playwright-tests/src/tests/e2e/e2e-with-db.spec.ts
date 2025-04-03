0120c1369f58fe021bccda26425f5b7ce13425d4
Abhinav S <abhinav.sharma2@nhs.net>
2025-04-03 12:37:32 +0100

    test: fixed test step nesting causing false test positive

--- tests/playwright-tests/src/tests/e2e/e2e-with-db.spec.ts
+++ tests/playwright-tests/src/tests/e2e/e2e-with-db.spec.ts
@@ -8,7 +8,7 @@ test.describe('Smoke Tests', () => {
 
     const nhsNumbers = ['1111110662', '2222211794']; // TODO use data generator utility to pass nhs numbers in run-time
 
-    await cleanupDatabase(nhsNumbers); //TODO re-think try remove participant api
+    await cleanupDatabase(nhsNumbers); //TODO re-think try remove-participant api instead
     await processFileViaStorage('ADD_2_RECORDS_-_CAAS_BREAST_SCREENING_COHORT.parquet');
 
     await test.step('Validation in Cohort', async () => {
@@ -50,55 +50,54 @@ test.describe('Smoke Tests', () => {
     });
   });
 
-  test('@smoke @DTOSS-6257 Verify file upload and cohort distribution process for ADD followed by AMENDED records', async () => {
+  test.only('@smoke @DTOSS-6257 Verify file upload and cohort distribution process for ADD followed by AMENDED records', async () => {
     const nhsNumbers = ['2312514176'];
+    await cleanupDatabase(nhsNumbers); //TODO move to before this test
 
-    test.step(`ADD process`, async () => {
-      await cleanupDatabase(nhsNumbers); //TODO move to before this test
-      await processFileViaStorage('ADD1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+    await processFileViaStorage('ADD1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet');
 
-      await test.step('Validation in Cohort For ADD', async () => {
-        const checkInDatabaseForNHSNumber = {
-          "validations": [
-            {
-              "validations": {
-                "tableName": "BS_COHORT_DISTRIBUTION",
-                "columnName": "NHS_Number",
-                "columnValue": nhsNumbers[0]
-              }
+    await test.step('Validation in Cohort For ADD', async () => {
+      const checkInDatabaseForNHSNumber = {
+        "validations": [
+          {
+            "validations": {
+              "tableName": "BS_COHORT_DISTRIBUTION",
+              "columnName": "NHS_Number",
+              "columnValue": nhsNumbers[0]
             }
-          ]
-        }
-        await validateSqlData(checkInDatabaseForNHSNumber.validations);
-      });
+          }
+        ]
+      }
+      await validateSqlData(checkInDatabaseForNHSNumber.validations);
     });
 
-    test.step(`AMEND process followed by ADD`, async () => {
-      await processFileViaStorage(`AMENDED1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet`);
 
-      test.step(`Validation in Cohort For AMEND followed by ADD`, async () => {
 
-        //DB Validation
-        const checkInDatabase = {
-          "validations": [
-            {
-              "validations": {
-                "tableName": "BS_COHORT_DISTRIBUTION",
-                "columnName": "NHS_Number",
-                "columnValue": nhsNumbers[0]
-              }
-            }, {
-              "validations": {
-                "tableName": "BS_COHORT_DISTRIBUTION",
-                "columnName": "GIVEN_NAME",
-                "columnValue": "AMENDEDNewTest1"
-              }
+    await processFileViaStorage(`AMENDED1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet`);
+
+    test.step(`Validation in Cohort For AMEND followed by ADD`, async () => {
+
+      //DB Validation
+      const checkInDatabase = {
+        "validations": [
+          {
+            "validations": {
+              "tableName": "BS_COHORT_DISTRIBUTION",
+              "columnName": "NHS_Number",
+              "columnValue": nhsNumbers[0]
             }
-          ]
-        }
-        await validateSqlDatabase(checkInDatabase.validations);
-      })
-    });
+          }, {
+            "validations": {
+              "tableName": "BS_COHORT_DISTRIBUTION",
+              "columnName": "GIVEN_NAME",
+              "columnValue": "AMENDEDNewTest1"
+            }
+          }
+        ]
+      }
+      await validateSqlDatabase(checkInDatabase.validations);
+    })
+
   });
 
   test('@smoke @DTOSS-7960 Verify GP Practice Code Exception, flag in participant management is set to 1', async () => {
