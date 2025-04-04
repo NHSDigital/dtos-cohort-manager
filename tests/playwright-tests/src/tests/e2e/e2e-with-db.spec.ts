9382f147808d3d9e3042a089fbad2603460475f3
Abhinav S <abhinav.sharma2@nhs.net>
2025-04-04 13:55:31 +0100

    test: externalise test validation data to json file

--- tests/playwright-tests/src/tests/e2e/e2e-with-db.spec.ts
+++ tests/playwright-tests/src/tests/e2e/e2e-with-db.spec.ts
@@ -1,132 +1,76 @@
 import { test } from '@playwright/test';
 import { cleanupDatabase, validateSqlData } from '../../database/sqlVerifier';
-import { processFileViaStorage, validateSqlDatabase } from '../steps/steps'
+import { getTestData, processFileViaStorage, validateSqlDatabase } from '../steps/steps'
+
 
 
 test.describe('Smoke Tests', () => {
   test('@smoke @DTOSS-6256 Verify file upload and cohort distribution process for ADD', async () => {
 
-    const nhsNumbers = ['1111110662', '2222211794']; // TODO use data generator utility to pass nhs numbers in run-time
+    const testFile = "ADD_2_RECORDS_-_CAAS_BREAST_SCREENING_COHORT";
+    const [checkInDatabase, nhsNumbers] = await getTestData(`${testFile}.json`);
 
-    await test.step(`Given database does not contain 2 ADD records that will be processed: ${nhsNumbers[0]} and ${nhsNumbers[1]}  `, async () => {
+    await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
       await cleanupDatabase(nhsNumbers); //TODO re-think try remove-participant api instead
     });
 
     await test.step(`When 2 ADD participants are processed via storage`, async () => {
-      await processFileViaStorage('ADD_2_RECORDS_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+      await processFileViaStorage(`${testFile}.parquet`);
     });
 
     await test.step(`Then NHS Numbers should be should be updated in the cohort`, async () => {
-      //TODO move to validation.json
-      const checkInDatabase = {
-        "validations": [{
-          "validations": {
-            "tableName": "BS_COHORT_DISTRIBUTION",
-            "columnName": "NHS_Number",
-            "columnValue": nhsNumbers[0]
-          }
-        }, {
-          "validations": {
-            "tableName": "BS_COHORT_DISTRIBUTION",
-            "columnName": "NHS_Number",
-            "columnValue": nhsNumbers[1]
-          }
-        }
-        ]
-      }
       await validateSqlDatabase(checkInDatabase.validations);
     });
 
   });
-
   test('@smoke @DTOSS-6257 Verify file upload and cohort distribution process for ADD followed by AMENDED records', async () => {
-    const nhsNumbers = ['2312514176'];
 
-    await test.step(`Given database does not contain record that will be processed: ${nhsNumbers[0]}  `, async () => {
+    //TODO move setup to test.before
+    const testFileAdd = "ADD1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT";
+    const testFileAmend = "AMENDED1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT";
+    const [checkInDatabaseAdd, nhsNumbers] = await getTestData(`${testFileAdd}.json`);
+    const [checkInDatabaseAmend] = await getTestData(`${testFileAmend}.json`);
+
+    await test.step(`Given database does not contain record that will be processed`, async () => {
       await cleanupDatabase(nhsNumbers); //TODO re-think try remove-participant api instead
     });
 
     await test.step(`When ADD participant is processed via storage`, async () => {
-      await processFileViaStorage('ADD1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet');
+      await processFileViaStorage(`${testFileAdd}.parquet`);
     });
 
     await test.step(`Then ADD record should be updated in the cohort`, async () => {
-
-      const checkInDatabaseForNHSNumber = {
-        "validations": [
-          {
-            "validations": {
-              "tableName": "BS_COHORT_DISTRIBUTION",
-              "columnName": "NHS_Number",
-              "columnValue": nhsNumbers[0]
-            }
-          }
-        ]
-      }
-      await validateSqlData(checkInDatabaseForNHSNumber.validations);
+      await validateSqlData(checkInDatabaseAdd.validations);
     });
 
     await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
-      await processFileViaStorage(`AMENDED1_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet`);
+      await processFileViaStorage(`${testFileAmend}.parquet`);
     });
 
     await test.step(`Then AMENDED record name should be updated in the cohort`, async () => {
-      //DB Validation
-      const checkInDatabase = {
-        "validations": [
-          {
-            "validations": {
-              "tableName": "BS_COHORT_DISTRIBUTION",
-              "columnName": "GIVEN_NAME",
-              "columnValue": "AMENDEDNewTest1",
-              "columnName2": "NHS_Number",
-              "columnValue2": nhsNumbers[0]
-            }
-          }
-        ]
-      }
-      await validateSqlDatabase(checkInDatabase.validations);
+      await validateSqlDatabase(checkInDatabaseAmend.validations);
     });
 
 
 
 
   });
-
   test('@smoke @DTOSS-7960 Verify GP Practice Code Exception, flag in participant management is set to 1', async () => {
 
-    const nhsNumbers = ['2612314172'];
+    const testFile = "Exception_1B8F53_-_CAAS_BREAST_SCREENING_COHORT";
+    const [checkInDatabase, nhsNumbers] = await getTestData(`${testFile}.json`);
 
     await test.step(`Given database does not contain records that will be processed: ${nhsNumbers}  `, async () => {
       await cleanupDatabase(nhsNumbers); //TODO re-think try remove-participant api instead
     });
 
     await test.step(`When participants are processed via storage`, async () => {
-      await processFileViaStorage(`Exception_1B8F53_-_CAAS_BREAST_SCREENING_COHORT.parquet`);
+      await processFileViaStorage(`${testFile}.parquet`);
     });
 
     await test.step(`Then records should be updated in the cohort`, async () => {
-      const checkInDatabase = {
-        "validations": [
-          {
-            "validations": {
-              "tableName": "PARTICIPANT_MANAGEMENT",
-              "columnName": "NHS_Number",
-              "columnValue": nhsNumbers[0],
-              "columnName2": "EXCEPTION_FLAG",
-              "columnValue2": "1"
-            }
-          }
-        ]
-      }
-      await validateSqlDatabase(checkInDatabase.validations);
-
+      await validateSqlDatabase(checkInDatabase);
     });
-
-
-
-
-
   });
 })
 
--- tests/playwright-tests/src/tests/steps/steps.ts
+++ tests/playwright-tests/src/tests/steps/steps.ts
@@ -1,11 +1,13 @@
 import { test, expect } from "@playwright/test";
 import { validateSqlData } from "../../database/sqlVerifier";
-import path from "path";
 import { uploadToLocalStorage } from "../../storage/azureStorage";
+import { InputData } from "../../interface/InputData";
+import * as fs from 'fs';
+import path from "path";
 
 export async function validateSqlDatabase(validations: any) {
   return test.step(`Validate database for assertions`, async () => {
- const hasFailures = await validateSqlData(validations);
+    const hasFailures = await validateSqlData(validations);
 
     try {
       expect(hasFailures).toBeTruthy();
@@ -18,8 +20,15 @@ export async function validateSqlDatabase(validations: any) {
 
 export async function processFileViaStorage(fileName: string) {
   return test.step(`Process file via Storage`, async () => {
-      const parquetFilePath = path.join(__dirname, `../`,`e2e/testFiles/${fileName}`); // TODO move static data to configuration file .env.*
-      await uploadToLocalStorage(parquetFilePath);
+    const parquetFilePath = path.join(__dirname, `../`, `e2e/testFiles/${fileName}`); // TODO move static data to configuration file .env.*
+    await uploadToLocalStorage(parquetFilePath);
   });
 }
 
+export async function getTestData(fileName: string): Promise<[any, string[]]> { //TODO fix return type
+  return test.step(`Extracting data to be validated from input JSON`, async () => {
+    const parsedData: InputData = JSON.parse(fs.readFileSync(path.join(__dirname, `../`, `e2e/testFiles/${fileName}`), 'utf-8')); // TODO move static data to configuration file .env
+    const nhsNumbers: string[] = parsedData.validations.map(item => item.validations.columnValue);
+    return [parsedData.validations, nhsNumbers];
+  });
+}
