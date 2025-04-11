6fa6317e0e5cbbd40643fe6e32063a576022524d
Abhinav S <abhinav.sharma2@nhs.net>
2025-04-11 09:33:57 +0100

    test: separate positive and exception flow

--- tests/playwright-tests/src/tests/e2e/e2e-with-db.spec.ts
+++ tests/playwright-tests/src/tests/e2e/e2e-with-db.spec.ts
@@ -1,12 +1,12 @@
 import { test } from '@playwright/test';
 import { cleanupDatabase } from '../../database/sqlVerifier'; //Explore Data Services route for cleanupDatabase
-import { getTestData, processFileViaStorage, validateSqlDatabase, validateSqlDatabaseFromAPI } from '../steps/steps'
+import { getTestData, processFileViaStorage, validateSqlDatabaseFromAPI } from '../steps/steps'
 
 
 
-test.describe('Smoke Tests', () => {
+test.describe.parallel('Positive @smoke Tests', () => {
 
-  test.only('01 @smoke @DTOSS-6256 @api Verify file upload and cohort distribution process for ADD', async ({ request }, testInfo) => {
+  test('01 @smoke @DTOSS-6256 @api Verify file upload and cohort distribution process for ADD', async ({ request }, testInfo) => {
     const [checkInDatabase, nhsNumbers, parquetFile] = await getTestData(testInfo.title);
 
     await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
@@ -37,7 +37,7 @@ test.describe('Smoke Tests', () => {
     });
 
     await test.step(`Then ADD record should be updated in the cohort`, async () => {
-      await validateSqlDatabase(checkInDatabase);
+      await validateSqlDatabaseFromAPI(request,checkInDatabase);
     });
 
     await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
@@ -49,6 +49,38 @@ test.describe('Smoke Tests', () => {
     });
 
   });
+
+  test('04 @smoke @DTOSS-6407 Verify file upload handles EmptyDOB Exception', async ({ request }, testInfo) => {
+
+    const [checkInDatabase, nhsNumber, parquetFileAdd] = await getTestData(testInfo.title);
+    const [checkInDatabaseAmend, nhsNumberAmend, parquetFileAmend] = await getTestData(testInfo.title, "AMENDED");
+
+    await test.step(`Given database does not contain record that will be processed`, async () => {
+      await cleanupDatabase(nhsNumber);
+    });
+
+    await test.step(`When ADD participant is processed via storage`, async () => {
+      await processFileViaStorage(parquetFileAdd!);
+    });
+
+    await test.step(`Then ADD record should be updated in the cohort`, async () => {
+      await validateSqlDatabaseFromAPI(request,checkInDatabase);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${nhsNumberAmend}`, async () => {
+      await processFileViaStorage(parquetFileAmend!);
+    });
+
+    await test.step(`Then the Exception table should contain the below details for the NHS Number`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabaseAmend);
+    });
+
+  });
+
+});
+
+
+test.describe.parallel('Exception @smoke Tests', () => {
 
   test('03 @smoke @DTOSS-6406 Verify file upload handles invalid GP Practice Code Exception', async ({ request }, testInfo) => {
 
@@ -67,36 +99,8 @@ test.describe('Smoke Tests', () => {
     });
   });
 
-  test('04 @smoke @DTOSS-6407 Verify file upload handles EmptyDOB Exception', async ({ request }, testInfo) => {
-
-    const [checkInDatabase, nhsNumber, parquetFileAdd] = await getTestData(testInfo.title);
-    const [checkInDatabaseAmend, nhsNumberAmend, parquetFileAmend] = await getTestData(testInfo.title, "AMENDED");
-
-    await test.step(`Given database does not contain record that will be processed`, async () => {
-      await cleanupDatabase(nhsNumber);
-    });
-
-    await test.step(`When ADD participant is processed via storage`, async () => {
-      await processFileViaStorage(parquetFileAdd!);
-    });
-
-    await test.step(`Then ADD record should be updated in the cohort`, async () => {
-      await validateSqlDatabase(checkInDatabase);
-    });
-
-    await test.step(`When same ADD participant record is AMENDED via storage for ${nhsNumberAmend}`, async () => {
-      await processFileViaStorage(parquetFileAmend!);
-    });
-
-    await test.step(`Then the Exception table should contain the below details for the NHS Number`, async () => {
-      await validateSqlDatabaseFromAPI(request, checkInDatabaseAmend);
-    });
-
-  });
-
-  //TODO: FIX validation logic for Exception flag = `1` via API
   test('05 @smoke @DTOSS-7960 @api Verify GP Practice Code Exception flag in participant management set to 1', async ({ request }, testInfo) => {
-    console.info(`Running test: ${testInfo.title}`); //TODO move to beforeEach
+    console.info(`Running test: ${testInfo.title}`);
 
     const [checkInDatabase, nhsNumbers, parquetFile] = await getTestData(testInfo.title);
 
@@ -114,5 +118,6 @@ test.describe('Smoke Tests', () => {
   });
 
 });
+
 
 
