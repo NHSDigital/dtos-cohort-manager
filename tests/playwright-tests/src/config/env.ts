5f92365f98f685611092ae8a2d0366ca169d81c0
Abhinav S <abhinav.sharma2@nhs.net>
2025-04-14 15:42:55 +0100

    test: api test file path added

--- tests/playwright-tests/src/config/env.ts
+++ tests/playwright-tests/src/config/env.ts
@@ -8,6 +8,7 @@ const baseURL = process.env.BASE_URL ?? '';
 const azureConnectionString = process.env.AZURITE_LOCAL_STORAGE_CONNECTION_STRING ?? '';
 const containerName = process.env.CONTAINER_NAME ?? '';
 const e2eTestFilesPath = process.env.E2E_TEST_FILES_PATH ?? '';
+const apiTestFilesPath = process.env.API_TEST_FILES_PATH ?? '';
 const apiRetry = Number(process.env.API_RETRIES ?? 1);
 const apiWaitTime = Number(process.env.API_WAIT_TIME ?? 2000);
 const endpointCohortDistributionDataService = process.env.ENDPOINT_COHORT_DISTRIBUTION_DATA_SERVICE ?? '';
@@ -19,6 +20,7 @@ export const config = {
   azureConnectionString,
   containerName,
   e2eTestFilesPath,
+  apiTestFilesPath,
   apiRetry,
   apiWaitTime,
   endpointCohortDistributionDataService,
