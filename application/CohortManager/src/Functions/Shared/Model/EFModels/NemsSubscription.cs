50dabfed68efc47f0cbbf4e4e4443e00d98ffe84
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-04-17 11:56:12 +0100

    Added bigint type name to long values in NemsSubscription EF Model

--- application/CohortManager/src/Functions/Shared/Model/EFModels/NemsSubscription.cs
+++ application/CohortManager/src/Functions/Shared/Model/EFModels/NemsSubscription.cs
@@ -7,16 +7,16 @@ public class NemsSubscription
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
-    [Column("SUBSCRIPTION_ID")]
+    [Column("SUBSCRIPTION_ID", TypeName = "bigint")]
     public long SubscriptionId { get; set; }
 
     [Required]
-    [Column("NHS_NUMBER")]
+    [Column("NHS_NUMBER", TypeName = "bigint")]
     public long NhsNumber { get; set; }
-    
+
     [Column("RECORD_INSERT_DATETIME", TypeName = "datetime")]
     public DateTime? RecordInsertDateTime { get; set; }
-    
+
     [Column("RECORD_UPDATE_DATETIME", TypeName = "datetime")]
     public DateTime? RecordUpdateDateTime { get; set; }
 }
