5ebc9a61631220ac389a1adb58eac24dd1c83c2b
Charan Thotakura <charan.thotakura1@nhs.net>
2025-04-22 15:27:23 +0100

    Formatted file

--- tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
@@ -4,23 +4,23 @@ import { cleanupDatabaseFromAPI, getApiTestData, getTestData, processFileViaStor
 
 test.only('06 @epic2-high-priority @regression @DTOSS-8544 @Implement Validation for Eligibility Flag for Add', async ({ request }, testInfo) => {
 
-    const [checkInDatabase, nhsNumber, parquetFileAdd] = await getTestData(testInfo.title);
+  const [checkInDatabase, nhsNumber, parquetFileAdd] = await getTestData(testInfo.title);
 
 
-    await test.step(`Given database does not contain record that will be processed`, async () => {
-      await cleanupDatabaseFromAPI(request, nhsNumber);
-    });
-
-    await test.step(`When ADD participant is processed via storage`, async () => {
-      await processFileViaStorage(parquetFileAdd!);
-    });
-
-    await test.step(`Then NHS Numbers should be should be updated in the cohort`, async () => {
-      await validateSqlDatabaseFromAPI(request, checkInDatabase);
-    });
-
-    await test.step(`Then validate eligibility flag is always set to true for ADD`, async () => {
-      await validateSqlDatabaseFromAPI(request, checkInDatabase);
-    });
-
+  await test.step(`Given database does not contain record that will be processed`, async () => {
+    await cleanupDatabaseFromAPI(request, nhsNumber);
   });
+
+  await test.step(`When ADD participant is processed via storage`, async () => {
+    await processFileViaStorage(parquetFileAdd!);
+  });
+
+  await test.step(`Then NHS Numbers should be should be updated in the cohort`, async () => {
+    await validateSqlDatabaseFromAPI(request, checkInDatabase);
+  });
+
+  await test.step(`Then validate eligibility flag is always set to true for ADD`, async () => {
+    await validateSqlDatabaseFromAPI(request, checkInDatabase);
+  });
+
+});
