b989a3ba3a72e7c5703ceba23ff51519277293d8
Charan Thotakura <charan.thotakura1@nhs.net>
2025-04-22 16:01:55 +0100

    Added test for AMENDED file type

--- tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
@@ -2,10 +2,8 @@ import { test } from '@playwright/test';
 import { cleanupDatabaseFromAPI, getApiTestData, getTestData, processFileViaStorage, validateSqlDatabaseFromAPI } from '../../steps/steps';
 
 
-test.only('06 @epic2-high-priority @regression @DTOSS-8544 @Implement Validation for Eligibility Flag for Add', async ({ request }, testInfo) => {
-
+test('06 @epic2-high-priority @DTOSS-8544 @Implement Validation for Eligibility Flag for Add', async ({ request }, testInfo) => {
   const [checkInDatabase, nhsNumber, parquetFileAdd] = await getTestData(testInfo.title);
-
 
   await test.step(`Given database does not contain record that will be processed`, async () => {
     await cleanupDatabaseFromAPI(request, nhsNumber);
@@ -22,5 +20,29 @@ test.only('06 @epic2-high-priority @regression @DTOSS-8544 @Implement Validation
   await test.step(`Then validate eligibility flag is always set to true for ADD`, async () => {
     await validateSqlDatabaseFromAPI(request, checkInDatabase);
   });
+});
 
+test.only('06 @epic2-high-priority @DTOSS-8086 @Implement Validation for Eligibility Flag for Amended', async ({ request }, testInfo) => {
+  const [checkInDatabase, nhsNumber, parquetFileAdd] = await getTestData(testInfo.title);
+  const [checkInDatabaseAmend, nhsNumberAmend, parquetFileAmend] = await getTestData(testInfo.title, "AMENDED");
+
+  await test.step(`Given database does not contain record that will be processed`, async () => {
+    await cleanupDatabaseFromAPI(request, nhsNumber);
+  });
+
+  await test.step(`When ADD participant is processed via storage`, async () => {
+    await processFileViaStorage(parquetFileAdd!);
+  });
+
+  await test.step(`Then ADD record should be updated in the cohort`, async () => {
+    await validateSqlDatabaseFromAPI(request, checkInDatabase);
+  });
+
+  await test.step(`When same ADD participant record is AMENDED via storage for ${nhsNumberAmend}`, async () => {
+    await processFileViaStorage(parquetFileAmend!);
+  });
+
+  await test.step(`Then validate eligibility flag is always set to true for ADD`, async () => {
+    await validateSqlDatabaseFromAPI(request, checkInDatabase);
+  });
 });
