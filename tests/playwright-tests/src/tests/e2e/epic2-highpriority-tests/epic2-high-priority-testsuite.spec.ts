38090323326219db238611ea6c6d349210cd5efd
Charan Thotakura <charan.thotakura1@nhs.net>
2025-04-22 15:06:11 +0100

    Complete test for ADD for eligibilityflag

---
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
@@ -0,0 +1,26 @@
+import { request, test } from '@playwright/test';
+import { cleanupDatabaseFromAPI, getApiTestData, getTestData, processFileViaStorage, validateSqlDatabaseFromAPI } from '../../steps/steps';
+
+
+test.only('06 @epic2-high-priority @regression @DTOSS-8544 @Implement Validation for Eligibility Flag for Add', async ({ request }, testInfo) => {
+
+    const [checkInDatabase, nhsNumber, parquetFileAdd] = await getTestData(testInfo.title);
+
+
+    await test.step(`Given database does not contain record that will be processed`, async () => {
+      await cleanupDatabaseFromAPI(request, nhsNumber);
+    });
+
+    await test.step(`When ADD participant is processed via storage`, async () => {
+      await processFileViaStorage(parquetFileAdd!);
+    });
+
+    await test.step(`Then NHS Numbers should be should be updated in the cohort`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabase);
+    });
+
+    await test.step(`Then validate eligibility flag is always set to true for ADD`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabase);
+    });
+
+  });
