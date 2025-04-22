4970dcc6479640fd05c2ab77b6c4f96fe24bfe29
SamRobinson75684 <sam.robinson24@nhs.net>
2025-04-22 10:05:35 +0100

    fix: fixing unit tests

--- tests/UnitTests/ScreeningDataServicesTests/MarkParticipantAsIneligibleTests/MarkParticipantAsIneligibleTests.cs
+++ tests/UnitTests/ScreeningDataServicesTests/MarkParticipantAsIneligibleTests/MarkParticipantAsIneligibleTests.cs
@@ -32,7 +32,8 @@ public class MarkParticipantAsIneligibleTests : DatabaseTestBaseSetup<MarkPartic
         _handleException.Object,
         _config.Object))
     {
-        Environment.SetEnvironmentVariable("LookupValidationURL", "LookupValidationURL");
+        TestInitialize();
+
         CreateHttpResponseMock();
     }
 
