5457344daeae453b46f80356aafaf3322675b69e
Shilpa-nhs <shilpa.morugali2@nhs.net>
2025-04-23 16:49:02 +0100

    test: epic1 test added

---
+++ tests/playwright-tests/src/tests/e2e/epic1-high.spec.ts
@@ -0,0 +1,24 @@
+import { test } from '@playwright/test';
+import { cleanupDatabaseFromAPI, getTestData, processFileViaStorage, validateSqlDatabaseFromAPI } from '../steps/steps'
+
+
+
+test.describe.parallel('Positive @smoke Tests', () => {
+
+  test.only('06 @smoke @DTOSS-8519 @api Verify file upload into participant table for ADD', async ({ request }, testInfo) => {
+    const [checkInDatabase, nhsNumbers, parquetFile] = await getTestData(testInfo.title);
+
+    await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
+      await cleanupDatabaseFromAPI(request, nhsNumbers);
+    });
+
+    await test.step(`When 2 ADD participants are processed via storage`, async () => {
+      await processFileViaStorage(parquetFile!);
+    });
+
+    await test.step(`Then NHS Numbers should be should be updated in the participant table`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabase);
+    });
+
+  })
+});
