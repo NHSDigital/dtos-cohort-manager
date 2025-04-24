23f4e2a1332bdb099eaa509259722e931b127996
Shilpa-nhs <shilpa.morugali2@nhs.net>
2025-04-24 09:05:59 +0100

    resolved comments

--- tests/playwright-tests/src/tests/e2e/epic1-high.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic1-high.spec.ts
@@ -5,7 +5,7 @@ import { cleanupDatabaseFromAPI, getTestData, processFileViaStorage, validateSql
 
 test.describe.parallel('Positive @smoke Tests', () => {
 
-  test('08 @smoke @DTOSS-8519 @api Verify file upload into participant table for ADD', async ({ request }, testInfo) => {
+  test('08 @smoke @DTOSS-8519 @api Verify file upload into participants table for ADD', async ({ request }, testInfo) => {
     const [checkInDatabase, nhsNumbers, parquetFile] = await getTestData(testInfo.title);
 
     await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
@@ -16,7 +16,7 @@ test.describe.parallel('Positive @smoke Tests', () => {
       await processFileViaStorage(parquetFile!);
     });
 
-    await test.step(`Then NHS Numbers should be should be updated in the participant table`, async () => {
+    await test.step(`Then NHS Numbers should be should be updated in the participants table`, async () => {
       await validateSqlDatabaseFromAPI(request, checkInDatabase);
     });
 
