b3f991ff00a9ed1f6e5acc865027cbfe5c4e9dda
Shilpa-nhs <shilpa.morugali2@nhs.net>
2025-04-25 13:31:33 +0100

    test: DTOSS-7583-Amend record validation test scenario

--- tests/playwright-tests/src/tests/e2e/epic1-highpriority-tests/epic1-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic1-highpriority-tests/epic1-high-priority-testsuite.spec.ts
@@ -26,4 +26,50 @@ test.describe.parallel('Positive @smoke Tests', () => {
     });
 
   })
+
+  test('@DTOSS-8521-01 @regression @e2e @ut @epic1-high-priority @DTOSS-8521 @Verify AMENDED records reach the participant tables', async ({ request }, testInfo) => {
+    const [checkInDatabaseAdd, nhsNumbers, parquetFile, inputParticipantRecord, testFilesPath] = await getTestData(testInfo.title, "ADD", true);
+
+    let runTimeParquetFileAdd: string = "";
+    if (!parquetFile) {
+      runTimeParquetFileAdd = await createParquetFromJson(nhsNumbers, inputParticipantRecord!, testFilesPath!, "ADD", false);
+    }
+
+
+    await test.step(`Given database does not contain record that will be processed`, async () => {
+      await cleanupDatabaseFromAPI(request, nhsNumbers);
+    });
+
+    await test.step(`When ADD participants is processed via storage`, async () => {
+      await processFileViaStorage(runTimeParquetFileAdd);
+    });
+
+    await test.step(`Then ADD record should be updated in the cohort`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabaseAdd);
+    });
+
+
+    const [checkInDatabaseAmend, nhsNumberAmend, parquetFileAmend, inputParticipantRecordAmend, testFilesPathAmend] = await getTestData(testInfo.title, "AMENDED", true);
+
+    let runTimeParquetFileAmend: string = "";
+
+    if (!parquetFileAmend) {
+      runTimeParquetFileAmend = await createParquetFromJson(nhsNumberAmend, inputParticipantRecordAmend!, testFilesPathAmend!, "AMENDED", false);
+    }
+
+
+    await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
+      await processFileViaStorage(runTimeParquetFileAmend!);
+    });
+
+    await test.step(`Then ADD record should be updated in the participants management table`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabaseAmend);
+    });
+
+
+    await test.step(`Then AMENDED record name should be updated in the participants demographic table: ${nhsNumberAmend}`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabaseAmend);
+    });
+  });
+
 });
