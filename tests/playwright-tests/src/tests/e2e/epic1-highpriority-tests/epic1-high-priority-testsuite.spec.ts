f5723ccfa17548eab48ee5506cbae863b745b97e
Abhinav S <abhinav.sharma2@nhs.net>
2025-04-29 18:46:07 +0100

    test: group tests into add, amended and invalid file add workflow

--- tests/playwright-tests/src/tests/e2e/epic1-highpriority-tests/epic1-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic1-highpriority-tests/epic1-high-priority-testsuite.spec.ts
@@ -1,195 +1,151 @@
 import path from 'path';
-import { test, testWithAmended, expect } from '../../fixtures/test-fixtures';
+import { test, testWithAmended } from '../../fixtures/test-fixtures';
 import fs from 'fs';
 import { cleanupDatabaseFromAPI, processFileViaStorage, validateSqlDatabaseFromAPI } from '../../steps/steps';
 
 
-test('@DTOSS-3648-01 @regression @e2e @epic1-high-priority Verify file upload into participants table for ADD', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
+
+
+test.describe('@regression @e2e @epic1-high-priority participant ADD process', () => {
+
+  test.beforeEach(async ({ request, testData }) => {
+    await test.step(`Given database does not contain ADD records that will be processed`, async () => {
+      await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
+    });
+
+    await test.step(`When ADD participants are processed via storage`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFile);
+    });
+
   });
 
-  await test.step(`When 2 ADD participants are processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFile);
+  test('@DTOSS-3648-01 Verify file upload into participants table for ADD', async ({ request, testData }) => {
+
+    await test.step(`Then participant NHS number should be should be updated in both management and demographic`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+
   });
 
-  await test.step(`Then NHS Numbers should be should be updated in the participants table`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-  });
-});
+  test('@DTOSS-3661-01 Confirm NHS Number Count Integrity Across Participant Tables After Processing for ADD record', async ({ request, testData }) => {
 
-testWithAmended('@DTOSS-3217-01 @regression @e2e @epic1-high-priority @Verify AMENDED records reach the participant tables', async ({ request, testData }) => {
+    await test.step(`Then participants records should be 1 in both management and demographic`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
 
-  await test.step(`Given database does not contain record that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
   });
 
-  await test.step(`When ADD participants is processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAdd);
-  });
+  test('@DTOSS-3662-01 Verify ADD records that trigger a non-fatal validation rule reach internal participant tables but not Cohort distribution', async ({ request, testData }) => {
 
-  await test.step(`Then ADD record should be updated in the participants management table`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
-  });
-
-  await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAmend);
-  });
-
-  await test.step(`Then AMENDED record name should be updated in the participants demographic table`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
-  });
-});
-
-test('@DTOSS-7584-01 @DTOSS-3661-01 @regression @e2e @epic1-high-priority Confirm NHS Number Count Integrity Across Participant Tables After Processing for ADD record', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  });
-
-  await test.step(`When 2 ADD participants are processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFile);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-  });
-
-
-});
-
-testWithAmended('@DTOSS-7584-02 @DTOSS-3661-02 @regression @e2e @epic1-high-priority Confirm NHS Number Count Integrity Across Participant Tables After Processing for AMENDED record', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  });
-
-  await test.step(`When 1 ADD participant is processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAdd);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
-  });
-
-  await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAmend);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic and cohort manager record count should be 2`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
-  });
-
-
-});
-
-test('@DTOSS-3662-01 @regression @e2e @epic1-high-priority Verify ADD records that trigger a non-fatal validation rule reach internal participant tables but not Cohort distribution', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  });
-
-  await test.step(`When 2 ADD participants are processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFile);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic
+    await test.step(`Then participants records should be 1 in both management and demographic
                    AND  cohort should have 0 records
                    AND  exception should have 1 record with Invalid primary care provider GP practice code and rule id 36`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+
   });
 
+  test('@DTOSS-3197-01 Verify ADD records is processed without any Exception', async ({ request, testData }) => {
 
-});
-
-testWithAmended('@DTOSS-3662-02 @regression @e2e @epic1-high-priority Verify AMENDED records with non-fatal validation issues reach participant tables with partial Cohort distribution entries', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 1 ADD record that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  });
-
-  await test.step(`When 1 ADD participant is processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAdd);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
-  });
-
-  await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAmend);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic
-                  AND cohort manager record count should be 1
-                  AND exception should have 1 record with Date of birth invalid and rule id 17`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
-  });
-
-
-});
-
-testWithAmended('@DTOSS-3217-02 @regression @e2e @epic1-high-priority Verify AMENDED records is processed without any Exception', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 1 ADD record that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  });
-
-  await test.step(`When 1 ADD participant is processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAdd);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
-  });
-
-  await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFileAmend);
-  });
-
-  await test.step(`Then participants records should be 1 in both management and demographic
-                  AND cohort manager record count should be 2
-                  AND exception should have 0 record`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
-  });
-
-
-});
-
-test('@DTOSS-3197-01 @regression @e2e @epic1-high-priority Verify ADD records is processed without any Exception', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  });
-
-  await test.step(`When 2 ADD participants are processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFile);
-  });
-
-  await test.step(`Then NHS Numbers should be should be updated in the participants table
+    await test.step(`Then NHS Numbers should be should be updated in the participants table
                    AND 1 record in cohort table
                    AND no records in exception table`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-  });
-});
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
 
-test('@DTOSS-3744-01 @regression @e2e @epic1-high-priority Verify a file is uploaded to storage successfully', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 2 ADD records that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
   });
 
-  await test.step(`When 2 ADD participants are processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFile);
-  });
+  test('@DTOSS-3744-01 Verify a file is uploaded to storage successfully', async ({ request, testData }) => {
 
-  await test.step(`Then NHS Numbers should be should be updated in the participants table
+    await test.step(`Then NHS Numbers should be should be updated in the participants table
                    AND content should match`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+
   });
+
+  test('@DTOSS-3660-01 Verify Successful Data Processing and Storage in Cohort Manager for Add', async ({ request, testData }) => {
+
+    await test.step(`Then NHS Numbers should be should be updated in the participants table
+                   AND 1 record in cohort table after data processing
+                   AND no records in exception table`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+
+  });
+
 });
 
-test('@DTOSS-3192-01 @regression @e2e @epic1-high-priority Verify that a file with an invalid name creates a validation exception', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 1 ADD record that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
+test.describe('@regression @e2e @epic1-high-priority participant AMENDED process', () => {
+
+  testWithAmended.beforeEach(async ({ request, testData }) => {
+
+    await test.step(`Given database does not contain record that will be processed`, async () => {
+      await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
+    });
+
+    await test.step(`When ADD participants is processed via storage`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAdd);
+    });
+
+    await test.step(`Then ADD record should be updated in the participants management table`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAmend);
+    });
   });
 
-  await test.step(`When 1 ADD participant is processed via storage`, async () => {
+  testWithAmended('@DTOSS-3217-01 Verify AMENDED records reach the participant tables', async ({ request, testData }) => {
+
+
+    await test.step(`Then AMENDED record name should be updated in the participants demographic table`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+  });
+
+  testWithAmended('@DTOSS-3661-02 Confirm NHS Number Count Integrity Across Participant Tables After Processing for AMENDED record', async ({ request, testData }) => {
+
+    await test.step(`Then participants records should be 1 in both management and demographic and cohort manager record count should be 2`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
+
+  });
+
+
+  testWithAmended('@DTOSS-3662-02 Verify AMENDED records with non-fatal validation issues reach participant tables with partial Cohort distribution entries', async ({ request, testData }) => {
+
+    await test.step(`Then participants records should be 1 in both management and demographic
+                  AND cohort manager record count should be 1
+                  AND exception should have 1 record with Date of birth invalid and rule id 17`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
+
+  });
+
+  testWithAmended('@DTOSS-3217-02 Verify AMENDED records is processed without any Exception', async ({ request, testData }) => {
+
+    await test.step(`Then participants records should be 1 in both management and demographic
+                  AND cohort manager record count should be 2
+                  AND exception should have 0 record`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
+  });
+
+});
+
+test.describe('@regression @e2e @epic1-high-priority invalid file for ADD process', () => {
+  test('@DTOSS-3192-01 Verify that a file with an invalid name creates a validation exception', async ({ request, testData }) => {
+    await test.step(`Given database does not contain 1 ADD record that will be processed`, async () => {
+      await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
+    });
+
     const tempFileName = "Exception_1B8F53_-_CAAS_BREAST_screening_@.parquet";
- const tempDir = path.join(__dirname, 'temp');
+    const tempDir = path.join(__dirname, 'temp');
     if (!fs.existsSync(tempDir)) {
       fs.mkdirSync(tempDir);
     }
@@ -198,30 +154,22 @@ test('@DTOSS-3192-01 @regression @e2e @epic1-high-priority Verify that a file wi
     await processFileViaStorage(tempFilePath);
     fs.unlinkSync(tempFilePath);
     fs.rmdirSync(tempDir);
-  });
-
-  await test.step(`Then exception should have 1 record with The file failed file validation. Check the file Exceptions blob store. and rule id 0`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-  });
 
 
-});
+    await test.step(`Then exception should have 1 record with The file failed file validation. Check the file Exceptions blob store. and rule id 0`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
 
 
-test('@DTOSS-3660-01 @regression @e2e @epic1-high-priority Verify Successful Data Processing and Storage in Cohort Manager for Add', async ({ request, testData }) => {
-  await test.step(`Given database does not contain 1 ADD record that will be processed`, async () => {
-    await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  });
-
-  await test.step(`When 1 ADD participant is processed via storage`, async () => {
-    await processFileViaStorage(testData.runTimeParquetFile);
-  });
-
-  await test.step(`Then NHS Numbers should be should be updated in the participants table
-                   AND 1 record in cohort table
-                   AND no records in exception table`, async () => {
-    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
   });
 });
+
+
+
+
+
+
+
+
 
 
