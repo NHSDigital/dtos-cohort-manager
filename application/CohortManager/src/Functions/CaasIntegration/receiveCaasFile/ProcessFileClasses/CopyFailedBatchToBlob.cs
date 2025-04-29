63ee07cdfcffbe654d9a82225a04f0ebebed9edd
SamRobinson75684 <166817125+SamRobinson75684@users.noreply.github.com>
2025-04-29 13:52:05 +0100

    feat: making sure we log when demographic durable function fails (#725)
    * fix: making sure that exceptons are working in the system
    
    * feat:  DataService Migration - Refactor GetCohortDistributionParticipantsByRequestId Method in CreateCohortDistributionData.cs to use DataServices
    
    * Revert "fix: making sure that exceptons are working in the system"
    
    This reverts commit 74b7106be586cd7fb3de9bb24f75e5ba7316155e.
    
    * feat: now adds failed records to blob storage storage and records to database when the function has failed
    
    * fix: making sure that if for recordsInserted is correct
    
    * chore: addressing sonar cloud issues
    
    * fix: fixing uinit tests for process caas file
    
    * fix: renaming function because of spelling mistakes
    
    * chore: addressing comments
    
    * fix: making sure that InvalidOperationException is thrown correclty
    
    * chore: trying to make the sonar cube happy
    
    * fix: making sure maxNumberOfChecks is correct name
    
    * feat: DTOS 0000 static variable for ReceiveCaasFile added maxNumberOfChecks equals 50
    
    * feat: DTOS 0000 static variable for ReceiveCaasFile added maxNumberOfChecks equals 50 for int and nft
    
    * chore: now gets instance id withouit looping through split string
    
    * chore: addressing comments
    
    * chore: addressing comments
    
    * chore: fixing lint terraform
    
    * fix: fixing unit tests and sorting out the sln file
    
    * fix: fixing unit tests
    
    * fix: restoring shared tests
    
    * fix: adding tests back in
    
    * fix: adding unit tests back
    
    * fix: fixing sln file
    
    * fix: removing http client from program files as it's no longer needed
    
    * chore: adding Ioptions back into CopyFailedBatchToBlob and CallDurableDemographicFunc
    
    * fix: fixing unit tests
    
    * fixing unit tests
    
    * fix: inverting if in durrable function now throws InvalidDataException when demographicJsonData is null or empty
    
    * chore: addressing comments
    
    ---------
    
    Co-authored-by: fahadbaigNC <fahad.baig@nordcloud.com>

---
+++ application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/ProcessFileClasses/CopyFailedBatchToBlob.cs
@@ -0,0 +1,54 @@
+namespace NHS.Screening.ReceiveCaasFile;
+
+using System.Text;
+using System.Text.Json;
+using Common;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Model;
+
+public class CopyFailedBatchToBlob : ICopyFailedBatchToBlob
+{
+    private readonly ILogger<CopyFailedBatchToBlob> _logger;
+    private readonly IBlobStorageHelper _blobStorageHelper;
+    private readonly IExceptionHandler _handleException;
+
+    private readonly ReceiveCaasFileConfig _config;
+
+    public CopyFailedBatchToBlob(ILogger<CopyFailedBatchToBlob> logger, IBlobStorageHelper blobStorageHelper, IExceptionHandler handleException, IOptions<ReceiveCaasFileConfig> config)
+    {
+        _config = config.Value;
+        _logger = logger;
+        _blobStorageHelper = blobStorageHelper;
+        _handleException = handleException;
+    }
+
+    public async Task<bool> writeBatchToBlob(string jsonFromBatch, InvalidOperationException invalidOperationException)
+    {
+        using (var stream = GenerateStreamFromString(jsonFromBatch))
+        {
+            var blobFile = new BlobFile(stream, $"failedBatch-{Guid.NewGuid()}.json");
+            var copied = await _blobStorageHelper.UploadFileToBlobStorage(_config.caasfolder_STORAGE, "failed-batch", blobFile);
+
+            if (copied)
+            {
+                _logger.LogInformation("adding failed batch to blob was unsuccessful");
+                await _handleException.CreateSystemExceptionLog(invalidOperationException, new Participant(), "file name unknown but batch was copied to FailedBatch blob store");
+                return true;
+            }
+            _logger.LogInformation("adding failed batch to blob was successful");
+            return false;
+        }
+    }
+
+    private static Stream GenerateStreamFromString(string s)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(s);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+}
+
