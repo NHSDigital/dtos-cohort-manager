db251c4412d2c18d048fb473ec36e030fa177cb8
Abhinav S <abhinav.sharma2@nhs.net>
2025-05-06 10:45:23 +0100

    test: added a dummy test to demo traceability

---
+++ tests/playwright-tests/src/tests/insprint/delete-participant-from-cohort.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@playwright/test'
+
+test('@DTOSS-7689-01 @insprint @epic4b Test to Verify that the participant should be deleted data from Cohort Manager when that participant has been marked as blocked and there is a request to delete their data by locating their breast screening specific records in cohort distribution using their NHS ID, Date of Birth and last name (3 point check).', async () => {
+  console.info(`Fail this test on purpose to demo traceability of user story to test execution in JIRA`)
+  expect(100).toBe(99);
+});
