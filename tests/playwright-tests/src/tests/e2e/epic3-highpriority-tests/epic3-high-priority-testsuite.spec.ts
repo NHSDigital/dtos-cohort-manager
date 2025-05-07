313dc1ab5c5fb8e8cdf563a6f89fa250c984c521
Abhinav S <abhinav.sharma2@nhs.net>
2025-05-07 15:36:41 +0100

    test: chore use hooks for setup and add test step for validation

--- tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite.spec.ts
@@ -1,29 +1,29 @@
 import { test } from '../../fixtures/test-fixtures';
-import { cleanupDatabaseFromAPI, processFileViaStorage, validateSqlDatabaseFromAPI } from "../../steps/steps";
+import { TestHooks } from '../../hooks/test-hooks';
+import { validateSqlDatabaseFromAPI } from "../../steps/steps";
 
 
-test.only('@DTOSS-6326-01 - Transformation - Invalid Flag triggers Reason for Removal logic - should apply correct transformations when invalidFlag is true', {
-  tag: ['@regression @e2e', '@epic3-high-priority'],
-  annotation: {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-5396',
-  },
-}, async ({ request, testData }) => {
+test.describe('@regression @e2e @epic3-high-priority Tests', () => {
 
-  await cleanupDatabaseFromAPI(request, testData.nhsNumbers);
-  await processFileViaStorage(testData.runTimeParquetFile);
+  TestHooks.setupAddTestHooks();
 
-  let checkInDatabaseRunTime = testData.checkInDatabase;
+  test.only('@DTOSS-6326-01 - Transformation - Invalid Flag triggers Reason for Removal logic - should apply correct transformations when invalidFlag is true', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-5396',
+    },
+  }, async ({ request, testData }) => {
 
-  checkInDatabaseRunTime = checkInDatabaseRunTime.map((record: any) => {
-    if (record.validations.ReasonForRemovalDate ) {
-      record.validations.ReasonForRemovalDate  = new Date().toISOString().split("T")[0] + "T00:00:00";
-    }
-    return record;
+    await test.step('Then Reason for Removal is set to ORR, Primary Care Provider to null, and  Reason for Removal Date to todays date', async () => {
+      let checkInDatabaseRunTime = testData.checkInDatabase;
+      checkInDatabaseRunTime = checkInDatabaseRunTime.map((record: any) => {
+        if (record.validations.ReasonForRemovalDate) {
+          record.validations.ReasonForRemovalDate = new Date().toISOString().split("T")[0] + "T00:00:00";
+        }
+        return record;
+      });
+      await validateSqlDatabaseFromAPI(request, checkInDatabaseRunTime);
+    });
   });
-
-  await validateSqlDatabaseFromAPI(request, checkInDatabaseRunTime);
-
-
 });
 
