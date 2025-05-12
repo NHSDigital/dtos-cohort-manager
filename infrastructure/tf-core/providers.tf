5fac65f95fd95273a60f898a731b42ca268e8311
Richard Kingston <richard.kingston@nordcloud.com>
2025-05-12 09:58:28 +0100

    Update TF provider version for testing

--- infrastructure/tf-core/providers.tf
+++ infrastructure/tf-core/providers.tf
@@ -4,8 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      # version = ">= 4.2.0"
-      version = "= 3.112.0"
+      version = "4.26"
     }
     azuread = {
       source  = "hashicorp/azuread"
