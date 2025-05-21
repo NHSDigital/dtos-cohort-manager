3232ca1c50c3f7045ff141a4b80936029f659307
ANMC20 <andrae.mckenzie1@nhs.net>
2025-05-21 07:34:18 +0100

    removed test.only

--- tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite.spec.ts
@@ -299,11 +299,8 @@ test.describe('@regression @e2e @epic3-high-priority Tests', () => {
     });
 
   });
-});
 
-  });
-
-  test.only('@DTOSS-5348-01  @Orchestration triggered via file upload and cohort record verified', async ({ request, testData }) => {
+  test('@DTOSS-5348-01  @Orchestration triggered via file upload and cohort record verified', async ({ request, testData }) => {
 
     await test.step('When eligible participant data file is uploaded to storage', async () => {
       calledEndpointsOrder.length = 0;
@@ -335,5 +332,4 @@ test.describe('@regression @e2e @epic3-high-priority Tests', () => {
     });
 
   });
-
 });
