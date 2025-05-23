f54cca32a1b3ba5914bb345e87d94aa3c10d7864
Antoine Rubeiz <antoine.rubeiz1@nhs.net>
2025-05-23 15:37:29 +0100

    feat: created separate file for delete participant request body

--- application/CohortManager/src/Functions/CohortDistributionServices/DeleteParticipant/DeleteParticipant.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/DeleteParticipant/DeleteParticipant.cs
@@ -96,9 +96,3 @@ public class DeleteParticipant
     }
 }
 
-public class DeleteParticipantRequestBody
-{
-    public string? NhsNumber { get; set; }
-    public string? FamilyName { get; set; }
-    public DateTime? DateOfBirth { get; set; }
-}
---
+++ application/CohortManager/src/Functions/CohortDistributionServices/DeleteParticipant/DeleteParticipantRequestBody.cs
@@ -0,0 +1,6 @@
+public class DeleteParticipantRequestBody
+{
+    public string? NhsNumber { get; set; }
+    public string? FamilyName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+}
