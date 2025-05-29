5282d354d4e8de9633330a0b996605664eb60210
Richard Kingston <richard.kingston@nordcloud.com>
2025-05-29 15:32:03 +0100

    Add App Service Plan zone redundancy

--- infrastructure/tf-core/app_service_plan.tf
+++ infrastructure/tf-core/app_service_plan.tf
@@ -32,12 +32,12 @@ module "app-service-plan" {
   monitor_diagnostic_setting_appserviceplan_metrics = local.monitor_diagnostic_setting_appserviceplan_metrics
   os_type                                           = lookup(each.value, "os_type", var.app_service_plan.os_type)
   sku_name                                          = lookup(each.value, "sku_name", var.app_service_plan.sku_name)
-  vnet_integration_subnet_id                        = module.subnets["${module.regions_config[each.value.region].names.subnet}-apps"].id
+  vnet_integration_subnet_id                        = var.app_service_plan.vnet_integration_enabled ? module.subnets["${module.regions_config[each.value.region].names.subnet}-apps"].id : null
   wildcard_ssl_cert_name                            = each.value.wildcard_ssl_cert_key
   wildcard_ssl_cert_pfx_blob_key_vault_secret_name  = each.value.wildcard_ssl_cert_key != null ? data.terraform_remote_state.hub.outputs.certificates[each.value.wildcard_ssl_cert_key].key_vault_certificate[each.value.region].pfx_blob_secret_name : null
   wildcard_ssl_cert_pfx_password                    = each.value.wildcard_ssl_cert_key != null ? data.terraform_remote_state.hub.outputs.certificates[each.value.wildcard_ssl_cert_key].key_vault_certificate[each.value.region].pfx_password : null
   wildcard_ssl_cert_key_vault_id                    = each.value.wildcard_ssl_cert_key != null ? data.terraform_remote_state.hub.outputs.key_vault[each.value.region].key_vault_id : null
-  zone_balancing_enabled                            = each.value.zone_balancing_enabled
+  zone_balancing_enabled                            = var.app_service_plan.zone_balancing_enabled
 
   tags = var.tags
 
