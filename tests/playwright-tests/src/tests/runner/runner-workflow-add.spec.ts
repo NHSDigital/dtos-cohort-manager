476159e8f5772f57aefbdd3e23e5ea3f7f0608fc
Abhinav S <abhinav.sharma2@nhs.net>
2025-05-29 10:12:01 +0100

    test: fix request isolation for before all as recommended by playwright

--- tests/playwright-tests/src/tests/runner/runner-workflow-add.spec.ts
+++ tests/playwright-tests/src/tests/runner/runner-workflow-add.spec.ts
@@ -1,28 +1,29 @@
-
-import { test } from '@playwright/test'
+import { test, request as playwrightRequest, APIRequestContext } from '@playwright/test'
 import { cleanupDatabaseFromAPI, getConsolidatedAllTestData, processFileViaStorage, validateSqlDatabaseFromAPI } from '../steps/steps';
 import { createParquetFromJson } from '../../parquet/parquet-multiplier';
 
 const epic123SmokeTests = "@DTOSS-6256-01|@DTOSS-6406-01|@DTOSS-7960-01";
 
+let apiContext: APIRequestContext;
 let addData = getConsolidatedAllTestData(epic123SmokeTests, "ADD");
 
-test.beforeAll(async ({ request }) => {
-  await cleanupDatabaseFromAPI(request, addData.nhsNumbers);
+test.beforeAll(async () => {
+  apiContext = await playwrightRequest.newContext();
+  await cleanupDatabaseFromAPI(apiContext, addData.nhsNumbers);
   const runTimeParquetFile = await createParquetFromJson(addData.nhsNumbers, addData.inputParticipantRecords, addData.testFilesPath, "ADD", false);
   await processFileViaStorage(runTimeParquetFile);
 });
 
-addData.validations.forEach(async (validations) => {
+test.afterAll(async () => {
+  await apiContext.dispose();
+});
 
+addData.validations.forEach((validations) => {
   test(`@runner-workflow-add ${JSON.stringify(validations)}`, {
-    annotation: [{
-      type: 'TestId',
-      description: validations.meta?.testJiraId ?? '',
-    }, {
-      type: 'RequirementId',
-      description: validations.meta?.requirementJiraId ?? '',
-    }],
+    annotation: [
+      { type: 'TestId', description: validations.meta?.testJiraId ?? '' },
+      { type: 'RequirementId', description: validations.meta?.requirementJiraId ?? '' },
+    ],
   }, async ({ request }) => {
     await validateSqlDatabaseFromAPI(request, [validations]);
   });
--- tests/playwright-tests/src/tests/runner/runner-workflow-amend.spec.ts
+++ tests/playwright-tests/src/tests/runner/runner-workflow-amend.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { test, request as playwrightRequest, APIRequestContext } from '@playwright/test'
 import { createParquetFromJson } from '../../parquet/parquet-multiplier';
 import { cleanupDatabaseFromAPI, getConsolidatedAllTestData, processFileViaStorage, validateSqlDatabaseFromAPI } from '../steps/steps';
 
@@ -8,19 +8,25 @@ const epic123SmokeTests = "@DTOSS-6257-01|@DTOSS-6407-01";
 let addData = getConsolidatedAllTestData(epic123SmokeTests, "ADD");
 let amendData = getConsolidatedAllTestData(epic123SmokeTests, "AMENDED");
 
-test.beforeAll(async ({ request }) => {
-  await cleanupDatabaseFromAPI(request, addData.nhsNumbers);
+let apiContext: APIRequestContext;
+
+test.beforeAll(async () => {
+  apiContext = await playwrightRequest.newContext();
+  await cleanupDatabaseFromAPI(apiContext, addData.nhsNumbers);
   const runTimeParquetFile = await createParquetFromJson(addData.nhsNumbers, addData.inputParticipantRecords, addData.testFilesPath, "ADD", false);
   await processFileViaStorage(runTimeParquetFile);
-  await validateSqlDatabaseFromAPI(request, addData.validations)
+  await validateSqlDatabaseFromAPI(apiContext, addData.validations);
 
   const runTimeParquetFileAmend = await createParquetFromJson(amendData.nhsNumbers, amendData.inputParticipantRecords, amendData.testFilesPath, "AMENDED", false);
   await processFileViaStorage(runTimeParquetFileAmend);
+});
 
+test.afterAll(async () => {
+  await apiContext.dispose();
 });
 
 
-amendData.validations.forEach(async (validations) => {
+amendData.validations.forEach((validations) => {
 
   test(`@runner-workflow-amend ${JSON.stringify(validations)}`, {
     annotation: [{
