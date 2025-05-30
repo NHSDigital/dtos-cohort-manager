4d4b357e5c34a6929cfafe21e351a8858a1b6c95
Joseph2910 <184524642+Joseph2910@users.noreply.github.com>
2025-05-30 12:04:50 +0100

    chore: Migrate config.cs

---
+++ application/CohortManager/src/Functions/ParticipantManagementServices/UnblockParticipant/UnblockParticipantConfig.cs
@@ -0,0 +1,13 @@
+namespace NHS.Screening.UnBlockParticipant;
+
+using System.ComponentModel.DataAnnotations;
+
+public class UnblockParticipantConfig
+{
+    [Required]
+    public string ParticipantManagementUrl {get; set;}
+    [Required]
+    public string ParticipantDemographicDataServiceURL {get; set;}
+    [Required]
+    public string ExceptionFunctionURL {get; set;}
+}
