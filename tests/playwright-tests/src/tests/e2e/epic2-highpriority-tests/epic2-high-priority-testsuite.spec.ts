ad75017427fb0966fc8fdebca463507a89ce4203
heenu-heenu <heenu.heenu1@nhs.net>
2025-06-02 16:29:53 +0100

    test: invalid date format for current posting effective date

--- tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
@@ -113,6 +113,17 @@ test.describe('@regression @e2e @epic2-high-priority Tests', () => {
       });
     })
 
+    test('@DTOSS-4328-01 Validate current posting effective date throw exception when invalid date format given for new participants', {
+      annotation: {
+        type: 'Requirement',
+        description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3136',
+      },
+    }, async ({ request, testData }) => {
+      await test.step(`Then Exception table should have RuleId as 101 & RuleDescription as CurrentPostingEffectiveFromDate`, async () => {
+        await validateSqlDatabaseFromAPI(request, testData.checkInDatabase); 
+      });
+    })
+
   }); // End of ADD Tests
 
   test.describe('AMENDED Tests', () => {
