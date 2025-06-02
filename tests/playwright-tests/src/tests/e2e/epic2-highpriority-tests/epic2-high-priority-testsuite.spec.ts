d51f7e0a9f911efbd946c7386e11297b83542b9f
Charan Thotakura <charan.thotakura1@nhs.net>
2025-06-02 13:51:27 +0100

    test: Full test for epic2 high level tests

--- tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
@@ -237,4 +237,24 @@ test.describe('@regression @e2e @epic2-high-priority Tests', () => {
     })
   });
 
+  testWithAmended.only('@DTOSS-5418-01 @Validate_GP_practice_code_empty_and_reason_for_removal_fields_AMENDED_noException', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-2759',
+    },
+  }, async ({ request, testData }) => {
+    await test.step(`Then ADD record should be updated in the cohort`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAmend);
+    });
+
+    await test.step(`Then the record should end up in exception management`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
+  });
+
 });
