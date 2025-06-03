3455da28db4747f9aa4aa6130e02ddf49b10fc7f
abhinavqtnhs <abhinav.sharma2@nhs.net>
2025-06-03 12:31:34 +0100

    test: migrate epic2 high pri tests to runner workflow (#1059)
    * test: chore cross-env package added to allow passing test type during execution and a variable
    
    * test: epic 2 tests migrated to improved runner workflow for add and amend
    
    * test: point default tests to smoke e2e

---
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite-migrated.ts
@@ -0,0 +1,29 @@
+// For Future Epic 2 High Regression Tests
+
+// Note:
+// These tests have been fully migrated to the new and improved test execution orchestration:
+//
+// - Add:
+//   tests/runner/runner-workflow-add
+// - Add followed by Amend:
+//   tests/runner/runner-workflow-amend
+//
+// This approach allows bulk loading of test data for all tests before proceeding with validation, instead of loading test data for each test individually.
+//
+// Guidance:
+//
+// - First, try adding new tests using the runner.
+// - If custom calls are needed, use new file for test addition. - tests\playwright-tests\src\tests\e2e\epic2-highpriority-tests\epic2-high-priority-testsuite.spec.ts
+//
+// Test Tags:
+// There is no impact to the test tags. The same tags can be used to trigger these tests.
+// For example, to run smoke tests, use:
+//
+//   npm test
+//
+// This equates to "@epic2-" tags, configured in the package.json at the playwright-tests root location.
+//
+
+export const runnerBasedEpic2TestScenariosAdd = "@DTOSS-5104-01|@DTOSS-5613-01|@DTOSS-4395-01|@DTOSS-4397-01|@DTOSS-4562-01|@DTOSS-4563-01|@DTOSS-3206-01|@DTOSS-4136-01|@DTOSS-4323-01|@DTOSS-4321-01";
+
+export const runnerBasedEpic2TestScenariosAmend = "@DTOSS-5605-01|@DTOSS-4396-01|@DTOSS-5419-01|@DTOSS-4068-01|@DTOSS-4070-01|@DTOSS-4564-01|@DTOSS-4322-01|@DTOSS-4324-01";
