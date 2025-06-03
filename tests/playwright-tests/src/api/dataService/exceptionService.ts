66f238a26c9f5320970b16184fe31b5efb26f80d
ANMC20 <andrae.mckenzie1@nhs.net>
2025-06-03 16:49:38 +0100

    revert changes to exceptionService

--- tests/playwright-tests/src/api/dataService/exceptionService.ts
+++ tests/playwright-tests/src/api/dataService/exceptionService.ts
@@ -1,16 +1,12 @@
-import { APIRequestContext, expect } from '@playwright/test';
+import { APIRequestContext } from '@playwright/test';
 import * as apiClient from '../apiClient';
 import { config } from '../../config/env';
 import { ApiResponse } from '../core/types';
 
-export const getRecordsFromExceptionService = async (
+export const getRecordsFromExceptionService = (
   request: APIRequestContext
 ): Promise<ApiResponse> => {
-
-  const url = `${config.endpointExceptionManagementDataService}api/${config.exceptionManagementService}`;
-  const response = await apiClient.get(request, url);
-
-  return response;
+  return apiClient.get(request, `${config.endpointExceptionManagementDataService}api/${config.exceptionManagementService}`);
 };
 
 
