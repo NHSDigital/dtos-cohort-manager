87a87ffa4c0b68728900fd8c6d4d4cd47b5d414a
ANMC20 <andrae.mckenzie1@nhs.net>
2025-06-03 15:52:42 +0100

    added Amended participant test

--- tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite.spec.ts
@@ -514,4 +514,52 @@ test.describe('@regression @e2e @epic3-high-priority Tests', () => {
         await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
     });
   });
+
+  test('@DTOSS-5221-01-Preferred Language Validation New Participant', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-5223',
+    },
+  }, async ({ request, testData }) => {
+
+    await test.step('ReceiveCaasFile processes the uploaded participant data file', async () => {
+        await processFileViaStorage(testData.runTimeParquetFile);
+    });
+
+    await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFile);
+
+    await test.step('Then the record should end up in exception management table', async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+    console.log('Validation complete');
+
+  });
+
+  testWithAmended('@DTOSS-4946-01-Preferred Language Amended Participant', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-5222',
+    },
+  }, async ({ request, testData }) => {
+
+    await test.step('ReceiveCaasFile processes the uploaded participant data file', async () => {
+        await processFileViaStorage(testData.runTimeParquetFileAdd);
+    });
+
+    await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
+
+    await test.step(`Then ADD record should be updated in the participants demographic table`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAmend);
+    });
+
+    await test.step(`Then the record should end up in exception management table`, async () => {
+        await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
+  });
+
 });
