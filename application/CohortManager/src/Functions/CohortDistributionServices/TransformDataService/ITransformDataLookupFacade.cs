764248926f873fd500ef041ecb815dc9e4e69b83
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2025-06-04 11:52:11 +0100

    fix: tests

--- application/CohortManager/src/Functions/CohortDistributionServices/TransformDataService/ITransformDataLookupFacade.cs
+++ application/CohortManager/src/Functions/CohortDistributionServices/TransformDataService/ITransformDataLookupFacade.cs
@@ -1,7 +1,9 @@
 namespace NHS.CohortManager.CohortDistribution;
+
 public interface ITransformDataLookupFacade
 {
     bool ValidateOutcode(string postcode);
     string GetBsoCode(string postcode);
     public string GetBsoCodeUsingPCP(string primaryCareProvider);
+    public bool ValidateLanguageCode(string languageCode);
 }
--- tests/UnitTests/ScreeningValidationServiceTests/LookupValidation/DataLookupFacadeBreastScreeningTests.cs
+++ tests/UnitTests/ScreeningValidationServiceTests/LookupValidation/DataLookupFacadeBreastScreeningTests.cs
@@ -13,7 +13,6 @@ public class DataLookupFacadeBreastScreeningTests
     private readonly Mock<ILogger<DataLookupFacadeBreastScreening>> _logger = new();
     private readonly Mock<IDataServiceClient<BsSelectGpPractice>> _gpPracticeServiceClient = new();
     private readonly Mock<IDataServiceClient<BsSelectOutCode>> _outcodeClient = new();
-    private readonly Mock<IDataServiceClient<LanguageCode>> _languageCodeClient = new();
     private readonly Mock<IDataServiceClient<CurrentPosting>> _currentPostingClient = new();
     private readonly Mock<IDataServiceClient<ExcludedSMULookup>> _excludedSMUClient = new();
 
@@ -21,7 +20,7 @@ public class DataLookupFacadeBreastScreeningTests
 
     public DataLookupFacadeBreastScreeningTests()
     {
-        _dataLookupFacade = new DataLookupFacadeBreastScreening(_logger.Object,_gpPracticeServiceClient.Object,_outcodeClient.Object,_languageCodeClient.Object,_currentPostingClient.Object,_excludedSMUClient.Object);
+        _dataLookupFacade = new DataLookupFacadeBreastScreening(_logger.Object,_gpPracticeServiceClient.Object,_outcodeClient.Object,_currentPostingClient.Object,_excludedSMUClient.Object);
     }
 
 
