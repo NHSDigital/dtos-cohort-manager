634f0693774ac3b96d04f25d358dab6bac1fc681
ANMC20 <andrae.mckenzie1@nhs.net>
2025-06-05 13:26:42 +0100

    Add test for invalid GP Practice Code for new participant DTOSS-4103-01

--- tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
@@ -53,11 +53,11 @@ test.describe('@regression @e2e @epic2-high-priority Tests', () => {
   test('@DTOSS-4103-01-Validate invalid GP Practice Code for a new participant', {
       annotation: {
         type: 'Requirement',
-        description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-4102',
+        description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-4103',
       },
     }, async ({ request, testData }) => {
 
-      await test.step(`Then the record should no appear in the participant management table`, async () => {
+      await test.step(`Then the record should appear in the exception table`, async () => {
         await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
       });
   })
