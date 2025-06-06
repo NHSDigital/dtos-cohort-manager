546293b27173fbf0701ab56311d784209df29d28
ANMC20 <andrae.mckenzie1@nhs.net>
2025-06-06 11:12:34 +0100

    test: dtoss 4103 validate invalid gp practice code for a new participant (#1082)
    * feat: add validation tests for invalid GP Practice Code and new participant records
    
    * Add test for invalid GP Practice Code for new participant DTOSS-4103-01

--- tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-highpriority-tests/epic2-high-priority-testsuite.spec.ts
@@ -1,5 +1,5 @@
 import { test, testWithAmended} from '../../fixtures/test-fixtures';
-import { processFileViaStorage, validateSqlDatabaseFromAPI, verifyBlobExists } from '../../steps/steps';
+import { processFileViaStorage, validateSqlDatabaseFromAPI } from '../../steps/steps';
 import { TestHooks } from '../../hooks/test-hooks';
 
 test.describe('@regression @e2e @epic2-high-priority Tests', () => {
@@ -60,7 +60,20 @@ test.describe('@regression @e2e @epic2-high-priority Tests', () => {
       });
     })
 
-  }); // End of ADD Tests
+  });
+
+  test('@DTOSS-4103-01-Validate invalid GP Practice Code for a new participant', {
+      annotation: {
+        type: 'Requirement',
+        description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-4103',
+      },
+    }, async ({ request, testData }) => {
+
+      await test.step(`Then the record should appear in the exception table`, async () => {
+        await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+      });
+  })
+  // End of ADD Tests
 
   testWithAmended('@DTOSS-4329-01 Validation current posting effective date throw exception when invalid date format given for update participants', {
       annotation: {
