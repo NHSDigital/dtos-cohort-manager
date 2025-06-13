631dbe66a5665d68add688a2ed862d107ae34a1d
Visalakshi <visalakshi@mac.home>
2025-06-13 15:33:51 +0100

    added 2 last tests

--- tests/playwright-tests/src/tests/e2e/epic4d-validation-tests/epic4d-6045-validation-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4d-validation-tests/epic4d-6045-validation-testsuite.spec.ts
@@ -5,94 +5,24 @@ import { TestHooks } from '../../hooks/test-hooks';
 test.describe('@regression @e2e @epic4d-validation-tests Tests', () => {
   TestHooks.setupAllTestHooks();
 
-  // test('@DTOSS-9492-01 - Verify participant data meets the 3 conditions to trigger Rule 3602 - AC01- ADD', {
-  //   annotation: {
-  //     type: 'Requirement',
-  //     description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
-  //   },
-  // }, async ({ request, testData }) => {
-
-  //   await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
-  //     await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-  //   });
-  // });
-
-  // testWithAmended('@DTOSS-9493-01 - Verify participant data meets the 3 conditions to trigger Rule 3602 - AC01- AMENDED', {
-  //   annotation: {
-  //     type: 'Requirement',
-  //     description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
-  //   },
-  // }, async ({ request, testData }) => {
-
-  //   await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
-
-  //   await test.step(`Given ADD participants are processed to cohort`, async () => {
-  //     await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
-  //   });
-
-  //   await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
-  //     await processFileViaStorage(testData.runTimeParquetFileAmend);
-  //   });
-
-  //   await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
-  //     await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
-  //   });
-
-  // });
-
-  // test('@DTOSS-9494-01 - Verify that exception is raised if one or more conditions are FALSE (CP - Not English - WGA, PCPC - NOT NULL - ZZZ123) - AC02 - ADD', {
-  //   annotation: {
-  //     type: 'Requirement',
-  //     description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
-  //   },
-  // }, async ({ request, testData }) => {
-
-  //     await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
-  //       await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-  //     });
-  //   });
-
-  //   testWithAmended('@DTOSS-9495-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English - BAA, PCPC - NOT NULL - E85121) - AC02- AMENDED', {
-  //     annotation: {
-  //       type: 'Requirement',
-  //       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
-  //     },
-  //   }, async ({ request, testData }) => {
-
-  //     await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
-
-  //     await test.step(`Given ADD participants are processed to cohort`, async () => {
-  //       await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
-  //     });
-
-  //     await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
-  //       await processFileViaStorage(testData.runTimeParquetFileAmend);
-  //     });
-
-  //     await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
-  //       await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
-  //     });
-
-  //   });
-//  test('@DTOSS-9496-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English, PCPC - B86019 or NULL) - AC02 - ADD', {
-//       annotation: {
-//         type: 'Requirement',
-//         description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
-//       },
-//     }, async ({ request, testData }) => {
-
-//       await test.step(`Then validate database tables`, async () => {
-//         await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-//       });
-//     });
-//   });
-
-  testWithAmended('@DTOSS-9497-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English, PCPC - B86019 or NULL) - AC02 - AMENDED', {
+  test('@DTOSS-9492-01 - Verify participant data meets the 3 conditions to trigger Rule 3602 - AC01- ADD', {
     annotation: {
       type: 'Requirement',
       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
     },
-    }, async ({ request, testData }) => {
+  }, async ({ request, testData }) => {
+
+    await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+  });
+
+  testWithAmended('@DTOSS-9493-01 - Verify participant data meets the 3 conditions to trigger Rule 3602 - AC01- AMENDED', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
+    },
+  }, async ({ request, testData }) => {
 
     await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
 
@@ -110,38 +40,107 @@ test.describe('@regression @e2e @epic4d-validation-tests Tests', () => {
 
   });
 
-  // test('@DTOSS-9498-01 - Verify that exception is raised if one or more conditions are FALSE (CP - NOT English, PCPC - NOT NULL) - AC02 - ADD', {
-  //   annotation: {
-  //     type: 'Requirement',
-  //     description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
-  //   },
-  //  }, async ({ request, testData }) => {
+  test('@DTOSS-9494-01 - Verify that exception is raised if one or more conditions are FALSE (CP - Not English - WGA, PCPC - NOT NULL - TWA123) - AC02 - ADD', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
+    },
+  }, async ({ request, testData }) => {
 
-  //   await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
-  //     await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
-  //   });
+    await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+  });
 
-  // testWithAmended('@DTOSS-9499-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English, PCPC - ZZZ or NULL) - AC02 - AMENDED', {
-  //   annotation: {
-  //     type: 'Requirement',
-  //     description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
-  //   },
-  //   }, async ({ request, testData }) => {
+  testWithAmended('@DTOSS-9495-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English - BAA, PCPC - NOT NULL - E85121) - AC02- AMENDED', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
+    },
+  }, async ({ request, testData }) => {
 
-  //   await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
+    await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
 
-  //   await test.step(`Given 3 ADD participants are processed to cohort`, async () => {
-  //     await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
-  //   });
+    await test.step(`Given ADD participants are processed to cohort`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+    });
 
-  //   await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
-  //     await processFileViaStorage(testData.runTimeParquetFileAmend);
-  //   });
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAmend);
+    });
 
-  //   await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
-  //     await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
-  //   });
+    await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
 
-//    });
+  });
+  test('@DTOSS-9496-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English, PCPC - B86019 or NULL) - AC02 - ADD', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
+    },
+  }, async ({ request, testData }) => {
 
- });
+    await test.step(`Then validate database tables`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+    });
+  });
+});
+
+testWithAmended('@DTOSS-9497-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English, PCPC - B86019 or NULL) - AC02 - AMENDED', {
+  annotation: {
+    type: 'Requirement',
+    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
+  },
+}, async ({ request, testData }) => {
+
+  await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
+
+  await test.step(`Given ADD participants are processed to cohort`, async () => {
+    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+  });
+
+  await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+    await processFileViaStorage(testData.runTimeParquetFileAmend);
+  });
+
+  await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
+    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+  });
+
+});
+
+test('@DTOSS-9496-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English, PCPC - B86019 or NULL) - AC02 - ADD', {
+  annotation: {
+    type: 'Requirement',
+    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
+  },
+}, async ({ request, testData }) => {
+
+  await test.step(`Then validate database tables`, async () => {
+    await validateSqlDatabaseFromAPI(request, testData.checkInDatabase);
+  });
+});
+
+testWithAmended('@DTOSS-9499-01 - Verify that exception is raised if one or more conditions are FALSE (CP - English, PCPC - TWA123 or NULL) - AC02 - AMENDED', {
+  annotation: {
+    type: 'Requirement',
+    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-6045',
+  },
+}, async ({ request, testData }) => {
+
+  await verifyBlobExists('Verify ProcessCaasFile data file', testData.runTimeParquetFileAdd);
+
+  await test.step(`Given ADD participants are processed to cohort`, async () => {
+    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+  });
+
+  await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+    await processFileViaStorage(testData.runTimeParquetFileAmend);
+  });
+
+  await test.step(`Then a manual exception will be raised via the Exception Handling Service`, async () => {
+    await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+  });
+});
+
