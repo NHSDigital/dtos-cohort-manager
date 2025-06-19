af888956d5f0e1d0acd47fa091fd70161a3b7560
Michael Clayson <michael.clayson1@nhs.net>
2025-06-19 10:03:49 +0100

    reduce duplicated code

--- application/CohortManager/src/Functions/screeningDataServices/ReferenceDataService/ReferenceDataService.cs
+++ application/CohortManager/src/Functions/screeningDataServices/ReferenceDataService/ReferenceDataService.cs
@@ -9,6 +9,9 @@ using Common;
 using Microsoft.Azure.Functions.Worker.Http;
 using System.Net;
 
+/// <summary>
+/// Reference Data Service used
+/// </summary>
 public class ReferenceDataService
 {
     private readonly ILogger<ReferenceDataService> _logger;
@@ -41,75 +44,37 @@ public class ReferenceDataService
     [Function("BsSelectGpPractice")]
     public async Task<HttpResponseData> RunBsSelectGpPractice([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "delete", Route = "BsSelectGpPractice/{*key}")] HttpRequestData req, string? key)
     {
-        try
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var result = await _gpPracticeRequestHandler.HandleRequest(req, key);
-            return result;
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error has occurred ");
-            return _createResponse.CreateHttpResponse(HttpStatusCode.InternalServerError, req, $"An error has occurred {ex.Message}");
-        }
+        return await RunHandlerAsync(_gpPracticeRequestHandler.HandleRequest, req, key);
     }
     [Function("BsSelectOutCode")]
     public async Task<HttpResponseData> RunBsSelectOutCode([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "delete", Route = "BsSelectOutCode/{*key}")] HttpRequestData req, string? key)
     {
-        try
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var result = await _outCodeRequestHandler.HandleRequest(req, key);
-            return result;
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error has occurred ");
-            return _createResponse.CreateHttpResponse(HttpStatusCode.InternalServerError, req, $"An error has occurred {ex.Message}");
-        }
+        return await RunHandlerAsync(_outCodeRequestHandler.HandleRequest, req, key);
     }
     [Function("LanguageCode")]
     public async Task<HttpResponseData> RunLanguageCode([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "delete", Route = "LanguageCode/{*key}")] HttpRequestData req, string? key)
     {
-        try
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var result = await _languageCodeRequestHandler.HandleRequest(req, key);
-            return result;
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error has occurred ");
-            return _createResponse.CreateHttpResponse(HttpStatusCode.InternalServerError, req, $"An error has occurred {ex.Message}");
-        }
+        return await RunHandlerAsync(_languageCodeRequestHandler.HandleRequest, req, key);
     }
+
     [Function("CurrentPosting")]
     public async Task<HttpResponseData> RunCurrentPosting([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "delete", Route = "CurrentPosting/{*key}")] HttpRequestData req, string? key)
     {
-        try
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var result = await _currentPostingRequestHandler.HandleRequest(req, key);
-            return result;
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error has occurred ");
-            return _createResponse.CreateHttpResponse(HttpStatusCode.InternalServerError, req, $"An error has occurred {ex.Message}");
-        }
+        return await RunHandlerAsync(_currentPostingRequestHandler.HandleRequest, req, key);
     }
+
     [Function("ExcludedSMULookup")]
     public async Task<HttpResponseData> RunExcludedSMU([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "delete", Route = "ExcludedSMU/{*key}")] HttpRequestData req, string? key)
+    {
+        return await RunHandlerAsync(_excludedSMURequestHandler.HandleRequest, req, key);
+    }
+
+    private async Task<HttpResponseData> RunHandlerAsync(Func<HttpRequestData, string?, Task<HttpResponseData>> handler, HttpRequestData req, string? key)
     {
         try
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var result = await _excludedSMURequestHandler.HandleRequest(req, key);
+            var result = await handler(req, key);
             return result;
         }
         catch (Exception ex)
