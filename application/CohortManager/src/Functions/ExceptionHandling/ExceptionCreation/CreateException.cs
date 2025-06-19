8368d81bca40b51a0cbdc0e50a9a9114c62bc62f
SamRobinson75684 <sam.robinson24@nhs.net>
2025-06-19 16:06:10 +0100

    fix: making sure create exception is in a durrable function
    Signed-off-by: SamRobinson75684 <sam.robinson24@nhs.net>

--- application/CohortManager/src/Functions/ExceptionHandling/ExceptionCreation/CreateException.cs
+++ application/CohortManager/src/Functions/ExceptionHandling/ExceptionCreation/CreateException.cs
@@ -10,17 +10,44 @@ using Model;
 using Common;
 using Data.Database;
 using Azure.Messaging.ServiceBus;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using System.Threading.Tasks;
 
 public class CreateException
 {
     public CreateException() { }
 
-    [Function("CreateException")]
-    public async Task ServiceBusMessageActionsFunction(
-    [ServiceBusTrigger("queue", Connection = "AzureWebJobsStorage", AutoCompleteMessages = false)]
-    ServiceBusReceivedMessage message,
-    ServiceBusMessageActions messageActions)
+
+    [Function(nameof(RunCreateException))]
+    public async Task<bool> RunCreateException(
+        [OrchestrationTrigger] TaskOrchestrationContext context)
     {
+        await context.CallActivityAsync<bool>(nameof(AddException));
+        return true;
+    }
+
+
+    [Function(nameof(AddException))]
+    public bool AddException([ActivityTrigger] FunctionContext executionContext)
+    {
+        return true;
+    }
+
+
+    [Function("RunCreateException")]
+    public async Task Run(
+       [ServiceBusTrigger("queue", Connection = "AzureWebJobsStorage", AutoCompleteMessages = false)]
+        [DurableClient] DurableTaskClient client,
+        FunctionContext executionContext,
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        await client.ScheduleNewOrchestrationInstanceAsync(
+               nameof(CreateException), CancellationToken.None);
+
         await messageActions.CompleteMessageAsync(message);
     }
+
+
 }
