0ea9db4e0d5ef3fecd568752b71480a3958e212a
SamRobinson75684 <sam.robinson24@nhs.net>
2025-06-23 11:36:44 +0100

    fix: removing the durrable ascept of the new create exception function

--- application/CohortManager/src/Functions/ExceptionHandling/ExceptionCreation/CreateException.cs
+++ application/CohortManager/src/Functions/ExceptionHandling/ExceptionCreation/CreateException.cs
@@ -2,42 +2,15 @@ namespace NHS.CohortManager.ExceptionService;
 
 using Microsoft.Azure.Functions.Worker;
 using Azure.Messaging.ServiceBus;
-using Microsoft.DurableTask;
-using Microsoft.DurableTask.Client;
-using System.Threading.Tasks;
 
 public class CreateException
 {
-    public CreateException() { }
-
-
-    [Function(nameof(RunCreateException))]
-    public async Task<bool> RunCreateException(
-        [OrchestrationTrigger] TaskOrchestrationContext context)
-    {
-        await context.CallActivityAsync<bool>(nameof(AddException));
-        return true;
-    }
-
-
-    [Function(nameof(AddException))]
-    public bool AddException([ActivityTrigger] FunctionContext executionContext)
-    {
-        return true;
-    }
-
-
     [Function("RunCreateException")]
     public async Task Run(
        [ServiceBusTrigger(topicName: "%ExceptionTopic%", subscriptionName: "%ExceptionSubscription%", Connection = "ServiceBusConnectionString", AutoCompleteMessages = false)]
-        [DurableClient] DurableTaskClient client,
-        FunctionContext executionContext,
         ServiceBusReceivedMessage message,
         ServiceBusMessageActions messageActions)
     {
-        await client.ScheduleNewOrchestrationInstanceAsync(
-               nameof(CreateException), CancellationToken.None);
-
         await messageActions.CompleteMessageAsync(message);
     }
 
