82c6485c6baa32b66b073b4697058cf2bec62740
SamRobinson75684 <166817125+SamRobinson75684@users.noreply.github.com>
2025-06-24 09:37:42 +0100

    feat: adding simple service bus function for create exception (#1145)
    * feat: migrate to service bus
    
    * feat: service bus migration
    
    * feat: adding simple service buss fucntion for create exception
    
    * feat: new functions
    
    * feat: distribute participant
    
    * feat: queue name switch
    
    * revert changes
    
    * fix: update name
    
    * Update Program.cs
    
    * fix: tests
    
    * fix: tests
    
    * feat: adding new service bus function for create exception
    
    * adding new function to compose
    
    * fix: making sure create exception is in a durrable function
    
    Signed-off-by: SamRobinson75684 <sam.robinson24@nhs.net>
    
    * fix: sonarqube issues
    
    * chore: removing unwanted imports
    
    * fix: adding the use UseNewFunctions to tf vars
    
    * chore: exention does not now use any input params
    
    * fix: calling the FetchDataActivity in the DistributeParticipant function code
    
    * chore: addressing comments
    
    * chore: addressing comments
    
    * fix: remvong required tag on new ServiceBusConnectionString in RCF
    
    * chore: addressing sonar cube isues and code review comments
    
    * fix: fixing unit tests
    
    * fix: adding config values to create-exception
    
    * adressing merge conflicts
    
    * fix: fixing unit tests
    
    * fix: fixing unit tests
    
    * fix: fixing unit tests
    
    * fix: fixing unit tests
    
    * fix: fixing unit tests
    
    * fix: trying to fix unit tests
    
    * fix: trying to fix unit tests
    
    * fix: verifying that Update is never called from the _mockParticipantDemographicClient
    
    * fix: fixing unit tests
    
    * fix: fixing unit tests
    
    * fix: removing the durrable ascept of the new create exception function
    
    * fix: removing merge problem
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * fix: addressing comments
    
    * using the corect dotnet runtimes for create expception
    
    ---------
    
    Signed-off-by: SamRobinson75684 <sam.robinson24@nhs.net>
    Co-authored-by: will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>

--- application/CohortManager/src/Functions/Shared/Common/Extensions/AzureQueueExtension.cs
+++ application/CohortManager/src/Functions/Shared/Common/Extensions/AzureQueueExtension.cs
@@ -5,15 +5,36 @@ using Microsoft.Extensions.Hosting;
 
 public static class AzureQueueExtension
 {
+    /// <summary>
+    /// Extension method for adding azure queue clients, if UseNewFunctions is set to true, it will inject a service bus queue client,
+    /// otherwise, it will inject a azure storage queue client
+    /// </summary>
+    public static IHostBuilder AddAzureQueues(this IHostBuilder hostBuilder, bool UseNewFunctions, string serviceBusConnectionString)
+    {
+        return hostBuilder.ConfigureServices(_ =>
+        {
+            if (UseNewFunctions)
+            {
+                _.AddSingleton<IQueueClient>(_ => new AzureServiceBusClient(serviceBusConnectionString));
+            }
+            else
+            {
+                _.AddTransient<IQueueClient, AzureStorageQueueClient>();
+                _.AddTransient<IQueueClientFactory, QueueClientFactory>();
+            }
+        });
+    }
+
+    /// <summary>
+    /// Overload that creates storage queue clients for instances where only storage queues
+    /// will be used and we do not need control what is injected via config
+    /// </summary>
     public static IHostBuilder AddAzureQueues(this IHostBuilder hostBuilder)
     {
         return hostBuilder.ConfigureServices(_ =>
         {
-            _.AddScoped<IQueueClient>(_ => new AzureServiceBusClient(Environment.GetEnvironmentVariable("ServiceBusConnectionString") ?? ""));
-
             _.AddTransient<IQueueClient, AzureStorageQueueClient>();
             _.AddTransient<IQueueClientFactory, QueueClientFactory>();
         });
     }
-
 }
