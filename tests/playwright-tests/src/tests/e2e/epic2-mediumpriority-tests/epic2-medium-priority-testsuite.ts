70a3d802bff98b8737c423f4d469ff546b1c7d64
charanNHS <charan.thotakura1@nhs.net>
2025-06-25 13:05:53 +0100

    test: dtoss-5399-epic2-medium-tests (#1183)
    * test: Epic2 high tests
    
    * test: removed only tag
    
    * test: created medium pri class for epic2
    
    * test: changed it to false

---
+++ tests/playwright-tests/src/tests/e2e/epic2-mediumpriority-tests/epic2-medium-priority-testsuite.ts
@@ -0,0 +1,11 @@
+// For Future Epic 2 High Regression Tests
+
+
+//
+//   npm run test:regression:e2e:epic2Med
+//
+// This equates to "@epic2-med" tags, configured in the package.json at the playwright-tests root location.
+//
+
+export const runnerBasedEpic2MedTestScenariosAdd = "@DTOSS-5399-01"
+
