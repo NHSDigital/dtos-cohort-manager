9574fd8ff291310d7fb9e4a955257892afff1d4a
sudipnhs <152979070+sudipnhs@users.noreply.github.com>
2025-06-25 17:23:04 +0100

    feat: DTOSS-9663 timer function to update participants received via servicenow (#1194)
    * DTOSS-9663 timer function to update participants received via servicenow
    
    * DTOSS-9663 unit test
    
    * DTOSS-9663 ConsolidatedTests.csproj fix

---
+++ application/CohortManager/src/Functions/screeningDataServices/ServiceNowCasesDataService/ServiceNowCasesDataService.cs
@@ -0,0 +1,41 @@
+namespace ServicenowCasesDataService;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+using Common;
+using DataServices.Core;
+using Model;
+
+public class ServiceNowCasesDataService
+{
+    private readonly ILogger<ServiceNowCasesDataService> _logger;
+    private readonly IRequestHandler<ServicenowCases> _requestHandler;
+    private readonly ICreateResponse _createResponse;
+
+    public ServiceNowCasesDataService(ILogger<ServiceNowCasesDataService> logger, IRequestHandler<ServicenowCases> requestHandler, ICreateResponse createResponse)
+    {
+        _logger = logger;
+        _requestHandler = requestHandler;
+        _createResponse = createResponse;
+    }
+
+    [Function("ServiceNowCasesDataService")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "delete", Route = "ServiceNowCasesDataService/{*key}")] HttpRequestData req, string? key)
+    {
+        try
+        {
+            _logger.LogInformation("DataService Request Received Method: {Method}, DataObject {DataType} ", req.Method, typeof(NemsSubscription));
+            var result = await _requestHandler.HandleRequest(req, key);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error has occurred ");
+            return _createResponse.CreateHttpResponse(HttpStatusCode.InternalServerError, req, $"An error has occurred {ex.Message}");
+        }
+    }
+
+}
+
