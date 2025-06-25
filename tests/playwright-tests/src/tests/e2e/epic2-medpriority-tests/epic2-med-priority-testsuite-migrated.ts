04963c32758531b811395b25d5e96d502510a887
Abhinav S <abhinav.sharma2@nhs.net>
2025-06-25 13:20:38 +0100

    test: merge epic 2 tests into 1 file

--- tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite-migrated.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite-migrated.ts
@@ -24,4 +24,4 @@
 // This equates to "@epic2-med" tags, configured in the package.json at the playwright-tests root location.
 //
 
-export const runnerBasedEpic2MedTestScenariosAdd = "@DTOSS-5111-01";
+export const runnerBasedEpic2MedTestScenariosAdd = "@DTOSS-5399-01|@DTOSS-5111-01";
