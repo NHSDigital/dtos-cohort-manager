63b9bba89a3ed9d052f479d81bb1f0444cd0a7af
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-06-27 15:06:18 +0100

    feat: Included extra properties

--- application/CohortManager/src/Functions/ServiceNowIntegration/ServiceNowMessageHandler/ReceiveServiceNowMessageRequestBody.cs
+++ application/CohortManager/src/Functions/ServiceNowIntegration/ServiceNowMessageHandler/ReceiveServiceNowMessageRequestBody.cs
@@ -3,10 +3,11 @@ namespace NHS.CohortManager.ServiceNowIntegrationService;
 using System.Text.Json.Serialization;
 using Common;
 
+// TODO: Check json property names and update where required once schema has been updated
 public class ReceiveServiceNowMessageRequestBody
 {
     [NotNullOrEmpty]
-    [JsonPropertyName("forename")]
+    [JsonPropertyName("forename_")]
     public required string FirstName { get; set; }
     [NotNullOrEmpty]
     [JsonPropertyName("surname_family_name")]
@@ -16,4 +17,9 @@ public class ReceiveServiceNowMessageRequestBody
     public required string NhsNumber { get; set; }
     [JsonPropertyName("date_of_birth")]
     public required DateOnly DateOfBirth { get; set; }
+    [NotNullOrEmpty]
+    [JsonPropertyName("BSO_code")]
+    public required string BsoCode { get; set; }
+    [JsonPropertyName("required_gp_code")]
+    public string? RequiredGpCode { get; set; }
 }
