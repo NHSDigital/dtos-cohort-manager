844ab387d859d9f865398be1edab8b6d3011ba47
alex-clayton-1 <alexander.clayton1@nhs.net>
2025-06-27 12:33:03 +0100

    revert: Reverted changes to SendServiceNowMessage function, those will be in a separate PR

--- application/CohortManager/src/Functions/ServiceNowIntegration/ServiceNowMessageHandler/ServiceNowRequestModel.cs
+++ application/CohortManager/src/Functions/ServiceNowIntegration/ServiceNowMessageHandler/ServiceNowRequestModel.cs
@@ -1,6 +1,6 @@
 namespace NHS.CohortManager.ServiceNowIntegrationService;
 
-public class SendServiceNowMessageRequestBody
+public class ServiceNowRequestModel
 {
     public required string WorkNotes { get; set; }
     public int State { get; set; }
