f76fb74255ed16296462178e67ab500413ba13a0
Alastair Lock <alastair.lock@gmail.com>
2025-06-30 13:33:32 +0100

    DTOSS-9711: Create Azure Service Health Alerts

--- infrastructure/tf-core/variables.tf
+++ infrastructure/tf-core/variables.tf
@@ -534,3 +534,39 @@ variable "function_app_slots" {
     function_app_slot_enabled = optional(bool, false)
   }))
 }
+
+variable "monitor_action_group" {
+  description = "Default configuration for the monitor action groups."
+  type = map(object({
+    short_name = string
+    email_receiver = optional(map(object({
+      name                    = string
+      email_address           = string
+      use_common_alert_schema = optional(bool, false)
+    })))
+    event_hub_receiver = optional(map(object({
+      name                    = string
+      event_hub_namespace     = string
+      event_hub_name          = string
+      subscription_id         = string
+      use_common_alert_schema = optional(bool, false)
+    })))
+    sms_receiver = optional(map(object({
+      name         = string
+      country_code = string
+      phone_number = string
+    })))
+    voice_receiver = optional(map(object({
+      name         = string
+      country_code = string
+      phone_number = string
+    })))
+    webhook_receiver = optional(map(object({
+      name                    = string
+      service_uri             = string
+      use_common_alert_schema = optional(bool, false)
+    })))
+
+  }))
+}
+
