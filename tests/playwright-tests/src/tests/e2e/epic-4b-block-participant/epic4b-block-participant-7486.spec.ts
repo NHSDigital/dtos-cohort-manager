b5965d12b4c604f00f159fc49251173c30df3c46
Visalakshi <visalakshi.borpatla1@nhs.net>
2025-07-07 20:59:47 +0100

    added tests for ac01

--- tests/playwright-tests/src/tests/e2e/epic-4b-block-participant/epic4b-block-participant-7486.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic-4b-block-participant/epic4b-block-participant-7486.spec.ts
@@ -1,13 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { createParquetFromJson } from '../../../parquet/parquet-multiplier';
 import { getApiTestData, processFileViaStorage, cleanupDatabaseFromAPI, validateSqlDatabaseFromAPI } from '../../steps/steps';
-import { BlockParticipant} from '../../../../api/distributionService/bsSelectService'
-import { getRecordsFromParticipantManagementService } from '../../../../api/distributionService/participantService';
+import {getRecordsFromParticipantManagementService, BlockParticipant} from '../../../api/distributionService/bsSelectService';
 
+test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
 
-test.describe('@regression @e2e @epic4d-validation-tests Tests', async () => {
-
-  test('@DTOSS-7720-01  @epic4d-block-participant - verify records returned matches block request and that NHS ID matches the correct person', async ({ request }, testInfo) => {
+  test('@DTOSS-7720-01  @epic4b-block-participant - AC01 - verify records returned matches block request and that NHS ID matches the correct person when 3 point check in cohort', async ({ request }, testInfo) => {
 
     const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
 
@@ -42,49 +40,52 @@ test.describe('@regression @e2e @epic4d-validation-tests Tests', async () => {
 
   });
 
-  test('@DTOSS-7728-01  @epic4d-block-participant - verify records returned matches block request and that NHS ID matches the correct person', async ({ request }, testInfo) => {
+  // Phase 2 test PDS integration
+  // test('@DTOSS-7728-01  @epic4b-block-participant - AC02 - verify records returned matches block request and that NHS ID matches the correct person', async ({ request }, testInfo) => {
 
-    const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+  //   const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
 
-    await cleanupDatabaseFromAPI(request, nhsNumbers);
+  //   await cleanupDatabaseFromAPI(request, nhsNumbers);
 
-    const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+  //   const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
 
-    await test.step(`When I ADD participant is processed via storage`, async () => {
-      await processFileViaStorage(parquetFile);
-    });
+  //   await test.step(`When I ADD participant is processed via storage`, async () => {
+  //     await processFileViaStorage(parquetFile);
+  //   });
 
-    await test.step(`Then participant should be in the participant management table`, async () => {
-      await validateSqlDatabaseFromAPI(request, checkInDatabase);
-    });
+  //   await test.step(`Then participant should be in the participant management table`, async () => {
+  //     await validateSqlDatabaseFromAPI(request, checkInDatabase);
+  //   });
 
     // Call the block participant function
+    //
     // write if else block to verify if the participant is NOT blocked and then go search RetrievePDSDemographic
-    await test.step(`When BlockParticipant function is invoked`, async () => {
-      const blockPayload = {
-      if{
-        NhsNumber: nhsNumbers[0],
-        FamilyName: inputParticipantRecord[0].family_name,
-        DateOfBirth: inputParticipantRecord[0].date_of_birth
-      };
-        const response = await BlockParticipant(request, blockPayload)
-      }else
-      {
-        NhsNumber: nhsNumbers[1],
-        FamilyName: inputParticipantRecord[1].family_name,
-        DateOfBirth: inputParticipantRecord[1].date_of_birth
-      };
-      const response = await RetrievePDSDemographic(request, blockPayload);
-    })
+  //
+  // await test.step(`When BlockParticipant function is invoked`, async () => {
+  //     const blockPayload = {
+  //     if{
+  //       NhsNumber: nhsNumbers[0],
+  //       FamilyName: inputParticipantRecord[0].family_name,
+  //       DateOfBirth: inputParticipantRecord[0].date_of_birth
+  //     };
+  //       const response = await BlockParticipant(request, blockPayload)
+  //     }else
+  //     {
+  //       NhsNumber: nhsNumbers[1],
+  //       FamilyName: inputParticipantRecord[1].family_name,
+  //       DateOfBirth: inputParticipantRecord[1].date_of_birth
+  //     };
+  //     const response = await RetrievePDSDemographic(request, blockPayload);
+  //   })
 
 
-    // Assert that the participant's blocked flag is set to 1 in participant management table.
-    await test.step('The participant received from the api should have the blocked flag set as 1', async () => {
-      const response = await getRecordsFromRetrievePDSDemographic(request);
-      expect(response.data[0].BlockedFlag).toBe(1);
-    })
+  //   // Assert that the participant's blocked flag is set to 1 in participant management table.
+  //   await test.step('The participant received from the api should have the blocked flag set as 1', async () => {
+  //     const response = await getRecordsFromRetrievePDSDemographic(request);
+  //     expect(response.data[0].BlockedFlag).toBe(1);
+  //   })
 
-  });
+  // });
 
 
 })
