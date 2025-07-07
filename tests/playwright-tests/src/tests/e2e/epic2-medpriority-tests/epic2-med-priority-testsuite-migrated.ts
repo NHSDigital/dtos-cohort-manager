b889a1c157b3a749dea69c69e56868368d2e5b6a
ANMC20 <andrae.mckenzie1@nhs.net>
2025-07-07 10:26:42 +0100

    fix: update epic2 test scenarios in package.json and migrate test scenarios in epic2-med priority tests

--- tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite-migrated.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite-migrated.ts
@@ -24,6 +24,6 @@
 // This equates to "@epic2-med" tags, configured in the package.json at the playwright-tests root location.
 //
 
-
-export const runnerBasedEpic2TestScenariosAmended = "@DTOSS-3629-01"
+export const runnerBasedEpic2MedTestScenariosAdd = "@DTOSS-5399-01|@DTOSS-5210-01|@DTOSS-5111-01";
+export const runnerBasedEpic2TestScenariosAmended = "@DTOSS-3629-01|@DTOSS-4813-01|@DTOSS-4065-01|@DTOSS-4385-01|@DTOSS-4398-01"
 
