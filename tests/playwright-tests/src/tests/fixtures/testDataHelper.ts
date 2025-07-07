1dc35bc39f7333fe945a8678ee48ff945a12d3a6
Abhinav S <abhinav.sharma2@nhs.net>
2025-07-07 11:51:59 +0100

    test: chore allow null, undefined, empty string, or string 'undefined' for negative tests where nhs number is not updated in exception table

--- tests/playwright-tests/src/tests/fixtures/testDataHelper.ts
+++ tests/playwright-tests/src/tests/fixtures/testDataHelper.ts
@@ -1,6 +1,6 @@
-export function ensureNhsNumbersStartWith999(nhsNumbers: (string | null)[]) {
+export function ensureNhsNumbersStartWith999(nhsNumbers: (string | null | undefined)[]) {
   nhsNumbers.forEach((original) => {
-    if (!original) return; // allow null or empty string
+    if (!original || original === '' || original === 'undefined') return; // allow null, undefined, empty string, or string 'undefined' for negative tests where nhs number is not updated
 
     let numeric = original.replace(/\D/g, "");
 
