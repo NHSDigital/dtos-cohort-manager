145de33cff38c874a83b077759b34f808cf89f83
Michael Clayson <michael.clayson1@nhs.net>
2025-07-08 15:13:52 +0100

    test project

---
+++ tests/UnitTests/ReconciliationServiceTests/InboundMetricClientTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using Castle.Core.Logging;
+using Common;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using ReconciliationServiceCore;
+
+namespace ReconciliationServiceTests;
+
+[TestClass]
+public sealed class InboundMetricClientTests
+{
+    private readonly Mock<ILogger<InboundMetricClient>> _mockLogger = new();
+    private readonly Mock<IQueueClient> _mockQueueClient = new();
+    private readonly Mock<IOptions<InboundMetricClientConfig>> _mockConfig = new();
+
+    private readonly InboundMetricClient _inboundMetricClient;
+
+    public InboundMetricClientTests()
+    {
+        _inboundMetricClient = new InboundMetricClient(_mockLogger.Object, _mockQueueClient.Object, _mockConfig.Object);
+
+    }
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
