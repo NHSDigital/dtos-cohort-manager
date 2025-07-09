6d6b4b9e5d93f8888c263e01c2828654008834e8
ANMC20 <andrae.mckenzie1@nhs.net>
2025-07-09 11:06:46 +0100

    refactor block participant test steps for clarity and organization

--- tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
@@ -12,17 +12,22 @@ test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
 
     const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
 
-    await test.step(`When 1 ADD participant is processed via storage`, async () => {
-      const blockPayload = {
-        NhsNumber: nhsNumbers[0],
-        FamilyName: inputParticipantRecord[0].family_name,
-        DateOfBirth: inputParticipantRecord[0].date_of_birth
-      };
+    await test.step(`BlockParticipant`, async () => {
 
-      await processFileViaStorage(parquetFile);
-      const response = await BlockParticipant(request, blockPayload);
-      expect(response.data[0].BlockedFlag).toBe(1);
+        const blockPayload = {
+          NhsNumber: nhsNumbers[0],
+          FamilyName: inputParticipantRecord[0].family_name,
+          DateOfBirth: inputParticipantRecord[0].date_of_birth
+        };
+
+        const response = await BlockParticipant(request, blockPayload);
+        expect(response.data[0].BlockedFlag).toBe(1);
     });
+
+    await test.step(`When 1 ADD participant is processed via storage`, async () => {
+      await processFileViaStorage(parquetFile);
+    });
+
 
     // Assert that the participant is in the participant management table.
      await test.step(`Then participant should be in the participant management table`, async () => {
