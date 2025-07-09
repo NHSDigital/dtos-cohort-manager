2e769c20b0335002df409469c5b00f9d4049a89e
Visalakshi <visalakshi.borpatla1@nhs.net>
2025-07-09 19:49:15 +0100

    created del interface

--- tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
@@ -86,40 +86,40 @@ test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
     });
    });
 
-// test('@DTOSS-7614-01 AC01 Verify block a participant not processed to COHORT - Delete', async ({ request }, testInfo) => {
+testwithDelete('@DTOSS-7614-01 AC01 Verify block a participant not processed to COHORT - Delete', async ({ request }, testInfo) => {
 
-//     const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+    const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
 
-//     const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+    const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
 
-//     await test.step(`When ADD participant is processed via storage`, async () => {
-//       await processFileViaStorage(parquetFile);
-//     });
+    await test.step(`When ADD participant is processed via storage`, async () => {
+      await processFileViaStorage(parquetFile);
+    });
 
-//     await test.step(`Then participant should be in the participant management table`, async () => {
-//       await validateSqlDatabaseFromAPI(request, checkInDatabase);
-//     });
+    await test.step(`Then participant should be in the participant management table`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabase);
+    });
 
-//     // Call the block participant function
-//     await test.step(`When BlockParticipant function is invoked`, async () => {
-//       const blockPayload = {
-//         NhsNumber: nhsNumbers[0],
-//         FamilyName: inputParticipantRecord[0].family_name,
-//         DateOfBirth: inputParticipantRecord[0].date_of_birth
-//       };
+    // Call the block participant function
+    await test.step(`When BlockParticipant function is invoked`, async () => {
+      const blockPayload = {
+        NhsNumber: nhsNumbers[0],
+        FamilyName: inputParticipantRecord[0].family_name,
+        DateOfBirth: inputParticipantRecord[0].date_of_birth
+      };
 
-//       const response = await BlockParticipant(request, blockPayload);
-    //   expect(response.data[0].BlockedFlag).toBe(1);
-    // })
+      const response = await BlockParticipant(request, blockPayload);
+      expect(response.data[0].BlockedFlag).toBe(1);
+    })
 
-    // await test.step(`When Delete participant is processed via storage`, async () => {
-    //   await processFileViaStorage(parquetFile);
-    // });
+    await test.step(`When Delete participant is processed via storage`, async () => {
+      await processFileViaStorage(parquetFile);
+    });
 
-    // await test.step(`Then exception should be in the exception management table`, async () => {
-    //   await validateSqlDatabaseFromAPI(request, checkInDatabase);
-    // });
-  //});
+    await test.step(`Then exception should be in the exception management table`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabase);
+    });
+  });
 });
 
 
