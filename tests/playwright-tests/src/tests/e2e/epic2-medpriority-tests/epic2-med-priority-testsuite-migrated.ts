67491107cf3c517502b5cba92fbfbfe5592e2a79
Abhinav S <abhinav.sharma2@nhs.net>
2025-07-09 11:10:44 +0100

    test: default set to epic2 for all high cases

--- tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite-migrated.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite-migrated.ts
@@ -25,4 +25,4 @@
 //
 
 export const runnerBasedEpic2MedTestScenariosAdd = "@DTOSS-5399-01|@DTOSS-5210-01|@DTOSS-5111-01";
-export const runnerBasedEpic2TestScenariosAmended = "@DTOSS-5404-01"
+export const runnerBasedEpic2TestScenariosAmended = "@DTOSS-4813-01|@DTOSS-4065-01|@DTOSS-4385-01|@DTOSS-4398-01|@DTOSS-4391-01|@DTOSS-4949-01|@DTOSS-5400-01|@DTOSS-5404-01"
