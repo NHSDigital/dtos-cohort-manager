7c9194f6c51fe341e8c44aed83c00a850149ae51
Visalakshi <visalakshi.borpatla1@nhs.net>
2025-07-09 22:26:22 +0100

    fix error for package json to run pipeline

--- tests/playwright-tests/src/tests/e2e/epic4b-block-tests/epic4b-3890-delete-block-participant-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4b-block-tests/epic4b-3890-delete-block-participant-testsuite.spec.ts
@@ -5,7 +5,7 @@ import { composeValidators, expectStatus, validateResponseByStatus } from '../..
 import { deleteParticipant } from '../../../api/distributionService/bsSelectService';
 import { getRecordsFromCohortDistributionService } from '../../../api/dataService/cohortDistributionService';
 
-test.describe.serial('@regression @e2e @epic4b Delete-Block-Participant - CohortDistribution Validation', () => {
+test.describe.serial('@regression @e2e @epic4b-block-tests Delete-Block-Participant - CohortDistribution Validation', () => {
   test('@DTOSS-7689-01 - Verify participant is deleted from CohortDistributionDataService', async ({ request }, testInfo) => {
     const [validations, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title, 'ADD');
 
