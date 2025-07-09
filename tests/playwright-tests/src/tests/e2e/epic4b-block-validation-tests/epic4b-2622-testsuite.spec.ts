66506721e8e0219a18b19ba487b1ef4d778afeb1
Visalakshi <visalakshi.borpatla1@nhs.net>
2025-07-09 14:14:25 +0100

    added delete test and input test file

--- tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
@@ -86,4 +86,48 @@ test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
       await validateSqlDatabaseFromAPI(request, checkInDatabase);
     });
   });
+
+test('@DTOSS-7614-01 AC01 Verify block a participant not processed to COHORT - Delete', async ({ request }, testInfo) => {
+
+    const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+
+    await cleanupDatabaseFromAPI(request, nhsNumbers);
+
+    const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+
+     await test.step(`When ADD participant is processed via storage`, async () => {
+      await processFileViaStorage(parquetFile);
+    });
+
+    await test.step(`Then participant should be in the participant management table`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabase);
+    });
+
+    // Call the block participant function
+    await test.step(`When BlockParticipant function is invoked`, async () => {
+      const blockPayload = {
+        NhsNumber: nhsNumbers[0],
+        FamilyName: inputParticipantRecord[0].family_name,
+        DateOfBirth: inputParticipantRecord[0].date_of_birth
+      };
+
+      const response = await BlockParticipant(request, blockPayload);
+    })
+
+    // Assert that the participant's blocked flag is set to 1 in participant management table.
+    await test.step('The participant received from the api should have the blocked flag set as 1', async () => {
+      const response = await getRecordsFromParticipantManagementService(request);
+      expect(response.data[0].BlockedFlag).toBe(1);
+    })
+
+    await test.step(`When Delete participant is processed via storage`, async () => {
+      await processFileViaStorage(parquetFile);
+    });
+
+    await test.step(`Then exception should be in the exception management table`, async () => {
+      await validateSqlDatabaseFromAPI(request, checkInDatabase);
+    });
+  });
 });
+
+
