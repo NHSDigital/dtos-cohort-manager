e64826b1c3a009d6051414f67978495880774ad2
Visalakshi <visalakshi.borpatla1@nhs.net>
2025-07-09 15:36:40 +0100

    add changes

--- tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4b-block-validation-tests/epic4b-2622-testsuite.spec.ts
@@ -49,12 +49,11 @@ test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
 
   testWithAmended('@DTOSS-7614-01 AC01 Verify block a participant not processed to COHORT - Amend', async ({ request , testData }, testInfo) => {
 
-    const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
-
-    const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+    // const [ inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+    // const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
 
     await test.step(`When ADD participant is processed via storage`, async () => {
-      await processFileViaStorage(parquetFile);
+      await processFileViaStorage(testData.runTimeParquetFileAdd);
     });
 
     await test.step(`Then participant should be in the participant management table`, async () => {
@@ -64,9 +63,13 @@ test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
     // Call the block participant function
     await test.step(`When BlockParticipant function is invoked`, async () => {
       const blockPayload = {
-        NhsNumber: nhsNumbers[0],
-        FamilyName: inputParticipantRecord[0].family_name,
-        DateOfBirth: inputParticipantRecord[0].date_of_birth
+        NhsNumber: testData.nhsNumbers[0],
+        FamilyName: testData.inputParticipantRecord?.[0]?.family_name,
+        DateOfBirth: testData.inputParticipantRecord?.[0]?.date_of_birth
+
+        // NhsNumber: nhsNumbers[0],
+        // FamilyName: inputParticipantRecord[0].family_name,
+        // DateOfBirth: inputParticipantRecord[0].date_of_birth
       };
 
       const response = await BlockParticipant(request, blockPayload);
@@ -74,7 +77,7 @@ test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
     })
 
     await test.step(`When Amend participant is processed via storage`, async () => {
-      await processFileViaStorage(parquetFile);
+      await processFileViaStorage(testData.runTimeParquetFileAmend);
     });
 
     await test.step(`Then exception should be in the exception management table`, async () => {
