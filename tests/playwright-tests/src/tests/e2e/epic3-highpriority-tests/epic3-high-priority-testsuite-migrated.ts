fa51b213b1a091168644f5df74438c21f36bcd15
Visalakshi <visalakshi.borpatla1@nhs.net>
2025-07-10 13:15:01 +0100

    run only updated tests

--- tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite-migrated.ts
+++ tests/playwright-tests/src/tests/e2e/epic3-highpriority-tests/epic3-high-priority-testsuite-migrated.ts
@@ -24,5 +24,7 @@
 // This equates to "@epic3-" tags, configured in the package.json at the playwright-tests root location.
 
 
-export const runnerBasedEpic3TestScenariosAdd = "@DTOSS-5539-01|@DTOSS-5348-01|@DTOSS-9723-01|@DTOSS-9719-01";
-export const runnerBasedEpic3TestScenariosAmend = "@DTOSS-5801-01|@DTOSS-5589-01|@DTOSS-5407-01|@DTOSS-5406-01|@DTOSS-5405-01|@DTOSS-9721-01|@DTOSS-5388-01|@DTOSS-5402-01|@DTOSS-9720-01";
+//export const runnerBasedEpic3TestScenariosAdd = "@DTOSS-5539-01|@DTOSS-5348-01|@DTOSS-9723-01|@DTOSS-9719-01";
+//export const runnerBasedEpic3TestScenariosAmend = "@DTOSS-5801-01|@DTOSS-5589-01|@DTOSS-5407-01|@DTOSS-5406-01|@DTOSS-5405-01|@DTOSS-9721-01|@DTOSS-5388-01|@DTOSS-5402-01|@DTOSS-9720-01";
+export const runnerBasedEpic3TestScenariosAdd = "@DTOSS-9723-01|@DTOSS-9719-01";
+export const runnerBasedEpic3TestScenariosAmend = "|@DTOSS-9721-01|@DTOSS-9720-01";
