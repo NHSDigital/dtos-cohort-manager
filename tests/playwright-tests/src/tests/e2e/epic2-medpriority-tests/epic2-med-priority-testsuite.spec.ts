1d81e8480afdbe7ed21aa9c0b5277ca575d7931e
ANMC20 <andrae.mckenzie1@nhs.net>
2025-07-14 11:21:33 +0100

    test: validations in valid supplied reason for removal not dea (#1280)
    * test: json file
    
    * test: updated test data
    
    * feat: update validation rules for participant records to reflect new reason for removal
    
    * feat: add new test scenario for DTOSS-5107 to the amended scenarios list
    
    * updated
    
    * feat: add test scenario for DTOSS-5107 to validate Reason for Removal processing
    
    * feat: add DTOSS-5107 to amended test scenarios for Epic 2 Med
    
    * fix: update testJiraId for DTOSS-5107 in CAAS Breast Screening Cohort test file
    
    * test: correct test JIRA ID and description
    
    ---------
    
    Co-authored-by: Harmeet <211702083+harmeetRandhawa1@users.noreply.github.com>
    Co-authored-by: Abhinav S <abhinav.sharma2@nhs.net>

--- tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic2-medpriority-tests/epic2-med-priority-testsuite.spec.ts
@@ -40,4 +40,37 @@ test.describe('@regression @e2e @epic2-medium-priority Tests', () => {
 
   });
 
+  testWithTwoAmendments('@DTOSS-5107-01 Tests Reason for Removal is set as SDL ', {
+      annotation: {
+        type: 'Requirement',
+        description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3040',
+      },
+    }, async ({ request, testData }) => {
+      await test.step(`When ADD participant is processed via storage`, async () => {
+        await processFileViaStorage(testData.runTimeParquetFileAdd);
+      });
+
+      await test.step(`Then ADD record should be updated in the cohort`, async () => {
+        await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+      });
+
+      await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+        await processFileViaStorage(testData.runTimeParquetFileAmend);
+      });
+
+      await test.step(`Then the record should end up in cohort distribution table`, async () => {
+        await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+      });
+
+      await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+        await processFileViaStorage(testData.runTimeParquetFileSecondAmend);
+
+      });
+
+      await test.step(`Then the record should end up in cohort distribution table`, async () => {
+        await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseSecondAmend);
+      });
+
+    });
+
 });
