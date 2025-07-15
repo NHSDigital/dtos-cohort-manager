be75aff7c2463ffcc10d829b7cc4e3c5d0c82e19
will-larkin <160748088+will-larkin-nhs@users.noreply.github.com>
2025-07-15 14:44:35 +0100

    feat: remove old functions

--- application/CohortManager/src/Functions/DemographicServices/DemographicDataManagementFunction/DemographicDataManagementFunctionConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace NHS.Screening.DemographicDataManagementFunction;
-
-using System.ComponentModel.DataAnnotations;
-
-public class DemographicDataManagementFunctionConfig
-{
-    [Required]
-    public string ParticipantDemographicDataServiceURL {get; set;}
-}
--- application/CohortManager/src/Functions/Shared/Common/AllocationConfigRequestBody.cs
+++
@@ -1,8 +0,0 @@
-namespace Common;
-public class AllocationConfigRequestBody
-{
-    public string? NhsNumber { get; set; }
-    public string? Postcode { get; set; }
-    public string? ScreeningAcronym { get; set; }
-    public string? ErrorRecord { get; set; }
-}
