2562ec0c8c6608acb23bb53a2dbf602a833b7c5f
Joseph2910 <184524642+Joseph2910@users.noreply.github.com>
2025-07-16 14:06:26 +0100

    chore: Remove unused environement variable

--- application/CohortManager/src/Functions/ParticipantManagementServices/RemoveParticipant/RemoveParticipantConfig.cs
+++ application/CohortManager/src/Functions/ParticipantManagementServices/RemoveParticipant/RemoveParticipantConfig.cs
@@ -5,8 +5,6 @@ using System.ComponentModel.DataAnnotations;
 public class RemoveParticipantConfig
 {
     [Required]
-    public required string UpdateParticipant { get; set; }
-    [Required]
     public required string ExceptionFunctionURL { get; set; }
     [Required]
     public required string ParticipantManagementUrl {get; set;}
