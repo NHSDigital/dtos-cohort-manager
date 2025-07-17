c65b3d8609dfe9bedb2bac9bd1323a884a9d9771
Visalakshi <visalakshi.borpatla1@nhs.net>
2025-07-17 13:24:24 +0100

    fix errors

--- tests/playwright-tests/src/tests/e2e/epic-4b-block-participant/epic4b-block-participant-7486.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic-4b-block-participant/epic4b-block-participant-7486.spec.ts
@@ -30,53 +30,7 @@ test.describe('@regression @e2e @epic4b-block-tests Tests', async () => {
 
 
       };
+    });
   });
+
 });
-
-  // Phase 2 test PDS integration
-  // test('@DTOSS-7728-01  @epic4b-block-participant - AC02 - verify records returned matches block request and that NHS ID matches the correct person', async ({ request }, testInfo) => {
-
-  //   const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
-
-  //   await cleanupDatabaseFromAPI(request, nhsNumbers);
-
-  //   const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
-
-  //   await test.step(`When I ADD participant is processed via storage`, async () => {
-  //     await processFileViaStorage(parquetFile);
-  //   });
-
-  //   await test.step(`Then participant should be in the participant management table`, async () => {
-  //     await validateSqlDatabaseFromAPI(request, checkInDatabase);
-  //   });
-
-  // Call the block participant function
-  //
-  // write if else block to verify if the participant is NOT blocked and then go search RetrievePDSDemographic
-  //
-  // await test.step(`When BlockParticipant function is invoked`, async () => {
-  //     const blockPayload = {
-  //     if{
-  //       NhsNumber: nhsNumbers[0],
-  //       FamilyName: inputParticipantRecord[0].family_name,
-  //       DateOfBirth: inputParticipantRecord[0].date_of_birth
-  //     };
-  //       const response = await BlockParticipant(request, blockPayload)
-  //     }else
-  //     {
-  //       NhsNumber: nhsNumbers[1],
-  //       FamilyName: inputParticipantRecord[1].family_name,
-  //       DateOfBirth: inputParticipantRecord[1].date_of_birth
-  //     };
-  //     const response = await RetrievePDSDemographic(request, blockPayload);
-  //   })
-
-
-  //   // Assert that the participant's blocked flag is set to 1 in participant management table.
-  //   await test.step('The participant received from the api should have the blocked flag set as 1', async () => {
-  //     const response = await getRecordsFromRetrievePDSDemographic(request);
-  //     expect(response.data[0].BlockedFlag).toBe(1);
-  //   })
-
-  // });
-}
