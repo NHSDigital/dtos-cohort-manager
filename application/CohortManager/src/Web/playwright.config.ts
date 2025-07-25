15f5d89be32bfec1470b381398182212bbc76033
Abhinav S <abhinav.sharma2@nhs.net>
2025-07-25 11:38:07 +0100

    test: fix pass ignore options from playwright config

--- application/CohortManager/src/Web/playwright.config.ts
+++ application/CohortManager/src/Web/playwright.config.ts
@@ -47,7 +47,13 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          args: ['--ignore-certificate-errors']
+        }
+      },
+
     },
 
     // {
