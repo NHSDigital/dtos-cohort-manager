d76df14daa16afece7cc82573331e6ec25af72ef
SamRobinson75684 <sam.robinson24@nhs.net>
2025-07-30 08:47:08 +0100

    fix: adding extra logging to the code and trying to properly import private key

--- application/CohortManager/src/Functions/Shared/Common/SingingCreditionalsProvider.cs
+++ application/CohortManager/src/Functions/Shared/Common/SingingCreditionalsProvider.cs
@@ -2,6 +2,7 @@ namespace Common;
 
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
@@ -10,40 +11,57 @@ public class SigningCredentialsProvider : ISigningCredentialsProvider
     private readonly JwtTokenServiceConfig _jwtTokenServiceConfig;
     private readonly JwtPrivateKey _jWTPrivateKey;
 
-    public SigningCredentialsProvider(IOptions<JwtTokenServiceConfig> jwtTokenServiceConfig, JwtPrivateKey jWTPrivateKey)
+    private readonly ILogger<SigningCredentialsProvider> _logger;
+
+    public SigningCredentialsProvider(IOptions<JwtTokenServiceConfig> jwtTokenServiceConfig, JwtPrivateKey jWTPrivateKey, ILogger<SigningCredentialsProvider> logger)
     {
+        _logger = logger;
         _jWTPrivateKey = jWTPrivateKey;
         _jwtTokenServiceConfig = jwtTokenServiceConfig.Value;
     }
 
     public SigningCredentials CreateSigningCredentials()
     {
-        var unescapedPrivateKey = SanitizePrivateKey(_jWTPrivateKey.PrivateKey);
-        var keyBytes = Convert.FromBase64String(unescapedPrivateKey);
-
-        var rsa = RSA.Create();
-        rsa.ImportPkcs8PrivateKey(keyBytes, out _);
-
-        var rsaSecurityKey = new RsaSecurityKey(rsa)
+        try
         {
-            KeyId = _jwtTokenServiceConfig.KId
-        };
+            _logger.LogInformation("attempting to  sanitize private key");
+            var unescapedPrivateKey = SanitizePrivateKey(_jWTPrivateKey.PrivateKey);
+            var keyBytes = Convert.FromBase64String(unescapedPrivateKey);
 
-        return new SigningCredentials(rsaSecurityKey, SecurityAlgorithms.RsaSha512)
+            var rsa = RSA.Create();
+            rsa.ImportPkcs8PrivateKey(keyBytes, out _);
+            _logger.LogInformation("successfully imported private key");
+
+            var rsaSecurityKey = new RsaSecurityKey(rsa)
+            {
+                KeyId = _jwtTokenServiceConfig.KId
+            };
+
+            return new SigningCredentials(rsaSecurityKey, SecurityAlgorithms.RsaSha512)
+            {
+                CryptoProviderFactory = new CryptoProviderFactory { CacheSignatureProviders = false }
+            };
+        }
+        catch (Exception ex)
         {
-            CryptoProviderFactory = new CryptoProviderFactory { CacheSignatureProviders = false }
-        };
+            _logger.LogError(ex, "there was an error while creating signing credentials");
+            throw;
+        }
     }
 
     private static string SanitizePrivateKey(string privateKey)
     {
-        var sb = new StringBuilder(privateKey);
-        sb.Replace("-----BEGIN PRIVATE KEY-----", "");
-        sb.Replace("-----END PRIVATE KEY-----", "");
-        sb.Replace("\t", "");
-        sb.Replace("\n", "");
-        sb.Replace("\r", "");
-        return sb.ToString();
+        if (privateKey.Contains("BEGIN PRIVATE KEY"))
+        {
+            var sb = new StringBuilder(privateKey);
+            sb.Replace("-----BEGIN PRIVATE KEY-----", "");
+            sb.Replace("-----END PRIVATE KEY-----", "");
+            sb.Replace("\t", "");
+            sb.Replace("\n", "");
+            sb.Replace("\r", "");
+            return sb.ToString();
+        }
+        return privateKey;
     }
 
 }
