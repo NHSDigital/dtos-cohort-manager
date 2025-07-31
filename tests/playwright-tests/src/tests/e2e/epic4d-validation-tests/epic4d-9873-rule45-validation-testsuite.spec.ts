ad577f3d67812ef254e632ef5304dd4f4f3bbdaf
Joseph2910 <184524642+Joseph2910@users.noreply.github.com>
2025-07-31 11:19:29 +0100

    test: Add incomplete playwright tests

--- tests/playwright-tests/src/tests/e2e/epic4d-validation-tests/epic4d-9873-rule45-validation-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4d-validation-tests/epic4d-9873-rule45-validation-testsuite.spec.ts
@@ -2,12 +2,12 @@ import { expect, test } from '@playwright/test';
 import { cleanupDatabaseFromAPI, getApiTestData, processFileViaStorage, validateSqlDatabaseFromAPI } from '../../steps/steps';
 import { createParquetFromJson } from '../../../parquet/parquet-multiplier';
 import { config } from "../../../config/env";
-import { getRecordsFromBsSelectRetrieveCohort } from '../../../api/distributionService/bsSelectService';
 import { composeValidators, expectStatus, validateResponseByStatus } from '../../../api/responseValidators';
 import { getRecordsFromExceptionService } from '../../../api/dataService/exceptionService';
+import { getRecordsFromBsSelectRetrieveCohort } from '../../../api/distributionService/bsSelectService';
 
 test.describe.serial(' @e2e @epic4d-validation-tests validate rule 45', async () => {
-    test('@DTOSS-9873-01 200 - @TC1_SIT Verify that a record with a valid GP Practice code does not trigger rule 45', async ({ request }, testInfo) => {
+    test('@DTOSS-10355 - @TC1_SIT Verify that record with an ENG current posting and an invalid GP Practice code triggers rule 45', async ({ request }, testInfo) => {
         const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
         
         await cleanupDatabaseFromAPI(request, nhsNumbers);
@@ -24,42 +24,10 @@ test.describe.serial(' @e2e @epic4d-validation-tests validate rule 45', async ()
         });
         
         
-        await test.step(`And participants received from api should be 1 with status code of 200`, async () => {
-            const ExpectedRowCount = 1;
-        
-            const response = await getRecordsFromBsSelectRetrieveCohort(request, { rowCount: 10 });
-        
-            const genericValidations = composeValidators(
-                expectStatus(200),
-                validateResponseByStatus()
-            );
-            await genericValidations(response);
-            //Extend custom assertions
-            expect(Array.isArray(response.data)).toBeTruthy();
-            expect(response.data.length).toBe(ExpectedRowCount);
-        });
-    });
+        await test.step(`And participants received from exception service api should be 2 with status code of 200`, async () => {
+            // There should be an exception for rule 45 and an exception to say that a participant cannot be added to cohort distribution as there is an exception
+            const ExpectedRowCount = 2; 
 
-    test('@DTOSS-9873-02 - @TC1_SIT Verify that records with an invalid GP Practice code triggers rule 45', async ({ request }, testInfo) => {
-        const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
-        
-        await cleanupDatabaseFromAPI(request, nhsNumbers);
-        
-        const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
-        
-        await test.step(`When 1 ADD participant is processed via storage`, async () => {
-            await processFileViaStorage(parquetFile);
-        });
-        
-        await test.step(`Then participants should be updated in the cohort ready to be picked up`, async () => {
-            await validateSqlDatabaseFromAPI(request, checkInDatabase);
-            await new Promise(resolve => setTimeout(resolve, config.apiWaitTime));
-        });
-        
-        
-        await test.step(`And participants received from exception service api should be 4 with status code of 200`, async () => {
-            const ExpectedRowCount = 4;
-        
             const response = await getRecordsFromExceptionService(request);
         
             const genericValidations = composeValidators(
@@ -72,4 +40,171 @@ test.describe.serial(' @e2e @epic4d-validation-tests validate rule 45', async ()
             expect(response.data.length).toBe(ExpectedRowCount);
         });
     });
+
+    test('@DTOSS-10356 - @TC1_SIT Verify that record with an IM current posting and an invalid GP Practice code triggers rule 45', async ({ request }, testInfo) => {
+        const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+        
+        await cleanupDatabaseFromAPI(request, nhsNumbers);
+        
+        const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+        
+        await test.step(`When 1 ADD participant is processed via storage`, async () => {
+            await processFileViaStorage(parquetFile);
+        });
+        
+        await test.step(`Then participants should be updated in the cohort ready to be picked up`, async () => {
+            await validateSqlDatabaseFromAPI(request, checkInDatabase);
+            await new Promise(resolve => setTimeout(resolve, config.apiWaitTime));
+        });
+        
+        
+        await test.step(`And participants received from exception service api should be 2 with status code of 200`, async () => {
+            // There should be an exception for rule 45 and an exception to say that a participant cannot be added to cohort distribution as there is an exception
+            const ExpectedRowCount = 2; 
+
+            const response = await getRecordsFromExceptionService(request);
+        
+            const genericValidations = composeValidators(
+                expectStatus(200),
+                validateResponseByStatus()
+            );
+            await genericValidations(response);
+            //Extend custom assertions
+            expect(Array.isArray(response.data)).toBeTruthy();
+            expect(response.data.length).toBe(ExpectedRowCount);
+        });
+    });
+
+    test('@DTOSS-10357 - @TC1_SIT Verify that record with a DMS current posting and an invalid GP Practice code triggers rule 45', async ({ request }, testInfo) => {
+        const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+        
+        await cleanupDatabaseFromAPI(request, nhsNumbers);
+        
+        const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+        
+        await test.step(`When 1 ADD participant is processed via storage`, async () => {
+            await processFileViaStorage(parquetFile);
+        });
+        
+        await test.step(`Then participants should be updated in the cohort ready to be picked up`, async () => {
+            await validateSqlDatabaseFromAPI(request, checkInDatabase);
+            await new Promise(resolve => setTimeout(resolve, config.apiWaitTime));
+        });
+        
+        
+        await test.step(`And participants received from exception service api should be 2 with status code of 200`, async () => {
+            // There should be an exception for rule 45 and an exception to say that a participant cannot be added to cohort distribution as there is an exception
+            const ExpectedRowCount = 2; 
+
+            const response = await getRecordsFromExceptionService(request);
+        
+            const genericValidations = composeValidators(
+                expectStatus(200),
+                validateResponseByStatus()
+            );
+            await genericValidations(response);
+            //Extend custom assertions
+            expect(Array.isArray(response.data)).toBeTruthy();
+            expect(response.data.length).toBe(ExpectedRowCount);
+        });
+    });
+
+    test('@DTOSS-10358-01 200 - @TC1_SIT Verify that a record with an ENG current posting and a valid GP Practice code does not trigger rule 45', async ({ request }, testInfo) => {
+        const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+        
+        await cleanupDatabaseFromAPI(request, nhsNumbers);
+        
+        const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+        
+        await test.step(`When 1 ADD participant is processed via storage`, async () => {
+            await processFileViaStorage(parquetFile);
+        });
+        
+        await test.step(`Then participants should be updated in the cohort ready to be picked up`, async () => {
+            await validateSqlDatabaseFromAPI(request, checkInDatabase);
+            await new Promise(resolve => setTimeout(resolve, config.apiWaitTime));
+        });
+        
+        await test.step(`And participants received from api should be 1 with status code of 200`, async () => {
+            const ExpectedRowCount = 1;
+        
+            const response = await getRecordsFromBsSelectRetrieveCohort(request, { rowCount: 1 });
+        
+            const genericValidations = composeValidators(
+                expectStatus(200),
+                validateResponseByStatus()
+            );
+            await genericValidations(response);
+            //Extend custom assertions
+            expect(Array.isArray(response.data)).toBeTruthy();
+            expect(response.data.length).toBe(ExpectedRowCount);
+        });
+    });
+
+    test('@DTOSS-10358-01 200 - @TC1_SIT Verify that a record with a DMS current posting and an excluded GP Practice code does not trigger rule 45', async ({ request }, testInfo) => {
+        const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+        
+        await cleanupDatabaseFromAPI(request, nhsNumbers);
+        
+        const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+        
+        await test.step(`When 1 ADD participant is processed via storage`, async () => {
+            await processFileViaStorage(parquetFile);
+        });
+        
+        await test.step(`Then participants should be updated in the cohort ready to be picked up`, async () => {
+            await validateSqlDatabaseFromAPI(request, checkInDatabase);
+            await new Promise(resolve => setTimeout(resolve, config.apiWaitTime));
+        });
+        
+        await test.step(`And participants received from api should be 1 with status code of 200`, async () => {
+            const ExpectedRowCount = 1;
+        
+            const response = await getRecordsFromBsSelectRetrieveCohort(request, { rowCount: 1 });
+        
+            const genericValidations = composeValidators(
+                expectStatus(200),
+                validateResponseByStatus()
+            );
+            await genericValidations(response);
+            //Extend custom assertions
+            expect(Array.isArray(response.data)).toBeTruthy();
+            expect(response.data.length).toBe(ExpectedRowCount);
+        });
+    });
+
+    test('@DTOSS-10358-01 200 - @TC1_SIT Verify that a record where 2 or more conditions are not met does not trigger rule 45', async ({ request }, testInfo) => {
+        const [checkInDatabase, inputParticipantRecord, nhsNumbers, testFilesPath] = await getApiTestData(testInfo.title);
+        
+        await cleanupDatabaseFromAPI(request, nhsNumbers);
+        
+        const parquetFile = await createParquetFromJson(nhsNumbers, inputParticipantRecord, testFilesPath);
+        
+        await test.step(`When 1 ADD participant is processed via storage`, async () => {
+            await processFileViaStorage(parquetFile);
+        });
+        
+        await test.step(`Then participants should be updated in the cohort ready to be picked up`, async () => {
+            await validateSqlDatabaseFromAPI(request, checkInDatabase);
+            await new Promise(resolve => setTimeout(resolve, config.apiWaitTime));
+        });
+        
+        await test.step(`And participants received from api should be 1 with status code of 200`, async () => {
+            const ExpectedRowCount = 1;
+        
+            const response = await getRecordsFromBsSelectRetrieveCohort(request, { rowCount: 1 });
+        
+            const genericValidations = composeValidators(
+                expectStatus(200),
+                validateResponseByStatus()
+            );
+            await genericValidations(response);
+            //Extend custom assertions
+            expect(Array.isArray(response.data)).toBeTruthy();
+            expect(response.data.length).toBe(ExpectedRowCount);
+        });
+    });
+            
+            
+
 })
