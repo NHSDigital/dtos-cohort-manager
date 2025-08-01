af34958dafe626ad6da15891eb51202bd6aad20f
ekpedeme james <ekpedeme.james1@nhs.net>
2025-08-01 17:07:38 +0100

    regression test failure fix

--- tests/playwright-tests/src/api/apiHelper.ts
+++ tests/playwright-tests/src/api/apiHelper.ts
@@ -270,9 +270,6 @@ async function validateFields(apiValidation: any, matchingObject: any, nhsNumber
       console.info(`Actual RuleDescription: "${actualValue}"`);
 
       // Regex based on message requirement
-      //const dynamicPattern = /Unable to add to cohort distribution\. As participant with ParticipantId: \d+\.\sHas an Exception against it/;
-      //const dynamicPattern = /Unable to add to cohort distribution\. As participant \d+\.\shas triggered a validation exception/;
-      //const dynamicPattern = /^Unable to add to cohort distribution\. As participant (\d+) has triggered a validation exception$/;
       const dynamicPattern = /Unable to add to cohort distribution\. As participant \d+ has triggered a validation exception/;
 
       try {
