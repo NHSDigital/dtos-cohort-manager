22c8b8420130fa060be769079cd32873993aec6a
Michael Clayson <michael.clayson1@nhs.net>
2025-08-06 10:43:26 +0100

    function signature overlap

--- application/CohortManager/src/Functions/Shared/Common/Utilities/HTTPStubUtilities.cs
+++ application/CohortManager/src/Functions/Shared/Common/Utilities/HTTPStubUtilities.cs
@@ -4,26 +4,6 @@ using System.Net;
 
 public static class HTTPStubUtilities
 {
-
-    /// <summary>
-    /// takes in a fake string content and returns 200 OK response
-    /// </summary>
-    /// <param name="url"></param>
-    /// <param name="content"></param>
-    /// <returns></returns>
-    public static HttpResponseMessage CreateFakeHttpResponse(string url, string content = "")
-    {
-        var HttpResponseData = new HttpResponseMessage();
-        if (string.IsNullOrEmpty(url))
-        {
-            HttpResponseData.StatusCode = HttpStatusCode.InternalServerError;
-            return HttpResponseData;
-        }
-
-        HttpResponseData.Content = new StringContent(content);
-        HttpResponseData.StatusCode = HttpStatusCode.OK;
-        return HttpResponseData;
-    }
     /// <summary>
     /// takes in a fake string content and returns 200 OK response or a Given Response
     /// </summary>
