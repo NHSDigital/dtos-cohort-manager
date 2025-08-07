40283cdbe90a9a0d8ea11fbf8b31fa322847dcb3
ekpedeme james <ekpedeme.james1@nhs.net>
2025-08-07 16:51:28 +0100

    updated_DTOSS-5409_to_fix_regression_failure

--- tests/playwright-tests/src/tests/e2e/epic3-medpriority-tests/epic3-med-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic3-medpriority-tests/epic3-med-priority-testsuite.spec.ts
@@ -4,6 +4,8 @@ import { expect, test } from '../../fixtures/test-fixtures';
 import { validateSqlDatabaseFromAPI } from "../../steps/steps";
 import { getRecordsFromCohortDistributionService } from '../../../api/dataService/cohortDistributionService';
 import { TestHooks } from '../../hooks/test-hooks';
+import { testWithAmended, testWithTwoAmendments } from '../../fixtures/test-fixtures';
+import { processFileViaStorage, verifyBlobExists } from "../../steps/steps";
 
 
 test.describe('@regression @e2e @epic3-med-priority Tests', () => {
@@ -75,7 +77,36 @@ test.describe('@regression @e2e @epic3-med-priority Tests', () => {
 
   });
 
+  testWithTwoAmendments('@DTOSS-5409-01 @not-runner-based @bs-select Provide Cohort to BS Select - Transformation_RFR_Rule_4_RDR(AMENDED Twice)', {
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-4577',
+    },
+  }, async ({ request, testData }) => {
+    await test.step(`When ADD participant is processed via storage`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAdd);
+    });
+
+    await test.step(`Then ADD record should be updated in the cohort`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAmend);
+    });
+
+    await test.step(`Then the record should end up in exception management`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileSecondAmend);
+    });
+
+    await test.step(`Then the record should end up in exception management`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseSecondAmend);
+    });
+
+  });
+
 });
-
-
-
