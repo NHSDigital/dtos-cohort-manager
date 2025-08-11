3e58a98d87f68c1cf6877a78d26e02845ee34eb1
Sudip Mondal <sudip.mondal1@nhs.net>
2025-08-11 14:41:54 +0100

    DTOSS-10107 unit test added

--- tests/UnitTests/ScreeningValidationServiceTests/StaticValidation/StaticValidationTests.cs
+++ tests/UnitTests/ScreeningValidationServiceTests/StaticValidation/StaticValidationTests.cs
@@ -210,6 +210,8 @@ public class StaticValidationTests
     [DataRow("ZZ99 9FZ")]
     [DataRow("ZZ999FZ")]
     [DataRow("ZZ99 3WZ")]
+    [DataRow("ZZ99")]
+    [DataRow("ZZZSECUR")]
     public async Task Run_ValidPostcode_ReturnNoContent(string postcode)
     {
         // Arrange
