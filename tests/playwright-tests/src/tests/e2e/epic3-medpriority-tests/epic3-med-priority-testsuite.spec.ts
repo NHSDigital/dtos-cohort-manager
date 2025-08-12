482cab6c1af2e75588a55ce511a5eb5fab791dd5
ekpedemejames <167083739+ekpedemejames@users.noreply.github.com>
2025-08-12 08:41:41 +0100

    test: dtoss-5409 fix rule54 test failure (#1457)
    * updated_DTOSS-5409_to_fix_regression_failure
    
    * set test to run smoke test on pipeline
    
    * updated_test_for_smoke_test
    
    * setting test to run smoke test on pipeline
    
    * test updated to remove smoke test dry run settings

--- tests/playwright-tests/src/tests/e2e/epic3-medpriority-tests/epic3-med-priority-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic3-medpriority-tests/epic3-med-priority-testsuite.spec.ts
@@ -4,9 +4,11 @@ import { expect, test } from '../../fixtures/test-fixtures';
 import { validateSqlDatabaseFromAPI } from "../../steps/steps";
 import { getRecordsFromCohortDistributionService } from '../../../api/dataService/cohortDistributionService';
 import { TestHooks } from '../../hooks/test-hooks';
+import { testWithAmended, testWithTwoAmendments } from '../../fixtures/test-fixtures';
+import { processFileViaStorage, verifyBlobExists } from "../../steps/steps";
 
 
-test.describe('@regression @e2e @epic3-med-priority Tests', () => {
+test.describe('@regression @e2e @epic3-med Tests', () => {
 
   TestHooks.setupAddTestHooks();
   test('@DTOSS-5561-01 @not-runner-based @bs-select - CohortDistribution_Requesting data from Cohort Manager and set record to extracted and add the request ID to the data table', {
@@ -75,7 +77,49 @@ test.describe('@regression @e2e @epic3-med-priority Tests', () => {
 
   });
 
+  testWithTwoAmendments('@DTOSS-5409-01 @not-runner-based @bs-select Provide Cohort to BS Select - Transformation_RFR_Rule_4_RDR', {
+    /*
+    Note: To be able to test Rule4 validation successfully, Rule54 must be prevented by setting IgnoreParticipantException to true.
+    As a workaround for test, the expectation for validation should be as below.
+
+    "RuleId":54,
+    "RuleDescription":"ValidateBsoCode"
+
+    And should be replace with below with the new solution in the AMENDED2 Test File.
+
+    "RuleId":0,
+    "RuleDescription":"4.ParticipantNotRegisteredToGPWithReasonForRemoval"
+    */
+
+    annotation: {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-4577',
+    },
+  }, async ({ request, testData }) => {
+    await test.step(`When ADD participant is processed via storage`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAdd);
+    });
+
+    await test.step(`Then ADD record should be updated in the cohort`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAdd);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileAmend);
+    });
+
+    await test.step(`Then the record should end up in exception management`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileSecondAmend);
+    });
+
+    await test.step(`Then the record should end up in exception management`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseSecondAmend);
+    });
+
+  });
+
 });
-
-
-
