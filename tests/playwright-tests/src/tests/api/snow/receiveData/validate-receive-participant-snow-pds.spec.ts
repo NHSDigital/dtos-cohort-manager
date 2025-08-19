162f7aea1ede347798721cf9e95ab60874a89da8
ekpedeme james <ekpedeme.james1@nhs.net>
2025-08-19 16:53:32 +0100

    updated-test-to-check-response-for-exception

--- tests/playwright-tests/src/tests/api/snow/receiveData/validate-receive-participant-snow-pds.spec.ts
+++ tests/playwright-tests/src/tests/api/snow/receiveData/validate-receive-participant-snow-pds.spec.ts
@@ -1,25 +1,24 @@
 import { test, expect, APIRequestContext } from '@playwright/test';
 import { composeValidators, expectStatus } from '../../../../api/responseValidators';
-import { receiveParticipantViaServiceNow, invalidServiceNowEndpoint, getRecordsFromParticipantManagementService, getRecordsFromParticipantDemographicService, getRecordsFromExceptionManagementService } from '../../../../api/distributionService/bsSelectService';
+import { receiveParticipantViaServiceNow, getRecordsFromParticipantManagementService, getRecordsFromParticipantDemographicService, getRecordsFromExceptionManagementService } from '../../../../api/distributionService/bsSelectService';
 import { loadParticipantPayloads, omitField } from '../../../fixtures/jsonDataReader';
 import { ParticipantRecord } from '../../../../interface/InputData';
-import { ApiResponse } from '../../../../api/core/types';
 import { cleanupDatabaseFromAPI } from '../../../steps/steps';
 
-test.describe.serial('@regression @service_now @api Verify Add Participant data', async () => {
+test.describe.serial('@regression @service_now @api @not-runner-based Verify Add Participant data', async () => {
 
   let participantData: Record<string, ParticipantRecord>;
 
-  const testNumbers = ["9998380197", "9000000009"];
+  const testNumbers = ["9993773360", "9998695511"];
 
-  test.beforeEach(async ({request}) => {
+  test.beforeAll(async ({ request }) => {
+    await cleanupDatabaseFromAPI(request, testNumbers);
     const folderName = '@DTOSS-8375-01';
     const fileName = 'ADD-participantPayload.json';
     participantData = loadParticipantPayloads(folderName, fileName);
-    await cleanupDatabaseFromAPI(request, testNumbers);
   });
 
-  test.only('@DTOSS-10006 Add - Verify Participant Record: Happy Path case of data match', async ({ request }) => {
+  test('@DTOSS-10006 Add - Verify Participant Record: Happy Path case of data match', async ({ request }) => {
     annotation: [{
       type: 'Requirement',
       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-10006',
@@ -28,7 +27,7 @@ test.describe.serial('@regression @service_now @api Verify Add Participant data'
       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8375',
     },]
 
-    await test.step('receiving data from serviceNow', async () => {
+    await test.step('DTOSS-10006 - receiving data from serviceNow', async () => {
 
       const payload = participantData['validParticipantRecordOnPDS'];
       const response = await receiveParticipantViaServiceNow(request, payload);
@@ -39,7 +38,7 @@ test.describe.serial('@regression @service_now @api Verify Add Participant data'
       await validators(response);
     });
 
-    await test.step('Check that data is in Participant Management table', async () => {
+    await test.step('DTOSS-10006 - Check that data is in Participant Management table', async () => {
       const response = await getRecordsFromParticipantManagementService(request);
 
       const validators = composeValidators(
@@ -47,14 +46,14 @@ test.describe.serial('@regression @service_now @api Verify Add Participant data'
       );
       await validators(response);
 
-      expect(response.data[0].NHSNumber).toBe(9998380197);
+      expect(response.data[0].NHSNumber).toBe(9993773360);
       expect(response.data[0].ScreeningId).toBe(1);
       expect(response.data[0].ReferralFlag).toBe(1);
       expect(response.data[0].ReferralFlag).toBe(1);
       expect(response.data[0].EligibilityFlag).toBe(1);
     });
 
-    await test.step('Check that data is in Participant Demographic table', async () => {
+    await test.step('DTOSS-10006 - Check that data is in Participant Demographic table', async () => {
       const response = await getRecordsFromParticipantDemographicService(request);
 
       const validators = composeValidators(
@@ -62,7 +61,7 @@ test.describe.serial('@regression @service_now @api Verify Add Participant data'
       );
       await validators(response);
 
-      expect(response.data[0].NhsNumber).toBe(9998380197);
+      expect(response.data[0].NhsNumber).toBe(9993773360);
       expect(response.data[0].GivenName).toBe("Jane");
       expect(response.data[0].FamilyName).toBe("Doe");
     });
@@ -78,7 +77,7 @@ test.describe.serial('@regression @service_now @api Verify Add Participant data'
       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8375',
     },]
 
-    await test.step('receiving data from serviceNow', async () => {
+    await test.step('DTOSS-10007 - receiving data from serviceNow ', async () => {
 
       const payload = participantData['validParticipantRecordOnPDSUnhappyPath'];
       const response = await receiveParticipantViaServiceNow(request, payload);
@@ -89,18 +88,13 @@ test.describe.serial('@regression @service_now @api Verify Add Participant data'
       await validators(response);
     });
 
-    await test.step('Check that exception is raised in Exception Manegement table', async () => {
+    await test.step('DTOSS-10007 - Check that exception status', async () => {
       const response = await getRecordsFromExceptionManagementService(request);
 
       const validators = composeValidators(
-        expectStatus(200)
+        expectStatus(204)
       );
       await validators(response);
-
-      expect(response.data[0].NhsNumber).toBe("9000000009");
-      expect(response.data[0].RuleId).toBe(-2146233088);
-      expect(response.data[0].RuleDescription).toBe("Participant data from ServiceNow does not match participant data from PDS");
     });
   });
 });
-
