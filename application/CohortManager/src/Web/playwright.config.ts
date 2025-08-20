5d4d7847f4400978ad142174c1f555ce3dc91de5
Adam <adam@chrimes.dev>
2025-08-20 11:33:01 +0100

    test: run the application from the workflow

--- application/CohortManager/src/Web/playwright.config.ts
+++ application/CohortManager/src/Web/playwright.config.ts
@@ -158,11 +158,11 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run dev:secure",
-    url: "https://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-    ignoreHTTPSErrors: true,
-  },
+  // /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: "npm run dev:secure",
+  //   url: "https://localhost:3000",
+  //   reuseExistingServer: !process.env.CI,
+  //   ignoreHTTPSErrors: true,
+  // },
 });
