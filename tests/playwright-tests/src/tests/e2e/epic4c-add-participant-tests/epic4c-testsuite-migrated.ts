7fb933427dbfbd3fafe53c65d39dc2d0d961d7c8
Michael Clayson <michael.clayson1@nhs.net>
2025-08-21 14:19:55 +0100

    test: amend workflow and example (#1527)
    amend workflow and example

--- tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-testsuite-migrated.ts
+++ tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-testsuite-migrated.ts
@@ -28,4 +28,7 @@
 
 export const runnerBasedEpic4cTestScenariosAdd = "@DTOSS-9337-01";
 export const runnerBasedEpic4cTestScenariosAmend = "@DTOSS-9337-01";
+// export const runnerBasedEpic4cTestScenariosManualAdd = "";
 export const runnerBasedEpic4cTestScenariosManualAdd = "@DTOSS-3883-01";
+export const runnerBasedEpic4cTestScenariosManualAmend = "@DTOSS-3883-01";
+//export const runnerBasedEpic4cTestScenariosManualAmend = "";
