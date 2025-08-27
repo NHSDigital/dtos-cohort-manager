ad2fd4647663742d96ce9296161e65acbc49f3e9
Sam Ainsworth <samuel.ainsworth1@nhs.net>
2025-08-27 17:09:56 +0100

    fix: test titles @DTOSS-8569-xx + ticket names - merged together test cases to match existing tests

--- tests/playwright-tests/src/tests/api/snow/system-error/dtoss-10026-system-error.spec.ts
+++ tests/playwright-tests/src/tests/api/snow/system-error/dtoss-10026-system-error.spec.ts
@@ -1,7 +1,8 @@
 import { test } from '@playwright/test';
-import { composeValidators, expectStatus } from '../../../../api/responseValidators';
-import { invalidServiceNowEndpoint } from '../../../../api/distributionService/bsSelectService';
+import { composeValidators, expectStatus, validateResponseByStatus } from '../../../../api/responseValidators';
+import { receiveParticipantViaServiceNow } from '../../../../api/distributionService/bsSelectService';
 import { loadParticipantPayloads } from '../../../fixtures/jsonDataReader';
+import { validateSqlDatabaseFromAPI } from '../../../steps/steps';
 
 test.describe('@regression @service_now @api DTOSS-10026 system error pathway', () => {
   let participantData: Record<string, any>;
@@ -12,10 +13,29 @@ test.describe('@regression @service_now @api DTOSS-10026 system error pathway', 
     participantData = loadParticipantPayloads(folderName, fileName);
   });
 
-  test('@DTOSS-10026-01 System error when ServiceNow endpoint invalid', async ({ request }) => {
-    const payload = participantData['validParticipantRecord-vhr'];
-    const response = await invalidServiceNowEndpoint(request, payload);
-    const validators = composeValidators(expectStatus(404));
+  test('@DTOSS-10026-01 Exception is raised when record fails due to system failure (PDS error)', async ({ request }) => {
+    const payload = participantData['validParticipantRecord-ceasing'];
+
+    const response = await receiveParticipantViaServiceNow(request, payload);
+    const validators = composeValidators(expectStatus(202), validateResponseByStatus());
     await validators(response);
+
+    const nhsNumber = payload.u_case_variable_data.nhs_number;
+    const validations = [
+      {
+        validations: {
+          apiEndpoint: 'api/ExceptionManagementDataService',
+          NhsNumber: String(nhsNumber),
+          expectedCount: 1,
+        },
+        meta: {
+          testJiraId: '@DTOSS-10026-01',
+          requirementJiraId: 'DTOSS-8569',
+          additionalTags: '@regression @service_now @api Exception path: exception raised',
+        },
+      },
+    ];
+
+    await validateSqlDatabaseFromAPI(request, validations);
   });
 });
