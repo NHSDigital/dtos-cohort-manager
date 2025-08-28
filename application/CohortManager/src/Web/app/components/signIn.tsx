71ccb3553839ebeb7a772a65732c886834781b2f
Adam <adam@chrimes.dev>
2025-08-28 19:56:45 +0100

    fix: update start page content and remove redundant elements

--- application/CohortManager/src/Web/app/components/signIn.tsx
+++ application/CohortManager/src/Web/app/components/signIn.tsx
@@ -17,14 +17,22 @@ export default function SignIn({
     <main className="nhsuk-main-wrapper" id="maincontent" role="main">
       <div className="nhsuk-grid-row">
         <div className="nhsuk-grid-column-two-thirds">
-          <HeadingTag className="nhsuk-heading-xl">
-            Log in with your Care Identity account
-          </HeadingTag>
+          <HeadingTag className="nhsuk-heading-xl">{serviceName}</HeadingTag>
+          <p>For use with the NHS breast screening programme.</p>
+          <p>You can use this service to:</p>
+          <ul>
+            <li>
+              view a summary of outstanding exceptions occurring in participant
+              data
+            </li>
+            <li>
+              access information to raise a service request for the data to be
+              corrected
+            </li>
+          </ul>
 
-          <p>
-            Setup your account for {serviceName} using your Care Identity
-            account.
-          </p>
+          <h2>Before you start</h2>
+          <p>We&apos;ll ask you to log in with your Care Identity account.</p>
 
           <p>
             If you are not currently using a computer with a smartcard reader
@@ -63,10 +71,7 @@ export default function SignIn({
                   d="M80 0v32H0V0h80ZM69 2.2c-5.8 0-11.6 2-11.6 8.8 0 7.4 10.2 5.8 10.2 10 0 2.6-3.4 3-5.6 3-2.2 0-5-.6-6.4-1.4L54 28c2.2.8 5.4 1.4 8 1.4 6.2 0 12.8-1.8 12.8-9 0-7.8-10.2-6.6-10.2-10.2 0-2.2 2.2-2.6 5-2.6 2.6 0 4.4.6 5.8 1.2L77 3.4c-1.8-.8-4.8-1.2-8-1.2ZM16.6 3H7.8L2.2 29h6.6l3.6-18h.2L18 29h8.6l5.6-26h-6.6L22 21h-.2L16.6 3Zm25.2 0h-7.2l-5.2 26h6.8l2.4-11.2h8.2L44.6 29h7L57 3h-7l-2.2 9.8h-8l2-9.8Z"
                 ></path>
               </svg>
-
-              <div>
-                <span>Log in with my</span> Care Identity
-              </div>
+              Log in with my Care Identity
             </button>
           </form>
           {process.env.APP_ENV === "development" && (
