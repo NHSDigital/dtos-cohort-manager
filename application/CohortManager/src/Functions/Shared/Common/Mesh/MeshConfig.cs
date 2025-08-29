a098fb7c0036e920c20ae63a40eec79ee1a57987
Michael Clayson <michael.clayson1@nhs.net>
2025-08-29 13:04:28 +0100

    mesh mailbox extension

---
+++ application/CohortManager/src/Functions/Shared/Common/Mesh/MeshConfig.cs
@@ -0,0 +1,20 @@
+namespace Common;
+
+public class MeshConfig
+{
+    public string? KeyVaultConnectionString { get; set; }
+    public string? MeshCACertName { get; set; }
+    public bool BypassServerCertificateValidation { get; set; } = false;
+    public required string MeshApiBaseUrl { get; set; }
+    public required List<MailboxConfig> MailboxConfigs { get; set; }
+}
+
+public class MailboxConfig
+{
+    public required string MailboxId { get; set; }
+    public string? MeshKeyName { get; set; }
+    public string? MeshKeyPassword { get; set; }
+    public required string MeshPassword { get; set; }
+    public required string SharedKey { get; set; }
+
+}
