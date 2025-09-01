ae83968cf7f7730015a83e9067281062a4a075fc
ekpedeme james <ekpedeme.james1@nhs.net>
2025-09-01 15:29:15 +0100

    initial-test-added

--- tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-testsuite.spec.ts
@@ -5,7 +5,7 @@ import { getLatestValidDatefromDatabase } from "../../steps/steps";
 
 test.describe('@regression @e2e @epic4c- Tests', () => {
 
-  test('DTOSS-10011 @not-runner-based - Verify subscribtion IDs on Nems table', {
+  test.only('DTOSS-10011 @not-runner-based - Verify subscribtion IDs on Nems table', {
     annotation: [{
       type: 'Requirement - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3881',
       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-10011',
@@ -13,19 +13,12 @@ test.describe('@regression @e2e @epic4c- Tests', () => {
   }, async ({ request }, testInfo) => {
 
 
-    await test.step(`Check latest ID by date`, async () => {
+    await test.step(`Then NEMS_SUBSCRIPTION should have subscriber ID`, async () => {
 
-      const genericValidations = composeValidators(
-        expectStatus(200),
-        validateResponseByStatus()
-      );
-
-      await test.step('Then NEMS_SUBSCRIPTION should have subscriber ID', async () => {
         const response = await getRecordsFromNemsSubscription(request);
         console.log(response);
         //const lastRecord = response.data[response.data.length - 1];
         //expect(lastRecord?.StatusCode).toBe("200");
-      });
     });
   });
 });
