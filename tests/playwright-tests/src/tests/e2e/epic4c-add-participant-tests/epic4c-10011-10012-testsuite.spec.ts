37dea7d635b48bcae288bb449f2a01c54add71b9
ekpedeme james <ekpedeme.james1@nhs.net>
2025-09-05 17:15:06 +0100

    fixed-test-failure

--- tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-10011-10012-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-10011-10012-testsuite.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '../../fixtures/test-fixtures';
-import { extractSubscriptionID, getRecordsFromNemsSubscription, getRecordsFromParticipantDemographicService, getRecordsFromParticipantManagementService, receiveParticipantViaServiceNow, retry } from "../../../api/distributionService/bsSelectService";
+import { extractSubscriptionID, getRecordsFromNemsSubscription, getRecordsFromParticipantDemographicDataService, getRecordsFromParticipantManagementDataService, receiveParticipantViaServiceNow } from "../../../api/distributionService/bsSelectService";
 import { composeValidators, expectStatus } from "../../../api/responseValidators";
 import { ParticipantRecord } from '../../../interface/InputData';
 import { loadParticipantPayloads } from '../../fixtures/jsonDataReader';
@@ -14,16 +14,20 @@ test.describe.serial('@DTOSS-3881-01 @e2e @epic4c- Cohort Manger subscribed the 
     participantData = await loadParticipantPayloads(folderName, fileName);
   });
 
-  test('@DTOSS-3881-01 DTOSS-10011 @not-runner-based - Verify subscription IDs on Nems table for ADD', {
-    annotation: [{
+  test('@DTOSS-3881-01 DTOSS-10011 @not-runner-based - Verify subscription IDs on Nems table for ADD', async ({ request }, testInfo) => {
+    testInfo.annotations.push({
       type: 'Requirement - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3881',
       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-10011',
-    }]
-  }, async ({ request }, testInfo) => {
+    });
+
+    const payload = participantData['inputParticipantRecord'];
+    const nhsNumber = participantData['inputParticipantRecord'].u_case_variable_data.nhs_number;
+    const givenName = participantData['inputParticipantRecord'].u_case_variable_data.forename_;
+    const familyName = participantData['inputParticipantRecord'].u_case_variable_data.surname_family_name;
 
     await test.step('Given Cohort manager receives data from ServiceNow and subscribed to PDS', async () => {
-      const payload = participantData['inputParticipantRecord'];
       const response = await receiveParticipantViaServiceNow(request, payload);
+      await new Promise(res => setTimeout(res, 2000));
       const validators = composeValidators(
         expectStatus(202)
       );
@@ -31,50 +35,36 @@ test.describe.serial('@DTOSS-3881-01 @e2e @epic4c- Cohort Manger subscribed the 
     });
 
     await test.step('Then NHSNumber, GivenName, FamilyName is written to Participant Demographic table', async () => {
-      const response = await retry(
-        () => getRecordsFromParticipantDemographicService(request),
-        (res) =>
-          res?.data?.[0]?.NhsNumber === 9997160908 &&
-          res?.data?.[0]?.GivenName === "Jane" &&
-          res?.data?.[0]?.FamilyName === "Doe",
-        { retries: 8, delayMs: 5000 }
+      const response = await getRecordsFromParticipantDemographicDataService(request);
+      await new Promise(res => setTimeout(res, 2000));
+      const validators = composeValidators(
+        expectStatus(200)
       );
+      await validators(response);
+
       expect(response?.data?.[0]?.NhsNumber).toBe(9997160908);
-      expect(response?.data?.[0]?.GivenName).toBe("Jane");
-      expect(response?.data?.[0]?.FamilyName).toBe("Doe");
+      expect(response?.data?.[0]?.GivenName).toBe(givenName);
+      expect(response?.data?.[0]?.FamilyName).toBe(familyName);
     });
 
     await test.step('And NHSNumber, ScreeningId, ReferralFlag is written to Participant Management table', async () => {
-      const response = await retry(
-        () => getRecordsFromParticipantManagementService(request),
-        (res) =>
-          res?.data?.[0]?.NHSNumber === 9997160908 &&
-          res?.data?.[0]?.ScreeningId === 1 &&
-          res?.data?.[0]?.ReferralFlag === 1,
-        { retries: 8, delayMs: 5000 }
-      );
+      const response = await getRecordsFromParticipantManagementDataService(request);
+      await new Promise(res => setTimeout(res, 2000));
       expect(response?.data?.[0]?.NHSNumber).toBe(9997160908);
       expect(response?.data?.[0]?.ScreeningId).toBe(1);
       expect(response?.data?.[0]?.ReferralFlag).toBe(1);
     });
 
     await test.step('DTOSS-10012 DTOSS-10012 verify NemsSubscription_Id in NEMS_SUBSCRIPTION table', async () => {
-      const nhsNumber = participantData['inputParticipantRecord'].u_case_variable_data.nhs_number;
 
-      const response = await retry(
-        async () => {
-          const res = await getRecordsFromNemsSubscription(request, nhsNumber);
-          const validators = composeValidators(expectStatus(200));
-          await validators(res);
+      const response = await getRecordsFromNemsSubscription(request, nhsNumber);
+      await new Promise(res => setTimeout(res, 2000));
 
-          return res;
-        },
-        (res) => {
-          const subscriptionID = extractSubscriptionID(res);
-          return subscriptionID !== null;
-        },
-        { retries: 8, delayMs: 5000 }
+      const validators = composeValidators(
+        expectStatus(200)
       );
+      await validators(response);
+
       const subscriptionID = extractSubscriptionID(response);
       expect(subscriptionID).not.toBeNull();
       console.log(`Extracted Subscription ID: ${subscriptionID} for number: ${nhsNumber}`);
