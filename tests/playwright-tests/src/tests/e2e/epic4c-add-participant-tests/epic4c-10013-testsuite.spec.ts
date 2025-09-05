a5597e29e82d4659400a91ccf0da44564e775fa4
ekpedeme james <ekpedeme.james1@nhs.net>
2025-09-05 08:11:31 +0100

    add-spef-file-for-10013

--- tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-10013-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-10013-testsuite.spec.ts
@@ -3,9 +3,11 @@ import { extractSubscriptionID, getRecordsFromNemsSubscription, getRecordsFromPa
 import { composeValidators, expectStatus } from "../../../api/responseValidators";
 import { ParticipantRecord } from '../../../interface/InputData';
 import { loadParticipantPayloads } from '../../fixtures/jsonDataReader';
+import { processFileViaStorage, validateSqlDatabaseFromAPI } from '../../steps/steps';
+import { TestHooks } from '../../hooks/test-hooks';
 
-test.describe.serial('@DTOSS-3881-01 @e2e @epic4c- Cohort Manger subscribed the Added record with PDS', () => {
-
+test.describe.serial.only('@DTOSS-3881-01 @e2e @epic4c- Verify Cohort Manager has received data amendments from PDS', () => {
+  TestHooks.setupAllTestHooks();
   let participantData: Record<string, ParticipantRecord>;
 
   test.beforeAll(async () => {
@@ -14,15 +16,15 @@ test.describe.serial('@DTOSS-3881-01 @e2e @epic4c- Cohort Manger subscribed the 
     participantData = await loadParticipantPayloads(folderName, fileName);
   });
 
-  test('@DTOSS-3881-01 DTOSS-10011 @not-runner-based - Verify subscription IDs on Nems table for ADD', {
+  test('@DTOSS-3881-01 DTOSS-10013 @not-runner-based - Verify subscription IDs on Nems table for ADD', {
     annotation: [{
       type: 'Requirement - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3881',
-      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-10011',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-10013',
     }]
   }, async ({ request }, testInfo) => {
 
     await test.step('Given Cohort manager receives data from ServiceNow and subscribed to PDS', async () => {
-      const payload = participantData['inputParticipantRecord'];
+      const payload = participantData['inputParticipantRecord10013'];
       const response = await receiveParticipantViaServiceNow(request, payload);
       const validators = composeValidators(
         expectStatus(202)
@@ -34,12 +36,12 @@ test.describe.serial('@DTOSS-3881-01 @e2e @epic4c- Cohort Manger subscribed the 
       const response = await retry(
         () => getRecordsFromParticipantDemographicService(request),
         (res) =>
-          res?.data?.[0]?.NhsNumber === 9997160908 &&
+          res?.data?.[0]?.NhsNumber === 9996534472 &&
           res?.data?.[0]?.GivenName === "Jane" &&
           res?.data?.[0]?.FamilyName === "Doe",
         { retries: 8, delayMs: 5000 }
       );
-      expect(response?.data?.[0]?.NhsNumber).toBe(9997160908);
+      expect(response?.data?.[0]?.NhsNumber).toBe(9996534472);
       expect(response?.data?.[0]?.GivenName).toBe("Jane");
       expect(response?.data?.[0]?.FamilyName).toBe("Doe");
     });
@@ -48,18 +50,18 @@ test.describe.serial('@DTOSS-3881-01 @e2e @epic4c- Cohort Manger subscribed the 
       const response = await retry(
         () => getRecordsFromParticipantManagementService(request),
         (res) =>
-          res?.data?.[0]?.NHSNumber === 9997160908 &&
+          res?.data?.[0]?.NHSNumber === 9996534472 &&
           res?.data?.[0]?.ScreeningId === 1 &&
           res?.data?.[0]?.ReferralFlag === 1,
         { retries: 8, delayMs: 5000 }
       );
-      expect(response?.data?.[0]?.NHSNumber).toBe(9997160908);
+      expect(response?.data?.[0]?.NHSNumber).toBe(9996534472);
       expect(response?.data?.[0]?.ScreeningId).toBe(1);
       expect(response?.data?.[0]?.ReferralFlag).toBe(1);
     });
 
     await test.step('DTOSS-10012 DTOSS-10012 verify NemsSubscription_Id in NEMS_SUBSCRIPTION table', async () => {
-      const nhsNumber = participantData['inputParticipantRecord'].u_case_variable_data.nhs_number;
+      const nhsNumber = participantData['inputParticipantRecord10013'].u_case_variable_data.nhs_number;
 
       const response = await retry(
         async () => {
@@ -79,5 +81,23 @@ test.describe.serial('@DTOSS-3881-01 @e2e @epic4c- Cohort Manger subscribed the 
       expect(subscriptionID).not.toBeNull();
       console.log(`Extracted Subscription ID: ${subscriptionID} for number: ${nhsNumber}`);
     });
+  });
+
+
+  test('DTOSS-10013 And send AMEND from CAS to Cohort Manager', {
+    annotation: [{
+      type: 'Requirement - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3881',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-10013',
+    }]
+  }, async ({ request, testData }) => {
+
+    await test.step(`When same ADD participant record is AMENDED via storage for ${testData.nhsNumberAmend}`, async () => {
+      await processFileViaStorage(testData.runTimeParquetFileRoutineAmend);
+    });
+
+    await test.step(`Then the record should end up in cohort distribution table`, async () => {
+      await validateSqlDatabaseFromAPI(request, testData.checkInDatabaseAmend);
+    });
+
   });
 });
--- tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-testsuite.spec.ts
+++ tests/playwright-tests/src/tests/e2e/epic4c-add-participant-tests/epic4c-testsuite.spec.ts
@@ -3,23 +3,192 @@ import { composeValidators, expectStatus } from '../../../api/responseValidators
 import { receiveParticipantViaServiceNow, invalidServiceNowEndpoint } from '../../../api/distributionService/bsSelectService';
 import { loadParticipantPayloads, omitField } from '../../fixtures/jsonDataReader';
 import { ParticipantRecord } from '../../../interface/InputData';
-import { cleanupDatabaseFromAPI } from '../../steps/steps';
 
 
-test.describe.serial('@regression @service_now @api @not-runner-based receive valid participant from serviceNow api', () => {
+test.describe.serial('@DTOSS-3880 @api @not-runner-based receive valid participant from serviceNow api', () => {
 
   let participantData: Record<string, ParticipantRecord>;
-  const testNumbers = ["9990089256", "9991182861","9997487850", "9994539760", "9998816440", "9996958116", "9993350508"];
-  cleanupDatabaseFromAPI(request, testNumbers);
 
-
-  test.beforeAll(() => {
+  test.beforeAll(async () => {
     const folderName = '@DTOSS-3880-01';
     const fileName = 'ADD-participantPayload.json';
-    participantData = loadParticipantPayloads(folderName, fileName);
+    participantData = await loadParticipantPayloads(folderName, fileName);
   });
 
-  test('@DTOSS-3880 @DTOSS-8424 Add a valid VHR participant successfully', async ({ request }) => {
+  test('@DTOSS-3880 @DTOSS-8424 Add a valid VHR participant successfully', {
+    annotation: [{
+      type: 'Requirement - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    }]
+  }, async ({ request }, testInfo) => {
+
+    const payload = participantData['validParticipantRecord-vhr'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(202)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 Add a valid CEASED participant successfully', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const payload = participantData['validParticipantRecord-ceasing'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(202)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 Add a valid ROUTINE participant successfully', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const payload = participantData['validParticipantRecord-routine'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(202)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 Return error when mandatory participant data attribute is missing', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const payload = participantData['inValidParticipantRecordMissingDateofBirthAndSurname'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(400)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 Return error when all mandatory participant data attribute is missing', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const payload = participantData['ParticipantRecordMissingAllMandatoryData'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(400)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 Return error when mandatory reason for add is empty', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const payload = participantData['inValidParticipantRecordMissingReasonForAdding'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(400)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 Return error when an invalid schema name is received', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const payload = participantData['invalidSchemaName'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(400)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 No error when dummy code is blank', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const payload = participantData['ValidParticipantWithNoDummyCode'];
+    const response = await receiveParticipantViaServiceNow(request, payload);
+
+    const validators = composeValidators(
+      expectStatus(202)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 participant with missing mandatory field is rejected', async ({ request }) => {
+
+    annotation: [{
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
+    }, {
+      type: 'Requirement',
+      description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
+    },]
+
+    const originalPayload = participantData['validParticipantRecord-vhr'];
+    const deleteAField = omitField(originalPayload, 'u_case_variable_data.date_of_birth');
+
+    expect((await deleteAField).u_case_variable_data?.date_of_birth).toBeUndefined();
+
+    const response = await receiveParticipantViaServiceNow(request, originalPayload);
+
+    const validators = composeValidators(
+      expectStatus(400)
+    );
+    await validators(response);
+  });
+
+  test('@DTOSS-3880 @DTOSS-8424 return error 404 when calling an invalid endpoint or resource', async ({ request }) => {
+
     annotation: [{
       type: 'Requirement',
       description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
@@ -29,185 +198,11 @@ test.describe.serial('@regression @service_now @api @not-runner-based receive va
     },]
 
     const payload = participantData['validParticipantRecord-vhr'];
-    const response = await receiveParticipantViaServiceNow(request, payload);
+    const response = await invalidServiceNowEndpoint(request, payload);
 
     const validators = composeValidators(
-      expectStatus(202)
+      expectStatus(404)
     );
     await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 Add a valid CEASED participant successfully', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['validParticipantRecord-ceasing'];
-  const response = await receiveParticipantViaServiceNow(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(202)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 Add a valid ROUTINE participant successfully', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['validParticipantRecord-routine'];
-  const response = await receiveParticipantViaServiceNow(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(202)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 Return error when mandatory participant data attribute is missing', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['inValidParticipantRecordMissingDateofBirthAndSurname'];
-  const response = await receiveParticipantViaServiceNow(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(400)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 Return error when all mandatory participant data attribute is missing', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['ParticipantRecordMissingAllMandatoryData'];
-  const response = await receiveParticipantViaServiceNow(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(400)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 Return error when mandatory reason for add is empty', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['inValidParticipantRecordMissingReasonForAdding'];
-  const response = await receiveParticipantViaServiceNow(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(400)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 Return error when an invalid schema name is received', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['invalidSchemaName'];
-  const response = await receiveParticipantViaServiceNow(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(400)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 No error when dummy code is blank', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['ValidParticipantWithNoDummyCode'];
-  const response = await receiveParticipantViaServiceNow(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(202)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 participant with missing mandatory field is rejected', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const originalPayload = participantData['validParticipantRecord-vhr'];
-  const deleteAField = omitField(originalPayload, 'u_case_variable_data.date_of_birth');
-
-  const response = await receiveParticipantViaServiceNow(request, deleteAField as ParticipantRecord);
-
-  const validators = composeValidators(
-    expectStatus(400)
-  );
-  await validators(response);
-});
-
-test('@DTOSS-3880 @DTOSS-8424 return error 404 when calling an invalid endpoint or resource', async ({ request }) => {
-
-  annotation: [{
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-3880',
-  }, {
-    type: 'Requirement',
-    description: 'Tests - https://nhsd-jira.digital.nhs.uk/browse/DTOSS-8424',
-  },]
-
-  const payload = participantData['validParticipantRecord-vhr'];
-  const response = await invalidServiceNowEndpoint(request, payload);
-
-  const validators = composeValidators(
-    expectStatus(404)
-  );
-  await validators(response);
-});
-
+  });
 });
