5937744c6c48be1a423bbc9f4ed9c2b8ba005766
SamRobinson75684 <sam.robinson24@nhs.net>
2025-10-07 11:07:44 +0100

    fix: adding unique constraint for screening id and nhs number

--- application/CohortManager/src/Functions/Shared/DataServices.Migrations/Migrations/20251007091742_add_unique_Screening_id_and_NHS_number_constraint.Designer.cs
+++ application/CohortManager/src/Functions/Shared/DataServices.Migrations/Migrations/20251007091742_add_unique_Screening_id_and_NHS_number_constraint.Designer.cs
@@ -4,6 +4,7 @@ using DataServices.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataServices.Migrations.Migrations
 {
     [DbContext(typeof(DataServicesContext))]
-    partial class DataServicesContextModelSnapshot : ModelSnapshot
+    [Migration("20251007091742_add_unique_Screening_id_and_NHS_number_constraint")]
+    partial class add_unique_Screening_id_and_NHS_number_constraint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
