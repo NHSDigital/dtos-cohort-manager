64bb1e6d4e09a8982056021dc364598b2a24d371
SamRobinson75684 <166817125+SamRobinson75684@users.noreply.github.com>
2025-10-08 09:32:21 +0100

    fix: 11230 add unique nhs number demograpghic table (#1716)
    * fix: adding unique constraint for nhs number
    
    * fix: making sure add date time and updated date time are correct for demograpghic records
    
    * fix: removing uwanted usings

--- application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/ProcessFileClasses/ProcessCaasFile.cs
+++ application/CohortManager/src/Functions/CaasIntegration/receiveCaasFile/ProcessFileClasses/ProcessCaasFile.cs
@@ -3,14 +3,12 @@ namespace NHS.Screening.ReceiveCaasFile;
 using System.Text.Json;
 using Common;
 using Common.Interfaces;
-using Data.Database;
 using DataServices.Client;
-using Hl7.Fhir.Rest;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Protocols.Configuration;
 using Model;
 using Model.Enums;
+using NHS.CohortManager.Shared.Utilities;
 
 public class ProcessCaasFile : IProcessCaasFile
 {
@@ -121,12 +119,12 @@ public class ProcessCaasFile : IProcessCaasFile
         {
 
             case Actions.New:
+
                 currentBatch.AddRecords.Enqueue(basicParticipantCsvRecord);
                 if (await UpdateOldDemographicRecord(basicParticipantCsvRecord, fileName))
                 {
                     break;
                 }
-
                 currentBatch.DemographicData.Enqueue(participant.ToParticipantDemographic());
                 break;
             case Actions.Amended:
@@ -185,8 +183,12 @@ public class ProcessCaasFile : IProcessCaasFile
                 return false;
             }
 
+            basicParticipantCsvRecord.Participant.RecordInsertDateTime = participant.RecordInsertDateTime?.ToString("yyyy-MM-dd HH:mm:ss");
             var participantForUpdate = basicParticipantCsvRecord.Participant.ToParticipantDemographic();
+
+            participantForUpdate.RecordUpdateDateTime = DateTime.UtcNow;
             participantForUpdate.ParticipantId = participant.ParticipantId;
+
 
             var updated = await _participantDemographic.Update(participantForUpdate);
             if (updated)
