3e31a5ae835b4ee0dbf63ba8c8bfc0be91bb3d84
warren <warren.pitterson1@nhs.net>
2025-10-29 17:00:35 +0000

    feat: PageSize added to fetchReports endpoint

--- application/CohortManager/src/Web/app/lib/fetchReports.ts
+++ application/CohortManager/src/Web/app/lib/fetchReports.ts
@@ -1,7 +1,7 @@
 "use server";
 
-export async function fetchReports(exceptionCategory: number, date: string) {
-  const apiUrl = `${process.env.EXCEPTIONS_API_URL}/api/GetValidationExceptions?exceptionStatus=1&sortOrder=1&exceptionCategory=${exceptionCategory}&isReport=1&reportDate=${date}`;
+export async function fetchReports(exceptionCategory: number, date: string, pageSize:number) {
+  const apiUrl = `${process.env.EXCEPTIONS_API_URL}/api/GetValidationExceptions?exceptionStatus=1&sortOrder=1&exceptionCategory=${exceptionCategory}&isReport=1&reportDate=${date}&pageSize=${pageSize}`;
 
   const response = await fetch(apiUrl);
   if (!response.ok) {
