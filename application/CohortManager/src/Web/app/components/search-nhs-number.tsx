a97fb3604f7d5e87281932d08914b73089652a80
warren <warren.pitterson1@nhs.net>
2025-11-27 16:02:48 +0000

    feat: Header tidy and search button added

--- application/CohortManager/src/Web/app/components/search-nhs-number.tsx
+++ application/CohortManager/src/Web/app/components/search-nhs-number.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useRouter } from "next/navigation";
 import { useState, FormEvent } from "react";
 
@@ -9,9 +8,7 @@ export function SearchNhsNumber() {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const cleanedNhsNumber = nhsNumber.replaceAll(" ", "");
-
     if (cleanedNhsNumber.length === 10 && /^\d+$/.test(cleanedNhsNumber)) {
       router.push(`/exceptions/search?nhsNumber=${cleanedNhsNumber}`);
     } else {
@@ -27,7 +24,7 @@ export function SearchNhsNumber() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="nhsuk-u-margin-bottom-0">
+    <form onSubmit={handleSubmit} className="app-search-form">
       <input
         className="nhsuk-input nhsuk-u-margin-bottom-0"
         id="nhs-number"
@@ -37,8 +34,21 @@ export function SearchNhsNumber() {
         value={nhsNumber}
         onChange={(e) => setNhsNumber(e.target.value)}
         onKeyDown={handleKeyDown}
-        style={{ height: "8px", padding: "8px" }}
       />
+      <button className="nhsuk-header__search-submit" type="submit">
+        <svg
+          className="nhsuk-icon nhsuk-icon--search"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+          focusable="false"
+          aria-label="Search"
+        >
+          <title>Search</title>
+          <path d="m20.7 19.3-4.1-4.1a7 7 0 1 0-1.4 1.4l4 4.1a1 1 0 0 0 1.5 0c.4-.4.4-1 0-1.4ZM6 11a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"></path>
+        </svg>
+      </button>
     </form>
   );
 }
