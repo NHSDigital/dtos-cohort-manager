ec7eb9b598e4971510e78a1b814bd161bb18f76b
warren <warren.pitterson1@nhs.net>
2025-12-04 14:00:19 +0000

    feat: used standard component, updated scss

--- application/CohortManager/src/Web/app/components/search-nhs-number.tsx
+++ application/CohortManager/src/Web/app/components/search-nhs-number.tsx
@@ -13,7 +13,6 @@ export function SearchNhsNumber() {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const cleanedNhsNumber = nhsNumber.replaceAll(" ", "");
 
     if (!isValidNhsNumber(nhsNumber)) {
@@ -40,33 +39,42 @@ export function SearchNhsNumber() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="app-search-form">
-      <div className="search-input-container">
+    <search className="nhsuk-header__search">
+      <form
+        className="nhsuk-header__search-form"
+        id="search"
+        onSubmit={handleSubmit}
+      >
+        <label className="nhsuk-u-visually-hidden" htmlFor="search-field">
+          Search by NHS number
+        </label>
         <input
-          className="nhsuk-input nhsuk-u-margin-bottom-0"
-          id="nhs-number"
-          name="nhsNumber"
-          type="text"
-          placeholder="Search by NHS number"
+          className="nhsuk-header__search-input nhsuk-input"
+          id="search-field"
+          name="q"
+          type="search"
+          placeholder="Search by NHS Number"
+          autoComplete="off"
           value={nhsNumber}
           onChange={handleInputChange}
           onKeyDown={handleKeyDown}
         />
-      </div>
-      <button className="nhsuk-header__search-submit" type="submit">
-        <svg
-          className="nhsuk-icon nhsuk-icon--search"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="16"
-          height="16"
-          focusable="false"
-          aria-label="Search"
-        >
-          <title>Search</title>
-          <path d="m20.7 19.3-4.1-4.1a7 7 0 1 0-1.4 1.4l4 4.1a1 1 0 0 0 1.5 0c.4-.4.4-1 0-1.4ZM6 11a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"></path>
-        </svg>
-      </button>
-    </form>
+        <button className="nhsuk-header__search-submit" type="submit">
+          <svg
+            className="nhsuk-icon nhsuk-icon--search"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="16"
+            height="16"
+            focusable="false"
+            role="img"
+            aria-label="Search"
+          >
+            <title>Search</title>
+            <path d="m20.7 19.3-4.1-4.1a7 7 0 1 0-1.4 1.4l4 4.1a1 1 0 0 0 1.5 0c.4-.4.4-1 0-1.4ZM6 11a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"></path>
+          </svg>
+        </button>
+      </form>
+    </search>
   );
 }
