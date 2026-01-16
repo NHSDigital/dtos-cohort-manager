d216ef833b85eda12d259fa6ccf30e3a2a0f1927
warren <warren.pitterson1@nhs.net>
2026-01-16 13:36:07 +0000

    feat: dynamic noResults page to accommodate ExceptionId

--- application/CohortManager/src/Web/app/exceptions/noResults/page.tsx
+++ application/CohortManager/src/Web/app/exceptions/noResults/page.tsx
@@ -3,7 +3,6 @@ import { auth } from "@/app/lib/auth";
 import { canAccessCohortManager } from "@/app/lib/access";
 import Breadcrumb from "@/app/components/breadcrumb";
 import Unauthorised from "@/app/components/unauthorised";
-import { ExceptionSearch } from "@/app/components/exception-search";
 
 export const metadata: Metadata = {
   title: `No results - ${process.env.SERVICE_NAME} - NHS`,
@@ -14,7 +13,14 @@ const BREADCRUMB_ITEMS = [
   { label: "Search exceptions", url: "/exceptions/search" },
 ];
 
-export default async function NoResultsPage() {
+interface SearchParams {
+  searchParams: {
+    searchType?: "NhsNumber" | "ExceptionId";
+    searchValue?: string;
+  };
+}
+
+export default async function NoResultsPage({ searchParams }: Readonly<SearchParams>) {
   const session = await auth();
   const isCohortManager = await canAccessCohortManager(session);
 
@@ -22,21 +28,34 @@ export default async function NoResultsPage() {
     return <Unauthorised />;
   }
 
+  const { searchType, searchValue } = searchParams;
+  const isExceptionIdSearch = searchType === "ExceptionId";
+
   return (
     <>
       <Breadcrumb items={BREADCRUMB_ITEMS} />
       <main className="nhsuk-main-wrapper" id="maincontent" role="main">
         <div className="nhsuk-grid-row">
           <div className="nhsuk-grid-column-two-thirds">
-            <h1>No results</h1>
-            <div className="nhsuk-u-margin-top-4">
-              <p>Try checking the NHS number:</p>
-              <ul className="nhsuk-list nhsuk-list--bullet">
-                <li>is 10 digits (like 999 123 4567)</li>
-                <li>does not contain special characters</li>
-                <li>does not contain letters</li>
-              </ul>
-            </div>
+            {isExceptionIdSearch ? (
+              <>
+                <h1>No results for Exception ID {searchValue}</h1>
+                <div className="nhsuk-u-margin-top-4">
+                </div>
+              </>
+            ) : (
+              <>
+                <h1>No results</h1>
+                <div className="nhsuk-u-margin-top-4">
+                  <p>Try checking the NHS number:</p>
+                  <ul className="nhsuk-list nhsuk-list--bullet">
+                    <li>is 10 digits (like 999 123 4567)</li>
+                    <li>does not contain special characters</li>
+                    <li>does not contain letters</li>
+                  </ul>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </main>
