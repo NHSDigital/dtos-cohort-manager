3584d4c83fcd1f3a736389d349810871388887fd
warren <warren.pitterson1@nhs.net>
2026-01-22 15:48:14 +0000

    feat: wording and test update

--- application/CohortManager/src/Functions/ServiceNowIntegration/ServiceNowMessageHandler/ServiceNowMessageTemplates.cs
+++ application/CohortManager/src/Functions/ServiceNowIntegration/ServiceNowMessageHandler/ServiceNowMessageTemplates.cs
@@ -2,7 +2,7 @@ namespace NHS.CohortManager.ServiceNowIntegrationService;
 
 public static class ServiceNowMessageTemplates
 {
-    public const string UnableToAddParticipantMessageTemplate = "Action needed {0}: This may be because the details entered do not match NHS records. Please check the participant's information and the reason for adding, then submit a new request if appropriate.";
+    public const string UnableToAddParticipantMessageTemplate = "Action needed for {0}: We could not add this participant. This may be because the details entered do not match NHS records. Please check the participant's information and the reason for adding, then submit a new request if appropriate.";
     public const string AddRequestInProgressMessageTemplate = "Update on {0}: Thank you for your request to add a participant to the breast screening programme. There may be a slight delay while the information continues to be validated. Please allow time for this process to complete before following up on your request.";
     public const string SuccessMessageTemplate = "Update on {0}: Validation checks are complete and your request has been sent to BS Select for processing. Please note that it may take up to 24 hours before it’s visible in BS Select.";
 }
--- tests/UnitTests/ServiceNowMessageHandlerTests/SendServiceNowMessageFunctionTests.cs
+++ tests/UnitTests/ServiceNowMessageHandlerTests/SendServiceNowMessageFunctionTests.cs
@@ -243,7 +243,7 @@ public class SendServiceNowMessageFunctionTests
     }
 
     [DataTestMethod]
-    [DataRow(ServiceNowMessageTemplates.UnableToAddParticipantMessageTemplate, "Action needed {0}: This may be because the details entered do not match NHS records. Please check the participant's information and the reason for adding, then submit a new request if appropriate.")]
+    [DataRow(ServiceNowMessageTemplates.UnableToAddParticipantMessageTemplate, "Action needed for {0}: We could not add this participant. This may be because the details entered do not match NHS records. Please check the participant's information and the reason for adding, then submit a new request if appropriate.")]
     [DataRow(ServiceNowMessageTemplates.AddRequestInProgressMessageTemplate, "Update on {0}: Thank you for your request to add a participant to the breast screening programme. There may be a slight delay while the information continues to be validated. Please allow time for this process to complete before following up on your request.")]
     [DataRow(ServiceNowMessageTemplates.SuccessMessageTemplate, "Update on {0}: Validation checks are complete and your request has been sent to BS Select for processing.")]
     public void ServiceNowMessageTemplates_ContainsExpectedWording(string actualTemplate, string expectedWording)
