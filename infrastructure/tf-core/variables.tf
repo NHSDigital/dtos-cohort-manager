f9cc333dec88d0d7297aeb09cffe880d2b930db8
Maciej Murawski <maciej.murawski@nordcloud.com>
2026-02-24 21:48:37 +0000

    feat: storage account data protection options configuration with variables validation in templates repository

--- infrastructure/tf-core/variables.tf
+++ infrastructure/tf-core/variables.tf
@@ -617,11 +617,16 @@ variable "storage_accounts" {
     replication_type                        = optional(string, "LRS")
     public_network_access_enabled           = optional(bool, false)
     access_tier                             = optional(string, "Hot")
+    container_delete_retention_policy_days  = optional(number, 7)
+    blob_properties_change_feed_enabled     = optional(bool, false)
+    blob_properties_restore_policy_days     = optional(number)
+    share_properties_retention_policy_days  = optional(number)
     containers = optional(map(object({
       container_name        = string
       container_access_type = optional(string, "private")
     })), {})
   }))
+
 }
 
 variable "tags" {
